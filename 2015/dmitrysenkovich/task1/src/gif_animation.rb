0958d39b671517256bd0f29383c61cce55fc7d59
Dmitry Senkovich <dmitrysenkovich@gmail.com>
2015-04-18 20:58:19 +0300

    added licking kitty

---
+++ 2015/dmitrysenkovich/task1/src/gif_animation.rb
@@ -0,0 +1,29 @@
+require "fileutils"
+
+require "asciiart"
+
+FRAMES_DIR="frames/"
+FRAME_NAME="frame%i.jpg"
+
+def animate_gif(gif_filename)
+    puts("Processing animation frames..")
+    frames = Magick::Image.read(gif_filename)
+    frames_count = frames.size()
+    frames_in_ascii = Array.new
+    FileUtils.rm_rf(FRAMES_DIR)
+    FileUtils.mkdir_p(FRAMES_DIR)
+    (0...frames_count).each do |i|
+        frames[i].write(FRAMES_DIR+FRAME_NAME%i)
+        frames_in_ascii.push(AsciiArt.new(FRAMES_DIR+FRAME_NAME%i).to_ascii_art(width:80))
+    end
+    FileUtils.rm_rf(FRAMES_DIR)
+
+    (0..10).each do
+        (0...frames_count).each do |i|
+            puts frames_in_ascii[i]
+            sleep(0.1)
+            puts("\e[H\e[2J")
+        end
+    end
+    system("clear")
+end
---
+++ 2015/dmitrysenkovich/task1/test/test.rb
@@ -0,0 +1,7 @@
+require "../src/gif_animation.rb"
+
+def gif_animation_test()
+    animate_gif("../data/snoop_dog.gif")
+end
+
+gif_animation_test()
