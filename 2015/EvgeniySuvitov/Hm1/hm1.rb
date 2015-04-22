bc73fff5c3391675aed177fde236472ea22c0129
yanezh <greeff96@gmail.com>
2015-04-23 00:10:14 +0300

    Domashka

---
+++ 2015/EvgeniySuvitov/Hm1/hm1.rb
@@ -0,0 +1,22 @@
+def my_clear
+	puts "\e[2J"
+end
+
+def hw
+	source=File.open("oslik.txt")
+	pict = source.to_a
+	i = 0
+	while i < 20
+		j = 0
+		while j < 26
+			pict[j].insert(0,'_')
+			j +=1
+		end
+		i += 1
+		my_clear
+		puts pict
+		sleep 0.2
+	end
+end
+
+hw
