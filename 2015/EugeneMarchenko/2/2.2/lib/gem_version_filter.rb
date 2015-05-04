9ef9c9b5edd2421b8085aa18a0b55e8e123c7be8
Eugene Marchenko <3.marchenko@gmail.com>
2015-05-04 12:03:18 +0300

    separate 1st and 2nd home works

--- 2015/EugeneMarchenko/2/2.2/lib/gem_version_filter.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env ruby
-
-class GemVersionFilter
-	
-end
--- 2015/EugeneMarchenko/2/2.2/lib/parameter_parser.rb
+++ 2015/EugeneMarchenko/2/2.2/lib/parameter_parser.rb
@@ -1,31 +1,39 @@
 #!/usr/bin/env ruby
 require 'optparse'
+require 'ostruct'
 
+# Author::    Eugene Marchenko  (mailto:3.marchenko@gmail.com)
+# Copyright:: Copyright (c) 2015 Standalone Programmer
+# License::   Distributes under the same terms as Ruby
+
+# This class holds the parse method takes one arguments
+# Result: parsing available arguments for program to run
 class ParameterParser
+
   def self.parse(args)
     options = OpenStruct.new
     options.library = []
     opt_parser = OptionParser.new do |opts|
-      opts.banner = "Usage: gemfiler [options]"
+      opts.banner = 'Usage: grepkiller [options]'
 
-      opts.on("-n GEM_NAME", "--name=GEM_NAME", "Gem name to find") do |n|
+      opts.on('-n GEM_NAME', '--name=GEM_NAME', 'Gem name to find') do |n|
         options.name = n
       end
 
-      opts.on("-v VERSION1,VERSION2", "--version=VERSION1,VERSION2", Array, "Versions to find") do |list|
+      opts.on('-v VERSION1,VERSION2', '--version=VERSION1,VERSION2',
+              Array, 'Versions to find') do |list|
         options.list = list
-        if list.size > 2
-          raise ArgumentError
-        end
+        fail ArgumentError if list.size > 2
       end
 
-      opts.on("-h", "--help", "Prints this help") do
+      opts.on('-h', '--help', 'Prints this help') do
         puts opts
         exit
       end
     end
-
     opt_parser.parse!(args)
-    return options
+    p options
+    p ARGV
+    options
   end
 end
