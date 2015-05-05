556f427619fbd214b43bb25ab7a61707fc461b53
Eugene Marchenko <3.marchenko@gmail.com>
2015-05-05 11:58:56 +0300

    continue to implement grepkiller at work

--- 2015/EugeneMarchenko/2/2.2/lib/list_directory.rb
+++ 2015/EugeneMarchenko/2/2.2/lib/list_directory.rb
@@ -1,3 +1,20 @@
 class ListDirectory
-
+  def list(pattern)
+    result =[]
+    if pattern == ['.'] or pattern == ['*']
+      pattern = pattern.join
+      # p pattern
+      Dir.entries(pattern).select { |f| !File.directory? f }.each do |item|
+        # p item
+        next if item == '.' or item == '..'
+        result << item
+      end
+    else
+      Dir.foreach(pattern) do |file|
+        p file
+        # result << file
+      end
+    end
+    result
+  end
 end
