3a12dd1184d7d3638b867909aefdfc4b9b19fdab
Oleg Zubchenko <RedGreenBlueDiamond@gmail.com>
2015-05-06 16:03:56 +0300

    use built-in Gem Requirement

--- 2015/RGBD/02-1/gemfiler.rb
+++ 2015/RGBD/02-1/gemfiler.rb
@@ -8,13 +8,6 @@ require "bundler/setup"
 
 require "colorize"
 
-# add method number?
-class String
-  def number?
-    true if Float(self) rescue false
-  end
-end
-
 # Searches packages meeting version specifications.
 module GemFiler
   API_SERVER = "rubygems.org"
@@ -34,69 +27,29 @@ module GemFiler
       puts MESSAGES[:usage]
       return
     end
-    specifiers = argv
-    find(gem_name, specifiers)
+    requirements = argv
+    find(gem_name, requirements)
   end
 
-  def find(gem_name, specifiers)
-    versions = request_versions(gem_name).sort_by { |v| version_split(v) }
+  def find(gem_name, requirements)
+    versions = get_versions(gem_name)
     if versions.size == 0
       puts MESSAGES[:not_found] + gem_name
       return
     end
-    matches = filter_versions(versions, specifiers)
-    print_with_matches(versions, matches)
+    requirement = Gem::Requirement.new(*requirements)
+    matches = versions.map { |v| requirement.satisfied_by?(v) }
+    print_matches(versions, matches)
   end
 
-  def request_versions(gem_name)
+  def get_versions(gem_name)
     response = Net::HTTP.get_response(API_SERVER,
                                       API_REQUEST + gem_name + ".json")
-    return [] unless response.is_a? Net::HTTPOK
-    JSON.parse(response.body).map { |entry| entry["number"] }
+    return nil unless response.is_a? Net::HTTPOK
+    JSON.parse(response.body).map { |entry| Gem::Version.new(entry["number"]) }
   end
 
-  def version_split(version)
-    version.split(".").map { |v| v.number? ? v.to_i : v }
-  end
-
-  def specifier_to_filter(specifier)
-    operator = /[^\d\s]+/.match(specifier).to_s
-    target = version_split(/\d.*/.match(specifier).to_s)
-    return nil unless target
-    get_filter(operator, target)
-  end
-
-  def pessimistic_greater(target)
-    proc do |vs|
-      minor = target.size - 1
-      vs.drop_while { |v| (v <=> target) < 0 }.take_while do |v|
-        v[0...minor] == target[0...minor] && v[minor] >= target[minor]
-      end
-    end
-  end
-
-  def get_filter(operator, target)
-    case operator
-    when "!=" then proc { |vs| vs.select { |v| v != target } }
-    when "", "=" then proc { |vs| vs.select { |v| v == target } }
-    when "<" then proc { |vs| vs.take_while { |v| (v <=> target) < 0 } }
-    when ">=" then proc { |vs| vs.drop_while { |v| (v <=> target) < 0 } }
-    when ">" then proc { |vs| vs.drop_while { |v| (v <=> target) <= 0 } }
-    when "<=" then proc { |vs| vs.take_while { |v| (v <=> target) <= 0 } }
-    when "~>" then pessimistic_greater(target)
-    end
-  end
-
-  def filter_versions(versions, specifiers)
-    possible = versions.map { |v| version_split(v) }
-    specifiers.map! { |s| specifier_to_filter(s) }
-    specifiers.each { |s| possible = s.call(possible) }
-    matches = [false] * versions.size
-    possible.each { |v| matches[versions.index(v.join("."))] = true }
-    matches.to_a
-  end
-
-  def print_with_matches(versions, matches)
+  def print_matches(versions, matches)
     versions.size.times.chunk { |i| matches[i] }.each do |match, indicies|
       print PREFIX[match]
       indicies.each { |i| puts versions[i] }
