99c438a4398478ed7a5523d5e76df8687ab9d029
gost402 <gost402@gmail.com>
2015-05-06 16:16:43 +0300

    implement home task 2

---
+++ 2015/KantinDmitry/2/Gemfiler.rb
@@ -0,0 +1,15 @@
+require_relative 'Gem_versions_getter.rb'
+require_relative 'Versions_printer.rb'
+
+if ARGV[0].nil?
+  puts "arguments incorrect, try:\ngem_name 'condition'"
+else
+  begin
+    gem_versions_getter = Gem_versions_getter.new()
+    versions_array = gem_versions_getter.get_versions(ARGV[0])
+    Versions_printer.output_with_condition(versions_array, ARGV[1], ARGV[2])
+  rescue Exception => e
+    puts e.message.red
+end
+end
+
