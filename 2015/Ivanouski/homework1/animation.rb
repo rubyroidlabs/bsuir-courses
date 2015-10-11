b5268dfb32b1e6b57f8f6fe161ec0c49aec94f79
mafflin <mafflin@gmail.com>
2015-10-11 12:25:15 +0300

    Version 0.1.1. Minor synatxis fixes (founded by hound)

--- 2015/Ivanouski/homework1/animation.rb
+++ 2015/Ivanouski/homework1/animation.rb
@@ -1,28 +1,29 @@
 #!/usr/bin/env ruby
 
-# @version 0.1
+# @version 0.1.1
 # @author S. Ivanouski
 
 class Signs
-    def initialize(sign, do_times, rand_max, sleeping)
-        @sign = sign            # sign to draw
-        @do_times = do_times    # times to repeat drawing line
-        @rand_max = rand_max    # maximum signs in the line
-        @sleeping = sleeping    # time between drawing lines (sec)
-    end
+  def initialize(sign, do_times, rand_max, sleeping)
+    @sign = sign            # sign to draw
+    @do_times = do_times    # times to repeat drawing line
+    @rand_max = rand_max    # maximum signs in the line
+    @sleeping = sleeping    # time between drawing lines (sec)
+  end
 
-    def print_sign
-        @do_times.times do
-            random_var = Random.new
-            puts @sign*random_var.rand(5..@rand_max); sleep @sleeping
-        end
+  def print_sign
+    @do_times.times do
+      random_var = Random.new
+      puts @sign*random_var.rand(5..@rand_max)
+      sleep @sleeping
     end
+  end
 end
 
-ziga = Signs.new(">", 27, 99, 0.1)
+ziga = Signs.new('=>', 27, 99, 0.1)
 
 ziga.print_sign
 
-system "clear"
+system 'clear'
 
 exit 0
