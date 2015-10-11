571d62edae8f44f98b3d2fa01ff54a8f12d883b1
mafflin <mafflin@gmail.com>
2015-10-11 14:57:27 +0300

    Version 0.1.4. Spacing added.

--- 2015/Ivanouski/homework1/animation.rb
+++ 2015/Ivanouski/homework1/animation.rb
@@ -1,20 +1,23 @@
 #!/usr/bin/env ruby
 
-# @version 0.1.3
+# @version 0.1.4
 # @author S. Ivanouski
 
 class Signs
-  def initialize(sign, do_times, rand_max, sleeping)
+  def initialize(sign, spaces, do_times, rand_max, rand_max0, sleeping)
     @sign = sign            # sign to draw
+    @spaces = spaces        # spacing
     @do_times = do_times    # times to repeat drawing line
     @rand_max = rand_max    # maximum signs in the line
+    @rand_max0 = rand_max0  # maximum pacers in the line
     @sleeping = sleeping    # time between drawing lines (sec)
   end
 
   def print_sign
     @do_times.times do
-      random_var = Random.new
-      puts @sign*random_var.rand(5..@rand_max)
+      sign1 = @sign*rand(5..@rand_max)
+      sign0 = @spaces*rand(3..@rand_max0)
+      puts sign0 + sign1
       sleep @sleeping
     end
   end
@@ -22,7 +25,7 @@ end
 
 system 'clear'
 
-ziga = Signs.new('@', 30, 98, 0.05)
+ziga = Signs.new('*', ' ', 30, 98, 20, 0.05)
 
 ziga.print_sign
 
