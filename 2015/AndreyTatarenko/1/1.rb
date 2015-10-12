287f827b7149cec2d61fb3119a88d7777cc3d4c5
System Administrator <root@Andrews-MacBook-Air.local>
2015-10-12 18:17:48 +0300

    update 1.rb

--- 2015/AndreyTatarenko/1/1.rb
+++ 2015/AndreyTatarenko/1/1.rb
@@ -1,18 +1,20 @@
 window_width = `/usr/bin/env tput cols`.to_i
 $window_hight = `/usr/bin/env tput li`.to_i
-$ruby_line = ['  _____       _                           _____       _ _',
-			  			' |  __ \\     | |                         |  __ \\     (_) |',
-			  			' | |__) |_  _| |__  _   _    ___  _ __   | |__) |__ _ _| |___',
-			  			' |  _  / | | |  _ \\| | | |  / _ \\|  _ \\  |  _  // _  | | / __|',
-			  			' | | \\ \\ |_| | |_) | |_| | | (_) | | | | | | \\ \\ (_| | | \\__ \\',
-			  			' |_|  \\_\\__ _|_ __/ \\__  |  \\___/|_| |_| |_|  \\_\\__ _|_|_|___/',
-			  			'                     __/ |',
-			  			'                    |___/']
+$ruby_line = [
+	'  _____       _                           _____       _ _',
+	' |  __ \\     | |                         |  __ \\     (_) |',
+	' | |__) |_  _| |__  _   _    ___  _ __   | |__) |__ _ _| |___',
+	' |  _  / | | |  _ \\| | | |  / _ \\|  _ \\  |  _  // _  | | / __|',
+	' | | \\ \\ |_| | |_) | |_| | | (_) | | | | | | \\ \\ (_| | | \\__ \\',
+	' |_|  \\_\\__ _|_ __/ \\__  |  \\___/|_| |_| |_|  \\_\\__ _|_|_|___/',
+	'                     __/ |',
+	'                    |___/'
+	]
 
 def next_frame
 	sleep(0.05)
-	system "clear"
-	(($window_hight - $ruby_line.length)/2).times { |i| puts ''}
+	system 'clear'
+	(($window_hight - $ruby_line.length) / 2).times { |i| puts ''}
 end
 
 space_line = '';
@@ -23,19 +25,20 @@ $ruby_line.length.times  { |i| space_line + $ruby_line[i] }
 
 window_width.times do
 	$ruby_line.length.times do |i|
-		out_line = (space_line + $ruby_line[i])[0..window_width-1]
+		out_line = (space_line + $ruby_line[i])[0..window_width - 1]
 		puts out_line
 	end
 	space_line = space_line[1..space_line.length]
 	next_frame
 end
 
-($ruby_line.max_by{ |elem| elem.size }).length.times do |i|
+$ruby_line.max_by { |elem| elem.size } .length.times do |i|
 	$ruby_line.length.times do |j|
-		out_line = $ruby_line[j][i..$ruby_line[j].length-1]
+		out_line = $ruby_line[j][i..$ruby_line[j].length - 1]
 		puts out_line
-	end
-	next_frame
+  	end
+  	next_frame
 end
 
-system "clear"
+system 'clear'
+puts $ruby_line.max_by { |elem| elem.size } .length
