50f5ac025a94758e6841f805115f8b2369886a02
mafflin <mafflin@gmail.com>
2015-10-13 01:15:05 +0300

    Pingchart: version 0.2.2. Hound syntax fixes.

--- 2015/Ivanouski/homework1/pingchart.rb
+++ 2015/Ivanouski/homework1/pingchart.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
 
 # Ping chart
-# @version 0.2.0
+# Required: Unix-like system, head, tail, awk.
+# @version 0.2.2
 # @author S.Ivanouski
 
 class String
@@ -11,24 +12,24 @@ class String
   end
 end
 
-class Ping_chart
-  def initialize(site, iterations, x, y, z, sleeping)
+class Ping
+  def initialize(site, iter, x, y, z, sleeping)
     @site = site
-    @iter = iterations
+    @iter = iter
     @x = x
     @y = y
     @z = z
     @sleeping = sleeping
   end
 
-  def ping_line
-    def wait_n_clear(a)
-      sleep @sleeping
-      system 'clear'
-      print @site.colorize(44) + ' '
-      print a
-    end
+  def wait_n_clear(a)
+    sleep @sleeping
+    system 'clear'
+    print @site.colorize(44) + ' '
+    print a
+  end
 
+  def ping_line
     @iter.times do
       ping = `ping -c 1 #{@site} | head -2 | tail -1 | awk '{print $7}'`
       a = ping.delete('time=').to_i
@@ -39,13 +40,13 @@ class Ping_chart
         print ' ' + linex
       when @x..@y then
         linex = (' ' * @x).colorize(42)
-        liney = (' ' * (a-@x)).colorize(43)
+        liney = (' ' * (a - @x)).colorize(43)
         wait_n_clear(a)
         print ' ' + linex + liney
       when @y..@z then
         linex = (' ' * @x).colorize(42)
-        liney = (' ' * (@y-@x)).colorize(43)
-        linez = (' ' * (a-@y)).colorize(41)
+        liney = (' ' * (@y - @x)).colorize(43)
+        linez = (' ' * (a - @y)).colorize(41)
         wait_n_clear(a)
         print ' ' + linex + liney + linez
       when @z..Float::INFINITY then
@@ -59,8 +60,8 @@ end
 
 system 'clear'
 
-googlecom = Ping_chart.new('google.com', 50, 20, 40, 100, 0.1)
-googleby = Ping_chart.new('google.by', 50, 10, 15, 50, 0.1)
+googlecom = Ping.new('google.com', 50, 20, 40, 100, 0.1)
+googleby = Ping.new('google.by', 50, 10, 15, 50, 0.1)
 googlecom.ping_line
 googleby.ping_line
 
