20d72e3173b092dff3d19b49d17b4c94481edb50
mafflin <mafflin@gmail.com>
2015-10-12 21:51:52 +0300

    Animation: version 0.2.1. Hound syntax fixes.

--- 2015/Ivanouski/homework1/animation.rb
+++ 2015/Ivanouski/homework1/animation.rb
@@ -1,28 +1,30 @@
 #!/usr/bin/env ruby
 
-# @version 0.2.0
+# @version 0.2.1
 # @author S. Ivanouski
 
 class String
   # colorization
-  def colorize(color_code)
+  def colorize(color_code) # 31..36 / 41..46
     "\e[#{color_code}m#{self}\e[0m"
   end
 end
 
 class Signs
-  def initialize(sign, spaces, do_times, rand_max, rand_max0, sleeping)
+  def initialize(sign, spaces, do_times, max, max0, sleeping)
     @sign = sign            # sign to draw
     @spaces = spaces        # spacing
     @do_times = do_times    # times to repeat drawing line
-    @rand_max = rand_max    # maximum signs per line
-    @rand_max0 = rand_max0  # maximum spacers per line
+    @max = max              # maximum signs per line
+    @max0 = max0            # maximum spacers per line
     @sleeping = sleeping    # time between drawing lines (sec)
   end
 
   def print_sign
     @do_times.times do
-      puts (@spaces * rand(3..@rand_max0)) + (@sign * rand(20..@rand_max)).colorize(rand(31..36))
+      spacing = @spaces * rand(5..@max0)
+      signing = @sign * rand(20..@max)
+      puts spacing + signing.colorize(rand(31..36))
       sleep @sleeping
     end
   end
@@ -30,10 +32,10 @@ end
 
 system 'clear'
 
-pic = Signs.new('*', ' ', 30, 98, 20, 0.05)
+pic = Signs.new('*', ' ', 36, 130, 20, 0.05)
 pic.print_sign
 
-sleep 1
+sleep 2
 system 'clear'
 
 exit 0
