5040d3a210b9c8f2165584eba7923e9fab762cc1
System Administrator <root@Andrews-MacBook-Air.local>
2015-10-13 22:27:08 +0300

    update 1.rb

--- 2015/AndreyTatarenko/1/1.rb
+++ 2015/AndreyTatarenko/1/1.rb
@@ -14,25 +14,31 @@ $ruby_line = [
 def next_frame
   sleep(0.05)
   system 'clear'
-  (($window_hight - $ruby_line.length) / 2).times { |i| puts ''}
+  (($window_hight - $ruby_line.length) / 2).times { puts '' }
 end
 
 space_line = ''
 window_width.times { space_line += ' ' }
 next_frame
 
-$ruby_line.length.times  { |i| space_line + $ruby_line[i] }
+$ruby_line.length.times { |i| space_line + $ruby_line[i] }
 
 window_width.times do
-  $ruby_line.length.times {|i| puts (space_line + $ruby_line[i])[0..window_width - 1] }
+  $ruby_line.length.times do |i| 
+    out_str = (space_line + $ruby_line[i])[0..window_width - 1]
+    puts out_str
+  end
   space_line = space_line[1..space_line.length]
   next_frame
 end
 
-max_str = $ruby_line.max_by { |elem| elem.size }
+max_str = $ruby_line.max_by { |elem| elem.length }
 max_str.length.times do |i|
-  $ruby_line.length.times { |j| puts $ruby_line[j][i..$ruby_line[j].length - 1] }
-    next_frame
+  $ruby_line.length.times do |j| 
+    out_str = $ruby_line[j][i..$ruby_line[j].length - 1]
+    puts out_str
+  end
+  next_frame
 end
 
 system 'clear'
