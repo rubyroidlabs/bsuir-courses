a572a6533cf5835e11974d17c8d9f61b3fed7dea
Keshafey <keshafey@gmail.com>
2015-10-14 02:06:13 +0300

    home task 1 ruby version 2.2.2

---
+++ 2015/NemytovTimothy/1/1.rb
@@ -0,0 +1,20 @@
+train = [
+["                                                              "],  
+["  FFFFFF  U     U  CCCCCCC  K  KK      GGGGGG   GGGGGG        "],  
+["  F       U     U  C        KKK        G        G             "],  
+["  FFFFF   U     U  C        K          G  GGG   G  GGG        "],  
+["  F        U   U   C        KKK        G    G   G    G        "], 
+["  F        UUUUU   CCCCCCC  K  KK      GGGGGG   GGGGGG        "]]
+
+train.map! do |x|
+	x = x[0].split(//)
+end
+
+while(true) do
+	system "clear"
+	train.each do |x|
+		puts x.join
+		x.rotate!
+	end	
+	sleep 0.2
+end
