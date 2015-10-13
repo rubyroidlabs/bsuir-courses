6105ef99d4bc72afef584a0b0b46cf0b32715564
Artyom Koush <artyom.koush@gmail.com>
2015-10-13 19:21:36 +0300

    add some changes

--- 2015/KoushArtyom/task1/task1.rb
+++ 2015/KoushArtyom/task1/task1.rb
@@ -1,32 +1,20 @@
-system "clear"
-
+system 'clear'
 animation = []
-animation[1] = %Q( (\°-°)\               ┬─┬)
-
-animation[2] = %Q( (╯°O°)╯FUS          ┬─┬)
-
-animation[3] = %Q( (╯°O°)╯FUS ROH      ┬─┬)
-
-animation[4] = %Q( (╯°O°)╯FUS ROH DAH! ︵  ┻━┻)
-
-animation[5] = %Q( (╯°O°)╯                    ┬─┬)
-
-animation[6] = %Q( (\°-°)\                     ︵  ┻━┻)
-
-animation[7] = %Q( (\°-°)\                            ┬─┬)
-
-animation[8] = %Q( (\°-°)\                             ︵  ┻━┻)
-
-animation[9] = %Q( (\°-°)\                                    ┬─┬) 
-
-animation[10] = %Q( (\°-°)\                                     ︵  ┻━┻)
-
-animation[11] = %Q( (\°-°)\                                            ┬─┬)
-
+animation[1] = %q( (\°-°)\               ┬─┬)
+animation[2] = %q( (╯°O°)╯FUS          ┬─┬)
+animation[3] = %q( (╯°O°)╯FUS ROH      ┬─┬)
+animation[4] = %q( (╯°O°)╯FUS ROH DAH! ︵  ┻━┻)
+animation[5] = %q( (╯°O°)╯                    ┬─┬)
+animation[6] = %q( (\°-°)\                     ︵  ┻━┻)
+animation[7] = %q( (\°-°)\                            ┬─┬)
+animation[8] = %q( (\°-°)\                             ︵  ┻━┻)
+animation[9] = %q( (\°-°)\                                    ┬─┬) 
+animation[10] = %q( (\°-°)\                                     ︵  ┻━┻)
+animation[11] = %q( (\°-°)\                                            ┬─┬)
 2.times do
 	animation.each do |animation|
 		puts animation
 		sleep 0.5
-		system "clear"
+		system 'clear'
 	end
 end
