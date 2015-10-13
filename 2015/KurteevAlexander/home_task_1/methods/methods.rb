7aa57f26123fe4070f16519e2be1f84cb2f0b101
Alexander <sounis@mail.ru>
2015-10-13 13:17:11 +0300

    impliment home task 1

---
+++ 2015/KurteevAlexander/home_task_1/methods/methods.rb
@@ -0,0 +1,12 @@
+require 'curses'
+Curses.init_screen
+def max_Size_engime
+	@maxsize = 0
+	0.upto(ENGINE_FIRST.size-1) do |i|
+		@l=ENGINE_FIRST[i].size
+		if @maxsize<@l
+			@maxsize = @l
+		end
+	end
+	return @maxsize
+end
