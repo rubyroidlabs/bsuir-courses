d93f1a8ad39c105c93a98ed103f31604302a2adf
magicpony <badabum007@gmail.com>
2015-10-14 23:23:38 +0300

    last_version_of_task1

--- 2015/AntonStahovskiy/1/ruby1_racket.rb
+++ 2015/AntonStahovskiy/1/ruby1_racket.rb
@@ -8,19 +8,19 @@ t.times do
     i.times { puts '' }
     i -= 1
   end
-puts '   _'
-puts '  / \\'
-puts ' /___\\'
-puts ' |   |'
-puts ' | O |'
-puts ' |___|'
-puts '/_| |_\\'
-puts 'tt   tt'
-  if i % 2 == 1
+  puts '   _'
+  puts '  / \\'
+  puts ' /___\\'
+  puts ' |   |'
+  puts ' | O |'
+  puts ' |___|'
+  puts '/_| |_\\'
+  puts 'tt   tt'
+  if i.odd?
     puts 't     t'
   else
     puts ' t   t '
   end
-  sleep (1.0 / 4.0)
+  sleep(1.0 / 4.0)
 end
 system 'clear'
