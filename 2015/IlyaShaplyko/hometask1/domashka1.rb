f566925e25f0b7a28bd89edc4d48892a9eebe1bd
Shaglock <goldilb10xa@Mail.ru>
2015-10-15 01:53:11 +0300

    fix #4 :((

--- 2015/IlyaShaplyko/hometask1/domashka1.rb
+++ 2015/IlyaShaplyko/hometask1/domashka1.rb
@@ -9,46 +9,46 @@ lol = 'LoL'
 f = 'how to move this fucking LOL'
 r = 'А русский покатит?'
 
-while i<= 7
-puts s * i * 2 + a
-puts s * i * 2 + b
-puts s * i * 2 + c
-puts s * i * 3 + d
-puts lol
-sleep 0.2
-system 'clear'
-puts s * i * 2 + a
-puts s * i * 2 + b
-puts s * i * 2 + c
-puts s * i * 3 + d
-puts lol
-sleep 0.2
-system 'clear'
-i+=1
+while i <= 7
+  puts s * i * 2 + a
+  puts s * i * 2 + b
+  puts s * i * 2 + c
+  puts s * i * 3 + d
+  puts lol
+  sleep 0.2
+  system 'clear'
+  puts s * i * 2 + a
+  puts s * i * 2 + b
+  puts s * i * 2 + c
+  puts s * i * 3 + d
+  puts lol
+  sleep 0.2
+  system 'clear'
+  i += 1
 end
-while i<= 20
-puts s * i * 2 + a
-puts s * i * 2 + b
-puts s * i * 2 + c
-puts s * i * 3 + d
-puts lol
-puts s * i * 2 + f
-puts s * i * 4 + r
-sleep 0.2
-system 'clear'
-i += 1
+while i <= 20
+  puts s * i * 2 + a
+  puts s * i * 2 + b
+  puts s * i * 2 + c
+  puts s * i * 3 + d
+  puts lol
+  puts s * i * 2 + f
+  puts s * i * 4 + r
+  sleep 0.2
+  system 'clear'
+  i += 1
 end
-while i<=40
-puts s * i * 2 + a
-puts s * i * 2 + b
-puts s * i * 2 + c
-puts s * i * 3 + d
-puts s * j * 4 + lol
-puts s * i * 2 + f
-sleep 0.2
-system 'clear'
-i += 1
-j += 1
+while i <= 40
+  puts s * i * 2 + a
+  puts s * i * 2 + b
+  puts s * i * 2 + c
+  puts s * i * 3 + d
+  puts s * j * 4 + lol
+  puts s * i * 2 + f
+  sleep 0.2
+  system 'clear'
+  i += 1
+  j += 1
 end
 system 'clear'
 puts '          <=
