7832780065c973468589a747a1abcfadeba9ec32
YOUR NAME <YOUR@EMAIL.com>
2015-10-14 18:02:26 +0300

    tabs removed

--- 2015/YauheniSushkevich/1/mosquito.rb
+++ 2015/YauheniSushkevich/1/mosquito.rb
@@ -1,6 +1,6 @@
 mosquito = []
-mosquito[0] = "         
-	   
+mosquito[0] = "
+
       |\\|\\
 ___.-.| \\ |
    `-.`-'`.___
@@ -24,38 +24,38 @@ def mosquito.fly
   system 'clear'
   cols = `stty size`.split.map { |x| x.to_i }.reverse[0]
   (cols-self.width).downto(0) do |i|
-  	self.each do |frame|
-  	  frame.split("\n").each do |string|
-	  	puts ' ' * i +	 string
-  	  end
-  	  system 'sleep 0.07'
+    self.each do |frame|
+      frame.split("\n").each do |string|
+        puts ' ' * i + string
+      end
+      system 'sleep 0.07'
       system 'clear'
-  	end
+    end
   end
 end
 
 def mosquito.appear
   cols = `stty size`.split.map { |x| x.to_i }.reverse[0]
   (cols).downto(cols-self.width) do |i|
-  	self.each do |frame|
-  	  frame.split("\n").each do |string|
-	  	puts ' ' * i +	 string[0, cols - i]
-  	  end
-  	  system 'sleep 0.07'
+    self.each do |frame|
+      frame.split("\n").each do |string|
+        puts ' ' * i + string[0, cols - i]
+      end
+      system 'sleep 0.07'
       system 'clear'
-  	end
+    end
   end
 end
 
 def mosquito.disappear
   0.upto(self.width) do |i|
-  	self.each do |frame|
-  	  frame.split("\n").each do |string|
-	  	puts string[i, string.length]
-  	  end
-  	  system 'sleep 0.07'
+    self.each do |frame|
+      frame.split("\n").each do |string|
+        puts string[i, string.length]
+      end
+      system 'sleep 0.07'
       system 'clear'
-  	end
+    end
   end
 end
 
