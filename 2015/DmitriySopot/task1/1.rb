2487c8adfd660d4e02b64e3bddcbdb3701907d34
0dminik <sopot.dmitriy@yandex.by>
2015-10-15 01:22:27 +0300

    Update 1.rb

--- 2015/DmitriySopot/task1/1.rb
+++ 2015/DmitriySopot/task1/1.rb
@@ -29,7 +29,7 @@ limit_size = winsize[1] - winsize[1] % animation_length
 
 def sleep_and_clear
 	sleep 0.06
-system "clear"
+system 'clear'
 end
 loop do|a|
   animation_length.upto(limit_size) do |b|
