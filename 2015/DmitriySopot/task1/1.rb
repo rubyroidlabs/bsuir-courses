a47cd01551c5010b0047465ad63fc2006b8e8588
0dminik <sopot.dmitriy@yandex.by>
2015-10-15 01:34:09 +0300

    Update 1.rb

--- 2015/DmitriySopot/task1/1.rb
+++ 2015/DmitriySopot/task1/1.rb
@@ -1,7 +1,6 @@
 require 'io/console'
-
 animation = 
-%Q[`````````````````````````/\\``````````/\,
+%[`````````````````````````/\\``````````/\,
 `````````````````````````(`\\````````//`),
 ``````````````````````````\`\\``````//`/,
 ```````````````````````````\_\\||||//_/`,
@@ -30,19 +29,17 @@ limit_size = winsize[1] - winsize[1] % animation_length
 
 def sleep_and_clear
 	sleep 0.06
-	system "clear"
+system "clear"
 end
-
 loop do|a|
   animation_length.upto(limit_size) do |b|
     animation.map! {|e| e.rjust(e.length + 1)}
 	sleep_and_clear
 	puts animation
   end
-  
   limit_size.downto(animation_length) do |c|
-	animation.map! {|e| e.slice!(1..-1)}
-	sleep_and_clear
-	puts animation
+animation.map! {|e| e.slice!(1..-1)}
+sleep_and_clear
+puts animation
   end
 end
