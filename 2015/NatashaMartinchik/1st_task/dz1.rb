2c718d15effa1c4467efdb88aae143ff542edc42
NatashaMartinchik <mno96@yandex.by>
2015-10-14 23:20:00 +0300

    xhange mistakes

--- 2015/NatashaMartinchik/1st_task/dz1.rb
+++ 2015/NatashaMartinchik/1st_task/dz1.rb
@@ -1,37 +1,28 @@
 #!/usr/bin/env ruby
 WIDTH = 70
-
 loop do
-
-file = File.open("ris.txt")
-mass=file.to_a
-rev=[]
-rev= mass.map {|p| ' '*WIDTH + p.chomp}
-
-
-WIDTH.times do |i| 
-    mass.map{|f| f.insert(0,' ')}
-	puts mass
-	sleep 0.1
-	puts "\e[H\e[2J"
-	i=i+1
+  file = File.open ('ris.txt')
+  mass = file.to_a
+  rev = []
+  rev = mass.map { |p| ' '*WIDTH + p.chomp }
+  WIDTH.times do |i| 
+    mass.map { |f| f.insert(0, ' ') }
+    puts mass
+    sleep 0.1
+    puts "\e[H\e[2J"
+    i = i+1
+  end
+  g = 2
+  while g < WIDTH
+    i = 0
+    while i <= WIDTH - g
+      rev.map! { |n| n.insert(0, ' ') }
+      i = i + 1
+    end
+    puts rev
+    sleep 0.1
+    puts "\e[H\e[2J"
+    rev.map! &:lstrip!  
+    g = g + 1
+  end
 end
-
-g=2
-while g<WIDTH
-	i=0
-	while i<=WIDTH-g
-		rev.map!{|n| n.insert(0,' ')}
-	    i= i+1
-	end
-puts rev
-sleep 0.1
-puts "\e[H\e[2J"
-
-rev.map! { |e| e.lstrip! } 
-g= g+1
-end
-
-end
-
-	
