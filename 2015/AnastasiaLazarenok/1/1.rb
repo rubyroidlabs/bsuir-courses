fd16b5ea8dee2214e37e3a401d10707924ecd93c
AnastasiaLazarenok <avgita@gmail.com>
2015-10-14 14:08:35 +0300

    implement home task 1

---
+++ 2015/AnastasiaLazarenok/1/1.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/ruby
+require 'io/console'
+lineWidth =15 
+i=0
+while i<=5
+
+puts "(*_*)/"
+puts "/( ( " 
+puts "  //"
+puts '==='*3
+
+sleep 0.5
+system("clear")
+
+puts ('\(*_*)'.center(lineWidth))
+puts ('  ) )\ '.center(lineWidth))
+puts (' \\\\'.center(lineWidth))
+puts '==='*3
+
+sleep 0.5
+system("clear")
+i+=1
+end
