671be27abd213c5018aca0cbedd61d9e6423f583
AntonDatsik <Anton.Dacik@gmail.com>
2015-10-14 21:25:18 +0300

    modified task1

--- 2015/AntonDatsik/task1/task1.rb
+++ 2015/AntonDatsik/task1/task1.rb
@@ -1,66 +1,62 @@
 require 'curses'
 
 class Car
-	def initialize()
-		@image = [	"                                $$$$$$$$$$$$$$$$$$$$$$$        ",
-					"                               $$$$$_____$$_____$$____$$       ",
-					"                             $$$$$_______$$______$$____$$      ",
-					"                           $$$$$_________$$_______$$____$$     ",
-					"                        $$$$$____________$$________$$____$$    ",
-					"                  $$$$$$$$$$$$$$$$$$$$$$$$$_________$$____$$   ",
-					"            $$$$$$________$$_____________$$__________$$$$$$$$  ",
-					"       $$$$$$_____________$$_____________$$_________________$$ ",
-					"     $$___________________$$_____________$$_________________$$ ",
-					"     $$________$$$$$______$$_____________$$________$$$$$____$$ ",
-					"     $$______$$$$$$$$$____$$_____________$$______$$$$$$$$$___$$",
-					"     $$$$$_$$$$_____$$$_$$$$$$$$$$$$$$$$$$$$$$__$$$$____$$$__$$",
-					"           $$$$_____$$$                         $$$$____$$$$   ",
-					"           $$$$____$$$                          $$$$$___$$$$   ",
-					"             $$$$$_$$$                           $$$$$$_$$$    ",
-					"               $$$$$$                               $$$$$$     "]
+  def initialize()
+  @image = [
+    "                                $$$$$$$$$$$$$$$$$$$$$$$        ",
+    "                               $$$$$_____$$_____$$____$$       ",
+    "                             $$$$$_______$$______$$____$$      ",
+    "                           $$$$$_________$$_______$$____$$     ",
+    "                        $$$$$____________$$________$$____$$    ",
+    "                  $$$$$$$$$$$$$$$$$$$$$$$$$_________$$____$$   ",
+    "            $$$$$$________$$_____________$$__________$$$$$$$$  ",
+    "       $$$$$$_____________$$_____________$$_________________$$ ",
+    "     $$___________________$$_____________$$_________________$$ ",
+    "     $$________$$$$$______$$_____________$$________$$$$$____$$ ",
+    "     $$______$$$$$$$$$____$$_____________$$______$$$$$$$$$___$$",
+    "     $$$$$_$$$$_____$$$_$$$$$$$$$$$$$$$$$$$$$$__$$$$____$$$__$$",
+    "           $$$$_____$$$                         $$$$____$$$$   ",
+    "           $$$$____$$$                          $$$$$___$$$$   ",
+    "             $$$$$_$$$                           $$$$$$_$$$    ",
+    "               $$$$$$                               $$$$$$     "]
 
 
-		@particle = ' *'
-	end
+    @particle = ' *'
+  end
 
-
-	def print(y, x)
-		
-		k = 0
-		
+  def print(y, x)
+    k = 0
 		prng = Random.new
 
-		while (k < @image.length) do
-			Curses.setpos(y + k, x)
-			if k.between?(9,12)
+    while (k < @image.length) do
+      Curses.setpos(y + k, x)
+      if k.between?(9,12)
+        tempStr = String.new(" ")
+        randVal = prng.rand(0..4)
+        i = 1
+        tempStr.concat(@image[k])
+        if (i < randVal)
+          i += 3
+          tempStr.concat(" ")
+        end
 
-				tempStr = String.new(" ")
-				randVal = prng.rand(0..4)
-				i = 1
-				tempStr.concat(@image[k])
-				if (i < randVal)
-					i += 3
-					tempStr.concat(" ")
-				end
+        while (i > randVal) do
+          tempStr.concat(@particle)
+          i -= 1
+        end
 
-				while (i > randVal) do
-					tempStr.concat(@particle)
-					i -= 1
-				end
+        Curses.addstr(tempStr)
+      else
+        Curses.addstr(@image[k])
+      end
 
-				Curses.addstr(tempStr)
-			else
-				Curses.addstr(@image[k])
-			end
+      k += 1
+    end
 
-			k += 1
-		end
-
-		Curses.refresh
-	end
+    Curses.refresh
+  end
 
 end
-
 
 car = Car.new()
 Curses.init_screen
@@ -73,13 +69,12 @@ Y = Curses.lines / 2
 X = Curses.cols / 2
 
 while shift_coord < 70 do
+  if 	X - shift_coord > 0
+    car.print(Y, X - shift_coord)
+    sleep(0.05)
+    Curses.clear
+  end
 
-	if 	X - shift_coord > 0
-		car.print(Y, X - shift_coord)
-		sleep(0.05)
-		Curses.clear
-	end
-
-	shift_coord += 1
+  shift_coord += 1
 end
 
