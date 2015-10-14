3c2b6b2b733e53621e07efa564b657fb38fc2457
MsMilkina <ms.milkina@gmail.com>
2015-10-15 00:03:15 +0300

    Fixed bot comments

--- 2015/ElizabethMilkina/Task_1/1.rb
+++ 2015/ElizabethMilkina/Task_1/1.rb
@@ -3,31 +3,31 @@ b = "OK\n"
 a = ""
 
 for i in 1..9
-    file = File.open('txt/'+i.to_s+'.txt', 'r')
-    image[i] = file.read()
+  file = File.open('txt/'+i.to_s+'.txt', 'r')
+  image[i] = file.read()
 end
 
 while a != b
-    begin
-        puts 'Please, move your terminal in full-screen mode. Then enter OK (Only uppercase letters in English).'
-        a = gets
-    end
+  begin
+    puts 'Please, move your terminal in full-screen mode. Then enter OK (Only uppercase letters in English).'
+    a = gets
+  end
 end
 
 (1..2).each { |j|
-    (1..9).each { |i|
-        puts image[i]
-        sleep(0.15)
-        system 'clear'
-    }
-    (7..9).each { |i|
-        puts image[i]
-        sleep(0.15)
-        system 'clear'
-    }
-    (7..9).each { |i|
-        puts image[i]
-        sleep(0.15)
-        system 'clear'
-    }
+  (1..9).each { |i|
+    puts image[i]
+    sleep(0.15)
+    system 'clear'
+  }
+  (7..9).each { |i|
+    puts image[i]
+    sleep(0.15)
+    system 'clear'
+  }
+  (7..9).each { |i|
+    puts image[i]
+    sleep(0.15)
+    system 'clear'
+  }
 }
