e587e31e8073ce0052cad7d94417ba109d247b42
YOUR NAME <YOUR@EMAIL.com>
2015-10-14 18:16:20 +0300

    reduced redundant self, surrounding space missing

--- 2015/YauheniSushkevich/1/mosquito.rb
+++ 2015/YauheniSushkevich/1/mosquito.rb
@@ -22,8 +22,8 @@ end
 
 def mosquito.fly
   system 'clear'
-  cols = `stty size`.split.map { |x| x.to_i }.reverse[0]
-  (cols-self.width).downto(0) do |i|
+  cols = `stty size`.split.map(&:to_i).reverse[0]
+  (cols - width).downto(0) do |i|
     self.each do |frame|
       frame.split("\n").each do |string|
         puts ' ' * i + string
@@ -36,7 +36,7 @@ end
 
 def mosquito.appear
   cols = `stty size`.split.map { |x| x.to_i }.reverse[0]
-  (cols).downto(cols-self.width) do |i|
+  (cols).downto(cols - width) do |i|
     self.each do |frame|
       frame.split("\n").each do |string|
         puts ' ' * i + string[0, cols - i]
@@ -48,7 +48,7 @@ def mosquito.appear
 end
 
 def mosquito.disappear
-  0.upto(self.width) do |i|
+  0.upto(width) do |i|
     self.each do |frame|
       frame.split("\n").each do |string|
         puts string[i, string.length]
