2ffd6a47bbb690492fa6687ffe36b4a85c7867c3
magicpony <badabum007@gmail.com>
2015-10-14 23:04:43 +0300

    modifed task1

--- 2015/AntonStahovskiy/1/ruby1_racket.rb
+++ 2015/AntonStahovskiy/1/ruby1_racket.rb
@@ -3,24 +3,24 @@
 i = 13
 
 t.times do
-  system "clear"
+  system 'clear'
   if i > 0
-    i.times { puts " " }
+    i.times { puts '' }
     i -= 1
   end
-puts "   _"
-puts "  / \\"
-puts " /___\\"
-puts " |   |"
-puts " | O |"
-puts " |___|"
-puts "/_| |_\\"
-puts "tt   tt"
-  if i%2 == 1
-    puts "t     t"
+puts '   _'
+puts '  / \\'
+puts ' /___\\'
+puts ' |   |'
+puts ' | O |'
+puts ' |___|'
+puts '/_| |_\\'
+puts 'tt   tt'
+  if i % 2 == 1
+    puts 't     t'
   else
-    puts " t   t "
+    puts ' t   t '
   end
   sleep (1.0 / 4.0)
 end
-system "clear"
+system 'clear'
