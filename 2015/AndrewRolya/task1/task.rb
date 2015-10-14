1ffccf3fe5f013c7ba7e3a42d4e96130149a8c4c
AndrewRolya <rolya_12@mail.ru>
2015-10-15 02:14:39 +0300

    home_task 1.2

--- 2015/AndrewRolya/task1/task.rb
+++ 2015/AndrewRolya/task1/task.rb
@@ -3,33 +3,33 @@ require 'colorize'
 def colorization(input_text, indicator)
   input_text.size.times do |i|
     case indicator
-      when 1
-        if input_text[i] == '%'
-          print input_text[i].red
-        else
-          print input_text[i]
-        end
-      when 2, 4
-        if input_text[i] == '@'
-          print input_text[i].yellow
-        else
-          print input_text[i]
-        end
-      when 3
-        if input_text[i] == '&'
-          print input_text[i].green
-        else
-          print input_text[i]
-        end
+    when 1
+      if input_text[i] == '%'
+        print input_text[i].red #output element red light
       else
         print input_text[i]
       end
+    when 2, 4
+      if input_text[i] == '@'
+        print input_text[i].yellow #output element yellow light
+      else
+        print input_text[i]
+      end
+    when 3
+      if input_text[i] == '&'
+        print input_text[i].green #output element green light
+      else
+        print input_text[i] #output element default light
+      end
+    else
+      print input_text[i] #output element default light
     end
+  end
 end
 
 input_source = File.open("./inputForTask.txt").read
 counter = 0
-50.times do |i|
+50.times do
   counter += 1
   colorization(input_source, counter)
   sleep(0.3)
