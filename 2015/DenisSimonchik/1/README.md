1a0d79be03921869ec674d8200f85b69325d4106
ddzyanis <ddzyanis@mail.ru>
2015-10-14 10:50:34 +0300

    home task 1

---
+++ 2015/DenisSimonchik/1/README.md
@@ -0,0 +1,2 @@
+Ruby_animation
+==============
---
+++ 2015/DenisSimonchik/1/image.rb
@@ -0,0 +1,21 @@
+                                          AIgg
+                                     gggggggggg_
+                                  ggggggggggg0gg
+                               ggggggggggggggggggg
+     ggg    ggggggg          gggggggggggg     ggggg
+  ggggggggggggggggggggggggggggggggggggggg
+_gggggg  ggggggggggggggggggggggggggggggg
+gggggg   gggggggggggggggggggggggggggggg
+ggggg    ggggggggggggggggggggggggggggg
+gggg     ggggggggggggggggggggggggggggg
+gg         ggggggggggggggggggggggggggg
+          gggggggg gggggggggggggggggg
+      gggggggggggg             ggggggg
+     gggg    gggg                ggggg
+     ggg     ggg                   gggg
+     gg     gggg           ggg  ggggggggg
+    ggg      ggg            gggggg    ggg
+  gggg         ggg                      gg
+  gg            ggg                      gg
+                 ggg                      ggg
+                  ggg                      ggg
