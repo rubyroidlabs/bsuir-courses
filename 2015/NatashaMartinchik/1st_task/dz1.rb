7d3746babaeb2f1f5de1934508a0c5724a48a706
NatashaMartinchik <mno96@yandex.by>
2015-10-14 23:27:30 +0300

    change mistakes 2

--- 2015/NatashaMartinchik/1st_task/dz1.rb
+++ 2015/NatashaMartinchik/1st_task/dz1.rb
@@ -3,26 +3,25 @@ WIDTH = 70
 loop do
   file = File.open ('ris.txt')
   mass = file.to_a
-  rev = []
-  rev = mass.map { |p| ' '*WIDTH + p.chomp }
-  WIDTH.times do |i| 
+  rev = mass.map { |p| ' ' * WIDTH + p.chomp }
+  WIDTH.times do |i|
     mass.map { |f| f.insert(0, ' ') }
     puts mass
     sleep 0.1
     puts "\e[H\e[2J"
-    i = i+1
+    i += 1
   end
   g = 2
   while g < WIDTH
     i = 0
     while i <= WIDTH - g
       rev.map! { |n| n.insert(0, ' ') }
-      i = i + 1
+      i += 1
     end
     puts rev
     sleep 0.1
     puts "\e[H\e[2J"
-    rev.map! &:lstrip!  
-    g = g + 1
+    rev.map! &:lstrip!
+    g += 1
   end
 end
