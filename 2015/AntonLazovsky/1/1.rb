023398a74e4ebfa2fca0b0e69227a9e373cf2624
AntonLazovsky <anton@fksis.by>
2015-10-14 23:40:08 +0300

    Update 1.rb

--- 2015/AntonLazovsky/1/1.rb
+++ 2015/AntonLazovsky/1/1.rb
@@ -1,9 +1,9 @@
-str=File.read('1.txt')
+str = File.read('1.txt')
 system 'clear'
 5.times do
-	for i in 0..5
-		puts str[(i * 456)..((i + 1) * 456 - 1)]
-		sleep(0.35)
-		system 'clear'
-	end
+  for i in 0..5
+    puts str[(i * 456)..((i + 1) * 456 - 1)]
+    sleep(0.35)
+    system 'clear'
+  end
 end
