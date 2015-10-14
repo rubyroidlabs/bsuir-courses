0deaa4648dd0927c66cb3196fff6028fbf79b17a
AndrewRolya <rolya_12@mail.ru>
2015-10-15 01:51:42 +0300

    home_task 1.1

--- 2015/AndrewRolya/task1/task.rb
+++ 2015/AndrewRolya/task1/task.rb
@@ -1,46 +1,40 @@
-require "colorize"
+require 'colorize'
 
-def colorization(inputText,indicator)
-    inputText.size.times do |i|
-        case indicator
-        when 1
-            if inputText[i]=="%"
-                print inputText[i].red
-            else
-                print inputText[i]
-            end
-        when 2
-            if inputText[i]=="@"
-                print inputText[i].yellow
-            else
-                print inputText[i]
-            end
-        when 3
-            if inputText[i]=="&"
-                print inputText[i].green
-            else
-                print inputText[i]
-            end
-        when 4
-            if inputText[i]=="@"
-                print inputText[i].yellow
-            else
-                print inputText[i]
-            end
+def colorization(input_text, indicator)
+  input_text.size.times do |i|
+    case indicator
+      when 1
+        if input_text[i] == '%'
+          print input_text[i].red
         else
-            print inputText[i]
+          print input_text[i]
         end
+      when 2, 4
+        if input_text[i] == '@'
+          print input_text[i].yellow
+        else
+          print input_text[i]
+        end
+      when 3
+        if input_text[i] == '&'
+          print input_text[i].green
+        else
+          print input_text[i]
+        end
+      else
+        print input_text[i]
+      end
     end
 end
 
-inputSource = File.open("./inputForTask.txt").read
+input_source = File.open("./inputForTask.txt").read
 counter = 0
 50.times do |i|
-    counter +=1
-    colorization(inputSource,counter)
-    sleep(0.3)
-    if counter==4
-        counter=0
-    end
-    puts "\e[H\e[2J"
+  counter += 1
+  colorization(input_source, counter)
+  sleep(0.3)
+  if counter == 4
+    counter = 0
+  end
+  puts "\e[H\e[2J"
 end
