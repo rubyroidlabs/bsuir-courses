a03cd49f60c3e82d7f801110dfccdb21adf4483d
yanezh <greeff96@gmail.com>
2015-10-14 21:56:25 +0300

    IHW try 2

--- 2015/SuvitovEvgeniy/1/hm1.rb
+++ 2015/SuvitovEvgeniy/1/hm1.rb
@@ -3,14 +3,14 @@ def my_clear
 end
 
 def hw
-	source=File.open("oslik.txt")
+	source = File.open("oslik.txt")
 	pict = source.to_a
 	i = 0
 	while i < 20
 		j = 0
 		while j < 26
 			pict[j].insert(0,'_')
-			j +=1
+			j += 1
 		end
 		i += 1
 		my_clear
