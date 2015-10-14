cbbf5ed64c32b9a1eefca96d55f1733c74deecc8
traffic <dmitrytrofimovich@outlook.com>
2015-10-14 10:39:28 +0300

    completed home task 1

---
+++ 2015/DmitryTrofimovich/1/1.rb
@@ -0,0 +1,15 @@
+cols = %x(tput cols).to_i
+shift_t = 2
+car = ['   *****  '.split(''),'   *   *  '.split(''),'****   ***'.split(''),'*        *'.split(''),'**********'.split(''),'  **   ** '.split('')]
+cols.downto(-car.max.size) do |shift|
+	background = Array.new(10){Array.new(cols){' '}}
+	0.upto(car.size-1) do |i|
+		0.upto(car.max.size-1) do |j|
+			background[shift_t+i][shift+j] = car[i][j] if shift+j<cols && shift+j>=0
+		end
+	end
+	sleep 0.05
+	print `clear`
+	print background.join
+end
+print `clear`
