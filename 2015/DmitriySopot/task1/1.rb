d5107ee208c6c99e3f7cd4bae52de44d1b369859
0dminik <sopot.dmitriy@yandex.by>
2015-10-15 01:18:15 +0300

    Update 1.rb

--- 2015/DmitriySopot/task1/1.rb
+++ 2015/DmitriySopot/task1/1.rb
@@ -38,8 +38,8 @@ loop do|a|
 	puts animation
   end
   limit_size.downto(animation_length) do |c|
-	animation.map! {|e| e.slice!(1..-1)}
-	sleep_and_clear
+animation.map! {|e| e.slice!(1..-1)}
+sleep_and_clear
 puts animation
   end
 end
