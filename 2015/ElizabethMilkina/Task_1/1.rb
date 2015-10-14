e51ded09609ebd8099f5c25ab90e95c185f6e387
MsMilkina <ms.milkina@gmail.com>
2015-10-15 00:48:43 +0300

    Fixed bot comments

--- 2015/ElizabethMilkina/Task_1/1.rb
+++ 2015/ElizabethMilkina/Task_1/1.rb
@@ -1,32 +1,33 @@
 image = []
-b = "OK\n"
-a = ""
+b = 'OK'
+a=''
 
-for i in 1..9
-  file = File.open('txt/'+i.to_s+'.txt', 'r')
-  image[i] = file.read()
-end
+(1..9).each { |i|
+  file = File.open("txt/#{i.to_s}.txt", 'r')
+  image[i] = file.read
+}
 
-while a != b
+while a.chomp != b
   begin
-    puts 'Please, move your terminal in full-screen mode. Then enter OK (Only uppercase letters in English).'
+    puts 'Please, move your terminal in full-screen mode.'\
+    'Then enter OK (Only uppercase letters in English).'
     a = gets
   end
 end
 
-(1..2).each { |j|
+(1..2).each {
   (1..9).each { |i|
     puts image[i]
     sleep(0.15)
     system 'clear'
   }
-  (7..9).each { |i|
-    puts image[i]
+  (7..9).each { |j|
+    puts image[j]
     sleep(0.15)
     system 'clear'
   }
-  (7..9).each { |i|
-    puts image[i]
+  (7..9).each { |k|
+    puts image[k]
     sleep(0.15)
     system 'clear'
   }
