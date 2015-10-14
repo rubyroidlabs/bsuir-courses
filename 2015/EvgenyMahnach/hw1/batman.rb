0c50500425880620e1e980c190463ab9d095e72e
Mahnch Evgeny <webdeluxe2014@gmail.com>
2015-10-14 13:51:44 +0300

    batman homework 1

--- 2015/EvgenyMahnach/hw1/batman.rb
+++ 2015/EvgenyMahnach/hw1/batman.rb
@@ -1,19 +1,18 @@
-file = File.open("1.txt")
-arr = file.to_a
+ file = File.open('1.txt')
+ arr = file.to_a
 
-def clear_screen
-  puts "\e[H\e[2J"
-end
+ def clear_screen 
+   puts "\e[H\e[2J"
+ end
 
-def go(arr)
- arr.insert(0, " ")
- arr.map {|x| x.insert(0, " ")}
-end
+ def go(arr)
+  arr.insert(0, ' ')
+  arr.map { |x| x.insert(0, ' ') }
+ end
 
-(40).times do
-  clear_screen
-  puts arr
-  sleep(0.1)
-  go(arr)
-end
-
+ (40).times do
+   clear_screen
+   puts arr
+   sleep(0.1)
+   go(arr)
+ end
