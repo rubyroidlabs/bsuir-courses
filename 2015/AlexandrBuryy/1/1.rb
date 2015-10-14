55d2bafcca9894bc140a23dddb1e467003a4b9e6
Alex <alexandrburyy@gmail.com>
2015-10-15 02:17:31 +0300

    fixed

--- 2015/AlexandrBuryy/1/1.rb
+++ 2015/AlexandrBuryy/1/1.rb
@@ -1,11 +1,11 @@
-system 'clear'
-a = []
-a[0] = '- - - - |>'
-a[1] = '- - - - -|>'
-a[2] = '- - - - -|>'
-a[3] = '- - - - |>'
-i = 0
-loop do
+	system 'clear'
+	a = []
+	a[0] = '- - - - |>'
+	a[1] = '- - - - -|>'
+	a[2] = '- - - - -|>'
+	a[3] = '- - - - |>'
+	i = 0
+	loop do
 		system 'clear'
 		i +=1
 		puts a
@@ -14,4 +14,4 @@ loop do
 		end
 		break if i == 70
 		sleep 0.03
-end
+	end
