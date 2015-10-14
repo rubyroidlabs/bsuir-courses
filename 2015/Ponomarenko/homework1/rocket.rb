18874e07c3b1d87b9819816957036b99c4158323
Владислав Пономаренко <nightnomad@GECK.local>
2015-10-14 14:38:50 +0300

    home work 1 v.2

--- 2015/Ponomarenko/homework1/rocket.rb
+++ 2015/Ponomarenko/homework1/rocket.rb
@@ -3,20 +3,20 @@
 
 class Rocket
 
-  def initialize(a, b, i, str)
+  def initialize(a, b, i, str) #Initializing variables
     @a, @b, @i, @str = a, b, i, str
   end
 
-  def rocket(a, b)
+  def rocket(a, b) #Making a rocket
     $c = a * 3 + '>' + b * 5 + '=>'
   end
 
-  def boom
+  def boom #Variables for ending explosion
     $a1 = "#{'o' * 10}\n"
     $a2 = "#{'o' * 2}" + '-BOOM-' + "#{'o' * 2}\n"
   end
 
-  def animate(i, str)
+  def animate(i, str) #Animate rocket
     loop do 
       system 'clear'
       str += ' '
@@ -33,10 +33,9 @@ class Rocket
   end
 end
 
-rocket = Rocket.new('|', '-', 0, ' ')
+rocket = Rocket.new('|', '-', 0, ' ') 
 boom = Rocket.new('|', '-', 0, ' ')
 animate = Rocket.new('|', '-', 0, ' ')
-rocket.rocket('|', '-')
+rocket.rocket('|', '-') #Calling methods from Rocket
 boom.boom
 animate.animate(0, ' ')
-
