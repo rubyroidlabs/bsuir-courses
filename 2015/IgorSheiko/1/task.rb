744b60d6cf9c3856e9eb7fb7c2cbaeebeddf8b9e
IgorSheiko <igor2109a@mail.ru>
2015-10-14 23:22:15 +0300

    modify task1

--- 2015/IgorSheiko/1/task.rb
+++ 2015/IgorSheiko/1/task.rb
@@ -1,29 +1,24 @@
-require  'pry'
-require  'colorize'
+require 'pry'
+require 'colorize'
 
 def color(string_file)
-	string_file.size.times do |i|
-     if string_file[i] == "█" 
-     print string_file[i].red
-     elsif string_file[i] == "▞"
-     	print string_file[i].green
-     else
-       print string_file[i].blue
-     end
-   end
-
-
+  string_file.size.times do |i|
+    if string_file[i] == '█' 
+      print string_file[i].red
+    elsif string_file[i] == '▞'
+      print string_file[i].green
+    else
+      print string_file[i].blue
+    end
+  end
 end
 
 1000.times do |i|
-	kadr = i % 18
-	filename = "#{kadr}.txt"
-	f = File.open(filename, "r")
-	file_body = f.read
-	#binding.pry
-	color(file_body)
-	#puts file_body.red		
-	sleep(0.1)
-	puts "\e[H\e[2J"
+  kadr = i % 18
+  filename = "#{kadr}.txt"
+  f = File.open(filename, "r")
+  file_body = f.read
+  color(file_body)
+  sleep(0.1)
+  puts "\e[H\e[2J"
 end
-
