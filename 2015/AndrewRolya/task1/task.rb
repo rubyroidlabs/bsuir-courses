e7bcd21559ebf983ea8bcbc06a62fe10b7726590
AndrewRolya <rolya_12@mail.ru>
2015-10-15 00:00:25 +0300

    update_task1

--- 2015/AndrewRolya/task1/task.rb
+++ 2015/AndrewRolya/task1/task.rb
@@ -1,46 +1,46 @@
 require "colorize"
 
 def colorization(inputText,indicator)
-	inputText.size.times do |i|
-		case indicator
-		when 1
-			if inputText[i]=="%" 
-				print inputText[i].red
-			else 
-				print inputText[i]
-			end
-		when 2
-			if inputText[i]=="@" 
-				print inputText[i].yellow
-			else 
-				print inputText[i]
-			end
-		when 3
-			if inputText[i]=="&" 
-				print inputText[i].green
-			else 
-				print inputText[i]
-			end
-		when 4
-			if inputText[i]=="@" 
-				print inputText[i].yellow
-			else 
-				print inputText[i]
-			end
-		else 
-			print inputText[i]
-		end
-	end 
+    inputText.size.times do |i|
+        case indicator
+        when 1
+            if inputText[i]=="%"
+                print inputText[i].red
+            else
+                print inputText[i]
+            end
+        when 2
+            if inputText[i]=="@"
+                print inputText[i].yellow
+            else
+                print inputText[i]
+            end
+        when 3
+            if inputText[i]=="&"
+                print inputText[i].green
+            else
+                print inputText[i]
+            end
+        when 4
+            if inputText[i]=="@"
+                print inputText[i].yellow
+            else
+                print inputText[i]
+            end
+        else
+            print inputText[i]
+        end
+    end
 end
 
 inputSource = File.open("./inputForTask.txt").read
 counter = 0
 50.times do |i|
-	counter +=1
+    counter +=1
     colorization(inputSource,counter)
     sleep(0.3)
-    if counter==4 
-    	counter=0
+    if counter==4
+        counter=0
     end
     puts "\e[H\e[2J"
 end
