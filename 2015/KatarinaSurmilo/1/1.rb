fb8b263001d5ddb75ffe3b946eb890c1eb803ab5
Surmilo <ekaterina.surmilo@gmail.com>
2015-10-15 09:27:44 +0300

    done

--- 2015/KatarinaSurmilo/1/1.rb
+++ 2015/KatarinaSurmilo/1/1.rb
@@ -1,16 +1,9 @@
 frames = []
 
-filenames = Dir.entries('frames').select do |fn|
-  fn != '.' and fn != '..'
-end
+sorted_filepathes = Dir[File.expand_path('frames/*.txt')].sort_by {|filepath| filepath.split('/').last.to_i }
 
-sortedFns = filenames.sort do |fn1, fn2|
-  fn1.to_i - fn2.to_i
-end
-
-sortedFns.each do |filename|
-  path = "#{Dir.pwd}/frames/#{filename}"
-  frame = File.readlines(path).join
+sorted_filepathes.each do |filepath|
+  frame = File.readlines(filepath).join
   frames.push frame
 end
 
@@ -18,6 +11,6 @@ end
   frames.each do |frame|
     puts frame
     sleep 0.2
-    system('clear')
+    system "clear"
   end
 end
