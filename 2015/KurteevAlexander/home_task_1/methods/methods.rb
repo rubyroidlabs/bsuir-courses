c9a49263f71308807135168223aa90870b518c70
Alexander <sounis@mail.ru>
2015-10-15 13:30:22 +0300

    correction task 1

--- 2015/KurteevAlexander/home_task_1/methods/methods.rb
+++ 2015/KurteevAlexander/home_task_1/methods/methods.rb
@@ -1,12 +1,11 @@
 require 'curses'
 Curses.init_screen
 def max_size_engine
-	@maxsize = 0
-	0.upto(ENGINE_FIRST.size-1) do |i|
-		@l=ENGINE_FIRST[i].size
-		if @maxsize<@l
-			@maxsize = @l
-		end
-	end
-	return @maxsize
+  @maxsize = 0
+  0.upto(ENGINE_FIRST.size - 1) do |i|
+    @l = ENGINE_FIRST[i].size
+    if @maxsize < @l
+      @maxsize = @l
+    end
+  end
 end
