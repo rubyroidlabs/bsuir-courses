bfe0b266579c894bf740dac29bd179bfba800e09
Dmitry <domo@3a.by>
2015-10-15 18:42:23 +0300

    code cleared after bot check

--- 2015/DmitryParkhomchik/task1/1.rb
+++ 2015/DmitryParkhomchik/task1/1.rb
@@ -3,11 +3,11 @@ lines = File.readlines('animation.txt')
 counter = 0
 
 lines.each do |line|
-  counter += 1 
+  counter += 1
   puts line
   if counter > 32
-  	counter = 0
-  	sleep(0.2)
-  	system("clear")
+    counter = 0
+    sleep(0.2)
+    system('clear')
   end
 end
