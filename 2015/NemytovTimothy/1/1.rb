b59027b862087c215e52f82fc7e4dc70262f7062
Keshafey <keshafey@gmail.com>
2015-10-15 19:02:05 +0300

    home task 1 ruby2.2.2

--- 2015/NemytovTimothy/1/1.rb
+++ 2015/NemytovTimothy/1/1.rb
@@ -1,10 +1,10 @@
 train = [
-['                                                              '],
-['  FFFFFF  U     U  CCCCCCC  K  KK      GGGGGG   GGGGGG        '],
-['  F       U     U  C        KKK        G        G             '],
-['  FFFFF   U     U  C        K          G  GGG   G  GGG        '],
-['  F        U   U   C        KKK        G    G   G    G        '],
-['  F        UUUUU   CCCCCCC  K  KK      GGGGGG   GGGGGG        ']]
+  ['                                                              '],
+  ['  FFFFFF  U     U  CCCCCCC  K  KK      GGGGGG   GGGGGG        '],
+  ['  F       U     U  C        KKK        G        G             '],
+  ['  FFFFF   U     U  C        K          G  GGG   G  GGG        '],
+  ['  F        U   U   C        KKK        G    G   G    G        '],
+  ['  F        UUUUU   CCCCCCC  K  KK      GGGGGG   GGGGGG        ']]
 
 train.map! { |x| x[0].split(//) }
 
