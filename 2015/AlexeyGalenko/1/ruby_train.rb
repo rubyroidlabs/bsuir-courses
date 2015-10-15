8476b39c81d708227cd0bbae8e923087d082ade4
Alexey Galenko <vazov.vazov@gmail.com>
2015-10-15 09:23:44 +0300

    some edit ruby_train

--- 2015/AlexeyGalenko/1/ruby_train.rb
+++ 2015/AlexeyGalenko/1/ruby_train.rb
@@ -1,24 +1,24 @@
-LOGO1 = "     ++      +------ "
-LOGO2 = "     ||      |+-+ |  "
-LOGO3 = "   /---------|| | |  "
-LOGO4 = "  + ========  +-+ |  "
-LWHL11 = " _|--O========O~\\-+  "
-LWHL12 = "//// \\_/      \\_/    "
+LOGO1 = '     ++      +------ '
+LOGO2 = '     ||      |+-+ |  '
+LOGO3 = '   /---------|| | |  '
+LOGO4 = '  + ========  +-+ |  '
+LWHL11 = ' _|--O========O~\\-+  '
+LWHL12 = '//// \\_/      \\_/    '
 
-LWHL21 = " _|--/O========O\\-+  "
-LWHL22 = "//// \\_/      \\_/    "
+LWHL21 = ' _|--/O========O\\-+  '
+LWHL22 = '//// \\_/      \\_/    '
 
-LWHL31 = " _|--/~O========O-+  "
-LWHL32 = "//// \\_/      \\_/    "
+LWHL31 = ' _|--/~O========O-+  '
+LWHL32 = '//// \\_/      \\_/    '
 
-LWHL41 = " _|--/~\\------/~\\-+  "
-LWHL42 = "//// \\_O========O    "
+LWHL41 = ' _|--/~\\------/~\\-+  '
+LWHL42 = '//// \\_O========O    '
 
-LWHL51 = " _|--/~\\------/~\\-+  "
-LWHL52 = "//// \\O========O/    "
+LWHL51 = ' _|--/~\\------/~\\-+  '
+LWHL52 = '//// \\O========O/    '
 
-LWHL61 = " _|--/~\\------/~\\-+  "
-LWHL62 = "//// O========O_/    "
+LWHL61 = ' _|--/~\\------/~\\-+  '
+LWHL62 = '//// O========O_/    '
 train = [LOGO1,LOGO2,LOGO3,LOGO4,"",""]
 wheels= [[LWHL11,LWHL12],[LWHL21,LWHL22],[LWHL31,LWHL32],[LWHL41,LWHL42],[LWHL51,LWHL52],[LWHL61,LWHL62]]
 n= 65
@@ -27,16 +27,16 @@ l = LOGO1.length
 i = n
 empty = "#{' '*n}"
 #Path from right wall to left
-while i != 0 do
-	k = (i+n)/3%6
-    	train[4] = wheels[k][0]
-    	train[5] = wheels[k][1]
+while i != 0
+    k = (i+n)/3%6
+    train[4] = wheels[k][0]
+    train[5] = wheels[k][1]
     for j in 0..5   	
     	sub = n - i
     	if sub <= l
-    		f = f + empty[0..i] + train[j][0..sub] + "\n"
+    	    f = f + empty[0..i] + train[j][0..sub] + "\n"
     	else    		
-    		f = f + empty[0..i] + train[j] + empty[i+l..n] + "\n"   		
+    	    f = f + empty[0..i] + train[j] + empty[i+l..n] + "\n"   		
     	end   	
     end
     $> << "\e[2J\e[f" + f 
