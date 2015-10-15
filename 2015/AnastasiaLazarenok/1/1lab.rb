a844fb9ff6c613afde2fb4b6f6fc5942a29fd312
AnastasiaLazarenok <avgita@gmail.com>
2015-10-15 21:46:55 +0300

    implement home task 1.1

--- 2015/AnastasiaLazarenok/1/1lab.rb
+++ 2015/AnastasiaLazarenok/1/1lab.rb
@@ -1,17 +1,27 @@
 #!/usr/bin/ruby
 require 'io/console'
-lineWidth = 15
-for i in 0..5
-puts '(*_*)/'
-puts '/( ('
-puts  '//'
-puts '===' * 3
-sleep 0.5
-system("clear")
-puts ('\(*_*)'.center(lineWidth))
-puts (  ') )\ '.center(lineWidth))
-puts ('\\\\'.center(lineWidth))
-puts '===' * 3
-sleep 0.5
-system("clear")
+a = []
+a[0] = [
+'```````|||```````
+``````(*_*)/`````
+``````/( (``````` 
+```````//````````
+`````=======`````']
+a[1] = [
+'```````|||````````
+`````\(*_*)```````
+```````) )\```````
+```````\\\\`````````
+`````=======``````']
+	5.times do 
+		a.each do |x|
+			puts x
+			sleep 0.5
+			system("clear")
+		end
+		a.reverse_each do |x|
+			puts x
+			sleep 0.5
+			system("clear")
+		end
 end
