e04279ddd92c4f0dd7898fc46ac240f4b69b138f
Keshafey <keshafey@gmail.com>
2015-10-15 18:43:20 +0300

    home task 1 ruby2.2.2

--- 2015/NemytovTimothy/1/1.rb
+++ 2015/NemytovTimothy/1/1.rb
@@ -1,5 +1,4 @@
-train = [#Use 2 spaces for indentation in an array, relative to the start of the line where the left bracket is.
-#Prefer single-quoted strings when you don't need string interpolation or special symbols.
+train = [
 ['                                                              '],
 ['  FFFFFF  U     U  CCCCCCC  K  KK      GGGGGG   GGGGGG        '],
 ['  F       U     U  C        KKK        G        G             '],
@@ -9,11 +8,11 @@ train = [#Use 2 spaces for indentation in an array, relative to the start of the
 
 train.map! {|x| x = x[0].split(//)}#Useless assignment to variable - x.Use 2 (not 1) spaces for indentation.
 
-while (true)
-    system 'clear'
-    train.each do |x|
-        puts x.join# Use 2 (not 1) spaces for identation
-        x.rotate!
-    end
-    sleep 0.2
+while true
+  system 'clear'
+  train.each do |x|
+    puts x.join
+    x.rotate!
+  end
+  sleep 0.2
 end
