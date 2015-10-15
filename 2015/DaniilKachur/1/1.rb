5a731b3c009350f524689d497898601ac1b027ef
DDKatch <daniil.field@yandex.ru>
2015-10-15 20:19:18 +0300

    task 1 fixed

--- 2015/DaniilKachur/1/1.rb
+++ 2015/DaniilKachur/1/1.rb
@@ -3,31 +3,31 @@ require 'curses'
 include Curses
 
 def win_init(height, width)
-	width += 6
-	win = Window.new(height, width, 0, 0)
-	win.setpos(0, 0)
-	win
+  width += 6
+  win = Window.new(height, width, 0, 0)
+  win.setpos(0, 0)
+  win
 end
 
 def show_pict(window, str_pict)
-	window.addstr(str_pict)
-	window.refresh
-	window.clear
+  window.addstr(str_pict)
+  window.refresh
+  window.clear
 end
 
 init_screen
 begin
-	crmode
+  crmode
 
-	WIDTH = 60
-	HEIGHT = WIDTH + 35
-	window = win_init(HEIGHT, WIDTH)
-	frame = Array.new(10) { |i| i =  AsciiArt.new("jp/#{i}.jpeg")}
+  WIDTH = 60
+  HEIGHT = WIDTH + 35
+  window = win_init(HEIGHT, WIDTH)
+  frame = Array.new(10) { |i| i =  AsciiArt.new("jp/#{i}.jpeg")}
 
-	while true
-		frame.each do |i|
-			show_pict window, i.to_ascii_art(width: WIDTH)
-			sleep 0.01
-		end
-	end
+  while true
+    frame.each do |i|
+      show_pict window, i.to_ascii_art(width: WIDTH)
+      sleep 0.01
+    end
+  end
 end
