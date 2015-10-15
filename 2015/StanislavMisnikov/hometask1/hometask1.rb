ca44ca60d4dab25fbcdcf1b910eb6c7ed18f6cc8
beckon111 <beckon.rus@gmail.com>
2015-10-15 20:40:57 +0300

    implement hometask1

--- 2015/StanislavMisnikov/hometask1/hometask1.rb
+++ 2015/StanislavMisnikov/hometask1/hometask1.rb
@@ -2,34 +2,34 @@ a=[" ____      _____","|  _ \    |__  /","| | | |     / / ","| |_| |    / /_ ","
 n=1
 k=0
 for j in 0..120
-	for i in 0..k
-		puts ""
-	end
-	if k>15
-		n=-1
-	end
-	if k==0
-		n=1
-	end
-	if n==1
-		k=k+1
-	end
-	if n==-1
-		k=k-1
-	end
-	for i in 0..4
-		puts a[i]
-	end
-	if j>60
-		for i in 0..4
-			a[i][0]=''
-		end
-	end
-	if j<60
-		for i in 0..4
-			a[i]=" "+a[i]
-		end
-	end
-	sleep 0.1
-	system("clear")
+     for i in 0..k
+          puts ""
+     end
+     if k > 15
+          n = -1
+     end
+     if k == 0
+          n = 1
+     end
+     if n == 1
+          k = k+1
+     end
+     if n == -1
+          k = k-1
+     end
+     for i in 0..4
+         puts a[i]
+     end
+     if j > 60
+          for i in 0..4
+               a[i][0]=''
+     end
+    end
+    if j < 60
+        for i in 0..4
+            a[i]=" "+a[i]
+        end
+    end
+    sleep 0.1
+    system("clear")
 end
