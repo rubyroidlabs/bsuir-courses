c5b31d3c040f3b0b69e9661da2a57319d65ffdda
Shastel <max.shastik@yandex.ru>
2015-10-15 19:06:21 +0300

    Fix 2

--- 2015/MaxShastel/task1/animation.rb
+++ 2015/MaxShastel/task1/animation.rb
@@ -1,24 +1,24 @@
 class Animation
-  def initialize(picture, console_width=80)
+  def initialize(picture, console_width = 80)
     @pict = picture
-    @output=[]
+    @output = []
     @pict.size.times do
       @output.push('')
     end
     console_width.times do
       (0..3).each do |i|
-        @output[i]+=' '
+        @output[i] += ' '
       end
     end
   end
 
-  def paint(console_width=80, pause = 0.05)
+  def paint(console_width = 80, pause = 0.05)
     tmp_pict = @pict
-    (console_width+tmp_pict[0].length).times do
+    (console_width + tmp_pict[0].length).times do
       (0..3).each do |i|
         @output[i].slice!(0)
         if tmp_pict[i].nil?
-          tmp_pict[i] +=' '
+          tmp_pict[i] += ' '
         else
 
           tmp = tmp_pict[i].slice! 0
@@ -33,5 +33,4 @@ class Animation
       system('clear')
     end
   end
-
 end
