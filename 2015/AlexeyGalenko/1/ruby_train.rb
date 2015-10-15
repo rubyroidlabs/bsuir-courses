e56f0c56bbe9bb58d06b242d4d5b910c2c07e03d
Alexey Galenko <vazov.vazov@gmail.com>
2015-10-15 15:10:23 +0300

    Update ruby_train.rb

--- 2015/AlexeyGalenko/1/ruby_train.rb
+++ 2015/AlexeyGalenko/1/ruby_train.rb
@@ -19,14 +19,36 @@ wheels = [[L11, L12], [L21, L22], [L31, L32], [L41, L42], [L51, L52], [L61, L62]
 n = 65
 f = ""
 l = LOGO1.length
+LOGO1 = '     ++      +------ '
+LOGO2 = '     ||      |+-+ |  '
+LOGO3 = '   /---------|| | |  '
+LOGO4 = '  + ========  +-+ |  '
+L11 = ' _|--O========O~\\-+  '
+L12 = '//// \\_/      \\_/    '
+L21 = ' _|--/O========O\\-+  '
+L22 = '//// \\_/      \\_/    '
+L31 = ' _|--/~O========O-+  '
+L32 = '//// \\_/      \\_/    '
+L41 = ' _|--/~\\------/~\\-+  '
+L42 = '//// \\_O========O    '
+L51 = ' _|--/~\\------/~\\-+  '
+L52 = '//// \\O========O/    '
+L61 = ' _|--/~\\------/~\\-+  '
+L62 = '//// O========O_/    '
+train = [LOGO1, LOGO2, LOGO3, LOGO4, '', '']
+wheels = [[L11, L12], [L21, L22], [L31, L32], [L41, L42], [L51, L52]] 
+wheels << [L61, L62]
+n = 65
+f = ""
+l = LOGO1.length
 i = n
 empty = "#{' ' * n}"
-ary = [0,1,2,3,4,5]
+ary = [0, 1, 2, 3, 4, 5]
 # Path from right wall to left
 while i != 0
   k = (i + n) / 3 % 6
-  train << wheels[k][0]
-  train << wheels[k][1]
+  train[4] = wheels[k][0]
+  train[5] = wheels[k][1]
   ary.each do |j|
     sub = n - i
     if sub <= l
@@ -44,8 +66,8 @@ end
 i = 0
 while i != l
   k = i / 3 % 6
-  train << wheels[k][0]
-  train << wheels[k][1]
+  train[4] = wheels[k][0]
+  train[5] = wheels[k][1]
   ary.each do |j|
     f = f + train[j][i..l] + empty[l - i..n] + "\n"
   end
