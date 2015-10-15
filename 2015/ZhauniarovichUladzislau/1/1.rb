87a50b210e68a0664e6f056f03750aadd4c3afd1
punkofnewsociety <punkofnewsociety@gmail.com>
2015-10-15 04:37:43 +0300

    we

--- 2015/ZhauniarovichUladzislau/1/1.rb
+++ 2015/ZhauniarovichUladzislau/1/1.rb
@@ -1,4 +1,4 @@
-sun ="11..................................................11
+sun = "11..................................................11
 10()     .     . .  .  .    O  .     .     .       .10
 9.  .    .    . .   .  .    .  .    .     .        . 9
 8.   .    .   . .   .  .   .  .    .     .        .  8
@@ -29,23 +29,21 @@ y = galaxy.size / 2
 x = (galaxy[y].size + 1) / 2
 puts x
 puts y
-until ( i > y && j > x)
+until i > y && j > x
 print `clear`
 0.upto(galaxy.size - 1) do |height|
 	0.upto(galaxy[height].size - 1) do |width|
 	(1).times do
 	print ' '
-	end	
-	case 
-	when (height < y - i || height > y + i) 
-		then print ''
-	when	(width > x + j)
-		then
-		print ''
-		when (height >= y - i && height <= y + i) &&(width >= x - j && width <= x + j)
-		then
-		print galaxy[height][width]
-		end
+	end
+	case
+	when (height < y - i || height > y + i) then
+	print ''
+	when (width > x + j) then
+	print ''
+	when (height >= y - i && height <= y + i) &&(width >= x - j && width <= x + j) then
+	print galaxy[height][width]
+	end
 	end
 	puts ""
 end
