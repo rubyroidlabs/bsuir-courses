5c7b46a72d81efa481dfab4a45fd3ea564d37dc8
Anastasiya <mainastia91@gmail.com>
2015-10-15 23:43:44 +0300

    mistakes fixed

--- 2015/Sadovaya/1/try.rb
+++ 2015/Sadovaya/1/try.rb
@@ -1,15 +1,10 @@
-filestrings = ["text1.txt", "text2.txt", "text3.txt", "text4.txt", "text5.txt", "text6.txt", "text7.txt", "text8.txt"]
-
-i = 0
-while i <= 10
-  filestrings.each do |filename|
-    IO.foreach(filename) do |line|
-      puts line 
+5.times {
+  for i in 1..8
+    IO.foreach("text#{i}.txt") do |line|
+      puts line
     end
     sleep 0.2
     system("clear")
   end
-  i += 1
-end
-
+}
 
