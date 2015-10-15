4aeec5286a306f828ad8e46b1a4a4972be16967b
Keshafey <keshafey@gmail.com>
2015-10-15 18:26:43 +0300

    home task 1 ruby2.2.2

--- 2015/NemytovTimothy/1/1.rb
+++ 2015/NemytovTimothy/1/1.rb
@@ -1,20 +1,19 @@
-train = [
-["                                                              "],  
-["  FFFFFF  U     U  CCCCCCC  K  KK      GGGGGG   GGGGGG        "],  
-["  F       U     U  C        KKK        G        G             "],  
-["  FFFFF   U     U  C        K          G  GGG   G  GGG        "],  
-["  F        U   U   C        KKK        G    G   G    G        "], 
-["  F        UUUUU   CCCCCCC  K  KK      GGGGGG   GGGGGG        "]]
+train = [#Use 2 spaces for indentation in an array, relative to the start of the line where the left bracket is.
+#Prefer single-quoted strings when you don't need string interpolation or special symbols.
+['                                                              '],
+['  FFFFFF  U     U  CCCCCCC  K  KK      GGGGGG   GGGGGG        '],
+['  F       U     U  C        KKK        G        G             '],
+['  FFFFF   U     U  C        K          G  GGG   G  GGG        '],
+['  F        U   U   C        KKK        G    G   G    G        '],
+['  F        UUUUU   CCCCCCC  K  KK      GGGGGG   GGGGGG        ']]
 
-train.map! do |x|
-	x = x[0].split(//)
-end
+train.map! {|x| x = x[0].split(//)}#Useless assignment to variable - x.Use 2 (not 1) spaces for indentation.
 
-while(true) do
-	system "clear"
-	train.each do |x|
-		puts x.join
-		x.rotate!
-	end	
-	sleep 0.2
+while (true)# do # while without do ? 
+    system 'clear'
+    train.each do |x|
+        puts x.join# Use 2 (not 1) spaces for identation
+        x.rotate!
+    end
+    sleep 0.2
 end
