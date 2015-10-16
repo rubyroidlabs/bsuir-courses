e6a9c456de6e6b3d2d7c87e173ef5e7a96f3c952
Dmi <dmi.nevsky@gmail.com>
2015-10-17 00:33:09 +0300

    Up hw1.rb

--- 2015/Abdurahmanov/hw1.rb
+++ 2015/Abdurahmanov/hw1.rb
@@ -1,21 +1,31 @@
+# !/usr/bin/env ruby
 # Bear ride
-# @version 0.3.9
+# @version 0.3.9.9
 # @author D.Abdurakhmanov
-# Global variables $s1-s6, $t1-t27 
-#Dir[File.expand_path('./lib/*.rb',__FILE__)].each {|f| require(f)}
+# Global variables $s1-s6, $t1-t27
 require_relative './lib/character.rb'
 require_relative './lib/simpleEncoder.rb'
 require 'colored'
 # animation engine
-s0 = ' '
-i = 0
-loop do 
-  system 'clear'
-  s0 += ' '
-  puts (s0 + $s1 + "\n" + s0 + $s2 + "\n" + s0 + $s3 + "\n" + s0 + $s4 + "\n" + s0 + $s5 + "\n" + s0 + $s6).green
-  sleep 0.05
-  i += 1
-  if i == 30
-  break loop
-  end
+class Bear
+	def initialize(ii = 10)
+		@ii = ii
+	end
+	def anim
+		s0 = ' '
+		i = 0
+		loop do
+  			system 'clear'
+  			s0 += ' '
+  			puts (s0 + $s1 + "\n" + s0 + $s2 + "\n" + s0 + $s3).red
+  			puts (s0 + $s4 + "\n" + s0 + $s5 + "\n" + s0 + $s6).green
+  			sleep 0.05
+  			i += 1
+  			if i == @ii
+  		break loop
+  			end
+		end
+	end
 end
+b = Bear.new(30)
+b.anim
