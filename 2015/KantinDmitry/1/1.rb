cec087733542aa2d1be1cf0fc2efecde0662ebff
KantinDmitry <kantindm@gmail.com>
2015-10-16 22:18:28 +0300

    home task 1 correction 1
    home task 1 correction 2
    
    home task 1 correction 3
    
    home task 1 correction 4
    
    home task 1 correction 5
    
    home task 1 correction 6
    
    home task 1 correction 7
    
    home task 1 correction 8
    
    home task 1 correction 9

--- 2015/KantinDmitry/1/1.rb
+++ 2015/KantinDmitry/1/1.rb
@@ -1,43 +1,42 @@
 #!/usr/bin/env ruby
 
-text = 	[		'                       *     ,MMM8&&&.            *    ',
-			'                            MMMM88&&&&&    .           ',
-			'                           MMMM88&&&&&&&               ',
-			'               *           MMM88&&&&&&&&               ',
-			'                           MMM88&&&&&&&&               ',
-			'                           MMM88&&&&&&&                 ',
-			'                             MMM8&&&&^     *            ',
-			'                    |\___/|                            ',
-			'                    )     (             .              ',
-			'                   =\     /=                           ',
-			'                     )===(       *                     ',
-			'                    /     \                            ',
-			'                    |     |                            ',
-			'                   /       \                           ',
-			'                   \       /                           ',
-			'            _/\_/\_/\__  _/_/\_/\_/\_/\_/\_/\_/\_/\_/\_',
-			'            |  |  |  |( (  |  |  |  |  |  |  |  |  |  |',
-			'            |  |  |  | ) ) |  |  |  |  |  |  |  |  |  |',
-			'            |  |  |  |(_(  |  |  |  |  |  |  |  |  |  |',
-			'            |  |  |  |  |  |  |  |  |  |  |  |  |  |  |',
-			'            |  |  |  |  |  |  |  |  |  |  |  |  |  |  |' ]
+text = [
+  '                       *     ,MMM8&&&.            *    ',
+  '                            MMMM88&&&&&    .           ',
+  '                           MMMM88&&&&&&&               ',
+  '               *           MMM88&&&&&&&&               ',
+  '                           MMM88&&&&&&&&               ',
+  '                           MMM88&&&&&&&                ',
+  '                             MMM8&&&&^     *           ',
+  '                    |\___/|                            ',
+  '                    )     (             .              ',
+  '                   =\     /=                           ',
+  '                     )===(       *                     ',
+  '                    /     \                            ',
+  '                    |     |                            ',
+  '                   /       \                           ',
+  '                   \       /                           ',
+  '            _/\_/\_/\__  _/_/\_/\_/\_/\_/\_/\_/\_/\_/\_',
+  '            |  |  |  |( (  |  |  |  |  |  |  |  |  |  |',
+  '            |  |  |  | ) ) |  |  |  |  |  |  |  |  |  |',
+  '            |  |  |  |(_(  |  |  |  |  |  |  |  |  |  |',
+  '            |  |  |  |  |  |  |  |  |  |  |  |  |  |  |',
+  '            |  |  |  |  |  |  |  |  |  |  |  |  |  |  |'
+ ]
 
-
-direction = 1;
-5.times do
-	15.times do
-		system 'clear'
-		text.each do |string|
-			puts(string)
-			if (direction == 0)
-				 string.slice!(0)
-			else 
-				string.insert(0, ' ')
-			end
-			end
-		sleep(0.06)
-	end
-	direction ^= 1 ;
- end
-
-
+  direction = 1
+  5.times do
+  15.times do
+    system 'clear'
+    text.each do |string|
+      puts(string)
+      if (direction == 0)
+        string.slice!(0)
+      else
+        string.insert(0, ' ')
+      end
+    end
+  sleep(0.05)
+  end
+  direction ^= 1
+  end
