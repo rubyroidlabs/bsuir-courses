fdcac4eb3000d6d35e3ff8fd4e533470cb7a700b
yanezh <greeff96@gmail.com>
2015-10-18 21:36:41 +0300

    Fixing hounds comments

--- 2015/SuvitovEvgeniy/2/lib/input.rb
+++ 2015/SuvitovEvgeniy/2/lib/input.rb
@@ -6,7 +6,7 @@ class InputParse
   def parse
     @opts = {}
     @args.each_index do |i|
-      self.add(@args[i]) if i != 0
+      add(@args[i]) if i != 0
     end
     @opts
   end
@@ -16,33 +16,31 @@ class InputParse
   end
 
   def add (x)
-    if (x.include?(">= "))
+    if (x.include?('>= '))
       if (@opts.has_key?(:greatorequal))
-        if (x.delete(">= ") > @opts[:greatorequal])
-          @opts[:greatorequal] = x.delete(">= ")
+        if (x.delete('>= ') > @opts[:greatorequal])
+          @opts[:greatorequal] = x.delete('>= ')
         end
       else
-        @opts[:greatorequal] = x.delete(">= ")
+        @opts[:greatorequal] = x.delete('>= ')
       end
       return
     end
-
-    if (x.include?("<= "))
+    if (x.include?('<= '))
       if (@opts.has_key?(:loworequal))
-        if (x.delete("<= ") < @opts[:loworequal])
-          @opts[:loworequal] = x.delete("<= ")
+        if (x.delete('<= ') < @opts[:loworequal])
+          @opts[:loworequal] = x.delete('<= ')
         end
       else
-        @opts[:loworequal] = x.delete("<= ")
+        @opts[:loworequal] = x.delete('<= ')
       end
       return
     end
-
-    if (x.include?("~> "))
-      self.add(">=" + x.delete("~>"))
-      temp_array = x.delete("~> ").split(".")
+    if (x.include?('~> '))
+      add('>=' + x.delete('~>'))
+      temp_array = x.delete('~> ').split('.')
       if (temp_array.size > 1)
-        temp_array[-1] = "0"
+        temp_array[-1] = '0'
         temp_char = temp_array[-2].to_i
         temp_char += 1
         temp_array[-2] = temp_char.to_s
@@ -51,39 +49,36 @@ class InputParse
         temp_char += 1
         temp_array[-1] = temp_char.to_s
       end
-      self.add("< " + temp_array.join("."))
+      add('< ' + temp_array.join('.'))
       return 
     end
-
-    if (x.include?("> "))
+    if (x.include?('> '))
       if (@opts.has_key?(:great))
-        if (x.delete("> ") > @opts[:great])
-          @opts[:great] = x.delete("> ")
+        if (x.delete('> ') > @opts[:great])
+          @opts[:great] = x.delete('> ')
         end
       else
-        @opts[:great] = x.delete("> ")
+        @opts[:great] = x.delete('> ')
       end
       return
     end
-
-    if (x.include?("< "))
+    if (x.include?('< '))
       if (@opts.has_key?(:low))
-        if (x.delete("< ") < @opts[:low])
-          @opts[:low] = x.delete("< ")
+        if (x.delete('< ') < @opts[:low])
+          @opts[:low] = x.delete('< ')
         end
       else
-        @opts[:low] = x.delete("< ")
+        @opts[:low] = x.delete('< ')
       end
       return
     end
-
-    if(@opts.has_key?(:equal))
+    if (@opts.has_key?(:equal))
       raise
     else
       @opts[:equal] = x
     end
   rescue
-    puts "Error, two equating"
+    puts 'Error, two equating'
     Kernel.abort
   end
 end
