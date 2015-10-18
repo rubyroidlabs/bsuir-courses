9beb252a978ffe7c5c882278345149c82debbce2
yanezh <greeff96@gmail.com>
2015-10-18 21:57:34 +0300

    fixing hounds comments №4

--- 2015/SuvitovEvgeniy/2/lib/fetcher.rb
+++ 2015/SuvitovEvgeniy/2/lib/fetcher.rb
@@ -1,17 +1,17 @@
 require 'json'
 class Fetcher
 
-def initialize(name)
-  @name = name
-end
+  def initialize(name)
+    @name = name
+  end
 
-def fetch
-  result = `curl https://rubygems.org/api/v1/versions/#{@name}.json`
-  json = JSON.parse(result)
-  raise if json.empty?
-  json.map! { |s| s['number'] }
-rescue
-  puts 'Wrong name of gem'
-  Kernel.abort
+  def fetch
+    result = `curl https://rubygems.org/api/v1/versions/#{@name}.json`
+    json = JSON.parse(result)
+    raise if json.empty?
+    json.map! { |s| s['number'] }
+  rescue
+    puts 'Wrong name of gem'
+    Kernel.abort
   end
 end
