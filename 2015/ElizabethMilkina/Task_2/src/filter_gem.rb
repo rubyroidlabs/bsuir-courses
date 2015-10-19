8fad6ad2a7551301101e2f6b1540be199196f77e
MsMilkina <ms.milkina@gmail.com>
2015-10-20 00:04:17 +0300

    Fix bot comment

--- 2015/ElizabethMilkina/Task_2/src/filter_gem.rb
+++ 2015/ElizabethMilkina/Task_2/src/filter_gem.rb
@@ -4,14 +4,20 @@ require 'colored'
 class FilterGem
   def get_filter
     puts 'Версии, удовлетворяющие условию фильтрации:'
-    $filtered_versions.empty? ? (puts 'Таких версий нет ;('.blue) :
-        ($filtered_versions.each do |version|
-          puts version.green
-        end)
+    if $filtered_versions.empty?
+      puts 'Таких версий нет ;('.blue
+    else
+      $filtered_versions.each do |version|
+        puts version.green
+      end
+    end
     puts 'Версии, не удовлетворяющие условию фильтрации:'
-    $not_matched_versions.empty? ? (puts 'Других версий нет :)'.yellow) :
-        ($not_matched_versions.each do |version|
-          puts version.red
-        end)
+    if $not_matched_versions.empty?
+      puts 'Других версий нет :)'.yellow
+    else
+      $not_matched_versions.each do |version|
+        puts version.red
+      end
+    end
   end
 end
