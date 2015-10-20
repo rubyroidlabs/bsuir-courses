542903f28b831c0c32e7f3c24f5166c6672a0c37
AlexStasjuk <Masherov14@tut.by>
2015-10-20 14:27:26 +0300

    Implemented task2

--- 2015/AlexStasjuk/1/1.rb
+++ 2015/AlexStasjuk/1/1.rb
@@ -3,57 +3,55 @@ include Curses
 
 class Car
   def initialize()
-    @image1 = [ 
-  "                                         AIgg",
-  "                                     gggggggggg ",
-  "                                  ggggggggggg0gg",
-  "                               ggggggggggggggggggg",
-  "      gg     ggggggg         gggggggggggg      ggggg",
-  "  ggggggggggggggggggggggggggggggggggggggg",
-  " gggggg  ggggggggggggggggggggggggggggggg",
-  "gggggg   gggggggggggggggggggggggggggggg",
-  "ggggg    ggggggggggggggggggggggggggggg",
-  "gggg     ggggggggggggggggggggggggggggg",
-  "gg        ggggggggggggggggggggggggggg",
-  "         ggggggggg  gggggggggggggggggg",
-  "      gggggggggggg             ggggggg",
-  "     gggg    gggg                ggggg",
-  "     ggg      ggg                  gggg",
-  "     gg      gggg          ggg   ggggggggg",
-  "    ggg       ggg            gggggg    ggg",
-  "   ggg         ggg                       gg",
-  "   gg           ggg                       gg",
-  "                 gggg                     ggg",
-  "                 gggg                     ggg"]
+    @image1 = [
+  '                                         AIgg',
+  '                                     gggggggggg ',
+  '                                  ggggggggggg0gg',
+  '                               ggggggggggggggggggg',
+  '      gg     ggggggg         gggggggggggg      ggggg',
+  '  ggggggggggggggggggggggggggggggggggggggg',
+  ' gggggg  ggggggggggggggggggggggggggggggg',
+  'gggggg   gggggggggggggggggggggggggggggg',
+  'ggggg    ggggggggggggggggggggggggggggg',
+  'gggg     ggggggggggggggggggggggggggggg',
+  'gg        ggggggggggggggggggggggggggg',
+  '         ggggggggg  gggggggggggggggggg',
+  '      gggggggggggg             ggggggg',
+  '     gggg    gggg                ggggg',
+  '     ggg      ggg                  gggg',
+  '     gg      gggg          ggg   ggggggggg',
+  '    ggg       ggg            gggggg    ggg',
+  '   ggg         ggg                       gg',
+  '   gg           ggg                       gg',
+  '                 gggg                     ggg',
+  '                 gggg                     ggg']
 
-    @image2 = [ 
-  "                                        AIgg",
-  "                                     gggggggggg ",
-  "                                  ggggggggggg0gg",
-  "                               ggggggggggggggggggg",
-  "      gg     ggggggg         gggggggggggg      ggggg",
-  "  ggggggggggggggggggggggggggggggggggggggg",
-  " gggggg  ggggggggggggggggggggggggggggggg",
-  "gggggg   gggggggggggggggggggggggggggggg",
-  "ggggg    ggggggggggggggggggggggggggggg",
-  "gggg     ggggggggggggggggggggggggggggg",
-  "gg        ggggggggggggggggggggggggggg",
-  "           gggggggg gggggggggggggggggg",
-  "            gggggggg            ggggggg",
-  "             ggggggg              ggggg",
-  "              gggggggg            gggggg",
-  "              gggg  ggggg       ggg  gggg",
-  "              gggg    gggg     ggg    ggg",
-  "              ggg       gggg   ggg      gg",
-  "            ggg          ggg  ggg     gg",
-  "           ggg            gg  ggg    ggg",
-  "         ggg                   ggg    gg"]
+    @image2 = [
+  '                                        AIgg',
+  '                                     gggggggggg ',
+  '                                  ggggggggggg0gg',
+  '                               ggggggggggggggggggg',
+  '      gg     ggggggg         gggggggggggg      ggggg',
+  '  ggggggggggggggggggggggggggggggggggggggg',
+  ' gggggg  ggggggggggggggggggggggggggggggg',
+  'gggggg   gggggggggggggggggggggggggggggg',
+  'ggggg    ggggggggggggggggggggggggggggg',
+  'gggg     ggggggggggggggggggggggggggggg',
+  'gg        ggggggggggggggggggggggggggg',
+  '           gggggggg gggggggggggggggggg',
+  '            gggggggg            ggggggg',
+  '             ggggggg              ggggg',
+  '              gggggggg            gggggg',
+  '              gggg  ggggg       ggg  gggg',
+  '              gggg    gggg     ggg    ggg',
+  '              ggg       gggg   ggg      gg',
+  '            ggg          ggg  ggg     gg',
+  '           ggg            gg  ggg    ggg',
+  '         ggg                   ggg    gg']
   end
 
   def print(y, x, item)
-    
     k = 0
-    
     while (k < @image1.length)
       Curses.setpos(y + k, x)
       if (item==0)
@@ -63,16 +61,14 @@ class Car
       end 
       k += 1
     end
-
     Curses.refresh
   end
 end
 
-
 car = Car.new()
 Curses.init_screen
 Curses.nl
-Curses.noecho     
+Curses.noecho
 Curses.curs_set(0)
 i = 0
 Y = Curses.lines / 5
