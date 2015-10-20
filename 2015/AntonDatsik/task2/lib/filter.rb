88a7ff361426cb52b59dfd6e5258de1b3dd06ffe
AntonDatsik <Anton.Dacik@gmail.com>
2015-10-20 16:15:35 +0300

    modified for hound

--- 2015/AntonDatsik/task2/lib/filter.rb
+++ 2015/AntonDatsik/task2/lib/filter.rb
@@ -10,7 +10,7 @@ class Filter
           result_versions.add(variable.text)
         end
       end
-    rescue ArgumentError => e
+    rescue ArgumentError
       puts 'Your parameters are wrong!'
       exit
     end
