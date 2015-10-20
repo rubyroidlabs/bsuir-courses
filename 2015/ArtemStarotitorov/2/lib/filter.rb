5245006f36aef737835f33727d8e65b04519d7e9
Starotitorov Artem <starotitorov1997@gmail.com>
2015-10-20 15:52:13 +0300

    Modified task 2

--- 2015/ArtemStarotitorov/2/lib/filter.rb
+++ 2015/ArtemStarotitorov/2/lib/filter.rb
@@ -1,7 +1,7 @@
 class Filter
   def self.get_hash_of_filtered_versions(versions, conditions)
+    hash_of_versions = {}
     begin
-      hash_of_versions = {}
       requirement = Gem::Requirement.new(conditions)
       versions.each do |v|
         hash_of_versions[v] = requirement.satisfied_by?(Gem::Version.new(v))
