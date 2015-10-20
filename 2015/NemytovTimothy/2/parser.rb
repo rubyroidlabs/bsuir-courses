8eeb2828b821520999ff6e6ebddc9a1c081c9269
Keshafey <keshafey@gmail.com>
2015-10-21 00:57:13 +0300

    task 2 ruby2.2.2

---
+++ 2015/NemytovTimothy/2/parser.rb
@@ -0,0 +1,21 @@
+require 'open-uri'
+require 'nokogiri'
+
+module Parser
+  public
+  def self.parse_str(name)
+    begin
+      url = "https://rubygems.org/gems/#{name}/versions"
+      html = open(url)
+      doc = Nokogiri::HTML(html)
+      versions = []
+      doc.css('.versions').each do |version|
+        versions  = version.css('.gem__version-wrap a').map{ |v| v.text}
+      end
+    rescue StandardError => exc
+      puts exc.message
+      exit
+    end
+    return versions
+  end
+end
