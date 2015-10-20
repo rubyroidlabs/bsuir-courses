7dde9d78e0cb58329f3a4b31c1ea41ad9ee4eaad
Vlad Klimenkoff <vlad.klimenkoff97@gmail.com>
2015-10-20 23:42:56 +0300

    last codestyle fixes

--- 2015/VladKlimenkoff/2/html_parser.rb
+++ 2015/VladKlimenkoff/2/html_parser.rb
@@ -1,11 +1,11 @@
 class HTMLParser
-  GEMS_URL = "https://rubygems.org/gems/"
+  GEMS_URL = 'https://rubygems.org/gems/'
   def initialize(gem_name)
     @gem_name = gem_name
   end
 
   def get_versions
-    page = Nokogiri::HTML(open(GEMS_URL + @gem_name + "/versions"));
+    page = Nokogiri::HTML(open(GEMS_URL + @gem_name + "/versions"))
     lis = page.css('li')
     lis.map do |li|
       li.css('a').text
