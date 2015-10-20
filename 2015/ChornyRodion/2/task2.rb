0c7a2c840cd3c1cb263f4dd6498b361d57350502
creeston <mityy2012@gmail.com>
2015-10-20 18:16:41 +0300

    another bunch of fixes

--- 2015/ChornyRodion/2/task2.rb
+++ 2015/ChornyRodion/2/task2.rb
@@ -4,21 +4,6 @@ require 'colorize'
 class URLParse
   @slice_range = 0..32
   @filter_pattern = '/versions/'
-
-  def initialize(url)
-    @content = URI.parse(url).read
-    rescue OpenURI::HTTPError => ex
-    puts 'Invalid url ;('
-    exit
-  end
-
-  def find_versions
-    #divide into array of tags
-    array = @content.split('>').select! { |str|  str[@filter_pattern] }
-    # a class="t-list__item" href="https://rubygems.org/gems/gems/versions/0.8.1" => https://rubygems.org/gems/gems/versions/0.8.1 => 0.8.1
-    return array.each { |str| str.slice!(@slice_range) && str.slice!(str.length - 1)  && str.sub!(@url, "")}
-  end
-
   def self.find_versions(url)
     begin
       content = URI.parse(url).read
@@ -26,17 +11,15 @@ class URLParse
       puts 'Invlid url ;('
       exit
     end
-    array = content.split('>').select! { |str|  str[@filter_pattern] }
-    return array.each { |str| str.slice!(@slice_range) && str.slice!(str.length - 1)  && str.sub!(url, "") && str.sub!('/', "")}
+    array = content.split('>').select! { |str| str[@filter_pattern] }
+    array.each { |str| str.slice!(@slice_range) }
+    array.each { |str| str.slice!(str.length - 1) && str.sub!(url, '') }
+    array.each { |str| str.sub!('/', '') }
   end
 end
 
 class InputSecure
-  def self.check?(stream)
-    if stream.size > 3
-      puts 'Invalid input => stream size'
-      exit
-    end
+  def self.check(stream)
     (stream.size - 1).times do |i|
       begin
         Gem::Dependency.new('', stream[i + 1]).match?('', '1.0.0')
@@ -44,6 +27,10 @@ class InputSecure
         puts "Invalid input => #{stream[i + 1]}"
         exit
       end
+    end
+    if stream.size > 3
+      puts 'Invalid input => stream size'
+      exit
     end
     begin
       URI.parse("https://rubygems.org/gems/#{stream[0]}/versions").read
@@ -71,39 +58,49 @@ class InputParse
   end
 
   def filter_options
-    filter_options = case @option_count
-      when 1 
-        @stream[1]
-      when 2
-        @stream[1..2]
-      else 
-        @stream[1]
-      end
+    case @option_count
+    when 1
+      @stream[1]
+    when 2
+      @stream[1..2]
+    else
+      @stream[1]
+    end
   end
 end
 
 class Filter
   def initialize(list, filter_options)
     @list = list
-    if filter_options.size == 2 
-      @filter_method = lambda { |x| return true if Gem::Dependency.new('', filter_options[0]).match?('', x) && Gem::Dependency.new('', filter_options[1]).match?('', x) } 
+    if filter_options.size == 2
+      @filter_method = lambda do |x|
+        if Gem::Dependency.new('', filter_options[0]).match?('', x) &&
+           Gem::Dependency.new('', filter_options[1]).match?('', x)
+          return true
+        end
+      end
     else
-      @filter_method = lambda { |x| return true if Gem::Dependency.new('', filter_options).match?('', x) }
+      @filter_method = lambda do |x|
+        if Gem::Dependency.new('', filter_options).match?('', x)
+          return true
+        end
+      end
     end
   end
 
-  def output 
+  def output
     @list.each do |element|
       if @filter_method.call(element)
         puts element.colorize(:green)
-      else 
+      else
         puts element
       end
     end
   end
 end
 
-input_parse = InputParse.new(ARGV) if InputSecure.check?(ARGV)
-versions = URLParse.find_versions("https://rubygems.org/gems/#{input_parse.gem_name}/versions")
+input_parse = InputParse.new(ARGV) if InputSecure.check(ARGV)
+url = "https://rubygems.org/gems/#{input_parse.gem_name}/versions"
+versions = URLParse.find_versions(url)
 filter = Filter.new(versions, input_parse.filter_options)
 filter.output
