dd8231bb09e56cf104ab82f7cd4130721824bd90
Starotitorov Artem <starotitorov1997@gmail.com>
2015-10-20 15:34:46 +0300

    Home task 2

---
+++ 2015/ArtemStarotitorov/2/lib/fetcher.rb
@@ -0,0 +1,18 @@
+require 'json'
+require 'net/http'
+
+class Fetcher
+  def self.get_array_of_versions(required_gem)
+    begin
+      uri = URI("https://rubygems.org/api/v1/versions/#{ required_gem }.json")
+      response = Net::HTTP.get_response(uri)
+      hashes = JSON.parse(response.body)
+      items = []
+      hashes.each { |h| items << h['number'] }
+    rescue StandardError => exc
+      puts exc.message
+      exit
+    end
+    items
+  end
+end
---
+++ 2015/ArtemStarotitorov/2/lib/filter.rb
@@ -0,0 +1,15 @@
+class Filter
+  def self.get_hash_of_filtered_versions(versions, conditions)
+    begin
+      hash_of_versions = {}
+      requirement = Gem::Requirement.new(conditions)
+      versions.each do |v|
+        hash_of_versions[v] = requirement.satisfied_by?(Gem::Version.new(v))
+      end
+      hash_of_versions
+    rescue StandardError => exc
+      puts exc.message
+      exit
+    end
+  end
+end
