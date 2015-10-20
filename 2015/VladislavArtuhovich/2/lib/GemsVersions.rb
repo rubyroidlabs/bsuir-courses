442eee6aa506352cf812e449f07ef1912cd3e36c
ArtuhovichVlad <artvlad96@gmail.com>
2015-10-21 02:58:28 +0300

    Implement hometask 2

--- 2015/VladislavArtuhovich/2/lib/GemsVersions.rb
+++ 2015/VladislavArtuhovich/2/lib/GemsVersions.rb
@@ -1,10 +1,9 @@
 require 'colored'
 
 class GemsVersions
-  def show_versions(gems_arr, first_condition, second_condition = '')
+  def show_versions(gems_arr, first_cond, second_cond = '')
     gems_arr.each do |vers|
-      if is_version_under_condition(vers, first_condition) && 
-        is_version_under_condition(vers, second_condition)
+      if v_under_cond?(vers, first_cond) && v_under_cond?(vers, second_cond)
         puts vers.red
       else
         puts vers
@@ -12,7 +11,7 @@ class GemsVersions
     end
   end
 
-  def is_version_under_condition(v, condition)
+  def v_under_cond?(v, condition)
     Gem::Dependency.new('', condition).match?('', v.gsub(/[.][a-z]+[0-9]/, ''))
   end
 end
