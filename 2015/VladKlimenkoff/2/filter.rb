76b27545a4621a375720470be93d81dc1f9a1d0b
Vlad Klimenkoff <vlad.klimenkoff97@gmail.com>
2015-10-20 18:49:18 +0300

    all is working

--- 2015/VladKlimenkoff/2/filter.rb
+++ 2015/VladKlimenkoff/2/filter.rb
@@ -1,10 +1,27 @@
 class Filter
-	def self.is_satisfied?(version, condition)
-		satisfied = false
-		needed_version = Gem::Requirement.new(condition)
-		if needed_version.satisfied_by?(Gem::Version.new(version))
-			satisfied = true
-		end
-		satisfied
+	def self.is_satisfied?(version, conditions)
+		satisfied = []
+
+    conditions.each do |condition|
+      begin
+        needed_version = Gem::Requirement.new(condition)
+        if needed_version.satisfied_by?(Gem::Version.new(version))
+          satisfied << true
+        else
+          satisfied << false
+        end
+      rescue StandardError
+        puts "Incorrect arguments"
+        exit
+      end
+    end
+
+		satisfied.each do |good_cond|
+      if good_cond == false
+        return false
+      end
+    end
+    
+    true
 	end
 end
--- 2015/VladKlimenkoff/2/main.rb
+++ 2015/VladKlimenkoff/2/main.rb
@@ -5,8 +5,20 @@ require './html_parser'
 require './version_checker'
 require './visualizer.rb'
 
-versions_list = HTMLParser.new(ARGV[0]).get_versions
 
-versions_hash = VersionChecker.check_versions(versions_list, ARGV[1])
+if ARGV.length < 2 || ARGV.length > 3
+  puts 'Incorrect number of arguments'
+  puts 'Template: ./gemfiler <gem_name> [\'version conditions\']'
+  exit
+end
+
+begin
+  versions_list = HTMLParser.new(ARGV[0]).get_versions
+rescue StandardError
+  puts 'Oops! Something with your internet connection'
+  exit
+end
+
+versions_hash = VersionChecker.check_versions(versions_list, ARGV[1...ARGV.length])
 
 Visualizer.new(versions_hash).print
