36570d77add26973b9f355d4aeb49e2abff7d5d8
AlexStasjuk <Masherov14@tut.by>
2015-10-20 16:07:12 +0300

    Implemented task1

--- 2015/AlexStasjuk/2/lib/printer.rb
+++ 2015/AlexStasjuk/2/lib/printer.rb
@@ -3,7 +3,7 @@ require 'colorize'
 class Printer
   def self.print(suitable_versions, available_versions)
     available_versions.each do |variable|
-      if  suitable_versions.intersect?(Set[variable.text])
+      if suitable_versions.intersect?(Set[variable.text])
         puts variable.text.red
       else
         puts variable.text
