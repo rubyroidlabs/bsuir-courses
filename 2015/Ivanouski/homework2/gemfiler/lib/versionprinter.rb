1db0348d61d020885a656e29e0d76319142a09a0
mafflin <mafflin@gmail.com>
2015-10-21 20:19:49 +0300

    Homework 2. Verison 0.1.0

---
+++ 2015/Ivanouski/homework2/gemfiler/lib/versionprinter.rb
@@ -0,0 +1,16 @@
+class VersionPrinter
+	def print_versions(version, name)
+		@version = version.reverse
+		@name = name.blue
+		@version.each do |version|
+			print "#{@name} #{version} \n"
+		end
+	end
+
+	def print_all(versions, name)
+		@name = name.blue
+		versions.each do |i|
+			print "#{@name} #{i["number"]} \n"
+		end
+	end
+end
