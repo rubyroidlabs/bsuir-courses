4fb371b9d0a5fa0b8189d45db43c44a864083038
andrei_vinokur <gobest.vini@gmail.com>
2015-10-21 13:54:19 +0300

    home work 2.1

--- 2015/AndreiVinokur/2/lib/VersionFilter.rb
+++ 2015/AndreiVinokur/2/lib/VersionFilter.rb
@@ -8,39 +8,37 @@ class VersionFilter
     needed_version = Gem::Version.new(needed_version)
     result = []
     case operator
-      when '='
-        result = needed_version
-        puts 1
-      when '!='
-        result -= needed_version
-        puts 2
-      when '>'
-        result = @versions.select do |version|
-        needed_version if version > needed_version
-        puts 3 
+    when '='
+      result = needed_version
+    when '!='
+      result -= needed_version
+    when '>'
+      @versions.select do |version|
+        result << version if version > needed_version
       end
-      when '<'
-        result = @versions.select do |version|
-        needed_version if version < needed_version
-        puts 4
+    when '<'
+      @versions.select do |version|
+        result << version if version < needed_version
       end
-      when '>='
-        result = @versions.select do |version|
-        needed_version if version >= needed_version
+    when '>='
+      @versions.select do |version|
+        result << version if version >= needed_version
       end
-      when '<='
-        @versions.select do |version|
+    when '<='
+      @versions.select do |version|
         result << version if version <= needed_version
       end
-      when '~>'
-        result = @versions.select do |version|
-        needed_version if (version >= needed_version) && (version < needed_version.bump)
+    when '~>'
+      result = @versions.select do |version|
+        if (version >= needed_version) && (version < needed_version.bump)
+          result << version
+        end
       end
-      else
+    else
         puts 'Incorrect comparison operator'
         puts 'Template of comparison operator: =; !=; >; <; >=; <=; ~>;'
         exit
       end
-      result
-    end
+    result
   end
+end
