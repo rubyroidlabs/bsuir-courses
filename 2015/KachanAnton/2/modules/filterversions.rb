97ff735fa50886095f90e9c61e60d930a5172909
KachanAnton <antonn.lida@mail.ru>
2015-10-21 20:02:42 +0300

    Fixed style code finish

--- 2015/KachanAnton/2/modules/filterversions.rb
+++ 2015/KachanAnton/2/modules/filterversions.rb
@@ -1,21 +1,21 @@
 class FilterVersions
   def initialize(versions_array, conditions)
-		@versions_array = versions_array
-		@conditions = conditions
-	end
+    @versions_array = versions_array
+    @conditions = conditions
+  end
 
-	def get_filtred_versions
-		filter_versions = {}
-		begin
-		requier = Gem::Requirement.new(@conditions)
-		@versions_array.each do |vers|
-      filter_versions[vers] = requier.satisfied_by?(Gem::Version.new(vers))
-    end
+  def get_filtred_versions
+    filter_versions = {}
+    begin
+      requier = Gem::Requirement.new(@conditions)
+      @versions_array.each do |vers|
+        filter_versions[vers] = requier.satisfied_by?(Gem::Version.new(vers))
+      end
     rescue StandardError => exc
-    	puts 'Server could not sort'
-    	puts exc.message
-  		exit
-  	end
+      puts 'Server could not sort'
+      puts exc.message
+      exit
+    end
     filter_versions
-	end
+  end
 end
