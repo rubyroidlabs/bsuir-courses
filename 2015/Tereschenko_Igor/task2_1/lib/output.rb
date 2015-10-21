193d59ab3389064388f69399a21d2ed6378d0136
poka poka <pokawork@gmail.com>
2015-10-22 00:20:12 +0300

    Fixed tabs

--- 2015/Tereschenko_Igor/task2_1/lib/output.rb
+++ 2015/Tereschenko_Igor/task2_1/lib/output.rb
@@ -1,35 +1,35 @@
 require 'colorize'
 
 class Parser
-    attr_accessor :versions
-    def initialize(inversions, operators)
-        @versions = inversions
-        @operators = operators.sort_by { |_key, value| value }
-    end
+  attr_accessor :versions
+  def initialize(inversions, operators)
+    @versions = inversions
+    @operators = operators.sort_by { |_key, value| value }
+  end
 
-    def match?(value)
-        res1 = Array.new
-        @operators.each do |k, v|
-            if Gem::Dependency.new('', k + v).match?('', value)
-                res1 << true
-            else
-                res1 << false
-            end
-        end
-        if res1.include?(false)
-            return false
-        else
-            return true
-        end
+  def match?(value)
+    res1 = Array.new
+    @operators.each do |k, v|
+      if Gem::Dependency.new('', k + v).match?('', value)
+        res1 << true
+      else
+        res1 << false
+      end
     end
+      if res1.include?(false)
+        return false
+      else
+        return true
+      end
+  end
 
-    def output
-        @versions.each do |i|
-            if match?(i) == true
-                puts i.red
-            else
-                puts i
-            end
-        end
+  def output
+    @versions.each do |i|
+      if match?(i) == true
+        puts i.red
+      else
+        puts i
+      end
     end
+  end
 end
--- 2015/Tereschenko_Igor/task2_1/lib/version_finder.rb
+++ 2015/Tereschenko_Igor/task2_1/lib/version_finder.rb
@@ -3,24 +3,24 @@ require 'json'
 require 'colorize'
 
 class VersionFinder
-    def initialize(name)
-        @name = name
+  def initialize(name)
+    @name = name
+  end
+
+  def vget
+    begin
+      versions = Curl::Easy.http_get("https://rubygems.org/api/v1/versions/#{@name}.json").body_str
+    rescue Curl::Err::HostResolutionError
+      puts 'Error aquired! Please, check your network connection.'.red
+      exit
     end
 
-    def vget
-        begin
-            versions = Curl::Easy.http_get("https://rubygems.org/api/v1/versions/#{@name}.json").body_str
-            rescue Curl::Err::HostResolutionError
-                puts 'Error aquired! Please, check your network connection.'.red
-                exit
-            end
-
-        begin
-            json = JSON.parse(versions)
-            json.map! { |v| v["number"] }
-            rescue JSON::ParserError
-                puts '(╯°□°)╯︵ ┻━┻ AAAAAAAAAAAAAARGH!!! (invalid gem name)'.red
-                exit
-            end
+    begin
+      json = JSON.parse(versions)
+      json.map! { |v| v["number"] }
+      rescue JSON::ParserError
+        puts '(╯°□°)╯︵ ┻━┻ AAAAAAAAAAAAAARGH!!! (invalid gem name)'.red
+        exit
     end
+  end
 end
