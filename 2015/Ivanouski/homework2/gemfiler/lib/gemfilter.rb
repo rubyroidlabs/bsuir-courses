b3dd75ce87a3130f7270e153fc45bdede9946dd8
mafflin <mafflin@gmail.com>
2015-10-21 22:56:04 +0300

    Verison 0.1.4. Syntax fixes.

--- 2015/Ivanouski/homework2/gemfiler/lib/gemfilter.rb
+++ 2015/Ivanouski/homework2/gemfiler/lib/gemfilter.rb
@@ -1,40 +1,32 @@
 class GemFilter
-  def initialize
-    @clz = Colorizer.new()
-  end
-
   def filter(gems_output, op, ver)
     gems_output.each do |i|
-    temp = i['number']
-      if op == '<' &&
-      i['number'] < ver
-        @clz.push_colored i['number']
-      elsif op == '>' &&
-      i['number'] > ver
-        @clz.push_colored i['number']
-      elsif op == '>=' &&
-      i['number'] >= ver
-        @clz.push_colored i['number']
+      vers = i['number']
+      if op == '<' && i['number'] < ver
+        $ver_array.push (i['number']).red
+      elsif op == '>' && i['number'] > ver
+        $ver_array.push (i['number']).red
+      elsif op == '>=' && i['number'] >= ver
+        $ver_array.push (i['number']).red
       elsif op == '~>' &&
-      i['number'] >= ver && temp[2] == ver[2] && temp[0] == ver[0]
-        @clz.push_colored i['number']
+        i['number'] >= ver && vers[2] == ver[2] && vers[0] == ver[0]
+        $ver_array.push (i['number']).red
       else
-        @clz.push_original i['number']
+        $ver_array.push i['number']
       end
     end
   end
 
-  def filter_long (gems_output, op1, ver1, op2, ver2)
+  def filter_long(gems_output, op1, ver1, op2, ver2)
     gems_output.each do |i|
-      temp = i['number']
-      if op1 == '>=' && op2 == '<' &&
-      i['number'] >= ver1 && i['number'] < ver2
-        @clz.push_colored i['number']
+      vers = i['number']
+      if op1 == '>=' && op2 == '<' && i['number'] >= ver1 && i['number'] < ver2
+        $ver_array.push (i['number']).red
       elsif op1 == '>' && op2 == '<' &&
       i['number'] > ver1 && i['number'] < ver2
-        @clz.push_colored i['number']
+        $ver_array.push (i['number']).red
       else
-        @clz.push_original i['number']
+        $ver_array.push i['number']
       end
     end
   end
