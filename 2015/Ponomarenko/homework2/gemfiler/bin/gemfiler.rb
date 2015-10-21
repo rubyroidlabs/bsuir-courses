940119b1d4975c9f04afb9145811ce94ff9e8abb
Владислав Пономаренко <nightnomad@GECK.local>
2015-10-21 20:57:56 +0300

    homework2

--- 2015/Ponomarenko/homework2/gemfiler/bin/gemfiler.rb
+++ 2015/Ponomarenko/homework2/gemfiler/bin/gemfiler.rb
@@ -3,12 +3,8 @@
 require 'optparse'
 require 'colorize'
 require 'gems'
-
-lib = File.expand_path('../lib', File.dirname(__FILE__))
-$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-
-require 'main'
-require 'search&filter'
+require '../lib/main'
+require '../lib/search&filter'
 
 a = Gemfiler.new
 a.execute
