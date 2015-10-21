4c157898a5a86579deb318000d0ebf4e01a30185
Zhezha Artem <531621@mail.ru>
2015-10-21 23:33:16 +0300

    second task

---
+++ 2015/ZhezhaArtem/2/lib/Printer.rb
@@ -0,0 +1,11 @@
+class Printer
+  def print(filter_versions)
+    filter_versions.each do |filtr|
+      if (filtr[1])
+        puts filtr[0].colorize(:red)
+      else
+        puts filtr[0]
+      end
+    end
+  end
+end
