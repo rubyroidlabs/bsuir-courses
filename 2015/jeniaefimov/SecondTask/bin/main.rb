5bf07ec745747c821397faab3f3c57308d1655c5
jeniaefimov <ser040172z@mail.ru>
2015-10-21 23:26:48 +0300

    Update main.rb

--- 2015/jeniaefimov/SecondTask/bin/main.rb
+++ 2015/jeniaefimov/SecondTask/bin/main.rb
@@ -17,7 +17,7 @@ firstgem = FindGem.new(ARGV[0], ARGV[1])
 versions = FindVersion.new(firstgem.name).find
 if versions
   filter_versions = FiltreVersion.new(versions, firstgem.key.dup).filter
-if filter_versions
-  PrintVersion.new(versions, filter_versions).write
-end
+  if filter_versions
+    PrintVersion.new(versions, filter_versions).write
+  end
 end
