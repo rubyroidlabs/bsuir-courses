155e4c1f293185fc4737ab016db48f50da9ded2a
DragonsKiller <dragon.airos@yandex.by>
2015-10-21 21:01:01 +0300

    Update Beautiful_output.rb

--- 2015/Pikoluk/2/gemfiler/lib/Beautiful_output.rb
+++ 2015/Pikoluk/2/gemfiler/lib/Beautiful_output.rb
@@ -7,6 +7,6 @@ class Beautiful_output
   end
 
   def write
-    @versions.each {|v| puts (@filter_versions.include?(v) ? v.red : v)}
+    @versions.each { |v| puts (@filter_versions.include?(v) ? v.red : v)}
   end
 end
