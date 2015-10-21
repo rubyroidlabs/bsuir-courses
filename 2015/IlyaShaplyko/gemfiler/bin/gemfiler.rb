bbc3c01d918a45719ed70ce2f28eeceeea84fc35
Shaglock <goldilb10xa@mail.ru>
2015-10-21 21:26:29 +0300

    Update gemfiler.rb

--- 2015/IlyaShaplyko/gemfiler/bin/gemfiler.rb
+++ 2015/IlyaShaplyko/gemfiler/bin/gemfiler.rb
@@ -14,8 +14,8 @@ begin
   entered_gem = Gemfiler.new(*ARGV)
   all_versions = VersionParser.new(entered_gem.gem_name).fetch
   if all_versions
-    filtered_versions = VersionFilter.new(all_versions,entered_gem.gem_version.dup).filter
-    ColoredOutput.new(all_versions,filtered_versions).output if filtered_versions
+    filtered_versions = VersionFilter.new(all_versions, entered_gem.gem_version.dup).filter
+    ColoredOutput.new(all_versions, filtered_versions).output if filtered_versions
   end
 rescue ArgumentError
   puts 'Argument Error. Input gem name and version'.green
