d77420ca66bf7faa8b4ea1bcd7059b02a68c55ac
Ubuntu <ser040172z@mail.ru>
2015-10-21 22:13:21 +0300

    task 2

---
+++ 2015/jeniaefimov/SecondTask/sourcelib/FiltreVersion.rb
@@ -0,0 +1,10 @@
+class FiltreVersion
+  def initialize(versions, gem_version)
+    @versions = versions
+    @gem_version = gem_version
+  end
+
+def filter
+  @versions.map { |v| v if Gem::Dependency.new('', @gem_version).match?('', v) }
+end
+end
