801df3d3470136688963b237996be4561baacf71
Anastasiya <mainastia91@gmail.com>
2015-10-21 20:01:08 +0300

    first version

---
+++ 2015/Sadovaya/2/gemfiler/lib/version_fetcher.rb
@@ -0,0 +1,9 @@
+require 'json'
+
+class VersionFetcher
+  def fetch(name)
+    result = `curl https://rubygems.org/api/v1/versions/#{name}.json`
+    json = JSON.parse(result)
+    json.map! { |s| s["number"]}
+  end
+end
