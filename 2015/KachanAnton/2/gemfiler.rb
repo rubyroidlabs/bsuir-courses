ff68518fb23cad9cde3a4867525cf5c2d7c45dbd
KachanAnton <antonn.lida@mail.ru>
2015-10-21 21:02:46 +0300

    Fixed errors. Task 2

--- 2015/KachanAnton/2/gemfiler.rb
+++ 2015/KachanAnton/2/gemfiler.rb
@@ -3,23 +3,18 @@ Dir[File.expand_path('./../modules/*.rb', __FILE__)].each do
 end
 
 class GemFiler
-  def show_versions
+  def self.show_versions
     if ARGV.length < 2 || ARGV.length > 3
       puts 'Incorrect number of arguments.'
       exit
     end
-    begin
-      gem = ARGV[0]
-      conditions = ARGV[1..ARGV.length - 1]
-      versions = GemVersions.new(gem).get_version
-      filter = FilterVersions.new(versions, conditions)
-      filtred_versions = filter.get_filtred_versions
-      OutputVersions.output_versions(filtred_versions)
-    rescue StandardError => exc
-      puts exc.message
-      exit
-    end
+    gem = ARGV[0]
+    conditions = ARGV[1..ARGV.length - 1]
+    versions = GemVersions.new(gem).get_version
+    filter = FilterVersions.new(versions, conditions)
+    filtred_versions = filter.get_filtred_versions
+    OutputVersions.output_versions(filtred_versions)
   end
 end
 
-GemFiler.new.show_versions
+GemFiler.show_versions
