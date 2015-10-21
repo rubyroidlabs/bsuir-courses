612bd003694841423c27392f6c3ffed20793aa2e
IgorSheiko <igor2109a@mail.ru>
2015-10-21 23:03:31 +0300

    home task 2.1

--- 2015/IgorSheiko/2/lib/get_all_versions.rb
+++ 2015/IgorSheiko/2/lib/get_all_versions.rb
@@ -1,18 +1,18 @@
 require 'mechanize'
 
 class GetAllVersions
-	def initialize(gem_name)
-		@gem_name = gem_name
-	end
+  def initialize(gem_name)
+    @gem_name = gem_name
+  end
 
-	def get_all_versions
-		url = "https://rubygems.org/gems/#{@gem_name}/versions"
-		agent = Mechanize.new
-		begin
-			page = agent.get(url)
-			versions = page.parser.css('.gem__version-wrap a').map(&:text)
-			rescue Mechanize::Error
-				puts 'This rubygem could not be found.'
-		end
-	end
+  def get_all_versions
+    url = "https://rubygems.org/gems/#{@gem_name}/versions"
+    agent = Mechanize.new
+    begin
+      page = agent.get(url)
+      versions = page.parser.css('.gem__version-wrap a').map(&:text)
+      rescue Mechanize::Error
+      puts 'This rubygem could not be found.'
+    end
+  end
 end
--- 2015/IgorSheiko/2/task.rb
+++ 2015/IgorSheiko/2/task.rb
@@ -1,7 +1,7 @@
-Dir['./lib/*.rb'].each{ |f| require_relative(f) }
+Dir['./lib/*.rb'].each { |f| require_relative(f) }
 
 if ARGV.count < 2
-	raise ArgumentError.new('Invalid count argument')
+  raise ArgumentError.new('Invalid count argument')
 end
 gem_version = []
 ARGV.each do |argument|
@@ -14,8 +14,8 @@ versions = versions.map { |version| Gem::Version.new(version) }
 true_versions = versions
 search_true_gems = ParseVersion.new
 gem_version.each do |parametr|
-	operator, version = parametr.split
-	version = Gem::Version.new(version)
-	true_versions = search_true_gems.filter(operator, version, true_versions)
+  operator, version = parametr.split
+  version = Gem::Version.new(version)
+  true_versions = search_true_gems.filter(operator, version, true_versions)
 end
 ColoredOutput.new(versions, true_versions).colored_output
