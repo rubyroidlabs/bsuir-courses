aac24d28f1500aa73879ab068730221a3f479d3d
KantinDmitry <kantindm@gmail.com>
2015-10-22 01:43:05 +0300

    hometask 2 hound corrections

--- 2015/KantinDmitry/2/gemfiler.rb
+++ 2015/KantinDmitry/2/gemfiler.rb
@@ -3,8 +3,8 @@ require_relative 'gem_versions_getter.rb'
 require_relative 'versions_printer.rb'
 
 class Gemfiler < Thor
-  desc "get name 'condition1' 'condition2'", "output versions"
-  def get( gemname, condition1 = nil, condition2 = nil)
+  desc "get name 'condition1' 'condition2'", 'output versions'
+  def get(gemname, condition1 = nil, condition2 = nil)
     gem_versions_getter = GemVersionsGetter.new
     versions_array = gem_versions_getter.get_versions(gemname)
     VersionsPrinter.
