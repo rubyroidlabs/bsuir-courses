1dab6368ea4029c865dec79c3083449f0aa7e6c4
mafflin <mafflin@gmail.com>
2015-10-21 20:38:37 +0300

    Verison 0.1.1. Syntax fixes.

--- 2015/Ivanouski/homework2/gemfiler/lib/colorizer.rb
+++ 2015/Ivanouski/homework2/gemfiler/lib/colorizer.rb
@@ -1,13 +1,13 @@
 class Colorizer < Array
-	def initialize
-		$ver_array = []
-	end
+  def initialize
+    $ver_array = []
+  end
 
-	def push_colored(version)
-		$ver_array.push version.red
-	end
+  def push_colored(version)
+    $ver_array.push version.red
+  end
 
-	def push_original(version)
-		$ver_array.push version
-	end
+  def push_original(version)
+    $ver_array.push version
+  end
 end
