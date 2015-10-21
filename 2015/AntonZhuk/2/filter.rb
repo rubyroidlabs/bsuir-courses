2579368c9734ebe8f7a584ed1258dbf53efd608e
Your Name <antoxa0073@gmail.com>
2015-10-21 05:27:13 +0300

    home task 2 v1

---
+++ 2015/AntonZhuk/2/filter.rb
@@ -0,0 +1,47 @@
+require_relative('parse.rb')
+
+class Filter
+  def initialize(data)
+    @versions = data.map do |ver|
+     @a = Gem::Version.new(ver)
+    end
+  end
+
+  def filter_data(param, version,param2,version2)
+    specified =  Gem::Version.new(version)
+    specified2 = Gem::Version.new(version2)
+
+    if !@versions.include?(specified) 
+      puts 'Incorrect version!'
+      exit
+    end
+
+      filtered_versions = []
+    case
+      when param == '>=' && param2 == nil
+         filtered_versions = @versions.select do |ver|
+           ver >= specified
+         end
+      when param == '<' && param2 == nil
+        filtered_versions = @versions.select do |ver|
+          ver < specified
+        end
+      when param == '~>' && param2 == nil
+        filtered_versions = @versions.select do |ver|
+          ver >= specified && ver < specified.bump
+        end
+      when param == '>=' && param2 == '<'
+        filtered_versions = @versions.select do |ver|
+          ver >= specified && ver < specified2
+        end
+      else
+        puts 'Incorrect parameters'
+        exit
+    end
+    filtered_versions.map do |ver|
+      ver.to_s
+    end
+  end
+end
+
+
---
+++ 2015/AntonZhuk/2/print.rb
@@ -0,0 +1,19 @@
+require 'colored'
+
+class Print
+  def initialize(filtered_versions,versions)
+    @filtered_versions = filtered_versions
+    @versions = versions
+    print_result
+  end
+
+  def print_result
+    @versions.each do |ver|
+       if (@filtered_versions.include?(ver))
+        puts ver.red
+      else
+        puts ver
+      end
+    end
+  end
+end
