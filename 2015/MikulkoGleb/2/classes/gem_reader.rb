9b3af74df3cac4f6fa66248531cc4ebd325bd1fb
Gleb Mikulko <glebmikulko@gmail.com>
2015-10-22 18:13:37 +0300

    2nd task: fix Roma's comments

--- 2015/MikulkoGleb/2/classes/gem_reader.rb
+++ 2015/MikulkoGleb/2/classes/gem_reader.rb
@@ -4,14 +4,13 @@ require 'yaml'
 class GemReader
   def initialize(gem_name)
     conf = YAML::load_file(File.expand_path('../../conf/gem.conf', __FILE__))
-    @source = conf['source'] + gem_name + '.' + conf['format']
+    @source = "#{conf['source']}#{gem_name}.#{conf['format']}"
   end
 
   def get_gems
     response = open(@source).read
     versions = response.scan(/"number":"[^,]+"/)
-    versions.map! { |string| string.gsub(/"number":/, '') }
-    versions.map { |str| str.delete("\"") }
+    versions.map { |string| string.gsub(/"number":/, '').delete("\"") }
   rescue => e
     abort(e.inspect)
   end
--- 2015/MikulkoGleb/2/classes/printer.rb
+++ 2015/MikulkoGleb/2/classes/printer.rb
@@ -4,8 +4,9 @@ class Printer
   def pretty_print(arr, new_arr)
     arr.each do |version_str|
       if new_arr.include?(version_str)
-        (puts version_str.colorize(:red))
-      else (puts version_str)
+        puts version_str.colorize(:red)
+      else
+        puts version_str
       end
     end
   rescue => e
