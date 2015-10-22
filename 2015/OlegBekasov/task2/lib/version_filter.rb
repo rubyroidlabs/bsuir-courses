918df2ee1396dfde018968fc292ff05b033b7e8e
bekasovoleg <o.bekasov96@gmail.com>
2015-10-22 17:38:38 +0300

    code style fix

--- 2015/OlegBekasov/task2/lib/version_filter.rb
+++ 2015/OlegBekasov/task2/lib/version_filter.rb
@@ -2,7 +2,6 @@ module VersionFilter
   def filter(version, option1, option2)
     Gem::Requirement.new(option1, option2).
       satisfied_by?(Gem::Version.new(version))
-    rescue Gem::Requirement::BadRequirementError
-    end
+  rescue Gem::Requirement::BadRequirementError
   end
 end
