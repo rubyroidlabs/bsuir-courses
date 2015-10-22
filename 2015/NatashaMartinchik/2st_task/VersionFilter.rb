f7a513f70a50725e91ce9865e7d33d04ee7f19c3
NatashaMartinchik <mno96@yandex.by>
2015-10-22 10:55:42 +0300

    task_2_2.1

---
+++ 2015/NatashaMartinchik/2st_task/VersionFilter.rb
@@ -0,0 +1,15 @@
+class VersionFilter
+  def initialize(result)
+    @version = result.map { |version| Gem::Version.new(version) }
+  end
+
+  def filter(specifier, operator)
+    
+    version_hash = {}
+    requirement = Gem::Requirement.new(operator)
+    specifier.each do |v|
+      version_hash[v] = requirement.satisfield_by Gem::Version.create(v)
+    end
+    version_hash
+  end 
+end
