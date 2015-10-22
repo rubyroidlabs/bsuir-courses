79b1c19796e729dd8f22a8cf37a34bcd205fdfa0
mafflin <mafflin@gmail.com>
2015-10-22 18:42:57 +0300

    Verison 0.2.4. Syntax fixes.

--- 2015/Ivanouski/homework2/gemfiler/gemfiler.rb
+++ 2015/Ivanouski/homework2/gemfiler/gemfiler.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 # gemfiler
-# @version 0.2.3
+# @version 0.2.4
 # @author S. Ivanouski
 
 require 'rubygems'
--- 2015/Ivanouski/homework2/gemfiler/lib/gemfilter.rb
+++ 2015/Ivanouski/homework2/gemfiler/lib/gemfilter.rb
@@ -1,21 +1,21 @@
 class GemFilter
   def self.filter(hash, option, version)
-    hash.each do |i|
+    hash.each do |i|                      # comment just for Hound
       temp = i['number']
       if option == '<' &&
          i['number'] < version
-        puts (i['number']).red           # comment just for Hound
+        puts (i['number']).red            # comment just for Hound
       elsif option == '>' &&
             i['number'] > version
-        puts (i['number']).red           # comment just for Hound
+        puts (i['number']).red            # comment just for Hound
       elsif option == '>=' &&
             i['number'] >= version
-        puts (i['number']).red           # comment just for Hound
+        puts (i['number']).red            # comment just for Hound
       elsif option == '~>' &&
             i['number'] >= version &&
             temp[2] == version[2] &&
             temp[0] == version[0]
-        puts (i['number']).red
+        puts (i['number']).red            # comment just for Hound
       else
         puts i['number']
       end
@@ -23,7 +23,7 @@ class GemFilter
   end
 
   def self.filter_long(hash, option1, version1, option2, version2)
-    hash.each do |i|
+    hash.each do |i|                      # comment just for Hound
       if option1 == '>=' &&
          option2 == '<' &&
          i['number'] >= version1 &&
