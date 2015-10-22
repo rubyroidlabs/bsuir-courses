d413cc1d4c7992892d80aedb96aa0629305ca283
blr_B <bobr8949@yandex.ru>
2015-10-22 17:40:29 +0300

    v0.2.0.1

--- 2015/Bobkov/2/lib/parser.rb
+++ 2015/Bobkov/2/lib/parser.rb
@@ -1,35 +1,35 @@
 class Parser
   def initialize(arguments)
-  	@bl=false
-  	if arguments.size < 2 && arguments.size > 3
+    @bl = false
+    if arguments.size < 2 && arguments.size > 3
       raise ArgumentError
     end
     @name, @specifier, @specifier2 = arguments
-    unless (@name =~ /\w+/) && (@specifier =~ /(~>|>=|<=|<|>)\s(\d+\.)*\d+/)
+    unless @name =~ /\w+/ && @specifier =~ /(~>|>=|<=|<|>)\s(\d+\.)*\d+/
       raise ArgumentError
     end
-    if (@specifier2 =~ /(<|<=)\s(\d+\.)*\d+/)
+    if @specifier2 =~ /(<|<=)\s(\d+\.)*\d+/
       @bl = true
     else
-      if (@specifier2 =~ /^.*/)
+      if @specifier2 =~ /^.*/
         raise ArgumentError
       end
     end
   end
 
-  def getName()
+  def getName
     @name
   end
 
-  def getSp1()
+  def getSp1
     @specifier
   end
 
-  def getSp2()
+  def getSp2
     @specifier2
   end
 
-  def getBl()
+  def getBl
     @bl
   end
 end
