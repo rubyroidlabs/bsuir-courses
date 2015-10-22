550413a33aab22059e0d5868ebcf46b7c13ccf15
jeniaefimov <ser040172z@mail.ru>
2015-10-22 10:10:54 +0300

    Update FiltreVersion.rb

--- 2015/jeniaefimov/SecondTask/sourcelib/FiltreVersion.rb
+++ 2015/jeniaefimov/SecondTask/sourcelib/FiltreVersion.rb
@@ -1,10 +1,10 @@
 class FiltreVersion
-def initialize(versions, gversion)
-  @versions = versions
-  @gversion = gversion
-end
+  def initialize(versions, gversion)
+    @versions = versions
+    @gversion = gversion
+  end
 
-def filter
-  @versions.map { |v| v if Gem::Dependency.new('', @gversion).match?('', v) }
-end
+  def filter
+    @versions.map { |v| v if Gem::Dependency.new('', @gversion).match?('', v) }
+  end
 end
