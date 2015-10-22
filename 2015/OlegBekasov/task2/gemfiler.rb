142b8d9a6938c4c0118baeefea179cdbe590cfda
bekasovoleg <o.bekasov96@gmail.com>
2015-10-22 16:39:55 +0300

    implement task 2

--- 2015/OlegBekasov/task2/gemfiler.rb
+++ 2015/OlegBekasov/task2/gemfiler.rb
@@ -1,5 +1,15 @@
 #!/usr/bin/env ruby
+require 'optparse'
 Dir[File.expand_path(('./../lib/*.rb'), __FILE__)].each { |file| require file }
-raise ArgumentError if ARGV.size < 1 || ARGV.size > 3
+OptionParser.new do |opts|
+  opts.banner = <<-STR
+    Usage: ./gemfilter.rb [name] [version]
+    For example:
+    ./gemfiler devise '~> 2.1.3'
+    ./gemfiler rails '>= 3.1'
+    ./gemfiler rails '>= 3.1' '< 4.0'
+  STR
+end.parse!
+raise ArgumentError.new("Use ./gemfilter.rb -h for help") if ARGV.size < 1 || ARGV.size > 3
 versions = VersionGet.new(ARGV[0]).collect
 VersionPuts.new(versions, ARGV[1], ARGV[2]).print_filtered
--- 2015/OlegBekasov/task2/lib/version_get.rb
+++ 2015/OlegBekasov/task2/lib/version_get.rb
@@ -1,12 +1,13 @@
 require 'json'
 require 'colored'
+require 'open-uri'
 class VersionGet
   def initialize(name)
     @name = name
   end
 
   def collect
-    versions = `curl https://rubygems.org/api/v1/versions/#{@name}.json`
+    versions = open("https://rubygems.org/api/v1/versions/#{@name}.json").read
     begin
       JSON.parse(versions).map { |ver| ver['number'] }
     rescue JSON::ParserError
