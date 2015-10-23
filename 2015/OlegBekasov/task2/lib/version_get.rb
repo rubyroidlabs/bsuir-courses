440990d82b7145fbc0749c4645e1ff58dfb910c2
bekasovoleg <o.bekasov96@gmail.com>
2015-10-23 16:10:19 +0300

    code style fix

--- 2015/OlegBekasov/task2/lib/version_get.rb
+++ 2015/OlegBekasov/task2/lib/version_get.rb
@@ -7,13 +7,11 @@ class VersionGet
   end
 
   def collect
-    begin
       versions = open("https://rubygems.org/api/v1/versions/#{@name}.json").read
       JSON.parse(versions).map { |ver| ver['number'] }
     rescue OpenURI::HTTPError
       puts 'Check gem name'.red
     rescue SocketError
       puts 'Check your internet connection'.red
-    end
   end
 end
