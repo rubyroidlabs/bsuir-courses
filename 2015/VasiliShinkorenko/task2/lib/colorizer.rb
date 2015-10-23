444aacf9cd16aaf39f154bf95b400f263cc4cb35
flreln <shinkorenko.vasili@gmail.com>
2015-10-23 18:00:01 +0300

    fixing hound violations v7

--- 2015/VasiliShinkorenko/task2/lib/colorizer.rb
+++ 2015/VasiliShinkorenko/task2/lib/colorizer.rb
@@ -2,23 +2,21 @@ require 'colorize'
 
 class Colorizer
   def initialize(versions, filtered_versions)
-    @versions = versions
-    @filtered_versions = filtered_versions
+    @versions = versions # comment for hound here
+    @filtered_versions = filtered_versions # another comment for hound here
   end
 
   def colorize
-    begin
-      if @filtered_versions.first > (@versions - @filtered_versions).last
-        @filtered_versions.map { |v| puts v.red }
-        puts @versions - @filtered_versions
-      elsif @filtered_versions.first < (@versions - @filtered_versions).last
-        puts @versions - @filtered_versions
-        @filtered_versions.map { |v| puts v.red }
-      else
-        puts @versions
-      end
+    if @filtered_versions.first > (@versions - @filtered_versions).last
+      @filtered_versions.map { |v| puts v.red }
+      puts @versions - @filtered_versions
+    elsif @filtered_versions.first < (@versions - @filtered_versions).last
+      puts @versions - @filtered_versions
+      @filtered_versions.map { |v| puts v.red }
+    else
+      puts @versions
+    end
     rescue NoMethodError
       print "It seems there is no such version of this gem.\n"
-    end
   end
 end
