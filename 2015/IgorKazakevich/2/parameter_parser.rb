29902c749e794ca9ad1027ada751383a9e61c935
igor-kazakevich <igor-kazakevich79@gmail.com>
2015-10-25 16:57:45 +0300

    Fixed hometask 2. Attempt 3

--- 2015/IgorKazakevich/2/parameter_parser.rb
+++ 2015/IgorKazakevich/2/parameter_parser.rb
@@ -17,13 +17,13 @@ class ParameterParser
     @address = "https://rubygems.org/gems/#{@search_gem}/versions"
     parse_html_data
 
-  rescue Exception
+  rescue StandardError
     puts 'Error parsing data! Check your input and connection to the internet.'
     exit
   end
 
   def parse_html_data
-    @filter_data = Array.new()
+    @filter_data = Array.new
 
     Nokogiri::HTML(open(@address)).xpath('//div//ul//li').each do |version|
       @filter_data.push(Gem::Version.new(version.text.split(' ').first))
