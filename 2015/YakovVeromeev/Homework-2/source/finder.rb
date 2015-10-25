e04a76ba40100deedd16eb4180c36a50356d3a99
JackVeromeev <veromeev1989@gmail.com>
2015-10-26 01:36:24 +0300

    fix some mistakes, houndchi found
    I'm very sorry, I've not found a way, how to edit several files in one commit

--- 2015/YakovVeromeev/Homework-2/source/finder.rb
+++ 2015/YakovVeromeev/Homework-2/source/finder.rb
@@ -1,15 +1,10 @@
-#finder
-
 require 'json'
 
 class Finder
-	
-	def self.search(name)
-		versions = []
-		json = `curl https://rubygems.org/api/v1/versions/#{name}.json`
-		JSON::parse(json).each { |current| versions << current.fetch('number') }
-		versions.reverse
-	end
-	
+  def self.search(name)
+    versions = []
+    json = `curl https://rubygems.org/api/v1/versions/#{name}.json`
+    JSON::parse(json).each { |current| versions << current.fetch('number') }
+    versions.reverse
+    end
 end
-
