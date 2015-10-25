3dfbe0b6e8a692d6799cf0b6d6f3fee011d5918b
igor-kazakevich <igor-kazakevich79@gmail.com>
2015-10-25 17:00:14 +0300

    Fixed hometask 2. Attempt 4

--- 2015/IgorKazakevich/2/version.rb
+++ 2015/IgorKazakevich/2/version.rb
@@ -1,14 +1,14 @@
 class Version
   def initialize(versions, requirements)
-   @versions = versions
-   @requirements = requirements
+    @versions = versions
+    @reqs = requirements
   end
 
   def find
     @find_versions = []
 
     @versions.each do |v|
-      if (@requirements[0].satisfied_by? v) && (@requirements[1].satisfied_by? v)
+      if (@reqs[0].satisfied_by? v) && (@reqs[1].satisfied_by? v)
         @find_versions.push(@versions.index(v))
       end
     end
