9886a4cf1012b298e5a74bc60fb5c986b100d620
Alexander <sounis@mail.ru>
2015-10-25 22:19:07 +0300

    version 1.0.1

--- 2015/KurteevAlexander/home_task_2/lib/Parser.rb
+++ 2015/KurteevAlexander/home_task_2/lib/Parser.rb
@@ -1,17 +1,19 @@
 require 'gems'
 require 'optparse'
+
 class Parser
   attr_accessor :processed_list
   def initialize
     @color = []
     if Gems.versions(ARGV[0]).class == String
-      raise "This rubygem could not be found." 
+      raise 'This rubygem could not be found.'
     end
     @gem_list = Gems.versions(ARGV[0]).map { |cur_gem| cur_gem['number'] }
     @gem_list = @gem_list.reverse.uniq
     @gem_list_size = @gem_list.size - 1
     @processed_list = []
   end
+  
   def color_check(color, index)
     if @color[index] == nil
       @color[index] = color
@@ -20,6 +22,7 @@ class Parser
     end
     @processed_list[index] = { 'version' => @gem_list[index], 'color' => @color[index] }
   end
+
   def searcher
     opt_parser = OptionParser.new do |opts|
       opts.on(/[<,>]/) do |current|
--- 2015/KurteevAlexander/home_task_2/lib/Print_Result.rb
+++ 2015/KurteevAlexander/home_task_2/lib/Print_Result.rb
@@ -1,14 +1,16 @@
 require 'colorize'
+
 class Print_Result
   def initialize(processed_list)
     @processed_list = processed_list
   end
+  
   def printer
     if @processed_list.size == 0
       raise 'Incorrect interval.'
     end
     @processed_list = @processed_list.compact
-    0.upto(@processed_list.size-1) do |i|
+    0.upto(@processed_list.size - 1) do |i|
       if @processed_list[i]['color'].rindex('excluded')
         puts @processed_list[i]['version'].green
       else
