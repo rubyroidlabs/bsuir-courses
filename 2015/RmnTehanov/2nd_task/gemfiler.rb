aa23322ffe996744d9bed6511af2dbf7d8ea5000
longbeardmen <longbeardmen@gmail.com>
2015-10-27 01:14:14 +0300

    War with bot

--- 2015/RmnTehanov/2nd_task/gemfiler.rb
+++ 2015/RmnTehanov/2nd_task/gemfiler.rb
@@ -4,12 +4,12 @@ require 'nokogiri'
 require 'colored'
 
 class Parser
-  def initialize (args)
+  def initialize(args)
     @args = args
   end
 
   def parse_param
-    opts = Slop.parse(@args) do |o|
+    Slop.parse(@args) do |o|
       o.banner = "Usage: ruby demo.rb GEM_NAME [gem_version].\nExample:
       ./gemfiler devise '~> 2.1.3'
       ./gemfiler rails '>= 3.1'
@@ -27,13 +27,14 @@ class Parser
 end
 
 class CheckVersions
-  def initialize (param)
+  def initialize(param)
     @gem_name = param[:name]
     @conditions = Gem::Requirement.new(param[:conditions])
   end
-  
+
   def get_versions
-    page = Nokogiri::HTML(open("https://rubygems.org/gems/#{@gem_name}/versions"))
+    page_link = "https://rubygems.org/gems/#{@gem_name}/versions"
+    page = Nokogiri::HTML(open(page_link))
     versions = []
     page.css('a.t-list__item').each { |i| versions << Gem::Version.new(i.text) }
     versions
@@ -54,7 +55,7 @@ class CheckVersions
 end
 
 class Output
-  def initialize (checked_versions)
+  def initialize(checked_versions)
     @checked_versions = checked_versions
   end
 
@@ -63,7 +64,7 @@ class Output
       if current_version.class == String
         puts current_version.red
       else
-        puts current_version 
+        puts current_version
       end
     end
   end
