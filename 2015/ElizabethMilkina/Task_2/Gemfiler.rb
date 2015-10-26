fbee252f00f72371a7517d1f0e314231cf1cbd73
MsMilkina <ms.milkina@gmail.com>
2015-10-26 21:18:17 +0300

    Added ability for 2 input conditions

--- 2015/ElizabethMilkina/Task_2/Gemfiler.rb
+++ 2015/ElizabethMilkina/Task_2/Gemfiler.rb
@@ -4,14 +4,11 @@ require_relative 'src/version_gem.rb'
 require_relative 'src/output_gem.rb'
 require 'slop'
 
-opts = Slop.parse do |option|
-  option.string '-n', '--name', 'gem name'
-  option.string '-c', '--command', 'how to check version and version'
-end
+opts = Slop.parse
 
 object_name = NameGem.new
 object_version = VersionGem.new
 filtered_versions, not_matched_versions = object_version.\
-get_version(object_name.get_name(opts[:name]), opts[:command])
+get_version(object_name.get_name(opts.arguments[0]), opts.arguments[1..opts.arguments.length])
 object_filter = OutputGem.new
 object_filter.output(filtered_versions, not_matched_versions)
--- 2015/ElizabethMilkina/Task_2/src/version_gem.rb
+++ 2015/ElizabethMilkina/Task_2/src/version_gem.rb
@@ -5,8 +5,16 @@ class VersionGem
     filtered_versions = []
     not_matched_versions = []
     gem_versions.each do |gem|
-      if Gem::Dependency.new('', command).match?('', gem.inner_html)
-        filtered_versions.push(gem.inner_html)
+      if Gem::Dependency.new('', command[0]).match?('', gem.inner_html)
+        if command.length == 2
+          if Gem::Dependency.new('', command[1]).match?('', gem.inner_html)
+            filtered_versions.push(gem.inner_html)
+          else
+            not_matched_versions.push(gem.inner_html)
+          end
+        else
+          filtered_versions.push(gem.inner_html)
+        end
       else
         not_matched_versions.push(gem.inner_html)
       end
