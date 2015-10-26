59efb6dc2fd3e3a6c1a7b34f70381bf353303a31
JackVeromeev <veromeev1989@gmail.com>
2015-10-26 19:03:38 +0300

    add some description to code

--- 2015/YakovVeromeev/Homework-2/source/filter.rb
+++ 2015/YakovVeromeev/Homework-2/source/filter.rb
@@ -1,20 +1,21 @@
 class Filter
   def self.filter(versions, parametr)
+    # conversion strings with vresion to version number to ba able to compare them
     versions = versions.map { |version| Gem::Version.new(version) }
     operator, needed = parametr.split
     needed = Gem::Version.new(needed)
     filtered = []
     if operator == '~>'
       filtered = versions.select do |version|
-      version >= needed && version < needed.bump
+      version >= needed && version < needed.bump # ~> 1.2 means > 1.2 and <2.0
       end
     end
     else
       filtered = versions.select do |version|
-        version.send(operator.to_sym, needed)
+        version.send(operator.to_sym, needed) # tha same as version >= needed if operator == '>='
       end
     end
-    filtered.map(&:to_s)
+    filtered.map(&:to_s) #at the end convert filtered versions to strings
   end
 end
 
