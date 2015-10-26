19ac959ccf3ebcb55b1f478b6029173b55d5ba43
jeniaefimov <ser040172z@mail.ru>
2015-10-26 23:38:59 +0300

    Update main.rb

--- 2015/jeniaefimov/SecondTask/bin/main.rb
+++ 2015/jeniaefimov/SecondTask/bin/main.rb
@@ -2,7 +2,7 @@
 
 require 'colorize'
 require 'thor'
-Dir['../sourcelib/*.rb'].each { |f| require_relative(f) } # include all libraries
+Dir['../sourcelib/*.rb'].each { |f| require_relative(f) }
 
 class FindGem
   attr_accessor :name, :key
