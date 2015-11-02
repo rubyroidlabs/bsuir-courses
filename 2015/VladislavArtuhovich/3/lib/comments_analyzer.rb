c2fa9662533b4af512d514d34e664741d88b892e
ArtuhovichVlad <artvlad96@gmail.com>
2015-11-02 23:59:21 +0300

    implement home task 3

--- 2015/VladislavArtuhovich/3/lib/comments_analyzer.rb
+++ 2015/VladislavArtuhovich/3/lib/comments_analyzer.rb
@@ -1,33 +1,33 @@
 class CommentsAnalyzer
-	def load_file
+  def load_file
     keywords = YAML.load_file('keywords.yml')
     keywords
   end
 
   def positive_comment?(comment)
-  	keywords = load_file
+    keywords = load_file
     positive = 0
     negative = 0
 
     comment_for_analyzing = Unicode::downcase(comment)
-		keywords['negative'].each do |keyword|
-			if comment_for_analyzing.include? keyword
-				negative += 1
-			end
+    keywords['negative'].each do |keyword|
+      if comment_for_analyzing.include? keyword
+        negative += 1
+      end
     end
 
     keywords['positive'].each do |keyword|
-    	if comment_for_analyzing.include? keyword
-				positive += 1
-			end
-   	end
+      if comment_for_analyzing.include? keyword
+        positive += 1
+      end
+    end
 
     if positive > negative
-    	1
+      1
     elsif positive < negative
-    	-1
+      -1
     else
-    	0
+      0
     end
   end
 end 
