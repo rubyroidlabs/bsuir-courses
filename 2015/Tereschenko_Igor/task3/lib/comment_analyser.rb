895776ac4ac3ea757cb1cc0762fefcac9dea8d85
poka poka <pokawork@gmail.com>
2015-11-02 21:55:05 +0300

    commit

--- 2015/Tereschenko_Igor/task3/lib/comment_analyser.rb
+++ 2015/Tereschenko_Igor/task3/lib/comment_analyser.rb
@@ -2,6 +2,13 @@ class CommentAnalisys
   attr_reader :frequent_words
   def initialize
     @frequent_words = Hash.new(0)
+    agent = Mechanize.new
+    tpage = agent.get('http://webtranslation.paralink.com/English-Spanish-Translation/')
+  end
+
+  def comment_translate(comment)
+    tpage.forms[0] = 'comment'
+
   end
 
   def find_frequent_words(comments)
@@ -11,4 +18,8 @@ class CommentAnalisys
     words.each { |word| @frequent_words[word.downcase] += 1 }
     @frequent_words.delete_if { |k, _v| k.length < 3 }.sort_by { |_k, v| v }
   end
+end
+
+begin
+
 end
