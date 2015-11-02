c83270871085bae6b12e21e5b12bc78adeefff8b
poka poka <pokawork@gmail.com>
2015-11-02 21:04:34 +0300

    fixes

--- 2015/Tereschenko_Igor/task3/lib/comment_fetcher.rb
+++ 2015/Tereschenko_Igor/task3/lib/comment_fetcher.rb
@@ -1,23 +1,27 @@
 require 'mechanize'
 
 class CommentFetcher
-  def get_comments(name)
-    comments = Array.new
+
+  def initialize
     agent = Mechanize.new
     @page = agent.get('http://www.bsuir-helper.ru/lectors')
+  end
+
+  def get_comments(name)
+    comments = Array.new
     begin
-      @page = @page.links_with(:text => /#{name}/)[0].click
+      page = @page.links_with(:text => /#{name}/)[0].click
     rescue NoMethodError
       puts 'Не найдено отзывов' + "\n"
       return
     end
-    @page.parser.css('//div.comment/div.content').each do |i|
+    page.parser.css('//div.comment/div.content').each do |i|
       unless i.text.strip.empty?
         comments << i.text + "\n"
       end
     end
     count = 0
-    @page.parser.css('//div.submitted/span.comment-date').each do |i|
+    page.parser.css('//div.submitted/span.comment-date').each do |i|
       comments[count].insert(0, i.text + ': ')
       count += 1
     end
