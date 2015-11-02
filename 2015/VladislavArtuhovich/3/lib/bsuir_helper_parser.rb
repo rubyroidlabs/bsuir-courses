8b4dbf08a268c8e841369d127376105e301d13fe
ArtuhovichVlad <artvlad96@gmail.com>
2015-11-02 23:45:12 +0300

    implement home task 3

---
+++ 2015/VladislavArtuhovich/3/lib/bsuir_helper_parser.rb
@@ -0,0 +1,51 @@
+class BsuirHelperParser
+	LECTORS_URL = 'http://bsuir-helper.ru/lectors'
+	HELPER_URL = 'http://bsuir-helper.ru'
+
+	def get_teacher_url(teacher_name)
+		teacher_id = get_teacher_id(teacher_name)
+		if  teacher_id != nil 
+			teacher_url = HELPER_URL + teacher_id
+		else 
+			teacher_url = "Not found"
+		end
+		teacher_url
+	end
+
+	def get_teacher_id(teacher_name)
+		mechanize = Mechanize.new
+		lector_link = nil
+		lectors_page = mechanize.get(LECTORS_URL).links
+		lectors_page.each do |teacher_link|
+			if teacher_name == teacher_link.text
+					lector_link = teacher_link.href
+					break
+			end
+		end	
+		lector_link
+	end
+
+	def get_teacher_comments(teacher_name)
+		comments = Array.new
+		comment_blocks = Array.new
+		mechanize = Mechanize.new
+		teacher_url = get_teacher_url(teacher_name)
+
+		if teacher_url != "Not found"
+			teacher_page = mechanize.get(teacher_url)
+		else
+			return -1
+		end
+
+		teacher_page.search('div.comment.clear-block').each do |comment_block|
+			comment_blocks.push(comment_block)
+		end
+
+		comment_blocks.each do |comment_block|
+			comment = comment_block.search('div.content p').text
+			time = comment_block.search('span.comment-date').text
+			comments.push("#{time}: #{comment}")
+		end
+		comments
+	end
+end 
---
+++ 2015/VladislavArtuhovich/3/lib/comments_analyzer.rb
@@ -0,0 +1,33 @@
+class CommentsAnalyzer
+	def load_file
+    keywords = YAML.load_file('keywords.yml')
+    keywords
+  end
+
+  def positive_comment?(comment)
+  	keywords = load_file
+    positive = 0
+    negative = 0
+
+    comment_for_analyzing = Unicode::downcase(comment)
+		keywords['negative'].each do |keyword|
+			if comment_for_analyzing.include? keyword
+				negative += 1
+			end
+    end
+
+    keywords['positive'].each do |keyword|
+    	if comment_for_analyzing.include? keyword
+				positive += 1
+			end
+   	end
+
+    if positive > negative
+    	1
+    elsif positive < negative
+    	-1
+    else
+    	0
+    end
+  end
+end 
