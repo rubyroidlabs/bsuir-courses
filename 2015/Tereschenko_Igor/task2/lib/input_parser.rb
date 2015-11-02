b515ce435d6957bd74cae75a0cac19424a056691
poka poka <pokawork@gmail.com>
2015-11-03 00:25:49 +0300

    returned task 2 files

---
+++ 2015/Tereschenko_Igor/task2/lib/input_parser.rb
@@ -0,0 +1,23 @@
+require 'colorize'
+
+class InputParser
+  attr_accessor :gname, :operators, :correct
+
+  def check
+    @operators.values.each do |i|
+      if Gem::Version.correct?(i) == false
+        puts 'Error aquired! Please, check your version formatting.'.red
+        exit
+      end
+    end
+  end
+
+  def initialize(gname, operators)
+    @gname = gname
+    operators.flatten!
+    operators.map! &:split
+    @operators = Hash[operators]
+    check @operators
+  end
+end
+
