141da06bc3b5a1f67796f5ddf388a65f284c54d7
ddzyanis <ddzyanis@mail.ru>
2015-11-03 12:38:38 +0300

    fix a    houndchi_comments task_3

--- 2015/DenisSimonchik/task_3/lib/parser.rb
+++ 2015/DenisSimonchik/task_3/lib/parser.rb
@@ -3,7 +3,7 @@ class Parser
   attr_reader :num_of_group
   def initialize
     OptionParser.new do |opts|
-      opts.banner = 'Usage: bsuir_reviews.rb <number of groupe>'
+      opts.banner = "Usage: bsuir_reviews.rb <number of groupe>"
     end.parse!
     @num_of_group = ARGV[0]
   end
