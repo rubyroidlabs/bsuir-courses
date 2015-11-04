946b8e96616478254b3ffe88086c6959ad7865ce
Your Name <antoxa0073@gmail.com>
2015-11-04 19:06:55 +0300

    home task 3

---
+++ 2015/AntonZhuk/3/comment_parser.rb
@@ -0,0 +1,55 @@
+require 'rubygems'
+require 'mechanize'
+
+class CommentParser
+  def initialize(names)
+    @names = names
+  end
+
+  def find_lector
+    @lectors = Hash.new
+    @agent = Mechanize.new
+    begin
+    page = @agent.get('http://bsuir-helper.ru/lectors')
+    rescue => e
+      puts "Cannot connect to bsuir-helper.ru: #{e.message}"
+    end
+
+    lector = page.search("#content-content div div a")
+    @names.each do |name|
+      lector.each do |link|
+        if name == convert_name(link.text)
+          @lectors[name] = parse_comments(link['href'])
+        elsif @lectors[name].nil?
+          @lectors[name] = Array.[]('Comments not found!')
+        end
+      end
+    end
+    @lectors
+  end
+
+  def parse_comments(href)
+    comments = []
+    begin
+      page = @agent.get("http://bsuir-helper.ru/#{href}")
+    rescue => e
+      puts "Cannot connect to bsuir-helper.ru: #{e.message}"
+    end
+      page.search("#comments div.rounded-outside div div").each do |comment|
+        message = comment.search("div.content")
+        date = comment.search("div.submitted span.comment-date")
+        comments.push(date.text + message.text)
+      end
+    comments
+  end
+
+  def convert_name(full_name)
+    surname, name, patronymic = full_name.split
+    name = name[0] + '.'
+    patronymic = patronymic[0] + '.'
+    converted_name = surname + ' ' + name + ' ' + patronymic
+  end
+end
+
+
+
