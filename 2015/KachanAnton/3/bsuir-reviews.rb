45fe1de47c7ea71e8017b411361400b740787028
KachanAnton <antonn.lida@mail.ru>
2015-11-05 21:15:01 +0300

    task3

---
+++ 2015/KachanAnton/3/bsuir-reviews.rb
@@ -0,0 +1,20 @@
+Dir['./lib/*.rb'].each { |f| require(f) }
+require 'optparse'
+
+parser = OptionParser.new do |opts|
+  opts.banner = "Usage: ./bsuir-reviews.rb [group_number]
+  Examples: ./bsuir-reviews.rb 350504"
+  opts.on("-h", "--help", "Output help") do
+    puts opts
+    exit
+  end
+end
+parser.parse!
+group_number = ARGV[0]
+if group_number == nil
+  puts parser
+  exit
+end
+lectors = GroupLectors.new(ARGV[0]).get_list
+lectors_comments = LectorsComments.new(lectors).get_comments
+Output.new(lectors_comments).output
---
+++ 2015/KachanAnton/3/lib/output.rb
@@ -0,0 +1,49 @@
+require 'yaml'
+require 'unicode'
+require 'colorize'
+class Output
+  def initialize(lectors_comments)
+    @lectors_comments = lectors_comments
+    begin
+      @keywords = YAML.load(File.open('keywords.yml'))
+    rescue ArgumentError => e
+      puts "Error: #{e.message}"
+    end
+  end
+
+  def output
+    @lectors_comments.each_pair do |name, comments|
+      puts name.colorize(:yellow)
+      puts '====='.colorize(:yellow)
+      comments.each do |comment|
+        output_comment(comment)
+      end
+    end
+  end
+
+  private
+
+  def output_comment(comment)
+    positive = 0
+    negative = 0
+    @keywords['negative'].each do |keyword|
+      if Unicode::downcase(comment).include?(keyword)
+        negative += 1
+      end
+    end
+    @keywords['positive'].each do |keyword|
+      if Unicode::downcase(comment).include?(keyword)
+        positive += 1
+      end
+    end
+    com =  positive <=> negative
+    case com
+        when -1
+          puts comment.colorize(:red)
+        when 1
+          puts comment.colorize(:green)
+        else
+          puts comment
+        end
+  end
+end
