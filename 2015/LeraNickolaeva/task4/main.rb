adbbf3b9e706be7048557d4b284937f0cdec6681
Lera <leranick29@gmail.com>
2015-11-08 22:43:34 +0300

    Implement Hometask 4

---
+++ 2015/LeraNickolaeva/task4/main.rb
@@ -0,0 +1,68 @@
+Dir[File.expand_path('./../*.rb', __FILE__)].each { |f| require(f) }
+require 'gosu'
+
+Class Window
+def initialize(height=1800, width=900)
+
+    @height = height
+    @width = width
+    super height, width, false
+    self.caption = "Game of Life"
+    @background = Gosu::Color.new(0xffdedede)
+    @alive = Gosu::Color.new(0xff121212)
+    @dead = Gosu::Color.new(0xffededed)
+    @rows = height/10
+    @columns = width/10
+    @world = World.new(@columns, @rows)
+    @game = Game.new(@world)
+    @row_height = height/@rows
+    @col_width = width/@columns
+    @game.world.randomly_populate
+    @generation = 0
+  end
+
+  def update
+    @game.tick!
+    @generation += 1
+    puts "Generation No: #{@generation}"
+  end
+
+  def draw
+    draw_background
+    @game.world.cells.each do |cell|
+      if cell.alive?
+        draw_quad(cell.x * @col_width, cell.y * @row_height, @alive,
+                  cell.x * @col_width + (@col_width - 1), cell.y * @row_height, @alive,
+                  cell.x * @col_width + (@col_width - 1), cell.y * @row_height + (@row_height - 1), @alive,
+                  cell.x * @col_width, cell.y * @row_height + (@row_height - 1), @alive)
+      else
+        draw_quad(cell.x * @col_width, cell.y * @row_height, @dead,
+                  cell.x * @col_width + (@col_width - 1), cell.y * @row_height, @dead,
+                  cell.x * @col_width + (@col_width - 1), cell.y * @row_height + (@row_height - 1), @dead,
+                  cell.x * @col_width, cell.y * @row_height + (@row_height - 1), @dead)
+      end
+    end
+  end
+
+  def button_down(id)
+    case id
+    when Gosu::KbSpace
+      @game.world.random_populate
+    when Gosu::KbEscape
+      close
+    end
+  end
+
+  def needs_cursor?
+    true
+  end
+
+  def draw_background
+    draw_quad(0, 0, @background,
+              width, 0, @background,
+              width, height, @background,
+              0, height, @background)
+  end
+end
+
+Window.new.show
