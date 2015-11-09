211cb2adca9f3e46d4246bcf35a0fc865a378c43
Artyom Koush <artyom.koush@gmail.com>
2015-11-09 23:32:16 +0300

    fix array

--- 2015/KoushArtyom/task4/task4.rb
+++ 2015/KoushArtyom/task4/task4.rb
@@ -3,8 +3,8 @@ require 'sinatra'
 Tilt.register Tilt::ERBTemplate, 'html.erb'
 
 get '/hi' do
-  @array = Array.new(10) do |i|
-    i = Array.new(10) { |a| a = rand(0..1) }
+  @array = Array.new(10) do
+    Array.new(10) { rand(0..1) }
   end
   erb :hi
 end
