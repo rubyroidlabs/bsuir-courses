554f3c4ba5452b6bf86ac097b83a782c871ba705
aprietof <adrianprieto00@gmail.com>
2016-07-20 12:46:31 -0400

    fixed typo in database connection environment file and controller route added to test server

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "password_security"
   end
 
+  get '/' do
+    "Hello' World"
+  end
+
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 require 'bundler'
 Bundler.require
 
-ActiveRecord::Base.establish_conection(
+ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/development.sqlite"
 )
