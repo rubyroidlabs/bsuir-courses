b4f12ee1a32bf0a0e5b0f39d7f4d6b4c9a36011b
aprietof <adrianprieto00@gmail.com>
2016-07-20 12:33:50 -0400

    Application Controller added

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
+  end
+
+end
