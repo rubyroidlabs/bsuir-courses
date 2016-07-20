b1ad1a06a487cb4091e2b19a78dfbc342482c865
aprietof <adrianprieto00@gmail.com>
2016-07-20 17:24:46 -0400

    Signup post route added and working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,19 @@ class UsersController < ApplicationController
     end
   end
 
+  post '/signup' do
+    if params[:username].empty? || params[:email].empty? || params[:password].empty?
+      flash[:message] = "Pleae don't leave blank content"
+      redirect to '/signup'
+    else
+      @user = User.create(username:params[:username], email:params[:email], password:params[:password])
+      @category = Category.create(name:"General", user_id:@user.id) #=> creates a category on initialization
+      session[:user_id] = @user.id
+      flash[:message] = "It's time to add expenses"
+      redirect_to_home_page
+    end
+  end
+
+
 
 end
