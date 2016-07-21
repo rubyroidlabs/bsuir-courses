8b06284a13a6ac8506a635616ca870eabe83982f
aprietof <adrianprieto00@gmail.com>
2016-07-20 21:01:14 -0400

    Added post '/expenses' route

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -17,5 +17,17 @@ class ExpensesController < ApplicationController
     end
   end
 
+  post '/expenses' do
+    if params[:description].empty? || params[:amount].empty? || params[:date].empty?
+      flash[:message] = "Please don't leave blank content"
+      redirect to "/expenses/new"
+    else
+      @user = current_user
+      @category = @user.categories.find {|category| category.name == params[:category_name]}
+      @expense = Expense.create(description:params[:description], amount:params[:amount], date:params[:date], category_id:@category.id, user_id:@user.id)
+      redirect to "/expenses/#{@expense.id}"
+    end
+  end
+
 
 end
