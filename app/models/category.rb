096c828caa06c0ca5410cb2ab3e892938aebf68c
aprietof <adrianprieto00@gmail.com>
2016-07-23 12:48:46 -0400

    Sort methods added to user and category models, and implemented into category views

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,8 @@ class Category < ActiveRecord::Base
   has_many :expenses, :dependent => :destroy
   belongs_to :user
 
+  def expenses_sort_by_date
+    self.expenses.sort_by {|hash| hash[:date]}.reverse
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ActiveRecord::Base
   has_many :categories, :dependent => :destroy
   has_many :expenses
 
+  def categories_sort_by_name
+    self.categories.all.sort_by {|hash| hash[:name]}
+  end
+
 end
