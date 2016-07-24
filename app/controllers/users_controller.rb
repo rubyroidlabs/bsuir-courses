c11c85f9dcd32a2d720b0ff8d86080ece536d3da
aprietof <adrianprieto00@gmail.com>
2016-07-23 23:30:04 -0400

    User show view and get '/users/:id' route added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -59,6 +59,15 @@ class UsersController < ApplicationController
     end
   end
 
+  # displays user info if logged in
+  get '/users/:id' do
+    if logged_in?
+      erb :'users/show'
+    else
+      redirect_if_not_logged_in
+    end
+  end
+
   # does not let a user edit with blank content
   patch '/users/:id' do
     if !params[:username].empty? && !params[:email].empty? && !params[:password].empty?
