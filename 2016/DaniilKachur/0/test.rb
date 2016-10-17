40490efe0cae629ae0954a29705444497f352dc8
DDKatch <daniil.field@yandex.ru>
2016-10-18 00:57:00 +0300

    slightly beautifyed tests

--- 2016/DaniilKachur/0/test.rb
+++ 2016/DaniilKachur/0/test.rb
@@ -1,37 +1,37 @@
 require 'rspec'
 require'./rpn_calculator'
 
-def expression_of(line)
-  line.tr(' ', "\n")
+def result_from
+  RPNCalculator.calculate(yield.tr(' ', "\n"))
 end
 
 describe RPNCalculator do
   it '+' do
-    expect(RPNCalculator.calculate(expression_of('12 2 +'))).to eq(14)
+    expect(result_from { '12 2 +' }).to eq(14)
   end
   it '+ with float' do
-    expect(RPNCalculator.calculate(expression_of('12.5 2 +'))).to eq(14.5)
+    expect(result_from { '12.5 2 +' }).to eq(14.5)
   end
   it '+ with negative' do
-    expect(RPNCalculator.calculate(expression_of('12 -2 +'))).to eq(10)
+    expect(result_from { '12 -2 +' }).to eq(10)
   end
   it '-' do
-    expect(RPNCalculator.calculate(expression_of('12 2 -'))).to eq(10)
+    expect(result_from { '12 2 -' }).to eq(10)
   end
   it '*' do
-    expect(RPNCalculator.calculate(expression_of('12 2 *'))).to eq(24)
+    expect(result_from { '12 2 *' }).to eq(24)
   end
   it '/' do
-    expect(RPNCalculator.calculate(expression_of('12 2 /'))).to eq(6)
+    expect(result_from { '12 2 /' }).to eq(6)
   end
   it '/ float result' do
-    expect(RPNCalculator.calculate(expression_of('12 10 /'))).to eq(1.2)
+    expect(result_from { '12 10 /' }).to eq(1.2)
   end
   it '!' do
-    expect(RPNCalculator.calculate(expression_of('93 3 !'))).to eq(80)
+    expect(result_from { '93 3 !' }).to eq(80)
   end
   it '! with float' do
-    expect(RPNCalculator.calculate(expression_of('2.5 1 !'))).to eq(2)
-    expect(RPNCalculator.calculate(expression_of('0.15625 1 !'))).to eq(0.125)
+    expect(result_from { '2.5 1 !' }).to eq(2)
+    expect(result_from { '0.15625 1 !' }).to eq(0.125)
   end
 end
