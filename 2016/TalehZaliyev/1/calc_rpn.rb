a0b00e99de536cba5cc90af42ab7c584faeb6cbf
Taleh Zaliyev <z.taleh007@ya.ru>
2016-10-19 02:53:54 +0300

    edit homework-1 #1

--- 2016/TalehZaliyev/1/calc_rpn.rb
+++ 2016/TalehZaliyev/1/calc_rpn.rb
@@ -1,21 +1,18 @@
 #!/usr/bin/env ruby
 class Numeric
-  def b!(num_bit)
+  def bit_rep(num_bit)
     a = self - self.to_i
     self.to_i.to_s(2).split('').reverse.map do |x|
       (x == "1" && num_bit>0) ? ((num_bit -= 1) && (x = "0")) : x
     end.reverse.join.to_i(2) + a
   end
 end
-def calc_rpn
-  stack = []
+def calc_rpn(stack = [])
   while(true)
-    input = gets.strip
-    case(input)
-    when /^(\d+|\d+[.]\d+)$/
-      stack.push(input)
-    when /^[-+*\/!]$/
-      input == "!" ? input = ".b! " + stack.pop : input += " " +stack.pop
+    case(input = gets.strip)
+    when %r{^(\d+|\d+[.]\d+)$}; stack.push(input)
+    when %r{^[-+*\/!]$}
+      input == "!" ? input = ".bit_rep " + stack.pop : input += " " + stack.pop
       stack.push(eval(stack.pop + ".to_f" + input).to_s)
       break if stack.size == 1
     end
