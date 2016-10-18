66e657eb314c5a17a3782f51e75cd13e85091c9f
Valentine Zavadsky <saicheg@gmail.com>
2016-10-18 11:11:02 +0300

    fix styles

--- 2016/ValentinZavadkiy/1/calculator.rb
+++ 2016/ValentinZavadkiy/1/calculator.rb
@@ -1,11 +1,11 @@
-puts '123'
+puts "123"
 a = b = 12
 
-if a > 3 and b > 4
-   a = a + 1
-   b = b- 1
+if a > 3 && b > 4
+  a += 1
+  b -= 1
 end
 
 puts "very long line very long line very long line very long line very long line very long line very long line"
 
-puts(a+b)
+puts(a + b)
