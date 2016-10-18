ba88173dd0749900cea5e84a6dcae421113934fe
andersann <a.kryuda@gmail.com>
2016-10-19 02:26:19 +0300

    v 7

--- 2016/AndreiKruyda/0/calculator.rb
+++ 2016/AndreiKruyda/0/calculator.rb
@@ -1,38 +1,33 @@
-def convert_pol(symbols)
-  symbols = symbols.split
-  values = []
-  operands = []
-  symbols.each do |x|
-    case x
-    when /\d/
-      values.push(x.to_i)
-    when "-", "+", "/", "*"
-      operands = values.pop(2)
-      values.push(operands[0].send(x, operands[1]))
-    when "!"
-      operands = values.pop(2)
-      values.push(binar_!(operands[0], operands[1]))
-    else
-      puts "Error: Invalid symbol !"
-      exit(0)
-    end
-  end
-  values
-end
-
 def binar_!(value, number)
   mask = 1
   p = 0
-  value.to_s(2).length.times do
+  value.to_s(2).length.times do |i|
     next if p == number
-    if (value & mask).nonzero?
-      value &= ~mask
+    if (value & (mask << i)).nonzero?
+      value &= ~(mask << i)
       p += 1
     end
-    mask <<= 1
   end
   value
 end
 
 symbols = gets
-puts convert_pol(symbols)
+symbols = symbols.split
+values = []
+operands = []
+symbols.each do |x|
+  case x
+  when /\d/
+    values.push(x.to_i)
+  when "-", "+", "/", "*"
+    operands = values.pop(2)
+    values.push(operands[0].send(x, operands[1]))
+  when "!"
+    operands = values.pop(2)
+    values.push(binar_!(operands[0], operands[1]))
+  else
+    puts "Error: Invalid symbol !"
+    exit(0)
+  end
+end
+puts values
