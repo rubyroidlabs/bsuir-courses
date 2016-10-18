92c40df5741b10d47505d9381225162820547a47
andersann <a.kryuda@gmail.com>
2016-10-18 23:33:29 +0300

    v 2

--- 2016/AndreiKruyda/0/calculator.rb
+++ 2016/AndreiKruyda/0/calculator.rb
@@ -6,33 +6,32 @@ def convert_pol(symbols)
     case x
       when /\d/
         values.push(x.to_i)
-      when '-', '+', '/', '*'
+      when "-", "+", "/", "*"
         operands = values.pop(2)
         values.push(operands[0].send(x, operands[1]))
-      when '!'
+      when "!"
         operands = values.pop(2)
         values.push(binar_!(operands[0], operands[1]))
       else
-        puts "Error: Invalid symbol ! "
+        puts "Error: Invalid symbol !"
         exit(0)
     end
   end
   values
 end
 
-def binar_!(value, number )
+def binar_!(value, number)
   mask = 1
   p = 0
   value.to_s(2).length.times do
-    unless p == number
-      if (value & mask).nonzero?
-        value = value&~mask
-        p += 1
-      end
-      mask = mask << 1
+    next if p == number
+    if (value & mask).nonzero?
+      value &= ~mask
+      p += 1
     end
+    mask = mask << 1
   end
-  value
+value
 end
 
 while symbols = gets
