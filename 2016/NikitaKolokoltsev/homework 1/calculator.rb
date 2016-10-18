69684807c5b144a554c84f6a5c458555c08eb3bb
Nikita Kolokoltsev <nikita.kolokoltsev@yahoo.com>
2016-10-18 21:27:28 +0300

    fixed code style 6

--- 2016/NikitaKolokoltsev/homework 1/calculator.rb
+++ 2016/NikitaKolokoltsev/homework 1/calculator.rb
@@ -1,17 +1,17 @@
 def remove_bits(bits_to_remove, number)
-  s = [number].pack("G").bytes.map{ |n| "%08b" % n }. # "G" - packing as float value with double precision; "%08b" - convert string bytes to binary
-        reverse.map{ |n| # reverse needed for more understandable float number representation: sign bit - exponent - mantissa
-          n.split('').reverse.map{ |i| # n.split will give us every bit of our number
-            if i == "1" && bits_to_remove > 0 then
+  s = [number].pack("G").bytes.map{ |n| sprintf("%08b", n) }. # "G" - packing as float value with double precision; "%08b" - convert string bytes to binary
+      reverse.map do |n| # reverse needed for more understandable float number representation: sign bit - exponent - mantissa
+          n.split("").reverse.map do |i| # n.split will give us every bit of our number
+            if i == "1" && bits_to_remove.positive?
               i = "0"
               bits_to_remove -= 1
             end
-            i 
-          }.reverse.
-              join 
-        }.reverse. # reverse to initial representation
-          map{ |n| ("%d" % "0b#{n}").to_i.chr }.join. # convert back to packed string and 
-            unpack("G")[0].to_f # unpack this string
+            i
+          end.reverse.
+            join
+        end.reverse. # reverse to initial representation
+      map { |n| (sprintf("%d", "0b#{n}")).to_i.chr }.join. # convert back to packed string and 
+      unpack("G")[0].to_f # unpack this string
 end
 
 expression = []
@@ -20,7 +20,7 @@ until (input = gets.chomp).empty?
 end
 
 if expression.include?("!")
-  puts expression.each_with_object([]){ |e, s|
+  puts expression.each_with_object([]) do |e, s|
     if e =~ /[0-9]/
       s.push(e.to_f)
     elsif e == "!"
@@ -29,7 +29,14 @@ if expression.include?("!")
       s.push(s.pop(2).inject(e).to_f)
     end
     s
-  }
+  end
 else
-  puts expression.each_with_object([]) { |e, s| e =~ /[0-9]/ ? s.push(e.to_f) : s.push(s.pop(2).inject(e).to_f); s }
+  puts expression.each_with_object([]) do |e, s|
+    if e =~ /[0-9]/
+      s.push(e.to_f)
+    else
+      s.push(s.pop(2).inject(e).to_f)
+    end
+    s
+  end
 end
