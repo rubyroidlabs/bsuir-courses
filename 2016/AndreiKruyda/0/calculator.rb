1a48f2aabb5b0227267f70fe82e84344466565da
andersann <a.kryuda@gmail.com>
2016-10-18 22:30:05 +0300

    implement hw 0

---
+++ 2016/AndreiKruyda/0/calculator.rb
@@ -0,0 +1,50 @@
+def convert_pol(symbols)
+
+  symbols = symbols.split
+
+  values = []
+  operands = []
+
+  symbols.each do |x|
+
+    case x
+
+      when /\d/
+        values.push(x.to_i)
+      when '-', '+', '/', '*'
+        operands = values.pop(2)
+        values.push(operands[0].send(x, operands[1]))
+      when '!'
+        operands = values.pop(2)
+        values.push(binar_!(operands[0], operands[1]))
+      else
+        # type code here
+    end
+
+  end
+
+  values
+
+end
+
+def binar_!(value, number )
+  mask = 1
+  p = 0
+
+  value.to_s(2).length.times do
+    unless p == number
+      if value & mask != 0
+        value = value&~mask
+        p += 1
+      end
+      mask = mask << 1
+    end
+  end
+
+  value
+end
+
+
+while symbols = gets
+  puts convert_pol(symbols)
+end
