7614c6f8fdd7c4c779275e5d64fce1af9f137f87
andersann <a.kryuda@gmail.com>
2016-10-19 01:05:03 +0300

    v 5

--- 2016/AndreiKruyda/0/calculator.rb
+++ 2016/AndreiKruyda/0/calculator.rb
@@ -1,42 +1,36 @@
-class Pol
-  def convert_pol(symbols)
-    symbols = symbols.split
-    values = []
-    operands = []
-    symbols.each do |x|
-      case x
-        when /\d/
-          values.push(x.to_i)
-        when "-", "+", "/", "*"
-          operands = values.pop(2)
-          values.push(operands[0].send(x, operands[1]))
-        when "!"
-          operands = values.pop(2)
-          values.push(binar_!(operands[0], operands[1]))
-        else
-          puts "Error: Invalid symbol !"
-          exit(0)
-      end
+def convert_pol(symbols)
+  symbols = symbols.split
+  values = [], operands = []
+  symbols.each do |x|
+    case x
+    when /\d/
+        values.push(x.to_i)
+    when "-", "+", "/", "*"
+        operands = values.pop(2)
+        values.push(operands[0].send(x, operands[1]))
+    when "!"
+        operands = values.pop(2)
+        values.push(binar_!(operands[0], operands[1]))
+    else
+        puts "Error: Invalid symbol !"
+        exit(0)
     end
-    values
   end
+  values
+end
 
-  def binar_!(value, number)
-    mask = 1
-    p = 0
-    value.to_s(2).length.times do
-      next if p == number
-      if (value & mask).nonzero?
-        value &= ~mask
-        p += 1
-      end
-      mask <<= 1
+def binar_!(value, number)
+  mask = 1, p = 0
+  value.to_s(2).length.times do
+    next if p == number
+    if (value & mask).nonzero?
+      value &= ~mask
+      p += 1
     end
-    value
+    mask <<= 1
   end
-
+  value
 end
 
 symbols = gets
-rpn = Pol.new
-puts rpn.convert_pol(symbols)
+puts convert_pol(symbols)
