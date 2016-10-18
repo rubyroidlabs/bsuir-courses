a2696d1b813c3dd6f588ad091749307b147b1139
storm2513 <storm25132513@gmail.com>
2016-10-19 00:26:43 +0300

    fixes

--- 2016/MaksimShylau/1/calculator.rb
+++ 2016/MaksimShylau/1/calculator.rb
@@ -1,31 +1,31 @@
 def bin (number) #Перевод из десятичной в двоичную систему
-	s=""
+	s = ""
 	while number.to_i > 0 do 
-		s+=(number.to_i%2).to_s
-		number/=2
+		s += (number.to_i%2).to_s
+		number /= 2
 	end
 	return s.reverse
 end
 
 def nulBin(dec, count) #Замена 1 на нули (операция !)
-	ind=-1
-	i=0
-	while i<count and dec[ind]!=nil 
+	ind = -1
+	i = 0
+	while i<count && dec[ind]!=nil 
 		if dec[ind]=='1'
-			i+=1
-			dec[ind]='0'
+			i += 1
+			dec[ind] = '0'
 		end
-		ind-=1
+		ind -= 1
 	end
 	return dec.to_s
 end
 
 def toDec(binary) #Перевод из двоичной в десятичную систему
-	sum=0
-	i=0
+	sum = 0
+	i = 0
 	1.upto(binary.size) do
 	    sum += (binary[i].to_i)*(2**(binary.size.to_i-i-1))
-	    i+=1
+	    i += 1
     end
     return sum
 end
@@ -33,13 +33,13 @@ end
 def oper (symbol, a, b) 
 	case symbol
 	when "+"
-		return a+b
+		return a + b
 	when "-"
-		return a-b
+		return a - b
 	when "/"
-		return a/b
+		return a / b
 	when "*"
-		return a*b
+		return a * b
 	when "!"
 		return toDec(nulBin(bin(a), b))
 	end
@@ -48,7 +48,7 @@ end
 a = []
 i = -1
 loop do 
-    i+=1
+    i += 1
     a[i] = gets.chomp
     break if a[i]=='+'||a[i]=='-'||a[i]=='*'||a[i]=='/'||a[i]=='!'
 end
@@ -56,9 +56,9 @@ j = 0
 res = oper(a[i], a[i-2].to_i, a[i-1].to_i)
 ind=i
 1.upto(a.size-3) do
-	j+=1
+	j += 1
 	a[i+j] = gets.chomp
 	res = oper(a[i+j], a[ind-3].to_i, res)
-	ind-=1
+	ind -= 1
 end
 puts "#=> " + res.to_s
