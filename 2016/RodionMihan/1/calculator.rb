390fcb7741a554e8c733a9a1f986f39017e58d29
Родион Михан <rodionmihan@93-125-42-103.static.unet.by>
2016-10-18 18:47:11 +0300

    implement hw 1

---
+++ 2016/RodionMihan/1/calculator.rb
@@ -0,0 +1,40 @@
+def kill_one_bits(count, number)
+  str = number.to_s(2).reverse
+  str.length.times do |i|
+    break if count <= 0
+    if str[i] == "1"
+      str[i] = "0"
+      count -= 1 
+    end
+  end
+  str.reverse.to_i(2)
+end
+
+def calculate(expression)
+  stack = []
+  expression.each do |element|
+    stack.push case element
+               when /\d/ then element.to_i
+               when /\+/ then stack.pop + stack.pop
+               when /\-/ then stack.pop - stack.pop
+               when /\*/ then stack.pop * stack.pop
+               when %r{\/} then stack.pop / stack.pop
+               when /\!/ then kill_one_bits(stack.pop, stack.pop)
+               end
+  end
+  stack.pop
+end
+
+expression = []
+puts "Enter '=' if you want to calculate your expression! ('q' - exit)"
+loop do
+  item = gets.chomp
+  if item.include? "="
+    puts "Result: #{calculate(expression)}"
+    expression.clear
+    next
+  elsif item.include? "q"
+    break
+  end
+  item =~ %r{[\d\+\*\/\!]} ? expression << item : puts("Incorrectly input!")
+end
