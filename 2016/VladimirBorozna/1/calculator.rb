1dbaa4a08cffb264afc7f7cdc195fdd6d07fa7e8
Vladimir Borozna <4laconic@gmail.com>
2016-10-19 01:06:27 +0300

    Implement homework 1

---
+++ 2016/VladimirBorozna/1/calculator.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/ruby
+require_relative 'lib/numeric_extension'
+
+stack = []
+loop do
+  line = gets.chomp!
+  if %w(+ - / * !).include?(line)
+    stack << stack.pop(2).inject(line.to_sym)
+    break if stack.size == 1
+  else
+    stack << line.to_f
+  end
+end
+
+puts format('%g', stack.first)
