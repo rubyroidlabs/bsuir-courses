d36a7d4468a0a143d6dcdf5a81ce20fafe3c5088
colinforzeal <colinforzeal@yandex.ru>
2016-10-20 00:29:16 +0300

    implement hw0

---
+++ 2016/UladzislauBondar/0/calculator.rb
@@ -0,0 +1,72 @@
+#!/usr/bin/env ruby
+
+class RPNCalc
+  def initialize
+    @operands = []
+  end
+
+  def start
+    until (sign = gets.chomp).nil?
+      if number?(sign)
+        add_operand(sign)
+      elsif operands_enough? && operation?(sign)
+        operate(sign)
+        break unless operands_enough?
+      else
+        puts 'invalid symbol. add some operands'
+      end
+    end
+  end
+
+  def show_result
+    puts "#=> #{@operands[0]}"
+  end
+
+  private
+
+  def operate(sign)
+    last = @operands.pop
+    pre_last = @operands.pop
+    if sign == '!'
+      @operands << set_bytes_to_zero(pre_last, last)
+    else
+      @operands << calculate(pre_last, last, sign)
+    end
+  end
+
+  def add_operand(operand)
+    @operands << operand.to_i
+  end
+
+  def calculate(first, second, operation)
+    first.send(operation, second)
+  end
+
+  def set_bytes_to_zero(value, n_of_zeros)
+    bin_str = value.to_s(2).reverse
+    bin_str.each_char do |bit|
+      if bit == '1'
+        bin_str[bit] = '0'
+        n_of_zeros -= 1
+      end
+      break if n_of_zeros < 1
+    end
+    bin_str.reverse.to_i(2)
+  end
+
+  def operands_enough?
+    @operands.length >= 2
+  end
+
+  def number?(sign)
+    sign =~ /^[0-9]+$/
+  end
+
+  def operation?(sign)
+    sign =~ /[+\-*\/!]/
+  end
+end
+
+calculator = RPNCalc.new
+calculator.start
+calculator.show_result
