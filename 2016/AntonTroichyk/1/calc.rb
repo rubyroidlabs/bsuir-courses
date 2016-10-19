4fd88313939de0eec1a19c953baf1338496ac454
freq <camvchoke@mail.ru>
2016-10-20 01:04:09 +0300

    fix issues

--- 2016/AntonTroichyk/1/calc.rb
+++ 2016/AntonTroichyk/1/calc.rb
@@ -1,48 +1,48 @@
 def main
- stack = []
- while(true)
- tmp = gets.chomp
-  case tmp
-   when /\d/
-    stack.push(tmp.to_f)
-   when "-"
-    if stack.size < 2
-     puts 'error'
-     break;
+  stack = []
+  while(true)
+    tmp = gets.chomp
+    case tmp
+      when /\d/
+      stack.push(tmp.to_f)
+      when "-"
+        if stack.size < 2
+        puts "error"
+        break;
+        end
+      operand_2 = stack.pop
+      operand_1 = stack.pop
+      stack.push(operand_1 - operand_2)
+      when "+"
+        if stack.size < 2
+        puts "error"
+        break;
+        end
+      operand_2 = stack.pop
+      operand_1 = stack.pop
+      stack.push(operand_1 + operand_2)
+      when "*"
+        if stack.size < 2
+        puts "error"
+        break;
+        end
+      operand_2 = stack.pop
+      operand_1 = stack.pop
+      stack.push(operand_1 * operand_2)
+      when "/"
+        if stack.size < 2
+        puts "error"
+        break;
+        end
+      operand_2 = stack.pop
+      operand_1 = stack.pop
+      stack.push(operand_1 / operand_2)
+      when "="
+      result = stack.pop
+      break;
     end
-    operand_2 = stack.pop
-    operand_1 = stack.pop
-    stack.push(operand_1 - operand_2)
-   when "+"
-    if stack.size < 2
-     puts 'error'
-     break;
-    end
-    operand_2 = stack.pop
-    operand_1 = stack.pop
-    stack.push(operand_1 + operand_2)
-   when "*"
-    if stack.size < 2
-     puts 'error'
-     break;
-    end
-    operand_2 = stack.pop
-    operand_1 = stack.pop
-    stack.push(operand_1 * operand_2)
-   when "/"
-    if stack.size < 2
-     puts 'error'
-     break;
-    end
-    operand_2 = stack.pop
-    operand_1 = stack.pop
-    stack.push(operand_1 / operand_2)
-   when "="
-    result = stack.pop
-    break;
-   end
   end
- puts result 
+puts result 
 end
 puts main
 
