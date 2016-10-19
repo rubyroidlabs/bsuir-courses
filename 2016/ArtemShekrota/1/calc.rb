49a2fa3da1be91277b91dc11c66f4984ca9d21ca
artisaev <artem.shekrota@gmail.com>
2016-10-20 00:39:33 +0300

    fix 11

--- 2016/ArtemShekrota/1/calc.rb
+++ 2016/ArtemShekrota/1/calc.rb
@@ -1,72 +1,57 @@
-# Calculating
-class Calc
-  def sum(array)
-    array[0].to_f + array[1].to_f
-  end
+def sum(array)
+  array[0].to_f + array[1].to_f
+end
 
-  def mult(array)
-    array[0].to_f * array[1].to_f
-  end
+def mult(array)
+  array[0].to_f * array[1].to_f
+end
 
-  def diff(array)
-    array[0].to_f - array[1].to_f
-  end
+def diff(array)
+  array[0].to_f - array[1].to_f
+end
 
-  def div(array)
-    array[0].to_f / array[1].to_f
-  end
+def div(array)
+  array[0].to_f / array[1].to_f
+end
 
-  def bit(array)
-    a = array[0].to_i.to_s(2).reverse.split(//)
-    b = array[1].to_i
-    a.map! do |i|
-      break if b <= 0
-      if i == "1"
-        b -= 1
-        "0"
-      end
+def bit(array)
+  a = array[0].to_i.to_s(2).reverse.split(//)
+  b = array[1].to_i
+  a.map! do |i|
+    break if b <= 0
+    if i == "1"
+      b -= 1
+      "0"
     end
-    a.join.reverse.to_i(2)
   end
+  a.join.reverse.to_i(2)
+end
+
+def calculate(expression)
+  expression_array = expression.split
+  operands = []
   
-  def add(s, opers)
-    if s == "+"
-      opers.push(sum(opers.pop(2)))
-    elsif s == "-"
-      opers.push(diff(opers.pop(2)))
+  expression_array.each do |i|
+    if !i.match(/[0-9]/).nil?
+      operands.push(i)
+    elsif i == "+"
+      operands.push(sum(operands.pop(2)))
+    elsif i == "-"
+      operands.push(diff(operands.pop(2)))
+    elsif i == "/"
+      operands.push(div(operands.pop(2)))
+    elsif i == "*"
+      operands.push(mult(operands.pop(2)))
+    elsif i == "!"
+      operands.push(bit(operands.pop(2)))
     end
   end
-  
-  def bdd(s, opers)
-    if s == "!"
-      opers.push(bit(opers.pop(2)))
-    elsif s == "*"
-      opers.push(mult(opers.pop(2)))
-    elsif s == "/"
-      opers.push(div(opers.pop(2)))
-    end
-  end
-  
-  def calculate(expression)
-    expression_array = expression.split
-    operands = []
-    expression_array.each do |i|
-      if !i.match(/[0-9]/).nil?
-        operands.push(i)
-      elsif i == "!" || i == "*" || i == "/"
-        bdd(i, operands)
-      elsif i == "+" || i == "-"
-        add(i, operands)
-      end
-    end
-    puts "Answer: #{operands}"
-  end
+  puts "Answer: #{operands}"
 end
 
 puts "Enter expression in polish notation using space between symbols"
 while a = gets
-begin
-  g = Calc.new()
-  g.calculate(a)
+  begin
+    calculate(a)
   end
 end
