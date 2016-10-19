6ae9124ee09a0c4e878aa845afc16fb8cdadc544
Nikita Kolokoltsev <nikita.kolokoltsev@yahoo.com>
2016-10-20 00:28:49 +0300

    Update

--- 2016/NikitaKolokoltsev/homework 1/calculator.rb
+++ 2016/NikitaKolokoltsev/homework 1/calculator.rb
@@ -1,47 +1,73 @@
 def convert_to_binary(number)
   [number].pack("G").bytes.map { |n| format("%08b", n) }
+  # Will return our number in binary (as array of bit sets - smth like: ["00000000", "01000100", "00111001", ...].
+  # If we use .join on this array - we will get our number in binary.
+  # But if we want to convert binary number back to float later we need this binary number in such form.
 end
 
 def convert_to_float(binary_number)
-  binary_number.reverse.map { |n| format("%d", "0b#{n}").to_i.chr }.join.unpack("G")[0].to_f
+  binary_number.map { |n| format("%d", "0b#{n}").to_i.chr }.join.unpack("G")[0].to_f
+  # Simply will convert our binary number to float
+  # !!! Binary number passed to that method should be in such form: ["01000000", "01001101", "11010010", ...]
 end
 
 def remove_bits(bits_to_remove, number)
-  binary_number = convert_to_binary(number).reverse.map do |n|
-    n.split("").reverse.map do |i|
+  binary_number = convert_to_binary(number).reverse.map do |n| # reverse our number(it bit sets) because we should delete bits from end
+    n.reverse.split("").map do |i| # reverse our bits in bit set beacuse they are still in the order which was when our number wasn't reversed
       if i == "1" && bits_to_remove.positive?
         i = "0"
         bits_to_remove -= 1
       end
       i
     end.reverse.join
-  end
+  end.reverse
   convert_to_float(binary_number)
 end
 
-expression = []
-until (input = gets.chomp).empty?
-  expression.push(input)
-end
-
-if expression.include?("!")
-  puts(expression.each_with_object([]) do |e, s|
-    if e =~ /[0-9]/
+def calculate(expression)
+  expression = expression.each_with_object([]) do |e, s|
+    case e
+    when /\d/
       s.push(e.to_f)
-    elsif e == "!"
+    when "!"
       s.push(remove_bits(s.pop, s.pop))
     else
       s.push(s.pop(2).inject(e).to_f)
     end
     s
-  end)
-else
-  puts(expression.each_with_object([]) do |e, s|
-    if e =~ /[0-9]/
-      s.push(e.to_f)
-    else
-      s.push(s.pop(2).inject(e).to_f)
+  end
+  expression
+end
+
+puts "Reverse Polish Notation calculator"
+puts "Type 'exit' to exit. Simple."
+puts "Enter your expression:"
+expression = []
+first_input = true
+loop do
+  input = gets.chomp
+  case input.downcase
+  when /(\d|[+-\/\*!])/ # regexp that allows only numbers, +, -, /, *, !
+    expression.push(input)
+    result = calculate(expression)
+    if result.length == 1 && !first_input
+      puts "Result: #{result[0]}\nWant to continue with that result?(yes/no)"
+      case gets.chomp.downcase
+      when "yes"
+        puts result[0]
+      when "exit"
+        break
+      else
+        result, expression = [], []
+        first_input = true
+        puts "New expression:"
+        next # needed here because of assignment of 'first_input = false' at 68 line
+      end
     end
-    s
-  end)
+    first_input = false
+  when "exit"
+    break
+  else
+    puts "InputError"
+  end
 end
