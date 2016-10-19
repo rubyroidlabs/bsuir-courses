121a14d44f0e8ca681e147cb376837192ff6b59f
lgor89 <igor.penkovsky@gmail.com>
2016-10-19 15:49:22 +0300

    implement hw 1

---
+++ 2016/IgorPenkovsky/1/polish_calculator_with_spec_operation.rb
@@ -0,0 +1,95 @@
+puts "input reverse polish notation"
+first_num=gets.chomp.to_i
+second_num=gets.chomp.to_i
+third=gets.chomp
+if third.match(/\D/)
+  if third=="+"
+    result=first_num+second_num
+  end
+  if third=="-"
+    result=first_num-second_num
+  end
+  if third=="*"
+    result=first_num*second_num
+  end
+  if third=="/"
+    result=first_num/second_num
+  end
+  if third=="!"
+    b=first_num.to_s(2)
+    c=b.length.to_i
+    d=b.length.to_i
+    e=1
+    g=[]
+    while c!=0
+      f=first_num[d-c]
+      if e<=second_num
+        if f==1
+          g[d-c]=first_num[~(d-c)]
+          e+=1
+        else
+          g[d-c]=first_num[d-c]
+        end
+      else
+        g[d-c]=first_num[d-c]
+      end
+      c-=1
+    end
+    j= g.reverse
+    l=j.length.to_i
+    i=0
+    k=""
+    while i!=l
+      k+=j[i].to_s
+      i+=1
+    end
+    result=k.to_i(2)
+  end
+  puts "result= "+result.to_s
+  exit
+end
+
+forth=gets.chomp
+if forth.match(/\D/)
+  fifth=gets.chomp
+  if fifth.match(/\D/)
+    if forth=="+"
+      result=second_num+third.to_f
+    end
+    if forth=="-"
+      result=second_num-third.to_f
+    end
+    if forth=="*"
+      result=second_num*third.to_f
+    end
+    if forth=="/"
+      result=second_num/third.to_f
+    end
+  end
+  if fifth=="+"
+    result=result+first_num
+  end
+  if fifth=="-"
+    result=result-first_num
+  end
+  if fifth=="*"
+    result=result*first_num
+  end
+  if fifth=="/"
+    result=result/first_num
+  end
+end
+puts "result= "+result.to_s
+
+
+
+
+
+
+
+
+
+
+
+
+
