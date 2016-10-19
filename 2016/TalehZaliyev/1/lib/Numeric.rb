48d5be08ba5b209fb47af2a43105b3ce6d849351
Taleh Zaliyev <z.taleh007@ya.ru>
2016-10-19 14:23:32 +0300

    fix homework-1 #3.a

--- 2016/TalehZaliyev/1/lib/Numeric.rb
+++
@@ -1,8 +0,0 @@
-class Numeric < Object # :nodoc:
-  def bit_rep(num_bit)
-    a = self - self.to_i
-    self.to_i.to_s(2).split('').reverse.map do |x|
-      (x == "1" && num_bit>0) ? ((num_bit -= 1) && (x = "0")) : x
-    end.reverse.join.to_i(2) + a
-  end
-end
