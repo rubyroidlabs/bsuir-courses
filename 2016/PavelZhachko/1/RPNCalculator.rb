ff916752b093ac88fff05d6fef27a804d96bf453
Pavel Zhachko <rammiel@yandex.ru>
2016-10-19 21:30:14 +0300

    another style fix after rubocop

--- 2016/PavelZhachko/1/RPNCalculator.rb
+++ 2016/PavelZhachko/1/RPNCalculator.rb
@@ -2,30 +2,28 @@
 # class RPNCalculator
 class RPNCalculator
   def special_func(first, second)
-    tmp_arr = ('0' + first.to_s(2)).split(//)
+    tmp_arr = ("0" + first.to_s(2)).split(//)
     index = (second * - 1) - 1
-    for i in index..0
-      tmp_arr[i] = '0'
-    end
-    result = '0' + tmp_arr.join('')
+    (index..0).each { |i| tmp_arr[i] = "0"}
+    result = "0" + tmp_arr.join('')
     result.to_i(2)
   end
 
   def evaluate(rpn)
-    a = rpn.split(' ')
+    a = rpn.split(" ")
     array = a.inject([]) do |stack, i|
-      if i =~ /\d+/ # begin of if
+      if i =~ /\d+/
         stack << i.to_i 
       else
         b = stack.pop(2)
           case
-          when i == '+' then stack << b[0] + b[1]
-          when i == '-' then stack << b[0] - b[1]
-          when i == '*' then stack << b[0] * b[1]
-          when i == '/' then stack << b[0] / b[1]
-          when i == '!' then stack << special_func(b[0], b[1])
+          when i == "+" then stack << b[0] + b[1]
+          when i == "-" then stack << b[0] - b[1]
+          when i == "*" then stack << b[0] * b[1]
+          when i == "/" then stack << b[0] / b[1]
+          when i == "!" then stack << special_func(b[0], b[1])
           end
-      end # end of if
+      end
     end
     p array.pop
   end
