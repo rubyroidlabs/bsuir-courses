dd8cafd77cb6ecdc3519e377675fed7a102bc251
freq <camvchoke@mail.ru>
2016-10-20 01:52:43 +0300

    fix issues6

--- 2016/AntonTroichyk/1/calc.rb
+++ 2016/AntonTroichyk/1/calc.rb
@@ -1,48 +1,45 @@
-def calc
-  stack = []
-  while
-    tmp = gets.chomp
-    case tmp
-    when /\d/
-      stack.push(tmp.to_f)
-    when "-"
-      if stack.size < 2
-        puts "error: dont have 2 operands before -"
-        break;
-      end
-      operand_2 = stack.pop
-      operand_1 = stack.pop
-      stack.push(operand_1 - operand_2)
-    when "+"
-      if stack.size < 2
-        puts "error: dont have 2 operands before +"
-        break;
-      end
-      operand_2 = stack.pop
-      operand_1 = stack.pop
-      stack.push(operand_1 + operand_2)
-    when "*"
-      if stack.size < 2
-        puts "error: dont have 2 operands before *"
-        break;
-      end
-      operand_2 = stack.pop
-      operand_1 = stack.pop
-      stack.push(operand_1 * operand_2)
-    when "/"
-      if stack.size < 2
-        puts "error: dont have 2 operands before /"
-        break;
-      end
-      operand_2 = stack.pop
-      operand_1 = stack.pop
-      stack.push(operand_1 / operand_2)
-    when "="
-      result = stack.pop
+stack = []
+loop do
+  tmp = gets.chomp
+  case tmp
+  when /\d/
+    stack.push(tmp.to_f)
+  when "-"
+    if stack.size < 2
+      puts "error: dont have 2 operands before -"
       break;
     end
+    operand_2 = stack.pop
+    operand_1 = stack.pop
+    stack.push(operand_1 - operand_2)
+  when "+"
+    if stack.size < 2
+      puts "error: dont have 2 operands before +"
+      break;
+    end
+    operand_2 = stack.pop
+    operand_1 = stack.pop
+    stack.push(operand_1 + operand_2)
+  when "*"
+    if stack.size < 2
+      puts "error: dont have 2 operands before *"
+      break;
+    end
+    operand_2 = stack.pop
+    operand_1 = stack.pop
+    stack.push(operand_1 * operand_2)
+  when "/"
+    if stack.size < 2
+      puts "error: dont have 2 operands before /"
+      break;
+    end
+    operand_2 = stack.pop
+    operand_1 = stack.pop
+    stack.push(operand_1 / operand_2)
+  when "="
+    result = stack.pop
+    break;
   end
-  puts result 
 end
-puts calc
+puts result 
 
