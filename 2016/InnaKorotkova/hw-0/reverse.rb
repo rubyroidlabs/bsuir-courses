f7333da781cc49a6eb965d52d5b94f0132d5f5c4
Inna Korotkova <madsenxx@gmail.com>
2016-10-19 22:10:43 +0300

    whitespaces, ABC & indents fixes

--- 2016/InnaKorotkova/hw-0/reverse.rb
+++ 2016/InnaKorotkova/hw-0/reverse.rb
@@ -4,11 +4,11 @@ operators = ["*", "/", "+", "-", "!"]
 flag = true
 count = 0
 values = []
-stack = [] 
+stack = []
 def bitoperation(value, count)
-  if (code = value.reverse).length > count && count.positive?
+  if (code = value.to_s(2).reverse).length > count && count.positive?
     for i in 0...code.length
-      if count.nonzero? && code[i] == "1" 
+      if count.nonzero? && code[i] == "1"
         code[i] = "0"
         count -= 1
       end
@@ -24,18 +24,18 @@ def operation!(symbol, stack)
   when "-" then stack[stack.length - 1] = stack[stack.length - 1] - var
   when "+" then stack[stack.length - 1] = stack[stack.length - 1] + var
   when "/" then stack[stack.length - 1] = stack[stack.length - 1].to_f / var.to_f
-  when "!" then stack[stack.length - 1] = bitoperation(stack[stack.length - 1].to_i.to_s(2), var)
+  when "!" then stack[stack.length - 1] = bitoperation(stack[stack.length - 1].to_i, var)
   end
 end
 while flag
- if (value = gets.chomp).match(/^\d+$/)
-   values.push(value = value.to_i)
-   count += 1	
- elsif operators.include?(value)
-   count -= 1
-   values.push(value)
-   flag = false if count == 1 || count <= 0
- end
-end 
+  if (value = gets.chomp).match(/^\d+$/)
+    values.push(value = value.to_i)
+    count += 1
+  elsif operators.include?(value)
+    count -= 1
+    values.push(value)
+    flag = false if count == 1 || count <= 0
+  end
+end
 values.each { |x| operators.include?(x) ? operation!(x, stack) : stack.push(x) }
 print stack[0], "\n"
