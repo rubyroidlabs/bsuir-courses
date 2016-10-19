88d2bfc4ec9bad9f18a353ba8ead3afca94eaf52
alexbazhin <alexbazhyn@gmail.com>
2016-10-19 23:26:00 +0300

    fix syntax

--- 2016/AlexBazhin/1/calc.rb
+++ 2016/AlexBazhin/1/calc.rb
@@ -4,19 +4,19 @@ result_stack = []
 stack_expression.each do |element|
   case element
     when '+'
-      result_stack[result_stack.size-2]=result_stack[result_stack.size-2]+result_stack[result_stack.size-1]
-      result_stack.delete_at(result_stack.size-1)
+      result_stack[result_stack.size - 2] = result_stack[result_stack.size - 2] + result_stack[result_stack.size - 1]
+      result_stack.delete_at(result_stack.size - 1)
     when '-'
-      result_stack[result_stack.size-2]=result_stack[result_stack.size-2]-result_stack[result_stack.size-1]
-      result_stack.delete_at(result_stack.size-1)
+      result_stack[result_stack.size - 2] = result_stack[result_stack.size - 2] - result_stack[result_stack.size - 1]
+      result_stack.delete_at(result_stack.size - 1)
     when '*'
-      result_stack[result_stack.size-2]=result_stack[result_stack.size-2]*result_stack[result_stack.size-1]
-      result_stack.delete_at(result_stack.size-1)
+      result_stack[result_stack.size - 2] = result_stack[result_stack.size - 2] * result_stack[result_stack.size - 1]
+      result_stack.delete_at(result_stack.size - 1)
     when '/'
-      result_stack[result_stack.size-2]=result_stack[result_stack.size-2]/result_stack[result_stack.size-1]
-      result_stack.delete_at(result_stack.size-1)
+      result_stack[result_stack.size - 2] = result_stack[result_stack.size - 2] / result_stack[result_stack.size - 1]
+      result_stack.delete_at(result_stack.size - 1)
     else
-      result_stack<<element.to_i
+      result_stack << element.to_i
   end
 end
 puts result_stack
