e6ba8791e1bc7637a767da42ea2ecbf09cb83f56
LeshaAK <qwe@qwe.qwe>
2016-10-19 15:07:31 +0300

    fix styling issues

--- 2016/AlexeiZhuk/1/calculator.rb
+++ 2016/AlexeiZhuk/1/calculator.rb
@@ -1,4 +1,4 @@
-input = ''
+input = ""
 numbers = []
 operator_regex = Regexp.new(%r/[\+\-\*\/]{1,}/)
 
@@ -6,30 +6,30 @@ def opz(operator, numbers)
   b = numbers.pop
   a = numbers.pop
   case operator
-  when '*' then numbers.push(a * b)
-  when '/' then numbers.push(a / b)
-  when '+' then numbers.push(a + b)
-  when '-' then numbers.push(a - b)
+  when "*" then numbers.push(a * b)
+  when "/" then numbers.push(a / b)
+  when "+" then numbers.push(a + b)
+  when "-" then numbers.push(a - b)
   end
 end
 
- loop do
-   if (input = gets.chomp) =~ /\d/
-     numbers.push input.to_i
-   elsif !input.match(operator_regex)
-     puts 'something wrong'
-   else
-     opz(input, numbers)
-     break
-   end
+loop do
+ if (input = gets.chomp) =~ /\d/
+   numbers.push input.to_i
+ elsif !input.match(operator_regex)
+   puts "something wrong"
+ else
+   opz(input, numbers)
+   break
  end
+end
 
 (numbers.count - 1).times do
   operator = gets.chomp
   if input.match(operator_regex)
     opz(operator, numbers)
   else
-    puts 'input operation'
+    puts "input operation"
   end
 end
 puts numbers.first
