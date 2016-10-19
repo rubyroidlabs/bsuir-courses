15155a5be2f717b4818e7ead12766582662f5f78
freq <camvchoke@mail.ru>
2016-10-20 01:30:46 +0300

    fix issues3

--- 2016/AntonTroichyk/1/calc.rb
+++ 2016/AntonTroichyk/1/calc.rb
@@ -7,32 +7,32 @@ def main
       stack.push(tmp.to_f)
     when "-"
       if stack.size < 2
-      puts "error"
-      break;
+        puts "error: dont have 2 operands before -"
+        break;
       end
       operand_2 = stack.pop
       operand_1 = stack.pop
       stack.push(operand_1 - operand_2)
     when "+"
       if stack.size < 2
-      puts "error"
-      break;
+        puts "error: dont have 2 operands before +"
+        break;
       end
       operand_2 = stack.pop
       operand_1 = stack.pop
       stack.push(operand_1 + operand_2)
     when "*"
       if stack.size < 2
-      puts "error"
-      break;
+        puts "error: dont have 2 operands before *"
+        break;
       end
       operand_2 = stack.pop
       operand_1 = stack.pop
       stack.push(operand_1 * operand_2)
     when "/"
       if stack.size < 2
-      puts "error"
-      break;
+        puts "error: dont have 2 operands before /"
+        break;
       end
       operand_2 = stack.pop
       operand_1 = stack.pop
