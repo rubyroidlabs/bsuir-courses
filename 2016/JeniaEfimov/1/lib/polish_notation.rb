bc9e7197f48a254c132c255c89e8a42956b27968
Ubuntu <ser040172z@mail.ru>
2016-10-19 17:47:03 +0300

    houndcy style

--- 2016/JeniaEfimov/1/lib/polish_notation.rb
+++ 2016/JeniaEfimov/1/lib/polish_notation.rb
@@ -1,47 +1,43 @@
 # Reverse Polish notation class
 class PolishNotation
+  def perform_action(symbol, evaluation)
+    case symbol
+    when /\d/
+      evaluation.push(symbol.to_i)
+    when "-", "/", "*", "+"
+      operands = evaluation.pop(2)
+      evaluation.push(operands[0].send(symbol, operands[1]))
+    when '!'
+      operands = evaluation.pop(2)
+      zero_bits(operands[0], operands[1])
+    end
+  end
+
   def evaluate(expression)
-    operands = []
     evaluation = []
     expression.each do |symbol|
-      case symbol
-      when /\d/
-        evaluation.push(symbol.to_i)
-      when "-", "/", "*", "+"
-        operands = evaluation.pop(2)
-        evaluation.push(operands[0].send(symbol, operands[1]))
-      when "!"
-        operands = evaluation.pop(2)
-        zero_bits(operands[0], operands[1])
-      end
+      perform_action(symbol, evaluation)
     end
     puts evaluation
   end
 
   def zero_bits(number, number_of_bits)
-    i = 0
     number = number.to_s(2).reverse
-    number.length.times do
+    number.length.times do |i|
       break if number_of_bits <= 0
-      number[i] = "0", number_of_bits -= 1 if number[i] == "1"
-      i += 1
+      if number[i] == '1'
+        number[i] = '0'
+        number_of_bits -= 1
+      end
     end
     p number.reverse.to_i(2)
   end
 
-  def right_input(expression)
+  def right_input?(expression)
     numbers = 0
     operations = 0
-    expression.each do |symbol|
-      case symbol
-      when /\d/
-        numbers += 1
-      when "-", "/", "*", "+", "!"
-        operations += 1
-      else
-        fail "Expression is not valid"
-      end
-    end
+    expression.each { |symbol| numbers += 1 if symbol =~ /\d/ }
+    expression.each { |symbol| operations += 1 if symbol =~ %r{/[\*\+\/\-\!]/} }
     (numbers - operations) == 1 && expression.size > 1
   end
 end
