a63cda7dcd990dccdcf164a60b74ea3fc0857e12
freq <camvchoke@mail.ru>
2016-10-20 01:24:02 +0300

    fix issues2

--- 2016/AntonTroichyk/1/calc.rb
+++ 2016/AntonTroichyk/1/calc.rb
@@ -3,46 +3,46 @@ def main
   while(true)
     tmp = gets.chomp
     case tmp
-      when /\d/
+    when /\d/
       stack.push(tmp.to_f)
-      when "-"
-        if stack.size < 2
-        puts "error"
-        break;
-        end
+    when "-"
+      if stack.size < 2
+      puts "error"
+      break;
+      end
       operand_2 = stack.pop
       operand_1 = stack.pop
       stack.push(operand_1 - operand_2)
-      when "+"
-        if stack.size < 2
-        puts "error"
-        break;
-        end
+    when "+"
+      if stack.size < 2
+      puts "error"
+      break;
+      end
       operand_2 = stack.pop
       operand_1 = stack.pop
       stack.push(operand_1 + operand_2)
-      when "*"
-        if stack.size < 2
-        puts "error"
-        break;
-        end
+    when "*"
+      if stack.size < 2
+      puts "error"
+      break;
+      end
       operand_2 = stack.pop
       operand_1 = stack.pop
       stack.push(operand_1 * operand_2)
-      when "/"
-        if stack.size < 2
-        puts "error"
-        break;
-        end
+    when "/"
+      if stack.size < 2
+      puts "error"
+      break;
+      end
       operand_2 = stack.pop
       operand_1 = stack.pop
       stack.push(operand_1 / operand_2)
-      when "="
+    when "="
       result = stack.pop
       break;
     end
   end
-puts result 
+  puts result 
 end
 puts main
 
