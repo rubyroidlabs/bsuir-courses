447c34cd256e0186201503ba3490578f76b01fa1
Pavel Zhachko <rammiel@yandex.ru>
2016-10-19 22:14:13 +0300

    fix

--- 2016/PavelZhachko/1/lib/RPNCalculator.rb
+++ 2016/PavelZhachko/1/lib/RPNCalculator.rb
@@ -5,22 +5,22 @@ class RPNCalculator
     tmp_arr = ("0" + first.to_s(2)).split(//)
     index = (second * - 1) - 1
     (index..0).each { |i| tmp_arr[i] = "0" }
-    result = "0" + tmp_arr.join('')
+    result = "0" + tmp_arr.join("")
     result.to_i(2)
   end
 
   def evaluate(rpn)
     array = rpn.split(" ").inject([]) do |stack, i|
       if i =~ /\d+/
-        stack << i.to_i 
+        stack << i.to_i
       else
         b = stack.pop(2)
-          case
-          when i == "+" then stack << b[0] + b[1]
-          when i == "-" then stack << b[0] - b[1]
-          when i == "*" then stack << b[0] * b[1]
-          when i == "/" then stack << b[0] / b[1]
-          when i == "!" then stack << special_func(b[0], b[1])
+          case i
+          when "+" then stack << b[0] + b[1]
+          when "-" then stack << b[0] - b[1]
+          when "*" then stack << b[0] * b[1]
+          when "/" then stack << b[0] / b[1]
+          when "!" then stack << special_func(b[0], b[1])
           end
       end
     end
