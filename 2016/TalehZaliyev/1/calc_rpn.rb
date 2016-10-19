dbeaf2990b070a5b218d66ec8b2d188008616335
Taleh Zaliyev <z.taleh007@ya.ru>
2016-10-19 23:00:50 +0300

    final fixx (but with EVAL)

--- 2016/TalehZaliyev/1/calc_rpn.rb
+++ 2016/TalehZaliyev/1/calc_rpn.rb
@@ -1,15 +1,13 @@
 #!/usr/bin/env ruby
 require "./lib/numeric.rb"
-def calc_rpn(stack = [])
-  loop do
-    case (input = gets.strip)
-    when /^(\d+|\d+[.]\d+)$/ then stack.push(input)
-    when %r{^[-+*\/!]$}
-      input == "!" ? input = ".bit_rep " + stack.pop : input += " " + stack.pop
-      stack.push(eval(stack.pop + ".to_f" + input).to_s)
-      break if stack.size == 1
-    end
+(stack = [])
+loop do
+  case (input = gets.strip)
+  when /^(\d+|\d+[.]\d+)$/ then stack.push(input)
+  when %r{^[-+*\/!]$}
+    input == "!" ? input = ".bit_rep " + stack.pop : input += " " + stack.pop
+    stack.push(eval(stack.pop + ".to_f" + input).to_s)
+    break if stack.size == 1
   end
-  stack
 end
-puts calc_rpn
+puts stack
--- 2016/TalehZaliyev/1/lib/numeric.rb
+++ 2016/TalehZaliyev/1/lib/numeric.rb
@@ -1,10 +1,10 @@
 class Numeric < Object # :nodoc:
   def bit_rep(num_bit)
-    a = self - self.to_i
-    self.to_i.to_s(2).split("").reverse.map do |x|
-      if (x == "1" && num_bit.positive?) then
+    a = self - to_i
+    to_i.to_s(2).split("").reverse.map do |x|
+      if x == "1" && num_bit.positive?
         num_bit -= 1
-        x = "0"
+        "0"
       else
         x
       end
