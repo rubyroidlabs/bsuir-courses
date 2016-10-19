808ad142d2900809759d76bc628e7ee28cf9e3bd
Pavel Zhachko <rammiel@yandex.ru>
2016-10-19 06:16:57 +0300

    post rubocop fix

--- 2016/PavelZhachko/1/RPN_calculator.rb
+++ 2016/PavelZhachko/1/RPN_calculator.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env rubys
-class RPN_Calculator
+class RPNCalculator
   def special_func(first, second)
     tmp_str = "0" + first.to_s(2)
     tmp_array = tmp_str.split(//)
@@ -8,16 +8,16 @@ class RPN_Calculator
       tmp_array[index] = "0"
       index += 1
     end
-    result = "0" + tmp_array.join("")
+    result = "0" + tmp_array.join('')
     result.to_i(2)
   end
   def evaluate(rpn)
-    a = rpn.split(" ")
+    a = rpn.split(' ')
     stack = a.inject([]) do |array, i|
       if i =~ /\d+/
-        array << i.to_i
+        stack << i.to_i
       else
-        b = array.pop(2)
+        b = stack.pop(2)
         case
         when i == "+" then stack << b[0] + b[1]
         when i == "-" then stack << b[0] - b[1]
--- 2016/PavelZhachko/1/homework_1.rb
+++ 2016/PavelZhachko/1/homework_1.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 require_relative "RPN_calculator"
 
-calc = RPN_Calculator.new
+calc = RPNCalculator.new
 p "Input rpn ex - reverse polish notation example"
 a = gets.chomp
 calc.evaluate(a)
