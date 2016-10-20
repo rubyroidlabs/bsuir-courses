1fb017c9cb25f70917737dc7eabff991a27c2a8c
Pavel Zhachko <rammiel@yandex.ru>
2016-10-20 03:17:38 +0300

    ABS metrics fix

--- 2016/PavelZhachko/1/lib/RPNCalculator.rb
+++ 2016/PavelZhachko/1/lib/RPNCalculator.rb
@@ -1,34 +1,43 @@
 #!/usr/bin/env ruby
 # class RPNCalculator
 class RPNCalculator
-  def special_func(first, second)
-    tmp_arr = ("0" + first.to_s(2)).split(//)
-    index = (second * - 1) - 1
-    (index..0).each { |i| tmp_arr[i] = "0" }
-    result = "0" + tmp_arr.join("")
-    result.to_i(2)
-  end
-
-  def evaluate(rpn)
-    array = rpn.split(" ").inject([]) do |stack, i|
-      if i =~ /\d+/
-        stack << i.to_i
-      else
-        case_func(stack, i)
+  def special_func(first, second, key)
+    if key == "!"
+      tmp_arr = ("0" + first.to_s(2)).split(//)
+      index = 0 - second
+      ((index - 1)..0).each do |i|
+        tmp_arr[i] = "0"
+      end
+      return ("0" + tmp_arr.join("")).to_i(2)
+    else
+      case key
+      when "+" then return first + second
+      when "-" then return first - second
+      when "*" then return first * second
+      when "/" then return first / second
       end
     end
-    p array.pop
+    result
   end
 
   def case_func(volume, key)
     b = volume.pop(2)
-    case key
-    when "+" then volume << b[0] + b[1]
-    when "-" then volume << b[0] - b[1]
-    when "*" then volume << b[0] * b[1]
-    when "/" then volume << b[0] / b[1]
-    when "!" then volume << special_func(b[0], b[1])
-    else p "Smthing is wrong. Check input"
+    if !b.empty?
+      special_func(b[0], b[1], key)
+    else
+      "Smthing is wrong. Check input"
     end
+  end
+
+  def evaluate(rpn)
+    array = rpn.split(" ").inject([]) do |stack, i|
+      stack <<
+        if i =~ /\d+/
+          i.to_i
+        else
+          case_func(stack, i)
+        end
+    end
+    p array.pop
   end
 end
