0315c744b94d0eb92455d40ca26f3ec49e878a61
Kirill Vershitsky <kirill.vershitsky@gmail.com>
2016-10-20 10:53:05 +0300

    implement hw 1

---
+++ 2016/KirillVershitsky/1/calculator.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+
+def calc(dig, operation)
+  dig[0].send(operation, dig[1])
+end
+
+def bin_oper(digs)
+  a = digs[0].to_i.to_s(2).reverse!.split('')
+  a.map! do |el|
+    if el == '1' && digs[1] > 0
+      digs[1] -= 1
+      '0'
+    else el
+    end
+  end
+  a.reverse!.join.to_i(2)
+end
+
+puts 'Enter expression separate by {Enter}. Supports integer operands and - / * + ! operators'
+stack = []
+flag = false
+while stack.length > 1 || !flag
+  element = gets.strip
+  if /\d/ =~ element
+    stack.push(element.to_f)
+  elsif %w(+ - / * !).include?(element) && stack.size > 1
+    rez = 0
+    if element != '!'
+      rez = calc(stack.pop(2), element)
+    else
+      rez = bin_oper(stack.pop(2))
+    end
+    stack.push(rez)
+    flag = true
+  else
+    puts 'Isn\'t correct input'
+  end
+end
+puts "#=> #{stack[0]}"
