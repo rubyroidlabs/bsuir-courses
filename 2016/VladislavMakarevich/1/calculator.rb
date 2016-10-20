3422dc6b38ac2c609048197fc705a57ee1e33c5f
Makar <makarmakar4o@gmail.com>
2016-10-20 12:30:06 +0300

    fix style

--- 2016/VladislavMakarevich/1/calculator.rb
+++ 2016/VladislavMakarevich/1/calculator.rb
@@ -6,35 +6,33 @@ stack = []
 $example.gsub(/[\+\*\/\-]|\d+\.?\d*/) { |register| array << register }
 
 def operation(operation, num1, num2)
-  case (operation)
+  case(operation)
     when '+' then
       return num1 + num2
     when '*' then
       return num1 * num2
     when '/' then
-      return num1 / num2
+      return num1 / num2 
     when '-' then
       return num1 - num2
   end
-
 end
 
 array.each do |element|
-  #puts "#{stack} writeStack"
-  #puts "#{element}"
-  #puts "1. #{/\d+\.?\d*/ === element}"
-  #puts "2. #{/[\+\*\/\-]/ === element}"
+  # puts '# {stack} writeStack'
+  # puts '# {element}'
+  # puts '1. # {/\d+\.?\d*/ === element}'
+  # puts '2. # {/[\+\*\/\-]/ === element}'
 
   if /\d+\.?\d*/ === element
-    #puts "element.to_i = #{element.to_i}"
+    # puts 'element.to_i = # {element.to_i}'
     stack << element.to_i
-    #puts "stack: #{stack}"
+    # puts "stack: # {stack}"
   elsif /[\+\*\/\-]/ === element
     a = stack.pop
     b = stack.pop
-    stack << operation(element, b, a )
+    stack << operation(element,b,a)
   end
 end
 
-puts "Result: #{stack.pop}"
-
+puts 'Result: # {stack.pop}'
