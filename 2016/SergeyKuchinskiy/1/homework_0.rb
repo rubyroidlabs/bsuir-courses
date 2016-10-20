da8d79c7d536b3102d7d3837dfce390065fa1f0f
Kuchick <kuchinskiysergeysergeevich@gmail.com>
2016-10-20 11:55:12 +0300

    fixed single quotes

--- 2016/SergeyKuchinskiy/1/homework_0.rb
+++ 2016/SergeyKuchinskiy/1/homework_0.rb
@@ -1,20 +1,20 @@
 def bit_operation(number, bits)
-  bit_float_string = [number].pack('f').unpack('b*')[0]
-  bits.to_i.times { bit_float_string.sub!('1', '0') }
-  [bit_float_string].pack('b*').unpack('f')[0]
+  bit_float_string = [number].pack("f").unpack("b*")[0]
+  bits.to_i.times { bit_float_string.sub!("1", "0") }
+  [bit_float_string].pack("b*").unpack("f")[0]
 end
 
 def operate(sign, first, second)
-  sign == '!' ? bit_operation(second, first) : [second, first].inject(sign)
+  sign == "!" ? bit_operation(second, first) : [second, first].inject(sign)
 end
 
 def numeric?(obj)
   obj.to_s.match(/\A[+-]?\d+(\.\d+)?([eE]\d+)?\Z/).nil? ? false : true
 end
 
-operators = ['+', '-', '*', '/', '!']
+operators = ["+", "-", "*", "/", "!"]
 
-puts 'Start input'
+puts "Start input"
 stack = []
 loop do
   input = gets.chomp
@@ -22,15 +22,15 @@ loop do
     stack << input.to_f
   elsif operators.include?(input)
     if stack.size < 2
-      puts 'There are no operands to calculate'
+      puts "There are no operands to calculate"
       next
     end
     answer = operate(input, stack.pop, stack.pop)
     stack << answer
-  elsif input == ''
+  elsif input == ""
     break if stack.size == 1
   else
-    puts 'Incorrect input'
+    puts "Incorrect input"
     next
   end
 end
