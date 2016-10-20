31ea7f2df1b84ea292f3a35bb21e7c9be96c385d
chenochek <goncharuk_alesya@mail.ru>
2016-10-20 04:45:43 +0300

    hm1 v 5

--- 2016/Goncharuk Alesya/1/calculator.rb
+++ 2016/Goncharuk Alesya/1/calculator.rb
@@ -33,24 +33,24 @@ quantity_numbers += 1
 stack_numbers = []
 stack_symbols = []
 stack_numbers.push(symbol)
-if (flag_input_item == true)
+if flag_input_item
   loop do
     symbol = gets.chomp
-      if !symbol.to_i.zero?
-        quantity_numbers += 1
-        stack_numbers.push(symbol.to_i)
-      elsif quantity_numbers < 2
-        puts"Сейчас должно следовать число."
-        puts"Убедитесь,что вы знакомы с постфиксной записью числа"
-        puts"Ну-ка соберитесь и попроуйте ввести снова значение"
+    if !symbol.to_i.zero?
+      quantity_numbers += 1
+      stack_numbers.push(symbol.to_i)
+    elsif quantity_numbers < 2
+      puts"Сейчас должно следовать число."
+      puts"Убедитесь,что вы знакомы с постфиксной записью числа"
+      puts"Ну-ка соберитесь и попроуйте ввести снова значение"
       elsif symbol.to_s == "+" || symbol.to_s == "-" || symbol.to_s == "*" || symbol.to_s == "/" || symbol.to_s == "!"
         quantity_symbols += 1
         stack_symbols.push(symbol)
         break
-      else
-        puts"Вы ввели какую-то ерунду"
-        puts"Попробуйте снова ввести допустимое значение"
-      end
+    else
+      puts"Вы ввели какую-то ерунду"
+      puts"Попробуйте снова ввести допустимое значение"
+    end
   end
   while quantity_symbols != quantity_numbers - 1
     symbol = gets.chomp
@@ -61,23 +61,23 @@ if (flag_input_item == true)
       puts"Соберитесь и начните вводить нормальные значения"
     end
   end
-until stack_symbols.empty? 
-   first_operand = stack_numbers.pop
-   second_operand = stack_numbers.pop
-   operation = stack_symbols.shift
-   case operation
-   when "+"
-     result = first_operand.to_i + second_operand.to_i
-   when "-"
-     result = second_operand.to_i - first_operand.to_i
-   when "*"
-     result = first_operand.to_i * second_operand.to_i
-   when "/"
-     result = second_operand.to_i / first_operand.to_i
-   when "!"
-     result = reset_bits second_operand.to_i, first_operand.to_i
-   end
-   stack_numbers.push(result)
-end
-puts"Ответ #{result}"
+  until stack_symbols.empty?
+    first_operand = stack_numbers.pop
+    second_operand = stack_numbers.pop
+    operation = stack_symbols.shift
+    case operation
+    when "+"
+      result = first_operand.to_i + second_operand.to_i
+    when "-"
+      result = second_operand.to_i - first_operand.to_i
+    when "*"
+      result = first_operand.to_i * second_operand.to_i
+    when "/"
+      result = second_operand.to_i / first_operand.to_i
+    when "!"
+      result = reset_bits second_operand.to_i, first_operand.to_i
+    end
+    stack_numbers.push(result)
+  end
+  puts"Ответ #{result}"
 end
