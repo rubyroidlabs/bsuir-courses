ad5e15b82bc949f97300b84f054f16c8718fce00
Dmitry Ryzhikov <d.ryzhykau@gmail.com>
2016-10-20 05:18:49 +0300

    Task ready

---
+++ 2016/DmitryRyzhikov/1/calculator.rb
@@ -0,0 +1,52 @@
+#!/usr/bin/env ruby -w
+
+def int?(str)
+  /^\-?\d*$/ =~ str ? true : false
+end
+
+def float?(str)
+  /^\-?\d*\.\d*$/ =~ str ? true : false
+end
+
+def operator?(str)
+  %r'^(\+|\-|\*{1,2}|/{1,2}|!)$' =~ str ? true : false
+end
+
+def execute_operator(a, b, op_line)
+  if op_line != '!'
+    eval "#{a} #{op_line} #{b}"
+  elsif a.is_a? Integer
+    bin = a.to_s(2)
+    b.times { bin[bin.rindex('1')] = '0' }
+    bin.to_i(2)
+  elsif a.is_a? Float
+    bin = [a].pack('g').bytes.map { |byte| '%08b'.format(byte) }.join
+    b.times { bin[bin.rindex('1')] = '0' }
+    bin.scan(/.{8}/).map { |s| s.to_i(2).chr }.join.unpack('g')
+  end
+end
+
+stack = []
+
+loop do
+  line = gets.strip
+
+  break if line == 'exit'
+
+  if int? line
+    stack.push(line.to_i)
+  elsif float? line
+    stack.push(line.to_f)
+  elsif operator? line
+    if stack.size < 2
+      puts 'Not enough numbers.'
+    else
+      b = stack.pop
+      a = stack.pop
+      stack.push(execute_operator(a, b, line))
+      puts "#=> #{stack.pop}" if stack.size == 1
+    end
+  else
+    puts 'Invalid input! Please, try again.'
+  end
+end
