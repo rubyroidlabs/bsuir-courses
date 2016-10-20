b51a4d0022a377a72c76f5b851cf2a3f4ad2d927
Alexandr Kolesenko <aikb2b@gmail.com>
2016-10-21 01:43:26 +0300

    fixes to cajole hound

--- 2016/AlexandrKolesenko/task0/task0.rb
+++ 2016/AlexandrKolesenko/task0/task0.rb
@@ -36,7 +36,7 @@ class RpnCalc
       "-" => { action: proc { |x, y| x - y } },
       "*" => { action: proc { |x, y| x * y } },
       "/" => { action: proc { |x, y| x / y } }
-    }
+     }
   end
 
   loop do
