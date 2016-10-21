985476ce17448c590956ffeaba14ecdd044ec8b4
kemchik <kerminsk@gmail.com>
2016-10-21 22:10:43 +0300

    second

--- 2016/KiraEmchik/1/calculator.rb
+++ 2016/KiraEmchik/1/calculator.rb
@@ -1,17 +1,17 @@
-def is_int?(str)
+def int?(str)
   !!(str =~ /^[-+]?[1-9]([0-9]*)?$/)
 end
 
-def reset_units (numbers, res)
+def reset_units(numbers, res)
   amount_numbers = 0
   number_of_units = 0
   argument = numbers.pop
   last_number_in_mas = argument.to_s(2).size - 1
   while number_of_units != res.to_i
-    number_of_units += 1 if argument.to_s(2)[last_number_in_mas - amount_numbers] == '1'
+    number_of_units += 1 if argument.to_s(2)[last_number_in_mas - amount_numbers] == "1"
     amount_numbers += 1
   end
-  (argument.to_s(2)[0..(last_number_in_mas - amount_numbers)] + '0' * amount_numbers).to_i(base = 2)
+  (argument.to_s(2)[0..(last_number_in_mas - amount_numbers)] + "0" * amount_numbers).to_i(base = 2)
 end
 
 def run
@@ -20,31 +20,29 @@ def run
 
   loop do
     number = gets.chomp
-
-    if is_int?(number)
+    if int?(number)
       numbers << number.to_i
-    elsif number.include?('+ - * !')
+    elsif %w(+ - * ! /).include?(number)
       signs << number
-      return p 'Error' if numbers.size < 2
+      return p "Error" if numbers.size < 2
       break if signs.size == numbers.size - 1
     else
-      return p 'Error'
+      return p "Error"
     end
   end
 
   res = numbers.pop
-  for n in 0..signs.length - 1
-    res =
-        case signs[n]
-        when '+'
+  signs.each do |n|
+    res = case n
+        when "+"
           res + numbers.pop
-        when '-'
+        when "-"
           numbers.pop - res
-        when '*'
+        when "*"
           res * numbers.pop
-        when '/'
+        when "/"
           numbers.pop / res
-        when '!'
+        when "!"
           reset_units(numbers, res)
         end
   end
