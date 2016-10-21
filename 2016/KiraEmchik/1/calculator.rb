cec03bbc73b7baec10697230d97bdeb90446bd85
kemchik <kerminsk@gmail.com>
2016-10-21 21:13:12 +0300

    first homework

---
+++ 2016/KiraEmchik/1/calculator.rb
@@ -0,0 +1,54 @@
+def is_int?(str)
+  !!(str =~ /^[-+]?[1-9]([0-9]*)?$/)
+end
+
+def reset_units (numbers, res)
+  amount_numbers = 0
+  number_of_units = 0
+  argument = numbers.pop
+  last_number_in_mas = argument.to_s(2).size - 1
+  while number_of_units != res.to_i
+    number_of_units += 1 if argument.to_s(2)[last_number_in_mas - amount_numbers] == '1'
+    amount_numbers += 1
+  end
+  (argument.to_s(2)[0..(last_number_in_mas - amount_numbers)] + '0' * amount_numbers).to_i(base = 2)
+end
+
+def run
+  numbers = []
+  signs = []
+
+  loop do
+    number = gets.chomp
+
+    if is_int?(number)
+      numbers << number.to_i
+    elsif number.include?('+ - * !')
+      signs << number
+      return p 'Error' if numbers.size < 2
+      break if signs.size == numbers.size - 1
+    else
+      return p 'Error'
+    end
+  end
+
+  res = numbers.pop
+  for n in 0..signs.length - 1
+    res =
+        case signs[n]
+        when '+'
+          res + numbers.pop
+        when '-'
+          numbers.pop - res
+        when '*'
+          res * numbers.pop
+        when '/'
+          numbers.pop / res
+        when '!'
+          reset_units(numbers, res)
+        end
+  end
+  p res
+end
+
+run
