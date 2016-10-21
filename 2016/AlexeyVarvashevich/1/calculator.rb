7ac85c022cf22a5c648bc7444af571863f8f36e9
Amix <RHL@tut.by>
2016-10-21 22:02:38 +0300

    RPN-Calc (HW1)

---
+++ 2016/AlexeyVarvashevich/1/calculator.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+def calc(dig, op)
+  dig[0].send(op, dig[1])
+end
+
+puts "Enter expression separated by {Enter}. Supported operators '+ - / *'."
+stack = []
+flag = false
+while stack.length > 1 || !flag
+  element = gets.strip
+  if /\d/ =~ element
+   stack.push(element.to_f)
+  elsif %w(+ - / * !).include?(element) && stack.size > 1
+   result = calc(stack.pop(2), element)	
+   stack.push(result)
+   flag = true
+  else puts "Incorrect input.Enter the correct value."
+  end 
+end
+
+if stack[0].to_s.split('.')[1]=="0" 
+  puts "Result is #{stack[0].to_i}"
+else
+  puts "Result is #{stack[0]}"
+end
+
