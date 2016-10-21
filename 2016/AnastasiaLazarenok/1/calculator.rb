d555bf8c71eb4506e2e10c6fef9adf596e2dcdfb
AnastasiaLazarenok <AnastasiaLazarenok@gmail.com>
2016-10-21 11:44:14 +0300

    fix 2

--- 2016/AnastasiaLazarenok/1/calculator.rb
+++ 2016/AnastasiaLazarenok/1/calculator.rb
@@ -5,11 +5,11 @@ loop do
 	a = gets.chomp
 	if a =~ /\d/
 		stack.push(a.to_i)
-		number+=1
+		number += 1
 	elsif (%w[* - + / ].include?(a)) && stack.size != 1
 		x1 = (stack.pop).to_i
 		x2 = (stack.pop).to_i
-		number-=1
+		number -= 1
 		case a
 		when '+'
 			result = x2 + x1
