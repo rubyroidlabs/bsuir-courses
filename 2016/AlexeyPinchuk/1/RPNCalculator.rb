b7086813f5f1983e9d40205295d3948116d637e7
PinchukAA <juggler7@mail.ru>
2016-10-23 21:23:01 +0300

    Update RPNCalculator.rb

--- 2016/AlexeyPinchuk/1/RPNCalculator.rb
+++ 2016/AlexeyPinchuk/1/RPNCalculator.rb
@@ -14,9 +14,7 @@ expression.map do |x|
   end
 end
 
-if numbers.size != operations.size + 1
-  puts "Incorrect expression!"
-end
+puts "Incorrect expression!" if numbers.size != operations.size + 1
 
 operations = operations.reverse
 
@@ -24,41 +22,40 @@ while numbers.size != 1
   second_operand = numbers.pop
   first_operand = numbers.pop
 
-case operations.pop
-when /\+/ then numbers.push(first_operand + second_operand)
-when /\-/ then numbers.push(first_operand - second_operand)
-when /\*/ then numbers.push(first_operand * second_operand)
-when %r{\/} then
-  if second_operand.zero?
-    puts "ivision by zero!"
-    exit
-  else
-    numbers.push(first_operand.to_f / second_operand.to_f)
-  end
-when /\!/ then
-  while first_operand.positive?
-    bits.push(first_operand % 2)
-    first_operand /= 2
-  end
+  case operations.pop
+  when /\+/ then numbers.push(first_operand + second_operand)
+  when /\-/ then numbers.push(first_operand - second_operand)
+  when /\*/ then numbers.push(first_operand * second_operand)
+  when %r{\/} then
+    if second_operand.zero?
+      puts "ivision by zero!"
+      exit
+    else
+      numbers.push(first_operand.to_f / second_operand.to_f)
+    end
+  when /\!/ then
+    while first_operand.positive?
+      bits.push(first_operand % 2)
+      first_operand /= 2
+    end
 
-  i = 0
-  j = 0
-  while i < second_operand
-    if bits[j].nonzero?
-      bits[j] = 0
+    i = 0
+    j = 0
+    while i < second_operand
+      if bits[j].nonzero?
+        bits[j] = 0
+        i += 1
+      end
+      j += 1
+    end
+
+    i = 0
+    result_operand = 0
+    while i < bits.size
+      result_operand += bits[i] * (2**i)
       i += 1
     end
-    j += 1
+    numbers.push(result_operand)
   end
-
-  i = 0
-  result_operand = 0
-  while i < bits.size
-    result_operand += bits[i] * (2**i)
-    i += 1
-  end
-  numbers.push(result_operand)
-end
 end
 puts "Result: " + numbers.pop.to_s
-
