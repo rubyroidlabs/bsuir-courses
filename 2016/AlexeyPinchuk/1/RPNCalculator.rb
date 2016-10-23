070277d519aded93224b0329b1ea9dec1c5b02be
PinchukAA <juggler7@mail.ru>
2016-10-23 22:37:27 +0300

    Update RPNCalculator.rb

--- 2016/AlexeyPinchuk/1/RPNCalculator.rb
+++ 2016/AlexeyPinchuk/1/RPNCalculator.rb
@@ -1,45 +1,73 @@
 #!/usr/bin/env ruby
-$numbers = []
-$operations = []
+@numbers = []
+@operations = []
 
-def zeroBit(first_operand, second_operand)
+def num_to_bit(f_op, s_op)
   bits = []
-  while first_operand.positive?
-    bits.push(first_operand % 2)
-    first_operand /= 2
+  while f_op.positive?
+    bits.push(f_op % 2)
+    f_op /= 2
   end
+  zero_bit(bits, s_op)
+end
 
+def zero_bit(bits, s_op)
   i = 0
   j = 0
-  while i < second_operand
+  while i < s_op
     if bits[j].nonzero?
       bits[j] = 0
       i += 1
     end
     j += 1
   end
+  bit_to_num(bits)
+end
 
+def bit_to_num(bits)
   i = 0
-  result_operand = 0
+  r_op = 0
   while i < bits.size
-    result_operand += bits[i] * (2**i)
+    r_op += bits[i] * (2**i)
     i += 1
   end
-  $numbers.push(result_operand)
-end 
+  @numbers.push(r_op)
+end
 
 expression = gets.chomp
 
 expression = expression.split(" ")
 expression.map do |x|
   if /[0-9]/ =~ x
-    $numbers.push(x.to_i)
+    @numbers.push(x.to_i)
   elsif %r{\+|\*|\/|\-|\!} =~ x
-    $operations.push(x.to_sym)
+    @operations.push(x.to_sym)
   end
 end
 
-puts "Incorrect expression!" if $numbers.size != $operations.size + 1
+puts "Incorrect expression!" if @numbers.size != @operations.size + 1
+
+@operations = @operations.reverse
+
+while @numbers.size != 1
+  s_op = @numbers.pop
+  f_op = @numbers.pop
+
+  case @operations.pop
+  when /\+/ then @numbers.push(f_op + s_op)
+  when /\-/ then @numbers.push(f_op - s_op)
+  when /\*/ then @numbers.push(f_op * s_op)
+  when %r{\/} then
+    if s_op.zero?
+      puts "Division by zero!"
+      exit
+    else
+      @numbers.push(f_op.to_f / s_op.to_f)
+    end
+  when /\!/ then num_to_bit(f_op, s_op)
+  end
+end
+puts "Result: " + @numbers.pop.to_s
 
 $operations = $operations.reverse
 
