d8b86798dda8fb65f199c18daa6c0d078183c051
prudnikoff <you@example.com>
2016-10-24 21:30:13 +0300

    fixed_5

--- 2016/Vlad_Prudnikov/1/calculator_ruby.rb
+++ 2016/Vlad_Prudnikov/1/calculator_ruby.rb
@@ -1,14 +1,11 @@
-def binary(num, power)
-  counter1 = 0
-  counter2 = 0
+def binary(num, power, counter1, counter2)
   l_num = num
   while counter1 < power && num > 0
-    div_mod = l_num.divmod(2)
-    if div_mod[1] == 1
+    if l_num.divmod(2)[1] == 1
       num -= 2**counter2
       counter1 += 1
     end
-    l_num = div_mod[0]
+    l_num = l_num.divmod(2)[0]
     counter2 += 1
   end
   num
@@ -16,20 +13,20 @@ end
 oper_arr = ["+\n", "-\n", "*\n", "/\n", "!\n"]
 stack = []
 k = 0
-loop do 
+loop do
   local = gets
   if oper_arr.include?(local.to_s)
-    case local.to_s 
+    case local.to_s
     when "+\n"
       stack[-2] = stack[-2] + stack[-1]
-    when "-\n" 
+    when "-\n"
       stack[-2] = stack[-2] - stack[-1]
-    when "*\n" 
+    when "*\n"
       stack[-2] = stack[-2] * stack[-1]
-    when "/\n" 
+    when "/\n"
       stack[-2] = stack[-2] / stack[-1]
-    when "!\n" 
-      stack[-2] = binary stack[-2], stack[-1]
+    when "!\n"
+      stack[-2] = binary stack[-2], stack[-1], 0, 0
     end
     stack.delete_at(stack.length - 1)
   else
