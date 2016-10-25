59d6520d9e5ea0ac3ff0242e84c1214cd95972d3
storm2513 <storm25132513@gmail.com>
2016-10-26 01:32:49 +0300

    Update calculator.rb

--- 2016/MaksimShylau/1/calculator.rb
+++ 2016/MaksimShylau/1/calculator.rb
@@ -12,26 +12,20 @@ def oper(symbol, a, b)
   when "!" then nul_bin(a.to_s(2), b).to_i(2)
   end
 end
-
-def calculate 
-  a = []
-  i = -1
-  loop do
-    i += 1
-    a[i] = gets.chomp
+a = []
+i = -1
+loop do
+  i += 1
+  a[i] = gets.chomp
   break if a[i] == "+" || a[i] == "-" || a[i] == "*" || a[i] == "/" || a[i] == "!"
-  end
-  j = 0
-  res = oper(a[i], a[i - 2].to_i, a[i - 1].to_i)
-  ind = i
-  1.upto(a.size - 3) do
+end
+j = 0
+res = oper(a[i], a[i - 2].to_i, a[i - 1].to_i)
+ind = i
+1.upto(a.size - 3) do
   j += 1
   a[i + j] = gets.chomp
   res = oper(a[i + j], a[ind - 3].to_i, res)
   ind -= 1
-  end
-  return res
 end
-
-res = calculate
 puts "#=> " + res.to_s
