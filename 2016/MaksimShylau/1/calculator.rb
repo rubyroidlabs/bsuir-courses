910437f558078f63df61bd7bafac3a6568fdad99
storm2513 <storm25132513@gmail.com>
2016-10-26 16:42:07 +0300

    Update calculator.rb

--- 2016/MaksimShylau/1/calculator.rb
+++ 2016/MaksimShylau/1/calculator.rb
@@ -25,12 +25,12 @@ end
 a = []
 i = get_nums(a)
 j = 0
-  res = oper(a[i], a[i - 2].to_i, a[i - 1].to_i)
-  ind = i
-  1.upto(a.size - 3) do
-    j += 1
-    a[i + j] = gets.chomp
-    res = oper(a[i + j], a[ind - 3].to_i, res)
-    ind -= 1
-  end
+res = oper(a[i], a[i - 2].to_i, a[i - 1].to_i)
+ind = i
+1.upto(a.size - 3) do
+  j += 1
+  a[i + j] = gets.chomp
+  res = oper(a[i + j], a[ind - 3].to_i, res)
+  ind -= 1
+end
 puts "#=> " + res.to_s
