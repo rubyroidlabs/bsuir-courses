f96dc0ba986088e26f922073028654d84605f934
Pavel Kozik <pashakozik3123736@gmail.com>
2016-10-28 21:31:20 +0200

    Update calculator.rb

--- 2016/PavelKozik/1/calculator.rb
+++ 2016/PavelKozik/1/calculator.rb
@@ -1,4 +1,4 @@
-# Partial RPilishCalc
+# Partial RPolishCalc
 class RPolishCalculator
 def initialize
   @operands = []
@@ -35,28 +35,28 @@ end
 
 private
 
-  def operation?(sign)
-    sign =~ %r{[+\-*/!]}
-  end
+def operation?(sign)
+  sign =~ %r{[+\-*/!]}
+end
 
-  def number?(symbol)
-    symbol =~ /^[-+]?[0-9]+$/
-  end
+def number?(symbol)
+  symbol =~ /^[-+]?[0-9]+$/
+end
 
-  def add_as_operand(operand)
-    @operands.push(operand.to_i)
-  end
+def add_as_operand(operand)
+  @operands.push(operand.to_i)
+end
 
-  def perform_operation(sign)
-    sum = @operands.pop(2)
-    if sign == "!"
-      @operands. push(operation_with_bits(sum[0], sum[1]))
-    else
-      @operands. push(send_operation(sum[0], sum[1], sign))
-    end
+def perform_operation(sign)
+  sum = @operands.pop(2)
+  if sign == "!"
+    @operands. push(operation_with_bits(sum[0], sum[1]))
+  else
+    @operands. push(send_operation(sum[0], sum[1], sign))
   end
+end
 
-  def send_operation(first, second, operation)
-    first.send(operation, second)
-  end
+def send_operation(first, second, operation)
+  first.send(operation, second)
+end
 end
