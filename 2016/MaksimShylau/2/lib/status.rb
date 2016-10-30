cb998340d06e1e23261bc28780a2849fa19008c2
storm2513 <storm25132513@gmail.com>
2016-10-31 00:20:23 +0300

    Style fixes

--- 2016/MaksimShylau/2/lib/status.rb
+++ 2016/MaksimShylau/2/lib/status.rb
@@ -9,9 +9,7 @@ class Status < Command
     time_start = Time.new(sem_start.year, sem_start.month, sem_start.day)
     time_end = Time.new(sem_end.year, sem_end.month, sem_end.day)
     time_now = Time.new
-    k = ((time_now - time_start).to_f) / (time_end-time_start).to_f
-    to_do = k * labs_count.to_i
-    to_do = to_do.to_i + 1
+    to_do = (((time_now - time_start).to_f) / (time_end-time_start).to_f * labs_count.to_i).to_i + 1
   if to_do > labs_count.to_i
 	  to_do = labs_count.to_i
 	elsif to_do.negative?
