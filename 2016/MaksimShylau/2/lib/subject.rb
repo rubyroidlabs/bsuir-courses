5fbfb82a9ee445995e42a840ccc99a66316eb5bc
storm2513 <storm25132513@gmail.com>
2016-10-31 00:40:02 +0300

    Style fixes

--- 2016/MaksimShylau/2/lib/subject.rb
+++ 2016/MaksimShylau/2/lib/subject.rb
@@ -1,11 +1,11 @@
 # It can send messages
 class Subject < Command
   def initialize(bot, message, name)
-	super(bot, message)
-	@name = name
-	@labs_count = nil
-	@done = 0
-	@to_do = 0
+    super(bot, message)
+    @name = name
+    @labs_count = nil
+    @done = 0
+    @to_do = 0
   end
   attr_accessor :name
   attr_accessor :labs_count
