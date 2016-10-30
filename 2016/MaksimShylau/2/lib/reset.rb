7c0c74a224e9748f31e69f22b25e9acb5c5975c6
storm2513 <storm25132513@gmail.com>
2016-10-31 00:39:11 +0300

    Style fixes

--- 2016/MaksimShylau/2/lib/reset.rb
+++ 2016/MaksimShylau/2/lib/reset.rb
@@ -1,8 +1,8 @@
 # It can send messages
 class Reset < Command
   def initialize(bot, message, redis, id)
-	super(bot, message)
-	hash = {}
+    super(bot, message)
+    hash = {}
     redis.set(id, hash.to_json)
     send_message("Все данные удалены")
   end
