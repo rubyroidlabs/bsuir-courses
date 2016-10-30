b5d588ea58ad0a73a16dbb9c5b6c82c8aa9b3135
storm2513 <storm25132513@gmail.com>
2016-10-31 00:37:12 +0300

    Style fixes

--- 2016/MaksimShylau/2/lib/semester.rb
+++ 2016/MaksimShylau/2/lib/semester.rb
@@ -2,8 +2,8 @@
 class Semester < Command
   def initialize(bot, message)
     super(bot, message)
-    sem_start = nil
-    sem_end = nil
+    @sem_start = nil
+    @sem_end = nil
   end
   attr_accessor :sem_start
   attr_accessor :sem_end
