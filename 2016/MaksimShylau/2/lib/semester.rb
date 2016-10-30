8c9ebb459ceab7a9a83dfa0187babce6011c8f3b
storm2513 <storm25132513@gmail.com>
2016-10-31 00:35:52 +0300

    Style fixes

--- 2016/MaksimShylau/2/lib/semester.rb
+++ 2016/MaksimShylau/2/lib/semester.rb
@@ -1,9 +1,9 @@
 # It can send messages
 class Semester < Command
-	def initialize(bot, message)
-		super(bot, message)
-		sem_start = nil
-		sem_end = nil
+  def initialize(bot, message)
+    super(bot, message)
+    sem_start = nil
+    sem_end = nil
   end
   attr_accessor :sem_start
   attr_accessor :sem_end
