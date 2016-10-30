07c8ea893cad6a812539b79d82412bcf05a56b7d
storm2513 <storm25132513@gmail.com>
2016-10-31 00:29:07 +0300

    Style fixes

--- 2016/MaksimShylau/2/lib/command.rb
+++ 2016/MaksimShylau/2/lib/command.rb
@@ -1,10 +1,10 @@
 class Command
-	def initialize(bot, message)
-		@bot = bot
-		@message = message
-	end
+  def initialize(bot, message)
+	@bot = bot
+	@message = message
+  end
 
-	def send_message(send_text)
+  def send_message(send_text)
     @bot.api.send_message(chat_id: @message.chat.id, parse_mode: "Markdown", text: send_text)
   end
 end
