585d5a39b541ec50ef0d735da0ceb40d36fdcd48
storm2513 <storm25132513@gmail.com>
2016-10-31 18:41:56 +0300

    Style fixes

--- 2016/MaksimShylau/2/lib/status.rb
+++ 2016/MaksimShylau/2/lib/status.rb
@@ -5,7 +5,7 @@ class Status < Command
     @str = []
     @to_do = nil
   end
-  
+
   def how_much?(time_now, time_end, time_start, count)
     @to_do = (((time_now - time_start).to_f) / (time_end - time_start).to_f * count.to_i).to_i + 1
     if @to_do > count.to_i
