e776e347a15c488c8ec75feb42e780ecd2b03eea
storm2513 <storm25132513@gmail.com>
2016-10-31 09:07:11 +0300

    Style fixes

--- 2016/MaksimShylau/2/lib/parse_date.rb
+++ 2016/MaksimShylau/2/lib/parse_date.rb
@@ -49,23 +49,29 @@ class DateParser
   end
 
   def self.is_correct?(date)
-    if (date =~ /\d\d.\d\d.\d\d\d\d$/).nil? then return false end
+    if (date =~ /\d\d.\d\d.\d\d\d\d$/).nil? then false end
     time_ar = []
-    if date.index(".").nil? then return false end
+    if date.index(".").nil? then false end
     0.upto(2) do |i|
     time_ar[i] = date[0..date.index(".").to_i - 1]
     date = date[(date.index(".").to_i + 1)..date.size]
+    end
+	  day = time_ar[0].to_i
+	  month = time_ar[1].to_i
+	  year = time_ar[2].to_i
+	  if day <= 0 || day > 31 || month <= 0 || month > 12 || (Time.now.year - year).abs > 1
+	  	false
+	  else
+	  	true
+	  end
   end
-  day = time_ar[0].to_i
-  month = time_ar[1].to_i
-  year = time_ar[2].to_i
-  if day <= 0 || day > 31 || month <= 0 || month > 12 || (Time.now.year - year).abs > 1 then return false end
-  return true
-  end
+
   def self.correct_count?(labs_count)
-  if labs_count.to_i <= 0 || labs_count.to_i > 25 then return false end
-  return true
-  end
+	  if labs_count.to_i <= 0 || labs_count.to_i > 25
+	  	false
+	  else
+	    true
+	end
   attr_accessor :day
   attr_accessor :month
   attr_accessor :year
