f07557cb117be411f9d2eeed3db111ec2686601e
storm2513 <storm25132513@gmail.com>
2016-10-31 09:09:23 +0300

    Style fixes

--- 2016/MaksimShylau/2/lib/db.rb
+++ 2016/MaksimShylau/2/lib/db.rb
@@ -1,7 +1,7 @@
 class Database
-	def initialize(id)
-		user = User.new(id)
-		@redis = Redis.new(url: ENV["redis://h:phl40bf5cs3286b1h8pflrroll@ec2-46-51-184-223.eu-west-1.compute.amazonaws.com:8139"])
+  def initialize(id)
+    user = User.new(id)
+    @redis = Redis.new(url: ENV["redis://h:phl40bf5cs3286b1h8pflrroll@ec2-46-51-184-223.eu-west-1.compute.amazonaws.com:8139"])
     if !@redis.get(user.id).nil?
       hash = JSON.parse(@redis.get(user.id)).to_hash
     else hash = {}
@@ -15,16 +15,16 @@ class Database
     if subject_count.nil? then subject_count = 0; hash["subject_count"]= 0 end
     @redis.set(user.id, hash.to_json)
     return @redis
-	end
+  end
 
-	def get_hash(id)
-		hash = JSON.parse(@redis.get(id)).to_hash
-		return hash
-	end
+  def get_hash(id)
+    hash = JSON.parse(@redis.get(id)).to_hash
+    return hash
+  end
 
-	def set_hash(hash, id)
-		@redis.set(id, hash.to_json)
-	end
+  def set_hash(hash, id)
+    @redis.set(id, hash.to_json)
+  end
 
-	attr_accessor :redis
+  attr_accessor :redis
 end
