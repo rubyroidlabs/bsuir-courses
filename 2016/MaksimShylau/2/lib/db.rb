863e823f4023f2b1f4d83235810d41c34d4b28ea
storm2513 <storm25132513@gmail.com>
2016-10-31 18:12:36 +0300

    Style fixes

--- 2016/MaksimShylau/2/lib/db.rb
+++ 2016/MaksimShylau/2/lib/db.rb
@@ -4,7 +4,7 @@ class Database
     user = User.new(id)
     @redis = Redis.new(url: ENV["redis://h:phl40bf5cs3286b1h8pflrroll@ec2-46-51-184-223.eu-west-1.compute.amazonaws.com:8139"])
     hash = if !@redis.get(user.id).nil?
-      JSON.parse(@redis.get(user.id)).to_hash
+    JSON.parse(@redis.get(user.id)).to_hash
     else {}
     end
     if hash["subject"].nil? then hash["subject"] = [{}] end
