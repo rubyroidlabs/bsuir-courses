db7029cf27a2beb87e479ffe487723b7d083c6bb
aik-stok <aikb2b@gmail.com>
2016-11-02 02:45:14 +0300

    fix lexis

--- 2016/AlexandrKolesenko/Task2 /logic.rb
+++ 2016/AlexandrKolesenko/Task2 /logic.rb
@@ -1,32 +1,30 @@
 #method returns how much time you have
 def countdown(bgn_d, end_d)
-    today = Date.today
-    if end_d > today
-      @eta = (end_d - today).to_i 
-      @sum_of_days = (end_d - bgn_d).to_i
-      true
-      else
-      false
-    end
+  today = Date.today
+  if end_d > today
+    @eta = (end_d - today).to_i 
+    @sum_of_days = (end_d - bgn_d).to_i
+    true
+  else
+    false
   end
-#method returns how much tasks you should accomplished already 
-def taskcalc(tasks)
- days_per_task = @sum_of_days / tasks
- days_gone = @sum_of_days - @eta
- @accomplished = days_gone / days_per_task
 end
 
-
+#method returns how much tasks you should accomplished already 
+def taskcalc(tasks)
+  days_per_task = @sum_of_days / tasks
+  days_gone = @sum_of_days - @eta
+  @accomplished = days_gone / days_per_task
+end
 
 #method checks input is date and if its valid in terms of calendar
 def v_date?(date_string)
   begin
-  x = Date.parse(date_string).to_s
-  y, m, d = x.split '-'
-  Date.valid_date? y.to_i, m.to_i, d.to_i
+   x = Date.parse(date_string).to_s
+   y, m, d = x.split '-'
+   Date.valid_date? y.to_i, m.to_i, d.to_i
   return true
   rescue
-  return false
- end
+    return false
+  end
 end
-
