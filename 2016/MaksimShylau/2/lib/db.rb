c8291bb4047bc00d2dc92fa86041ae3774cceb08
storm2513 <storm25132513@gmail.com>
2016-11-01 08:20:48 +0300

    Style fixes

--- 2016/MaksimShylau/2/lib/db.rb
+++ 2016/MaksimShylau/2/lib/db.rb
@@ -3,14 +3,19 @@ class Database
   def initialize(id)
     user = User.new(id)
     @redis = Redis.new(url: ENV["redis://h:phl40bf5cs3286b1h8pflrroll@ec2-46-51-184-223.eu-west-1.compute.amazonaws.com:8139"])
+    hash = get_std_hash(user)
+    @redis.set(user.id, hash.to_json)
+    @redis
+  end
+
+  def get_std_hash(user)
     hash = if !@redis.get(user.id).nil?
              JSON.parse(@redis.get(user.id)).to_hash
              else {}
            end
-    if hash["subject"].nil? then hash["subject"] = [{}] end
-    if hash["subject_count"].nil? then hash["subject_count"] = 0 end
-    @redis.set(user.id, hash.to_json)
-    @redis
+    hash["subject"] = [{}] if hash["subject"].nil?
+    hash["subject_count"] = 0 if hash["subject_count"].nil?
+    hash
   end
 
   def get_hash(id)
