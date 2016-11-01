74a023145a5ef519af969fe1ac4aff949b76e995
aik-stok <aikb2b@gmail.com>
2016-11-02 01:44:58 +0300

    fix lexis

--- 2016/AlexandrKolesenko/Task2 /start_bot.rb
+++ 2016/AlexandrKolesenko/Task2 /start_bot.rb
@@ -8,18 +8,17 @@ token = 'YOUR_TELEGRAM_TOKEN'
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
 
-  
     case message.text
     when "/start"
       bot.api.sendMessage(chat_id: message.chat.id, text: "Привет, #{message.from.first_name}")
       bot.api.sendMessage(chat_id: message.chat.id, text: "/start - выводит приветствие и описание всех доступных команд
         /semester - запоминает даты начала и конца семестра
 		/subject - добавляет предмет и количество лабораторных работ по нему
-		/status - выводит твой список лаб, которые тебе предстоит сдать
-		/reset - сбрасывает для пользователя все данные.")
+        /status - выводит твой список лаб, которые тебе предстоит сдать
+        /reset - сбрасывает для пользователя все данные.")
     when '/semester'
-       bot.api.sendMessage(chat_id: message.chat.id, text: "Когда начинаем учиться?(ГГГГ-ММ-ДД или ДД-ММ-ГГГГ)")
-         bot.listen do |answer|
+      bot.api.sendMessage(chat_id: message.chat.id, text: "Когда начинаем учиться?(ГГГГ-ММ-ДД или ДД-ММ-ГГГГ)")
+        bot.listen do |answer|
    		  if v_date?(answer.text) == false
    			 bot.api.sendMessage(chat_id: message.chat.id, text: "#{answer.from.first_name}, ты пишешь шляпу. Нормально введи дату!")
 		  else
@@ -64,26 +63,22 @@ Telegram::Bot::Client.run(token) do |bot|
            @stack = @stack.merge(@abgx)
            
     when "/status"
-     if @eta != nil
-      bot.api.sendMessage(chat_id: message.chat.id, text: "Осталось времени #{@eta} дней")
-    else
-     bot.api.sendMessage(chat_id: message.chat.id, text: "Введи начало и конец семестров")
-    end
-    
-    if !@eta.nil?  
-    @stack.each do |key, value|
-     taskcalc(value.to_i)
-     bot.api.sendMessage(chat_id: message.chat.id, text: "#{key} - #{@accomplished} из #{value} предметов должны быть уже сданы")
-    end
-    end
-    
-    
-    
-    
+      if @eta != nil
+        bot.api.sendMessage(chat_id: message.chat.id, text: "Осталось времени #{@eta} дней")
+      else
+       bot.api.sendMessage(chat_id: message.chat.id, text: "Введи начало и конец семестров")
+      end
+
+      if !@eta.nil?  
+      @stack.each do |key, value|
+      taskcalc(value.to_i)
+      bot.api.sendMessage(chat_id: message.chat.id, text: "#{key} - #{@accomplished} из #{value} предметов должны быть уже сданы")
+      end
+      end
     when "/reset"
-    @stack = nil
-    @eta = nil
-    @sum_of_days = nil
-        end
-	end
-	end
+      @stack = nil
+      @eta = nil
+      @sum_of_days = nil
+    end
+  end
+end
