8e49865860324cc074ec2511505afe4c5b07686a
Владимир Сернацкий <byport1112@gmail.com>
2016-11-03 02:28:09 +0300

    homework-2

---
+++ 2016/VladimirSernatsky/checkmylabs-bot/session.rb
@@ -0,0 +1,61 @@
+require 'redis'
+
+# session keeper
+module Session
+  class << self
+    attr_accessor :id, :request
+  end
+  @redis = Redis.new url: '__CUT__'
+
+  def self.get(key)
+    get_absolute @id.to_s + ':' + key
+  end
+
+  def self.get_absolute(key)
+    if @redis.type(key) == 'list'
+      @redis.lrange key, 0, -1
+    else
+      @redis.get key
+    end
+  end
+
+  def self.set(key, val)
+    @redis.set @id.to_s + ':' + key, val
+  end
+
+  def self.del(key)
+    @redis.del @id.to_s + ':' + key
+  end
+
+  def self.append(key, val)
+    @redis.rpush @id.to_s + ':' + key, val
+  end
+
+  def self.extend(key, vals)
+    vals.each { |v| append key, v }
+  end
+
+  def self.remove(key, val, count)
+    @redis.lrem @id.to_s + ':' + key, count, val
+  end
+
+  def self.len(key)
+    @redis.llen @id.to_s + ':' + key
+  end
+
+  def self.clear
+    @redis.keys("#{@id}:*").each { |key| @redis.del(key) }
+  end
+
+  def self.clear_absolute
+    @redis.keys.each { |key| @redis.del(key) }
+  end
+
+  def self.keys
+    @redis.keys "#{@id}:*"
+  end
+
+  def self.keys_absolute
+    @redis.keys
+  end
+end
