033c19b9323ddc4ce303ed4b5d166f6b8c008bb0
kyalex <rosenrot333777@gmail.com>
2016-11-02 16:18:20 +0300

    hw2_l1

---
+++ commands/status.rb
@@ -0,0 +1,29 @@
+class Status < Command
+  def message
+    if semester?
+      @bot.api.sendMessage(chat_id: @message.chat.id, text: "Тебе осталось сдать:\n")
+      @subjects = @data["subjects"]
+      @subjects.each do |key, val|
+        @bot.api.sendMessage(chat_id: @message.chat.id,
+         text: "#{ key } - #{ calc_labs(val) } из #{val} лаб\n")
+      end
+    end
+  end
+
+  private
+
+  def semester?
+    if @data["start"].nil? || @data["end"].nil?
+      @bot.api.sendMessage(chat_id: @message.chat.id, text: "Ты не ввел границы семеста. /semester чтобы ввести")
+      false
+    else
+      true
+    end
+  end
+
+  def calc_labs(lab)
+    semester_length = (Date.parse(@data["end"])-Date.parse(@data["start"])).to_i
+    remaining_days = (Date.parse(@data["end"]) - DateTime.now.to_date).to_i
+    ((semester_length-remaining_days).to_f/semester_length*lab.to_i).to_i
+  end
+end
