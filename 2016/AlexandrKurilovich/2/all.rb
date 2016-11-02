281a83bde81e89a52a2985ac43518b18dfb960c3
kyalex <rosenrot333777@gmail.com>
2016-11-02 17:54:59 +0300

    hw2_l1

--- 2016/AlexandrKurilovich/2/all.rb
+++ 2016/AlexandrKurilovich/2/all.rb
@@ -2,5 +2,5 @@ require "redis"
 redis = Redis.new(host: "redis-18148.c10.us-east-1-3.ec2.cloud.redislabs.com",
                   port: "18148",
                   password: "111111111111")
-keys = redis.keys('*')
+keys = redis.keys("*")
 keys.each { |key| puts "key:#{key} value:#{redis.get(key)}" }
