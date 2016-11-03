2a796d0f3920b97201fa78024c0f1846e69b9d16
Irisha <irishasidorenko17@gmail.com>
2016-11-03 12:27:11 +0300

    Update connection.rb

--- 2016/SidorenkoIrina/2/connection.rb
+++ 2016/SidorenkoIrina/2/connection.rb
@@ -1,6 +1,6 @@
 class Connection			
-	def get_token
-		token = '291794432:AAGn5P_ELcRGaMHJHX7dLxSppjCRPkUDtrs'
-	    token
-	end
+      def get_token
+	  token = '291794432:AAGn5P_ELcRGaMHJHX7dLxSppjCRPkUDtrs'
+          token
+      end
 end
