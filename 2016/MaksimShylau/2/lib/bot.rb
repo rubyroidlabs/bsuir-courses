1a5a47efeebfd94d851ab40d9d971214bc73ab93
storm2513 <storm25132513@gmail.com>
2016-11-03 10:36:14 +0200

    Changed return on brake

--- 2016/MaksimShylau/2/lib/bot.rb
+++ 2016/MaksimShylau/2/lib/bot.rb
@@ -32,7 +32,7 @@ class TelegramBot
       command.send_message(SEM_START)
       hash["user_status"] = "wanna_sem_start"
       db.set_hash(hash, id)
-      return
+      break
     end
     command.send_message(DateParser.difference(DateParser.new(hash["sem_end"]), DateParser.new(hash["sem_start"])))
   end
