fa12df1b9701d8719d63dc69e419179b672b7f03
storm2513 <storm25132513@gmail.com>
2016-11-04 18:08:12 +0300

    Corrected spaces

--- 2016/MaksimShylau/2/test/test.rb
+++ 2016/MaksimShylau/2/test/test.rb
@@ -1,11 +1,11 @@
 require_relative "../lib/parse_date"
 RSpec.describe DateParser do
-	describe "DateParser test" do
-		it "Tests DateParser initializer" do
-			date = DateParser.new("01.09.2016")
-			expect(date.year).to eq(2016)
-			expect(date.month).to eq(9)
-			expect(date.day).to eq(1)
-		end
-	end
+  describe "DateParser test" do
+    it "Tests DateParser initializer" do
+      date = DateParser.new("01.09.2016")
+      expect(date.year).to eq(2016)
+      expect(date.month).to eq(9)
+      expect(date.day).to eq(1)
+    end
+  end
 end
