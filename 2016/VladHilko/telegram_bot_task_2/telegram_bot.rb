b005ae8a225fb9b89deadccbe1e4a7331a00c25d
Evan <ilitejuggler@gmail.com>
2016-11-07 22:35:41 +0300

    double-quoted strings

--- 2016/VladHilko/telegram_bot_task_2/telegram_bot.rb
+++ 2016/VladHilko/telegram_bot_task_2/telegram_bot.rb
@@ -1,16 +1,16 @@
-require 'telegram/bot'
-require 'date'
-require 'redis'
-require 'timeout'
+require "telegram/bot"
+require "date"
+require "redis"
+require "timeout"
 
-require_relative 'lib/base'
-require_relative 'lib/reset'
-require_relative 'lib/semester'
-require_relative 'lib/status'
-require_relative 'lib/subject'
-require_relative 'lib/submit'
+require_relative "lib/base"
+require_relative "lib/reset"
+require_relative "lib/semester"
+require_relative "lib/status"
+require_relative "lib/subject"
+require_relative "lib/submit"
 
-token = '274685898:AAEuPrF8KsgxHyenEt_OGEOO8PGdP2lPTIM'
+token = "274685898:AAEuPrF8KsgxHyenEt_OGEOO8PGdP2lPTIM"
 
 SKILLS = "Привет. Я тебе смогу помочь сдать все лабы, чтобы мамка не ругалась.
 Смотри список что я умею:\n
@@ -24,21 +24,21 @@ SKILLS = "Привет. Я тебе смогу помочь сдать все л
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
-    when '/start'
+    when "/start"
       bot.api.sendMessage(chat_id: message.chat.id, text: SKILLS)
-    when '/semester'
+    when "/semester"
       semester = Semester.new(bot, message.chat.id)
       semester.send_messages
-    when '/subject'
+    when "/subject"
       subject = Subject.new(bot, message.chat.id)
       subject.send_messages
-    when '/status'
+    when "/status"
       status = Status.new(bot, message.chat.id)
       status.send_messages
-    when '/reset'
+    when "/reset"
       reset = Reset.new(bot, message.chat.id)
       reset.reset_redis
-    when '/submit', /я сдалa?/
+    when "/submit", /я сдалa?/
       submit = Submit.new(bot, message.chat.id)
       submit.send_messages
     end
