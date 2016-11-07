ed41c72be9a73242fed7cf8c4e7e2c4bf71db99a
Vlad Ermolkin <stigmatervlad@gmail.com>
2016-11-07 03:28:41 +0300

    TowelBot v. 2.0.7

--- 2016/VladislavErmolkin/2/lib/reset.rb
+++ 2016/VladislavErmolkin/2/lib/reset.rb
@@ -1,11 +1,11 @@
-require_relative 'action'
-require_relative 'user'
+require_relative "action"
+require_relative "user"
 
 # Class Reset.
 class Reset < Action
   def run
     @user.reset
     @user.save
-    'Clean sheet.'
+    "Clean sheet."
   end
 end
