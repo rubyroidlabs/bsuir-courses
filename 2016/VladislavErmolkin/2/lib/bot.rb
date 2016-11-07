2170d06a8b6e45571a188dcc329d15748bc39b61
Vlad Ermolkin <stigmatervlad@gmail.com>
2016-11-07 03:30:10 +0300

    TowelBot v. 2.0.8

--- 2016/VladislavErmolkin/2/lib/bot.rb
+++ 2016/VladislavErmolkin/2/lib/bot.rb
@@ -1,10 +1,10 @@
-require 'telegram/bot'
-require 'webrick'
-require 'json'
-require 'redis'
-require_relative 'servlet'
+require "telegram/bot"
+require "webrick"
+require "json"
+require "redis"
+require_relative "servlet"
 
-TOKEN = '289410618:AAGCYzweaG-rTtTR15mvamdJ_yFxACmJMLU'.freeze
+TOKEN = "289410618:AAGCYzweaG-rTtTR15mvamdJ_yFxACmJMLU".freeze
 
 # https://api.telegram.org/bot289410618:AAGCYzweaG-rTtTR15mvamdJ_yFxACmJMLU/setWebhook?url=https://d5ec8f12.ngrok.io/webhooks/telegram_Xi39zZFru6J3oop897cz
 
@@ -17,8 +17,8 @@ class Bot
   def start
     Telegram::Bot::Client.run(@token) { |bot| @bot = bot }
     server = WEBrick::HTTPServer.new(Port: 8080)
-    server.mount('/', MyServlet, @bot)
-    trap('INT') { server.shutdown }
+    server.mount("/", MyServlet, @bot)
+    trap("INT") { server.shutdown }
     server.start
   end
 end
