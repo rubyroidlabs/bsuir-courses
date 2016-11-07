c9bad9f8d56c9e1523e45ad62cdae18775710e35
ADwinion <ADwinion@gmail.com>
2016-11-07 23:41:42 +0300

    Homework 2

--- 2016/AlexeyDobry/2/bot.rb
+++ 2016/AlexeyDobry/2/bot.rb
@@ -5,42 +5,42 @@ require_relative './lib/calendar.rb'
 class Base
 token = '273396926:AAEP9YaekvsgpG0gcMSmKMOWMXZWrrfZHLs'
 Telegram::Bot::Client.run(token) do |bot|
-	bot.listen do |message|
-		case message.text
-			when '/start'
-				bot.api.send_message(chat_id: message.chat.id, text: "Здраствуй, #{message.from.first_name}!")
-				bot.api.send_message(chat_id: message.chat.id, text: "Пару вещей, которые ты должен знать: \n
-					/start - ты его видишь \n
-					/semestr - напоминание, чтобы не приуныть перед сессией (Даты вводить в стиле год-месяц-день или наоборот)\n
-					/subject - выплесни свой страх сюда, и кол-во страха тоже с: \n
-					/status - познай страх, чтобы не приуныть \n
-					/reset - rm -rf /dev/null \n
-					Развлекайся ;3")
-			when '/semestr'
-				bot.api.send_message(chat_id: message.chat.id, text: "Напомни дату, когда ТЗ было оформлено: ")
-				bot.listen do |anwser|
-					if valid?(anwser.text) == false
-						bot.api.send_message(chat_id: message.chat.id, text: "Так и приуныть можно, введи дату правильно")
-					else 
-						@date1 = Date.parse(anwser.text)
-						break
-					end
-				end
+  bot.listen do |message|
+    case message.text
+      when '/start'
+        bot.api.send_message(chat_id: message.chat.id, text: "Здраствуй, #{message.from.first_name}!")
+        bot.api.send_message(chat_id: message.chat.id, text: "Пару вещей, которые ты должен знать: \n
+          /start - ты его видишь \n
+          /semestr - напоминание, чтобы не приуныть перед сессией (Даты вводить в стиле год-месяц-день или наоборот)\n
+          /subject - выплесни свой страх сюда, и кол-во страха тоже с: \n
+          /status - познай страх, чтобы не приуныть \n
+          /reset - rm -rf /dev/null \n
+          Развлекайся ;3")
+      when '/semestr'
+        bot.api.send_message(chat_id: message.chat.id, text: "Напомни дату, когда ТЗ было оформлено: ")
+        bot.listen do |anwser|
+          if valid?(anwser.text) == false
+            bot.api.send_message(chat_id: message.chat.id, text: "Так и приуныть можно, введи дату правильно")
+          else 
+            @begin = Date.parse(anwser.text)
+            break
+          end
+        end
 
-				bot.api.send_message(chat_id: message.chat.id, text: "Когда дедлайн?")
-				bot.listen do |anwser|
-					if valid?(anwser.text) == false
-						bot.api.send_message(chat_id: message.chat.id, text: "Стебешься? Введи дату правильно")
-					else 
-						@date2 = Date.parse(anwser.text)
-						break
-					end
-				end
+        bot.api.send_message(chat_id: message.chat.id, text: "Когда дедлайн?")
+        bot.listen do |anwser|
+          if valid?(anwser.text) == false
+            bot.api.send_message(chat_id: message.chat.id, text: "Стебешься? Введи дату правильно")
+          else 
+            @end = Date.parse(anwser.text)
+            break
+          end
+        end
 
-				if sem_date(@date1, @date2) == true					
-					bot.api.send_message(chat_id: message.chat.id, text: "У тя #{@today} дней до дедлайна")
-				end
-			end
-		end
-	end
+        if sem_date(@date1, @date2) == true					
+          bot.api.send_message(chat_id: message.chat.id, text: "У тя #{@today} дней до дедлайна")
+        end
+      end
+    end
+  end
 end	
--- 2016/AlexeyDobry/2/lib/calendar.rb
+++ 2016/AlexeyDobry/2/lib/calendar.rb
@@ -1,18 +1,18 @@
 def initislization
-	@end_sem = end_sem
+  @end_sem = end_sem
 end
 
 def sem_date (begin_sem, end_sem)
-	a = Date.today
-	@today = (end_sem - a).to_i
-	true
+  a = Date.today
+  @today = (end_sem - a).to_i
+  true
 end
 
 def valid?(date_string)
-	x = Date.parse(date_string).to_s
-	y, m, d = x.split "-"
-	Date.valid_date? y.to_i, m.to_i, d.to_i
-		return true
-	rescue
-		return false
+  x = Date.parse(date_string).to_s
+  y, m, d = x.split "-"
+  Date.valid_date? y.to_i, m.to_i, d.to_i
+  return true
+rescue
+  return false
 end
