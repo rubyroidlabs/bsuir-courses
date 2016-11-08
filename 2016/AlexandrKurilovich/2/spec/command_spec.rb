8ef76cd86c2eb2f05f7ece6df61427a4e6a5f66f
kyalex <rosenrot333777@gmail.com>
2016-11-08 21:51:18 +0300

    fgdsfg

--- 2016/AlexandrKurilovich/2/spec/command_spec.rb
+++ 2016/AlexandrKurilovich/2/spec/command_spec.rb
@@ -2,12 +2,17 @@ require_relative "../config"
 redis = Redis.new(REDIS_HOST)
 Telegram::Bot::Client.run(BOT_TOKEN) do |bot|
   bot.listen do |message|
+    command = Command.new(bot, message, redis)
     RSpec.describe Command do
       describe "#parse_data" do
         it "returns data" do
-          command = Command.new(bot, message, redis)
           data = JSON.parse(redis.get(message.chat.id.to_s))
           expect(command.parse_data).to eq(data)
+        end
+        it "reset status" do
+          command.reset_status
+          data = command.parse_data
+          expect([data["semester"], data["subject"]]).to eq([0, 0])
         end
       end
     end
