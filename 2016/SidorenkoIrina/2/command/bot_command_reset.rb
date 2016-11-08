4bc920954079398fce21c96a0b30de8a6eabdfd9
Irisha <irishasidorenko17@gmail.com>
2016-11-08 22:50:40 +0300

    Update bot_command_reset.rb

--- 2016/SidorenkoIrina/2/command/bot_command_reset.rb
+++ 2016/SidorenkoIrina/2/command/bot_command_reset.rb
@@ -2,20 +2,20 @@ require_relative 'bot_command'
 require_relative 'chat_history'
 
 class ResetCommand < Command
-     attr_accessor :bot, :message_chat_id, :text
-     @@text ="\xF0\x9F\x98\x94 Хорошо, я забуду все, что между нами было..."
+    attr_accessor :bot, :message_chat_id, :text
+    @@text ="\xF0\x9F\x98\x94 Хорошо, я забуду все, что между нами было..."
 
-     def initialize (bot, message_chat_id)
+    def initialize (bot, message_chat_id)
 	@bot = bot
-	@message_chat_id = message_chat_id
-     end	
+        @message_chat_id = message_chat_id
+    end	
     	
-     def reset
-	ChatHistory.new(message_chat_id, "reset").delete_directory
-	send_message(@@text)
-     end
+    def reset
+        ChatHistory.new(message_chat_id, "reset").delete_directory
+        send_message(@@text)
+    end
 
-     def send_message(text) 
-	super(message_chat_id, text)
-     end
+    def send_message(text) 
+        super(message_chat_id, text)
+    end
 end
