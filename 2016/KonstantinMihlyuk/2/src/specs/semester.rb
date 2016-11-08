93c000fbdf15be87a73f23d1d3fb5a47b1769aef
Mihlyuk <konstantin.mihlyuk@yandex.ru>
2016-11-08 10:43:03 +0300

    fix

--- 2016/KonstantinMihlyuk/2/src/specs/semester.rb
+++ 2016/KonstantinMihlyuk/2/src/specs/semester.rb
@@ -1,26 +1,26 @@
-require 'rspec'
+require "rspec"
 require_relative "../commands/semester.rb"
 require_relative "../constants/answer.rb"
 
 RSpec.describe Semester do
 
   user = {
-    'subjects'=> {
-      'mrz'=> {
-        'labs_count'=> 10,
-        'made_labs'=> [2, 4, 5, 6]
+    "subjects"=> {
+      "mrz"=> {
+        "labs_count"=> 10,
+        "made_labs"=> [2, 4, 5, 6]
       },
-      'ppvis'=> {
-        'labs_count'=> 10,
-        'labs'=> [2, 4, 5, 6]
+      "ppvis"=> {
+        "labs_count"=> 10,
+        "labs"=> [2, 4, 5, 6]
       }
     },
-    'start_date' => '01-09-2016',
-    'finish_date' => '03-11-2016',
-    'available_days' => '41',
-    'reminders'=> [{
-      'days'=> 1,
-      'hour'=> 12
+    "start_date" => "01-09-2016",
+    "finish_date" => "03-11-2016",
+    "available_days" => "41",
+    "reminders"=> [{
+      "days"=> 1,
+      "hour"=> 12
     }]
   }
 
@@ -28,36 +28,36 @@ RSpec.describe Semester do
 
     it "should return incorrect date message" do
       semester = Semester.new(user)
-      semester.say('/start')
+      semester.say("/start")
 
-      expect(semester.say('-12-01-2016')).to eq(Answer::FAIL_START_LEARNING_DATE)
-      expect(semester.say('32-11-2016')).to eq(Answer::FAIL_START_LEARNING_DATE)
-      expect(semester.say('15-13-2016')).to eq(Answer::FAIL_START_LEARNING_DATE)
-      expect(semester.say('13-10-1997')).to eq(Answer::FAIL_START_LEARNING_DATE)
-      expect(semester.say('12-10-2016')).to eq(Answer::WHEN_END_TO_LEARN)
+      expect(semester.say("-12-01-2016")).to eq(Answer::FAIL_START_LEARNING_DATE)
+      expect(semester.say("32-11-2016")).to eq(Answer::FAIL_START_LEARNING_DATE)
+      expect(semester.say("15-13-2016")).to eq(Answer::FAIL_START_LEARNING_DATE)
+      expect(semester.say("13-10-1997")).to eq(Answer::FAIL_START_LEARNING_DATE)
+      expect(semester.say("12-10-2016")).to eq(Answer::WHEN_END_TO_LEARN)
     end
 
     it "should return incorrect date message" do
       semester = Semester.new(user)
-      semester.say('/start')
+      semester.say("/start")
 
-      expect(semester.say('01-09-2016')).to eq(Answer::WHEN_END_TO_LEARN)
+      expect(semester.say("01-09-2016")).to eq(Answer::WHEN_END_TO_LEARN)
     end
 
     it "should return incorrect available days message" do
       semester = Semester.new(user)
-      semester.say('/start')
-      semester.say('12-10-2016')
+      semester.say("/start")
+      semester.say("12-10-2016")
 
-      expect(semester.say('01-09-2016')).to eq(Answer.FAIL_AVAILABLE_DAYS(user['available_days']))
+      expect(semester.say("01-09-2016")).to eq(Answer.FAIL_AVAILABLE_DAYS(user["available_days"]))
     end
 
     it "should return correct finish message" do
       semester = Semester.new(user)
-      semester.say('/start')
-      semester.say('01-09-2016')
+      semester.say("/start")
+      semester.say("01-09-2016")
 
-      expect(semester.say('12-10-2016')).to eq(Answer::HOW_MANY_DAYS_YOU_HAVE(user['available_days']))
+      expect(semester.say("12-10-2016")).to eq(Answer::HOW_MANY_DAYS_YOU_HAVE(user["available_days"]))
     end
   end
 end
--- 2016/KonstantinMihlyuk/2/src/specs/submit.rb
+++ 2016/KonstantinMihlyuk/2/src/specs/submit.rb
@@ -1,26 +1,26 @@
-require 'rspec'
+require "rspec"
 require_relative "../commands/submit.rb"
 require_relative "../constants/answer.rb"
 
 RSpec.describe Submit do
 
   user = {
-    'subjects'=> {
-      'mrz'=> {
-        'labs_count'=> 10,
-        'made_labs'=> [2, 4, 5, 6]
+    "subjects"=> {
+      "mrz"=> {
+        "labs_count"=> 10,
+        "made_labs"=> [2, 4, 5, 6]
       },
-      'ppvis'=> {
-        'labs_count'=> 10,
-        'labs'=> [2, 4, 5, 6]
+      "ppvis"=> {
+        "labs_count"=> 10,
+        "labs"=> [2, 4, 5, 6]
       }
     },
-    'start_date' => '01-09-2016',
-    'finish_date' => '03-11-2016',
-    'available_days' => '41',
-    'reminders'=> [{
-      'days'=> 1,
-      'hour'=> 12
+    "start_date" => "01-09-2016",
+    "finish_date" => "03-11-2016",
+    "available_days" => "41",
+    "reminders"=> [{
+      "days"=> 1,
+      "hour"=> 12
     }]
   }
 
@@ -29,21 +29,21 @@ RSpec.describe Submit do
     it "should return correct subject message" do
       subject = Submit.new(user)
 
-      expect(subject.say('/submit')).to eq(Answer::WHAT_SUBJECT_PASSED(user['subjects']))
+      expect(subject.say("/submit")).to eq(Answer::WHAT_SUBJECT_PASSED(user["subjects"]))
     end
 
     it "should return incorrect labs count message" do
       semester = Submit.new(user)
-      semester.say('/submit')
+      semester.say("/submit")
 
-      expect(semester.say('-12')).to eq(Answer::INCORRECT_LABS_COUNT)
+      expect(semester.say("-12")).to eq(Answer::INCORRECT_LABS_COUNT)
     end
 
     it "should return incorrect subjects message" do
       semester = Submit.new(user)
-      semester.say('/submit')
-      semester.say('1')
-      expect(semester.say('5')).to eq(Answer::DONT_HAVE_LABS)
+      semester.say("/submit")
+      semester.say("1")
+      expect(semester.say("5")).to eq(Answer::DONT_HAVE_LABS)
     end
   end
 end
