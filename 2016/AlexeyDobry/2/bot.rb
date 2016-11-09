cd13eba22fafd1baa9bf1f582ce8c98cd576d8f2
ADwinion <ADwinion@gmail.com>
2016-11-09 21:28:21 +0300

    Homework-2

--- 2016/AlexeyDobry/2/bot.rb
+++ 2016/AlexeyDobry/2/bot.rb
@@ -10,18 +10,18 @@ class Base
         when "/start"
           bot.api.send_message(chat_id: message.chat.id, text: "Здраствуй, #{message.from.first_name}!")
           bot.api.send_message(chat_id: message.chat.id, text: "Пару вещей, которые ты должен знать: \n
-            /start - ты его видишь \n
-            /semestr - напоминание, чтобы не приуныть перед сессией (Даты вводить в стиле год-месяц-день или наоборот)\n
-            /subject - выплесни свой страх сюда, и кол-во страха тоже с: \n
-            /status - познай страх, чтобы не приуныть \n
-            /reset - rm -rf /dev/null \n
-            Развлекайся ;3")
+/start - ты его видишь \n
+/semestr - напоминание, чтобы не приуныть перед сессией (Даты вводить в стиле год-месяц-день или наоборот)\n
+/subject - выплесни свой страх сюда, и кол-во страха тоже с: \n
+/status - познай страх, чтобы не приуныть \n
+/reset - rm -rf /dev/null \n
+Развлекайся ;3")
         when "/semestr"
           bot.api.send_message(chat_id: message.chat.id, text: "Напомни дату, когда ТЗ было оформлено: ")
           bot.listen do |anwser|
             if valid?(anwser.text) == false
               bot.api.send_message(chat_id: message.chat.id, text: "Так и приуныть можно, введи дату правильно")
-            else 
+            else
               @begin = Date.parse(anwser.text)
               break
             end
@@ -30,15 +30,15 @@ class Base
           bot.listen do |anwser|
             if valid?(anwser.text) == false
               bot.api.send_message(chat_id: message.chat.id, text: "Стебешься? Введи дату правильно")
-            else 
+            else
               @end = Date.parse(anwser.text)
               break
             end
           end
-          if sem_date(@begin, @end) == true					
+          if sem_date(@begin, @end) == true
             bot.api.send_message(chat_id: message.chat.id, text: "У тя #{@today} дней до дедлайна")
           end
       end
     end
   end
-end	
+end
