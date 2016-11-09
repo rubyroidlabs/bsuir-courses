1fdf811e6ceee06a3ef0d893093033136419d27e
Nikita Kolokoltsev <nikita.kolokoltsev@yahoo.com>
2016-11-09 18:53:12 +0300

    Style

--- 2016/NikitaKolokoltsev/homework-2/lib/bot.rb
+++ 2016/NikitaKolokoltsev/homework-2/lib/bot.rb
@@ -1,18 +1,16 @@
 # Class for basic Bot actions
 class Bot
-  attr_reader :bot, :database
-
   def initialize(bot, database)
-    @@bot = bot
-    @@database = database
+    BOT = bot
+    DATABASE = database
   end
 
   def database_save(chat_id, user)
-    @@database.set(chat_id, user.to_json)
+    DATABASE.set(chat_id, user.to_json)
   end
 
   def database_load(chat_id)
-    user = @@database.get(chat_id) || {}
+    user = DATABASE.get(chat_id) || {}
     if user.is_a?(String)
       JSON.parse(user).keys_to_syms
     else
@@ -21,14 +19,14 @@ class Bot
   end
 
   def send_message(chat_id: "", text: "", reply_markup: "")
-    @@bot.api.send_message(chat_id: chat_id, text: text, reply_markup: reply_markup)
+    BOT.api.send_message(chat_id: chat_id, text: text, reply_markup: reply_markup)
   end
 
   def update
-    @@bot.fetch_updates { |message| yield(message) if block_given? }
+    BOT.fetch_updates { |message| yield(message) if block_given? }
   end
 
   def listen
-    @@bot.listen { |message| yield(message) if block_given? }
+    BOT.listen { |message| yield(message) if block_given? }
   end
 end
