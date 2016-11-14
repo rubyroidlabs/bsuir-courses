51e7f5362975e8ac5f620ca053fa1554b48e92d3
artisaev <artem.shekrota@gmail.com>
2016-11-14 12:20:45 +0300

    optimization

--- 2016/ArtemShekrota/2/lib/donelabs.rb
+++ 2016/ArtemShekrota/2/lib/donelabs.rb
@@ -1,6 +1,4 @@
 # amount of labs that has to be done
 def donelabs(tasks)
-  days_per_task = @timeall / tasks
-  days_gone = @timeall - @time
-  @done = days_gone / days_per_task
+  @done = (@timeall - @time) / (@timeall / tasks)
 end
