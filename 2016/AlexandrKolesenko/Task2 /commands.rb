4d025576a0ca5a9c62ad1eabcdf2d8bf56c4f63e
aik-stok <aikb2b@gmail.com>
2016-11-19 20:29:34 +0300

    complete rewrite

---
+++ 2016/AlexandrKolesenko/Task2 /commands.rb
@@ -0,0 +1,98 @@
+class Base
+  attr_accessor :bot
+
+  def initialize(bot, message)
+    @redis = Redis.new
+    @message = message
+    @bot = bot
+    @user_id = message.chat.id
+    @sm = sm = lambda {|ms| bot.api.sendMessage(chat_id: @message.chat.id, text: ms)}
+    @name = message.from.first_name
+  end
+end
+
+class Start < Base
+  def run
+  @sm.call("/start - выводит приветствие и описание всех доступных команд
+/semester - запоминает даты начала и конца семестра
+/subject - добавляет предмет и количество лабораторных работ по нему
+/status - выводит твой список лаб, которые тебе предстоит сдать
+/reset - сбрасывает для пользователя все данные.")
+  end
+end
+
+class Semester < Base
+
+  def run
+    @sm.call("Когда начинаем учиться?(ГГГГ-ММ-ДД или ДД-ММ-ГГГГ)")
+    @bot.listen do |message|
+      if v_date?(message.text) == false
+        @sm.call("#{@name}, ты пишешь шляпу. Нормально введи дату!")
+      else
+        @date1 = Date.parse(message.text)
+        @sm.call("Когда заканчиваем учиться?(ГГГГ-ММ-ДД или ДД-ММ-ГГГГ)")
+        break
+      end
+  end
+    @bot.listen do |answer|
+      if v_date?(answer.text) == false
+        @sm.call("#{name}, ты пишешь шляпу. Нормально введи дату!")
+      else
+        @date2 = Date.parse(answer.text)
+        break
+      end  
+    end
+      if countdown(@date1, @date2) == true
+      @redis.hmset("#{@user_id}-date", "begin", @date1, "end", @date2)
+        @sm.call("В запасе дней:#{@eta}")
+      else
+        @sm.call("Время вышло")
+      end    
+    end
+end
+
+class Subject < Base
+  def run
+    @sm.call("Как называется предмет?")
+      bot.listen do |answer|
+        @task = answer.text
+        break
+      end
+      @sm.call("Сколько лаб нужно сдать?")
+        bot.listen do |answer|
+          if !/\d+/.match(answer.text) == true
+            @sm.call("#{@name}, будь человеком введи число!")
+          else
+            @task_num = answer.text
+            @sm.call("Принял.")
+            break
+          end
+        end
+
+      @redis.hmset("#{@user_id}-subj", @task, @task_num)
+  end
+end
+
+ class Status < Base
+   def run 
+  if @redis.hget("#{@user_id}-date", "begin").nil?
+        @sm.call("Сначала введи начало и конец семестров (/semester)")
+      else
+        d1 = Date.parse(@redis.hget("#{@user_id}-date", "begin"))
+        d2 = Date.parse(@redis.hget("#{@user_id}-date", "end"))
+        countdown(d1, d2)
+        @sm.call("Осталось времени #{@eta} дней")
+        stack = @redis.hgetall("#{@user_id}-subj")
+        stack.each do |key, value|
+        taskcalc(value.to_i)
+        @sm.call("#{key} - #{@accomplished} из #{value} предметов должны быть уже сданы")
+      end
+   end
+end
+end
+class Reset < Base
+  def run
+    @redis.del("#{@user_id}-date", "#{@user_id}-subj")
+    @sm.call("Твои данные удалены")
+  end
+end
--- 2016/AlexandrKolesenko/Task2 /start_bot.rb
+++ 2016/AlexandrKolesenko/Task2 /start_bot.rb
@@ -1,79 +1,28 @@
-
 require_relative "logic.rb"
+require_relative "commands.rb"
+require "redis"
 require "telegram/bot"
-token = "YOUR_TELEGRAM_TOKEN"
+token = "278085899:AAG_kHHWkT5DUiEMg-wCKLAQUvNaKV60bAk"
 
-@stack = {}
+
+
 
 Telegram::Bot::Client.run(token) do |bot|
+  
   bot.listen do |message|
+  name = message.from.first_name
     case message.text
     when "/start"
-      bot.api.sendMessage(chat_id: message.chat.id, text: "Привет, #{message.from.first_name}")
-      bot.api.sendMessage(chat_id: message.chat.id, text: "/start - выводит приветствие и описание всех доступных команд
-        /semester - запоминает даты начала и конца семестра
-		/subject - добавляет предмет и количество лабораторных работ по нему
-        /status - выводит твой список лаб, которые тебе предстоит сдать
-        /reset - сбрасывает для пользователя все данные.")
+       Start.new(bot, message).run
     when "/semester"
-      bot.api.sendMessage(chat_id: message.chat.id, text: "Когда начинаем учиться?(ГГГГ-ММ-ДД или ДД-ММ-ГГГГ)")
-      bot.listen do |answer|
-        if v_date?(answer.text) == false
-          bot.api.sendMessage(chat_id: message.chat.id, text: "#{answer.from.first_name}, ты пишешь шляпу. Нормально введи дату!")
-        else
-          @date1 = Date.parse(answer.text)
-          bot.api.sendMessage(chat_id: message.chat.id, text: "Когда заканчиваем учиться?(ГГГГ-ММ-ДД или ДД-ММ-ГГГГ)")
-          break
-        end
-      end
-      bot.listen do |answer|
-        if v_date?(answer.text) == false
-          bot.api.sendMessage(chat_id: message.chat.id, text: "#{answer.from.first_name}, ты пишешь шляпу. Нормально введи дату!")
-        else
-          @date2 = Date.parse(answer.text)
-          break
-        end
-      end
-      if countdown(@date1, @date2) == true
-        bot.api.sendMessage(chat_id: message.chat.id, text: "В запасе дней:#{@eta}")
-      else
-        bot.api.sendMessage(chat_id: message.chat.id, text: "Время вышло")
-      end
-
+       Semester.new(bot, message).run
     when "/subject"
-      bot.api.sendMessage(chat_id: message.chat.id, text: "Как называется предмет?")
-      bot.listen do |answer|
-        @task = answer.text
-        break
-      end
-
-      bot.api.sendMessage(chat_id: message.chat.id, text: "Сколько лаб нужно сдать?")
-      bot.listen do |answer|
-        if !/\d+/.match(answer.text) == true
-          bot.api.sendMessage(chat_id: message.chat.id, text: "#{answer.from.first_name}, будь человеком введи число!")
-        else
-          @task_num = answer.text
-          bot.api.sendMessage(chat_id: message.chat.id, text: "Принял.")
-          break
-        end
-      end
-      @abgx = Hash[@task, @task_num]
-      @stack = @stack.merge(@abgx)
-
+       Subject.new(bot, message).run
     when "/status"
-      if @eta.nil?
-        bot.api.sendMessage(chat_id: message.chat.id, text: "Введи начало и конец семестров")
-      else
-        bot.api.sendMessage(chat_id: message.chat.id, text: "Осталось времени #{@eta} дней")
-        @stack.each do |key, value|
-          taskcalc(value.to_i)
-          bot.api.sendMessage(chat_id: message.chat.id, text: "#{key} - #{@accomplished} из #{value} предметов должны быть уже сданы")
-        end
-      end
+       Status.new(bot, message).run
     when "/reset"
-      @stack = {}
-      @eta = nil
-      @sum_of_days = nil
+       Reset.new(bot,message).run
     end
   end
 end
+
