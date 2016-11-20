c936693a015125810e7795d18c2833eabe30158d
Kuchick <kuchinskiysergeysergeevich@gmail.com>
2016-11-20 22:59:20 +0300

    remove single quotes

--- 2016/SergeyKuchinskiy/3/app/models/phrase.rb
+++ 2016/SergeyKuchinskiy/3/app/models/phrase.rb
@@ -3,12 +3,12 @@ class Phrase < ActiveRecord::Base
   has_many :words
 
   def full_phrase
-    words.map { |v| v.data + ' ' }.inject('+')
+    words.map { |v| v.data + " " }.inject("+")
   end
 
   def color(current_id)
-    return 'green' if current_id != last_user
-    'red'
+    return "green" if current_id != last_user
+    "red"
   end
 
   def can_add?(current_id)
