dba3ca47fad46937ba27a04d537e0a7c0877941f
kyalex <rosenrot333777@gmail.com>
2016-11-21 05:02:13 +0300

    task 1.5

---
+++ 2016/AlexandrKurilovich/3/helpers/index.rb
@@ -0,0 +1,33 @@
+class IndexHelp
+  def initialize(redis, session)
+    @redis = redis
+    @session = session
+  end
+
+  def keys
+    @redis.keys.select {|x| x.include?("phrase:")}
+  end
+
+  def phrases
+    keys.map do |k|
+      @key = k
+      [text, k.split(":")[1], check_user]
+    end
+  end
+
+  def all_data
+    JSON.parse(@redis.get(@key))
+  end
+
+  def text
+    all_data.map { |w| w.split(".").first }
+  end
+
+  def last_user
+    all_data.last.split(".").last
+  end
+
+  def check_user
+    last_user == @session["user"]
+  end
+end
---
+++ 2016/AlexandrKurilovich/3/helpers/new.rb
@@ -0,0 +1,44 @@
+class NewHelp
+  def initialize(redis, session, params)
+    @redis = redis
+    @session = session
+    @params = params
+  end
+
+  def text
+    @params[:word]
+  end
+
+  def word
+    text["name"]
+  end
+
+  def word_data
+    "#{word}.#{current_time}.#{@session["user"]}"
+  end
+
+  def current_time
+    DateTime.now.strftime("%Y-%m-%d %H:%M")
+  end
+
+  def valid?
+    word.chars.select {|x| x =~ /[,!:;\"-]/}.size > 1 || word.include?(".")
+  end
+
+  def keys
+    @redis.keys.select {|x| x.include?("phrase:")}
+  end
+
+  def ind
+    keys.map { |k| k.split(":")[1] }
+  end
+
+  def phrase_index
+    phrase_index = ind.empty? ? 1 : ind.max.to_i + 1
+  end
+
+  def save
+    wordes = [word_data]
+    @redis.set("phrase:#{phrase_index}", wordes.to_json)
+  end
+end
