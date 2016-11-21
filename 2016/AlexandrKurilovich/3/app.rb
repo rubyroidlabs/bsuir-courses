9440bdd21d3ac390832c2828954504dbdc9bb150
kyalex <rosenrot333777@gmail.com>
2016-11-21 05:24:55 +0300

    fix

--- 2016/AlexandrKurilovich/3/app.rb
+++ 2016/AlexandrKurilovich/3/app.rb
@@ -12,7 +12,7 @@ REDIS_HOST = { host: "redis-18148.c10.us-east-1-3.ec2.cloud.redislabs.com",
 redis = Redis.new(REDIS_HOST)
 enable :sessions
 
-get '/' do
+get "/" do
   session["user"] ||= nil
   index_help = IndexHelp.new(redis, session)
   @phrases = index_help.phrases
@@ -22,42 +22,42 @@ get '/' do
   slim :index
 end
 
-get '/login' do
+get "/login" do
   slim :login
 end
 
-post '/login' do
+post "/login" do
   login_params = params[:login]
   session["user"] = login_params["name"]
-  redirect to('/')
+  redirect to("/")
 end
 
-get '/exit' do
+get "/exit" do
   session["user"] = nil
-  redirect to('/')
+  redirect to("/")
 end
 
 get '/new' do
-  redirect to('/') if session["user"].nil?
+  redirect to("/") if session["user"].nil?
   slim :new
 end
 
-post '/new' do
+post "/new" do
   new_help = NewHelp.new(redis, session, params)
   if new_help.valid?
     @alert = "Может быть только 1 знак препинания. Нельзя использовать точки"
     slim :new
   else
     new_help.save
-    redirect to('/')
+    redirect to("/")
   end
 end
 
-get '/show' do
+get "/show" do
   show_help = ShowHelp.new(redis, session, params)
-  redirect to('/login') if session["user"].nil?
+  redirect to("/login") if session["user"].nil?
   @phrase_id = show_help.phrase_id
-  redirect to('/') if show_help.valid_params?
+  redirect to("/") if show_help.valid_params?
   @phrase = show_help.phrase
   @phrase_info = show_help.info_show
   @user_last = show_help.user_last?
@@ -65,7 +65,7 @@ get '/show' do
   slim :show
 end
 
-post '/show' do
+post "/show" do
   show_help = ShowHelp.new(redis, session, params)
   if show_help.valid_word?
     redirect to("/show?id=#{@params[:word]["id"]}&fail=true")
