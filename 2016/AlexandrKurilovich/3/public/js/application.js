bac31ed02a8a4ae9a2c6c9e7247417ce3ce92faf
kyalex <rosenrot333777@gmail.com>
2016-11-23 04:13:46 +0300

    ajax

--- 2016/AlexandrKurilovich/3/public/js/application.js
+++ 2016/AlexandrKurilovich/3/public/js/application.js
@@ -6,9 +6,9 @@ $(document).ready(function() {
     $(this).find("div.form_content").fadeOut(0);
   });
   $("input.submit_form").click(function() {
-    $this = $(this);
-    value = $this.parent().find("input[type='text']").val();
-    id = $this.parents(".can_update").attr("data-id");
+    var $this = $(this);
+    var value = $this.parent().find("input[type='text']").val();
+    var id = $this.parents(".can_update").attr("data-id");
     $.ajax({
       url: "/create",
       type: "POST",
@@ -20,13 +20,13 @@ $(document).ready(function() {
             alert("Можно использовать только 1 знак препинания. Нельзя использовать точки");
             break;
           case "success":
-            $can = $this.parents(".can_update");
+            var $can = $this.parents(".can_update");
             $can.removeClass("can_update");
             $can.addClass("not_update");
             $can.find(".form_content").remove();
             $can.css("border-left", "3px solid rgb(238, 62, 31)");
             $can.find("a").css("color", "rgb(143, 143, 143)");
-            text = $can.find("a").text();
+            var text = $can.find("a").text();
             $can.find("a").text(text + " " + value);
             $("span.success_update").text("Слово успешно добавлено");
             break;
