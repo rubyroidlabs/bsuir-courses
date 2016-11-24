5f01b46d1ea1372d480ac9d433f353b1ad373415
Irisha <irishasidorenko17@gmail.com>
2016-11-24 23:50:23 +0300

    Update start.rb

--- 2016/SidorenkoIrina/3/start.rb
+++ 2016/SidorenkoIrina/3/start.rb
@@ -7,51 +7,51 @@ require './Check'
 set :sessions, true
 
 configure do 
-    @db = db_get
-    @db.execute "CREATE TABLE IF NOT EXISTS `users` (
-    `id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    `username`	TEXT NOT NULL UNIQUE,
-    `password`	TEXT NOT NULL);
+  @db = db_get
+  @db.execute "CREATE TABLE IF NOT EXISTS `users` (
+  `id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  `username`	TEXT NOT NULL UNIQUE,
+  `password`	TEXT NOT NULL);
       
-    CREATE TABLE IF NOT EXISTS `phrases` (
-	`id`	INTEGER NOT NULL,
-	`word`	TEXT NOT NULL,
-	`id_user`	INTEGER NOT NULL,
-	`flag`	INTEGER NOT NULL,
-	`date_time`	TEXT NOT NULL);" 
+  CREATE TABLE IF NOT EXISTS `phrases` (
+  `id`	INTEGER NOT NULL,
+  `word`	TEXT NOT NULL,
+  `id_user`	INTEGER NOT NULL,
+  `flag`	INTEGER NOT NULL,
+  `date_time`	TEXT NOT NULL);" 
 end
 
 
 get "/" do
-    unless session[:current_user].nil?
-	   @user = session[:current_user]
-    end
-    @db = db_get
-    @result = @db.execute "SELECT GROUP_CONCAT (word, ' ')  AS word, id, flag FROM phrases GROUP BY id;"
-    erb :index
+  unless session[:current_user].nil?
+    @user = session[:current_user]
+  end
+  @db = db_get
+  @result = @db.execute "SELECT GROUP_CONCAT (word, ' ')  AS word, id, flag FROM phrases GROUP BY id;"
+  erb :index
 end
 
 get "/login" do
-    redirect to("/") unless @user.nil?
-    erb :login
+  redirect to("/") unless @user.nil?
+  erb :login
 end
 
 post "/login" do
-    @username = params[:username]
-    @password = params[:password]
-    session[:current_user] = @username
-    if check_unique_login(@username)
-        @db = db_get
-        @db.execute 'INSERT INTO users (username, password) VALUES (?, ?);', [@username, @password]
-        redirect to("/")
-    elsif check_password(@username, @password)
-        redirect to("/")
-    else
-        @error = "This username is not available! Or incorrect password!"
-        if @error
-          erb :login
-        end
+  @username = params[:username]
+  @password = params[:password]
+  session[:current_user] = @username
+  if check_unique_login(@username)
+    @db = db_get
+    @db.execute "INSERT INTO users (username, password) VALUES (?, ?);", [@username, @password]
+    redirect to("/")
+  elsif check_password(@username, @password)
+    redirect to("/")
+  else
+    @error = "This username is not available! Or incorrect password!"
+    if @error
+      erb :login
     end
+  end
 end
 
 post "/add_word" do
