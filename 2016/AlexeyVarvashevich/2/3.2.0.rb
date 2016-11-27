6a388370ce534922c6279d4440fa200a9be12eda
Amix <RHL@tut.by>
2016-11-28 01:27:34 +0300

    Level 2

--- 2016/AlexeyVarvashevich/2/3.2.0.rb
+++ 2016/AlexeyVarvashevich/2/3.2.0.rb
@@ -15,6 +15,30 @@ class BotMain
   def telegram_send_message(text)
     @bot.api.send_message(chat_id: @user_id, text: text)
   end
+
+  def telegram_send_keybord(text, marker, subjects_numlabs = nil)
+    case marker
+    when 'reset'
+      kb = [
+        Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Удаляем', callback_data: 'delete'),
+        Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Отмена', callback_data: 'cancel')
+      ]
+    when 'submit_list'
+      kb = []
+      subjects_hash = @redis.hgetall("#{@user_id}-subject")
+      subjects_hash.each do |key, value|
+        kb.push(Telegram::Bot::Types::InlineKeyboardButton.new(text: key, callback_data: key))
+      end
+    when 'submit_numlabs'
+      array_numlabs = @redis.hget("#{@user_id}-subject-numlab", subjects_numlabs).delete('[,]').split
+      kb = []
+      array_numlabs.each do |count|
+        kb.push(Telegram::Bot::Types::InlineKeyboardButton.new(text: count, callback_data: count))
+      end
+    end
+    markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
+    @bot.api.send_message(chat_id: @user_id, text: text, reply_markup: markup)
+  end
 end
 
 # Class command '/start'
@@ -91,7 +115,7 @@ class BotSubject < BotMain
       if /^\d(\d)*?$/ =~ answer.text && answer.text.to_i <= 28
         @lesson_numlab = answer.text
         telegram_send_message('Ок, записал.')
-        @redis.hmset("#{@user_id}-subject", @lesson, @lesson_numlab)
+        @redis.hset("#{@user_id}-subject", @lesson, @lesson_numlab)
         break
       elsif /^\d(\d)*?$/ =~ answer.text && answer.text.to_i > 28
         telegram_send_message('Ух ты, а в универе знают об этом?.')
@@ -136,7 +160,43 @@ end
 # Class command '/submit'
 class BotSubmit < BotMain
   def submit_message
-    telegram_send_message('Молодец! Какой предмет сдал(а)?')
+    if @redis.hgetall("#{@user_id}-subject") == {}
+      telegram_send_message("Список пуст.\nДобавить предмет и количество лабораторных работ по нему можно с помощью '/subject'")
+    else
+      telegram_send_keybord('Молодец! Какой предмет сдал(а)?', 'submit_list')
+    end
+  end
+
+  def submit_hundler(input)
+    input_submit = input
+    if /^[a-zA-Z]+$|^[а-яА-ЯЁё]+$/ =~ input_submit
+      @subject_name = input
+      if @redis.hget("#{@user_id}-subject-numlab", input).nil?
+        num_lab(input_submit)
+      elsif @redis.hget("#{@user_id}-subject-numlab", input) == '[]'
+        telegram_send_message('Ты уже рассчитался по этому предмету!')
+      else
+        telegram_send_keybord('Какая лаба?', 'submit_numlabs', input)
+      end
+    elsif /^\d(\d)*?$/ =~ input_submit
+      lab_remove(@subject_name, input)
+      telegram_send_message('Красавчег!')
+    end
+  end
+
+  def num_lab(name)
+    num = @redis.hget("#{@user_id}-subject", name).to_i
+    numlabs = (1..num).to_a
+    @redis.hset("#{@user_id}-subject-numlab", name, numlabs.to_s)
+    telegram_send_keybord('Какая лаба?', 'submit_numlabs', name)
+  end
+
+  def lab_remove(name, num_labs)
+    array_numlabs = @redis.hget("#{@user_id}-subject-numlab", name).delete('[,]').split
+    array_numlabs.delete(num_labs)
+    array_numlabs_numbers = []
+    array_numlabs.each { |unit| array_numlabs_numbers.push(unit.to_i) }
+    @redis.hset("#{@user_id}-subject-numlab", name, array_numlabs_numbers.to_s)
   end
 end
 
@@ -146,10 +206,13 @@ class BotReset < BotMain
     if @redis.hgetall("#{@user_id}-semestr") == {} && @redis.hgetall("#{@user_id}-subject") == {}
       telegram_send_message("Что бы что-то удалить, сначала надо что-то добавить...\nПодробнее смотри '/start'")
     else
-      telegram_send_message('Твои данные будут удалены безвозвратно. Продолжить?')
-      @redis.del("#{@user_id}-subject", "#{@user_id}-semestr")
-      telegram_send_message('Готово. Все успешно очищено.')
+      telegram_send_keybord('Твои данные будут удалены безвозвратно. Продолжить?', 'reset')
     end
+  end
+
+  def reset_command
+    @redis.del("#{@user_id}-subject", "#{@user_id}-semestr", "#{@user_id}-subject-numlab")
+    telegram_send_message('Готово. Все успешно очищено.')
   end
 end
 
@@ -166,21 +229,35 @@ Command_list = "Я могу тебе помочь сдать вовремя вс
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
-    case message.text
-    when "/start"
-      StartMessage.new(bot, message.chat.id).start_command
-    when "/semester"
-      BotSemester.new(bot, message.chat.id).semester_message
-    when "/subject"
-      BotSubject.new(bot, message.chat.id).subject_message
-    when "/status"
-      BotStatus.new(bot, message.chat.id).status_message
-    when "/submit"
-      BotSubmit.new(bot, message.chat.id).submit_message
-    when "/reset"
-      BotReset.new(bot, message.chat.id).reset_message
-    else
-      bot.api.send_message(chat_id: message.chat.id, text: "И что бы это значило..? Я тебя не понимаю =(\nСпиок известных мне комманд можно посмотреть с помощью '/start'.")
+    case message
+    when Telegram::Bot::Types::Message
+      case message.text
+      when '/start'
+        StartMessage.new(bot, message.chat.id).start_command
+      when '/semester'
+        BotSemester.new(bot, message.chat.id).semester_message
+      when '/subject'
+        BotSubject.new(bot, message.chat.id).subject_message
+      when '/status'
+        BotStatus.new(bot, message.chat.id).status_message
+      when '/submit', 'я сдал', 'я сдала', 'Я сдал', 'Я сдала'
+        @submit_class = BotSubmit.new(bot, message.chat.id)
+        @submit_class.submit_message
+      when '/reset'
+        @reset_class = BotReset.new(bot, message.chat.id)
+        @reset_class.reset_message
+      else
+        bot.api.send_message(chat_id: message.chat.id, text: "И что бы это значило..? Я тебя не понимаю =(\nСпиок известных мне комманд можно посмотреть с помощью '/start'.")
+      end
+    when Telegram::Bot::Types::CallbackQuery
+      case message.data
+      when 'delete'
+        @reset_class.reset_command
+      when 'cancel'
+        bot.api.send_message(chat_id: message.from.id, text: 'Фууух, еле успел... Сброс отменен.')
+      else
+        @submit_class.submit_hundler(message.data)
+      end
     end
   end
 end
