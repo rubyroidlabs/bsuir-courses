67b4053b4efb49985c55e9f73a850cbcc8a496c7
SashaShostyr <sashashostyr@gmail.com>
2017-10-10 22:02:42 +0300

    Fixed issues-2

--- 2017/SashaShostyr/1/app.rb
+++ 2017/SashaShostyr/1/app.rb
@@ -4,36 +4,38 @@ require 'pry'
 require 'json'
 
 list_of_files = []
-Dir.foreach("trees") {| filename | list_of_files << filename if filename =~ /^\w/}
+Dir.foreach('trees') do |filename|
+	list_of_files << filename if filename =~ /^\w/
+end
 list_of_files.sort!
-if ENV["NAME"] == nil
+if ENV['NAME'].nil?
   answer = ''
-  list_of_files.each do | filename |
+  list_of_files.each do |filename|
     puts filename
-    file_name = "trees/" + filename
-    tree_str = File.read(file_name).gsub!("\n", "")
+    file_name = 'trees/' + filename
+    tree_str = File.read(file_name).delete!("\n")
     tree_arr = JSON.parse(tree_str)
     tr = Tree.new(tree_arr.shift)
     tr.create_tree(tree_arr)
-    tr.print_tree()
+    tr.print_tree
     loop do
-      print "Желаете продолжить? [y/n] "
+      print 'Желаете продолжить? [y/n] '
       answer = gets.chomp
-      break if answer == 'n' || answer == 'y'
-      puts "Введите: [y/n]"
+      break if ['n', 'y'].include? answer
+      puts 'Введите: [y/n]'
     end
     if answer == 'n'
-      puts "Спасибо что были в нашем лесу."
+      puts 'Спасибо что были в нашем лесу.'
       break
     end
   end
-elsif list_of_files.include? ENV["NAME"] + ".tree"
-  file_name = "trees/" + ENV["NAME"] + ".tree"
-  tree_str = File.read(file_name).gsub!("\n", "")
+elsif list_of_files.include? ENV['NAME'] + ".tree"
+  file_name = 'trees/' + ENV['NAME'] + '.tree'
+  tree_str = File.read(file_name).delete!("\n")
   tree_arr = JSON.parse(tree_str)
   tr = Tree.new(tree_arr.shift)
   tr.create_tree(tree_arr)
-  tr.print_tree()
+  tr.print_tree
 else
-  puts "Данное дерево не растет в данном лесу."
+  puts 'Данное дерево не растет в данном лесу.'
 end
