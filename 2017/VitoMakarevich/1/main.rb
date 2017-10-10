ec390a53a339d004706f0cdfdf847199543754af
v.makarevich@hackspace.by <vitaliy.makarevich.work@gmail.com>
2017-10-10 19:17:46 +0300

    change chop condition, beautify code

--- 2017/VitoMakarevich/1/main.rb
+++ 2017/VitoMakarevich/1/main.rb
@@ -1,5 +1,9 @@
 require_relative './tree_parser'
 require_relative './tree_printer'
+MAX_DEPTH = 5
+MAX_SUM = 5000
+CONTINUE_STATUS = 'y'
+STOP_STATUS = 'n'
 
 def print_tree(name)
   path = "./trees/#{name}"
@@ -7,14 +11,12 @@ def print_tree(name)
   tree_parser = TreeParser.new(json_tree)
   parsed_tree = tree_parser.get_layers
   tree_printer = TreePrinter.new(parsed_tree)
-  puts
-  puts name
+  puts '', name
   puts tree_printer.render
-  puts
-  if tree_parser.depth > 5 || tree_parser.sum > 100
-    puts 'Срубить.'
+  if parsed_tree.count > MAX_DEPTH || tree_parser.sum > MAX_SUM
+    puts '', 'Срубить.'
   else
-    puts 'Оставить.'
+    puts '', 'Оставить.'
   end
   puts
   print 'Желаете продолжить? [y/n] '
@@ -27,12 +29,10 @@ tree_names =
 iterator = 1
 print_tree(tree_names[0])
 while iterator != (tree_names.count - 1)
-  continue_status = 'y'
-  stop_status = 'n'
   user_answer = gets.chomp
-  if user_answer == continue_status
+  if user_answer == CONTINUE_STATUS
     print_tree(tree_names[iterator += 1])
-  elsif user_answer == stop_status
+  else user_answer == STOP_STATUS
     abort 'Спасибо что были в нашем лесу'
   end
 end
