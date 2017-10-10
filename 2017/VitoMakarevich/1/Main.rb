559ff3fab5771897312e4673d8c053382f0d0ae0
v.makarevich@hackspace.by <vitaliy.makarevich.work@gmail.com>
2017-10-10 11:49:48 +0300

    impl hw1

---
+++ 2017/VitoMakarevich/1/Main.rb
@@ -0,0 +1,39 @@
+require_relative "./TreeParser"
+require_relative "./TreePrinter"
+
+def print_tree(name)
+  path = "./trees/#{name}"
+  json_tree = File.read(path)
+  tree_parser = TreeParser.new(json_tree)
+  parsed_tree = tree_parser.get_layers()
+  tree_printer = TreePrinter.new (parsed_tree)
+  puts
+  puts name
+  puts tree_printer.render()
+  puts
+  if (tree_parser.depth > 5 || tree_parser.sum() > 100)
+    puts 'Срубить.'
+  else
+    puts 'Оставить.'
+  end
+  puts
+  print 'Желаете продолжить? [y/n] ' 
+end
+
+tree_names = Dir.entries('./trees').delete_if{|filename| filename == '.' || filename =='..'}.sort!
+
+iterator = 1
+print_tree(tree_names[0])
+while iterator != (tree_names.count - 1)
+  continue_status = 'y'
+  stop_status = 'n'
+  user_answer = gets.chomp
+  if user_answer == continue_status
+    print_tree(tree_names[iterator += 1])
+  elsif user_answer == stop_status
+    abort 'Спасибо что были в нашем лесу'
+  end
+end
+
+puts 'Спасибо что были в нашем лесу'
+
---
+++ 2017/VitoMakarevich/1/TreePrinter.rb
@@ -0,0 +1,41 @@
+class TreePrinter
+
+  def initialize(parsed_tree)
+  	@parsed_tree = parsed_tree
+    @rendered_tree = []
+  end
+
+  def render()
+  	@parsed_tree.reverse!
+   	@parsed_tree[0...@parsed_tree.length].each_with_index do |layer, index|
+      numbers_level = ''
+      numbers_level << ' ' * (2**(index + 1) - 2)
+      pointers_level = ''
+      pointers_level << ' ' * (2**(index + 1) - 2)
+      pointers = ['/', '\\']
+      layer.each_with_index do |element, inner_index|
+        if inner_index.even?
+          pointers_level << ' '
+          pointers_level << pointers[inner_index % 2]
+        else
+          pointers_level << pointers[inner_index % 2]
+          pointers_level << ' '
+        end
+        if inner_index != (layer.count - 1)
+          pointers_level << ' ' * (2**(index + 2) - 2)
+        end
+        numbers_level << (element / 10 > 0 ? element / 10 : ' ').to_s
+        numbers_level << (element % 10).to_s
+        if inner_index != (layer.count - 1)
+          numbers_level << ' ' * (2**(index + 2) - 2)
+        end
+      end
+      @rendered_tree.push(numbers_level)
+      if index != (@parsed_tree.count - 1)
+        @rendered_tree.push(pointers_level)
+      end
+
+    end
+    @rendered_tree.reverse
+  end
+end
