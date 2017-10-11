48f5b25a454d5755c887a2ace047148e1aef3a26
dmlaziuk <dm.laziuk@gmail.com>
2017-10-12 00:29:05 +0300

    deleted comments for rubocop

--- 2017/DmLaziuk/1/trees.rb
+++ 2017/DmLaziuk/1/trees.rb
@@ -3,16 +3,16 @@ require 'zip'
 require 'json'
 
 # @n -- глубина дерева
-@n=0
+@n = 0
 
 # hash -- хэш-таблица {[y, x] => элемент}
 #         y -- позиция вглубь дерева
 #         x -- позиция по оси x
-@hash = Hash.new([0,0])
+@hash = Hash.new([0, 0])
 
 # x[y] = текущая позиция x для каждого уровня y
 #        10 -- максимальная глубина дерева
-@x = Array.new(10) {0}
+@x = Array.new(10) { 0 }
 
 #
 # pasre -- проход по всем узлам массива arr
@@ -23,8 +23,8 @@ require 'json'
 #
 #          lvl -- текущая глубина
 #
-def parse (arr, lvl = 0)
-  @n = lvl>@n ? lvl : @n
+def parse(arr, lvl = 0)
+  @n = lvl > @n ? lvl : @n
   left = arr[0]
   right = arr[1]
 
@@ -44,29 +44,32 @@ def parse (arr, lvl = 0)
     @hash[[lvl, @x[lvl]]] = right
     @x[lvl] += 1
   end
-
 end
 
-
 # переменная окружения NAME
-name = ENV["NAME"]
+name = ENV['NAME']
 
 if name
   Zip::File.open('trees.zip') do |zip_file|
     if zip_file.find_entry("trees/#{name}.tree")
-      content = zip_file.read("trees/#{name}.tree")    # content - строка данных
-      tree = JSON.parse(content)    # tree - массив данных в виде дерева
+      content = zip_file.read("trees/#{name}.tree")
+      # content - строка данных
+      tree = JSON.parse(content)
+      # tree - массив данных в виде дерева
       parse(tree)
 
       # все сделано, осталось только вывести на экран
 
-      str = []    # str -- массив строк для вывода на экран
-      (0..2*@n).each {|i| str[i] = ' '*(4*2**@n)}    # инициализация пробелами
+      # str -- массив строк для вывода на экран
+      #        размером 2*n, длина каждой строки 4*2^n
+      str = []
+      # инициализация пробелами
+      (0..2 * @n).each { |i| str[i] = ' ' * (4 * 2**@n) }
       # расстановка веток
-      (0...@n).each {|y| (0...2**y).each {|x| str[2*y+1][2**(@n+1-y) + x*2**(@n+2-y)+1, 2] = '/\\'}}
+      (0...@n).each { |y| (0...2**y).each { |x| str[2 * y + 1][2**(@n + 1 - y) + x * 2**(@n + 2 - y) + 1, 2] = '/\\' } }
       # расстановка узлов
-      @hash.each {|key, value| str[key[0]*2][2**(@n+1-key[0]) + key[1]*2**(@n+2-key[0]), 4] = "%3d " % value}
-      str.each {|s| puts s}
+      @hash.each { |key, value| str[key[0] * 2][2**(@n + 1 - key[0]) + key[1] * 2**(@n + 2 - key[0]), 4] = '%3d ' % value }
+      str.each { |s| puts s }
     else
       puts 'Данное дерево не растет в данном лесу.'
     end
