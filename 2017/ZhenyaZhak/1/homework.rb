0f3e7f2a891692a6a06c5975ef595ef9fc4aa462
ZhenyaZhak <zhenya-zhak@mail.ru>
2017-10-12 17:26:36 +0300

    implement hw 1

---
+++ 2017/ZhenyaZhak/1/homework.rb
@@ -0,0 +1,98 @@
+require 'json'
+
+def steep(value)
+  (0...value).each do
+    print " "
+  end             
+end
+
+def decision(kol, level)
+  if kol > 5000
+    puts "Срубить"
+  else
+    puts "Оставить"
+  end
+  if level > 6 
+    puts "Дерево обрезанно"
+  else
+    puts "Дерево целое"
+  end
+end
+
+def rendering(vec, level)
+  level > 6 ? vrem = 6 : vrem = level
+  t = 0
+  steep(2**vrem - 2)
+  vec.length.times do |i|
+    if vec[i] == nil
+      puts " "
+      vrem -= 1
+      if vrem == 0
+        break
+      end
+      steep(2**vrem - 2)
+      t = 0
+    else
+      t == 1 ? steep(2**(vrem + 1) - 2) : t = 1
+      if vec[i] < 10
+        print " "
+      end
+      print vec[i]
+    end
+  end
+end
+
+class Tree
+  def self.draw(name)   
+    vec = Array.new()
+    array = Array.new()
+    array_temp = Array.new()
+    file_name = "/home/zhenya/Документы/trees/" + name
+    tree_string = File.read(file_name)
+    tree_inf = JSON.parse(tree_string)
+    array << tree_inf
+    while array.length != 0
+      array.length.times do |i|       
+        if array[i][0].is_a? Integer          
+          vec << array[i][0]
+        else
+          array_temp << array[i][0]
+        end
+        if array[i][1].is_a? Integer          
+          vec << array[i][1]
+        else
+          array_temp << array[i][1]
+        end       
+      end     
+      if vec[vec.length - 1] != nil
+        vec << nil      
+      end
+      array = Array.new()
+      array += array_temp
+      array_temp = Array.new()
+    end
+    kol = 0
+    level = 0
+    vec.length.times do |i|
+      vec[i] == nil ? level += 1 : kol += vec[i]
+    end   
+    rendering(vec, level)   
+    decision(kol, level)
+  end
+end
+
+if ENV['NAME'].nil?
+  list = Dir.entries("/home/zhenya/Документы/trees")  
+  list.sort!  
+  list.length.times do |i|
+    if list[i].include?(".tree")
+      Tree.draw(list[i])
+      print "Do it! [y/n]"; per = gets  
+      if per.include?("n")        
+        break
+      end
+    end   
+  end   
+else
+  Tree.draw(ENV['NAME'] + ".tree")
+end
