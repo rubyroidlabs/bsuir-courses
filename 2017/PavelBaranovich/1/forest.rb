aa7f2afa63b5864454d35da6d0c59bc407c874cc
Pavel Baranovich <baranovich.pavel@yandex.ru>
2017-10-13 19:37:17 +0300

    fix styling issues

--- 2017/PavelBaranovich/1/forest.rb
+++ 2017/PavelBaranovich/1/forest.rb
@@ -10,35 +10,35 @@ end.compact.sort
 tree_name = ENV['NAME']
 
 if tree_name
-	tree_name += '.tree'
+  tree_name += '.tree'
 
-	if tree_name == '.tree'
-		puts 'Безымянных деревьев у нас не растёт'
-	elsif path.include?(tree_name)
-		solve(tree_name)
-	else
-		puts 'Данное дерево не растет в данном лесу'
-	end
+  if tree_name == '.tree'
+    puts 'Безымянных деревьев у нас не растёт'
+  elsif path.include?(tree_name)
+    solve(tree_name)
+  else
+    puts 'Данное дерево не растет в данном лесу'
+  end
 else
-	path.each do |file_name|
-		puts file_name
-		
-		array = solve(file_name)
+  path.each do |file_name|
+    puts file_name
+    
+    array = solve(file_name)
 
-		if	array.inject{ |sum, n| sum.to_i + n.to_i }.to_i > 5000
-			puts 'Срубить'
-		elsif get_depth(array.size) > 5
-			puts 'Обрезать'	
-		else
-			puts 'Оставить'	
-		end
+    if  array.inject{ |sum, n| sum.to_i + n.to_i }.to_i > 5000
+      puts 'Срубить'
+    elsif get_depth(array.size) > 5
+      puts 'Обрезать' 
+    else
+      puts 'Оставить' 
+    end
 
-		print 'Желаете продолжить? [y/n] '	
-		input = gets.chomp.downcase
-		if input == 'n' || 'trees/' + path[path.size - 1] == file_name
-			break;
-		end
-	end
+    print 'Желаете продолжить? [y/n] '  
+    input = gets.chomp.downcase
+    if input == 'n' || 'trees/' + path[path.size - 1] == file_name
+      break;
+    end
+  end
 
-	puts 'Спасибо что были в нашём лесу'
+  puts 'Спасибо что были в нашём лесу'
 end
--- 2017/PavelBaranovich/1/methods.rb
+++ 2017/PavelBaranovich/1/methods.rb
@@ -1,98 +1,98 @@
 #!/usr/bin/env ruby
 
 def print_tree(depth, space_count, prev_space_count, tree, max_depth)
-	if depth >= 0 
-		print_tree(depth - 1, 2 * space_count + 1, space_count - 1, tree, max_depth)
+  if depth >= 0 
+    print_tree(depth - 1, 2 * space_count + 1, space_count - 1, tree, max_depth)
 
-		count = 1 << depth
-		answer= " " * prev_space_count
-		while count < (1 << (depth + 1))
-			if tree[count] == nil
-				answer += " " * (space_count + 1)
-			elsif tree[count].to_i < 10
-				answer += tree[count] + " " * space_count
-			else
-				answer += tree[count] + " " * (space_count - 1)
-			end
-			count += 1
-		end	
+    count = 1 << depth
+    answer= " " * prev_space_count
+    while count < (1 << (depth + 1))
+      if tree[count] == nil
+        answer += " " * (space_count + 1)
+      elsif tree[count].to_i < 10
+        answer += tree[count] + " " * space_count
+      else
+        answer += tree[count] + " " * (space_count - 1)
+      end
+      count += 1
+    end 
 
-		if depth > 0	
-			slashes = " " * answer.size
-			i = space_count
-			indent = 1 << (max_depth - depth)
-			distance = indent << 3
-			while i < answer.size
-				slashes[i - indent - 1] = '/'
-				slashes[i + indent - 1] = '\\'
-				i += distance;
-			end
+    if depth > 0  
+      slashes = " " * answer.size
+      i = space_count
+      indent = 1 << (max_depth - depth)
+      distance = indent << 3
+      while i < answer.size
+        slashes[i - indent - 1] = '/'
+        slashes[i + indent - 1] = '\\'
+        i += distance;
+      end
 
-			puts slashes
-		end
-		puts answer
-	end
+      puts slashes
+    end
+    puts answer
+  end
 end
 
 def create_tree(file, numbers, str)
-	vertex = 1
-	i = 1
-	j = 0
+  vertex = 1
+  i = 1
+  j = 0
 
-	tree = []
-	tree[vertex] = numbers[0]
-	while i < str.size - 1
-		if str[i] + str[i + 1] == ',['
-			vertex <<= 1
+  tree = []
+  tree[vertex] = numbers[0]
+  while i < str.size - 1
+    if str[i] + str[i + 1] == ',['
+      vertex <<= 1
 
-			if tree[vertex << 1] != nil
-				vertex += 1	
-			end
+      if tree[vertex << 1] != nil
+        vertex += 1 
+      end
 
-			j += 1
-			tree[vertex] = numbers[j]
-		elsif str[i] == ','
-			vertex += 1
-			j += 1
-			tree[vertex] = numbers[j]
-		elsif str[i] == ']'
-			if tree[vertex << 1] != nil && tree[(vertex << 1) + 1] == nil
-				vertex <<= 1
-			end
-			vertex >>= 1
-		end
+      j += 1
+      tree[vertex] = numbers[j]
+    elsif str[i] == ','
+      vertex += 1
+      j += 1
+      tree[vertex] = numbers[j]
+    elsif str[i] == ']'
+      if tree[vertex << 1] != nil && tree[(vertex << 1) + 1] == nil
+        vertex <<= 1
+      end
+      vertex >>= 1
+    end
 
-		i += 1
-	end
+    i += 1
+  end
 
-	tree
+  tree
 end
 
 def get_depth(size)
-	depth = 0
-	count = size - 1
-	while count != 0
-		depth += 1
-		count >>= 1
-	end
+  depth = 0
+  count = size - 1
+  while count != 0
+    depth += 1
+    count >>= 1
+  end
 
-	depth
+  depth
 end
 
 def solve(file_name)
-	file_name = 'trees/' + file_name
+  file_name = 'trees/' + file_name
 
-	File.open(file_name) do |file|
-		file = file.read
-		numbers = file.scan(/\d\d*/)
-		str = file.delete('0-9')
-			
-		tree = create_tree(file, numbers, str)
+  File.open(file_name) do |file|
+    file = file.read
+    numbers = file.scan(/\d\d*/)
+    str = file.delete('0-9')
+      
+    tree = create_tree(file, numbers, str)
 
-		depth = get_depth(tree.size)
+    depth = get_depth(tree.size)
 
-		print_tree(depth - 1, 3, 0, tree, depth - 1)
+    print_tree(depth - 1, 3, 0, tree, depth - 1)
 
-		numbers
-	end
+    numbers
+  end
 end
