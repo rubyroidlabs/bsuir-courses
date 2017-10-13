2711b1e18a185be39b6812887c5af45241d79a2c
Аля <ala@MacBook-Air-Ala.local>
2017-10-13 16:37:02 +0300

    fix styling issues 2

--- 2017/AlyaTorshina/1/homework1.rb
+++ 2017/AlyaTorshina/1/homework1.rb
@@ -7,15 +7,15 @@ class Tree
     @root = Node.new(root)
     @root.level = 1
     @depth = 0
-    @sum = 0   
+    @sum = 0
   end
 
   def create(arr)
     @elements = [[@root.level, @root.value]]
-    add_node @root, arr
+    add_node(@root, arr)
     print_list
     check_attributes
-  end  
+  end
 
   def add_node(node, arr)
     left = arr[0][0]
@@ -23,31 +23,31 @@ class Tree
     if left.class.equal?Array
       node.left = Node.new(left.shift)
       node.left.root = node
-      check_level node.left, node.left
-      @elements.push [node.left.level, node.left.value] 
+      check_level(node.left, node.left)
+      @elements.push([node.left.level, node.left.value])
       @sum += node.left.value
-      add_node node.left, left   
-    elsif left.class.equal?Integer 
+      add_node(node.left, left)
+    elsif left.class.equal?Integer
       node.left = Node.new(left)
       node.left.root = node
-      check_level node.left, node.left
-      @elements.push [node.left.level, node.left.value] 
-      @sum += node.left.value   
-    end  
+      check_level(node.left, node.left)
+      @elements.push([node.left.level, node.left.value])
+      @sum += node.left.value
+    end
     if right.class.equal?Array
       node.right = Node.new(right.shift)
       node.right.root = node
-      check_level node.right, node.right
-      @elements.push [node.right.level, node.right.value] 
+      check_level(node.right, node.right)
+      @elements.push([node.right.level, node.right.value])
       @sum += node.left.value
-      add_node node.right, right 
+      add_node(node.right, right)
     elsif right.class.equal?Integer
       node.right = Node.new(right)
       node.right.root = node
-      check_level node.right, node.right
-      @elements.push [node.right.level, node.right.value] 
-      @sum += node.left.value    
-    end   
+      check_level(node.right, node.right)
+      @elements.push([node.right.level, node.right.value])
+      @sum += node.left.value
+    end
   end
 
   def check_level(current_node, next_node)
@@ -58,12 +58,12 @@ class Tree
     end
     if continue
       current_node.level += 1
-      check_level current_node, link
-    end    
-  end   
-  
-  def check_attributes 
-    @elements.each do |item| 
+      check_level(current_node, link)
+    end
+  end
+
+  def check_attributes
+    @elements.each do |item|
       if @depth < item[0]
         @depth = item[0]
       end
@@ -71,19 +71,19 @@ class Tree
     if @sum > 5000
       puts 'Срубить'
     elsif @depth > 5
-      puts 'Обрезать'  
+      puts 'Обрезать'
     else
-      puts 'Оставить'  
-    end 
+      puts 'Оставить'
+    end
   end
 
   def print_list
     str = ['', '', '', '', '']
     @elements.each do |item|
       if item[0] - 1 < 5
-        str[item[0]-1] << "#{item[1]} " 
-      end 
-    end 
+        str[item[0] - 1] << "#{item[1]} "
+      end
+    end
     str.each { |item| puts item }
   end
 end
@@ -102,9 +102,9 @@ end
 
 files = []
 Dir['trees/*.tree'].sort.each do |filename|
-  filename.slice!(0..5) 
+  filename.slice!(0..5)
   files.push(filename)
-end 
+end
 if ENV['NAME'].nil?
   answer = ''
   files.each do |filename|
@@ -112,7 +112,7 @@ if ENV['NAME'].nil?
     content = File.read('trees/' + filename)
     input = JSON.parse(content)
     our_tree = Tree.new(input.shift)
-    our_tree.create input
+    our_tree.create(input)
     print 'Желаете продолжить? [y/n] : '
     answer = gets
     if answer[0] == 'n'
@@ -120,9 +120,9 @@ if ENV['NAME'].nil?
       break
     end
     if answer[0] != 'y'
-      puts 'Вы ввели неправильный ответ. Спасибо что были в нашем лесу.'  
+      puts 'Вы ввели неправильный ответ. Спасибо что были в нашем лесу.'
       break
-    end  
+    end
   end
 elsif files.include?(ENV['NAME'] + '.tree')
   filename = ENV['NAME'] + '.tree'
@@ -130,7 +130,7 @@ elsif files.include?(ENV['NAME'] + '.tree')
   content = File.read('trees/' + filename)
   input = JSON.parse(content)
   our_tree = Tree.new(input.shift)
-  our_tree.create input
-else 
+  our_tree.create(input)
+else
   puts 'Данное дерево не растет в данном лесу.'
 end
