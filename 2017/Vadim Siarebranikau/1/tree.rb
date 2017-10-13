616daad2a7d564517a403e2f82bbcdc1b54ef84e
Vadser <vadser1999@gmail.com>
2017-10-13 20:55:23 +0300

    fix styling issues

--- 2017/Vadim Siarebranikau/1/tree.rb
+++ 2017/Vadim Siarebranikau/1/tree.rb
@@ -11,10 +11,10 @@ $bool = true
 fh = open filename
 content = fh.read
 fh.close
-content=JSON.parse(content)
+content = JSON.parse(content)
 class Node
 attr_accessor :value,:left,:right
-  def initialize (value,left=nil,right=nil)
+  def initialize (value,left = nil,right = nil)
     @value=value
     @left=left
     @right=right
@@ -22,67 +22,65 @@ attr_accessor :value,:left,:right
 end
 class Tree
   def make_tree(array)
-      node=Node.new(array[0])
-      node.left=array[1][0].is_a?(Numeric) ? Node.new(array[1][0]) : make_tree(array[1][0])
-      node.right=array[1][1].is_a?(Numeric) ? Node.new(array[1][1]) : make_tree(array[1][1])
-      node
+    node = Node.new(array[0])
+    node.left = array[1][0].is_a?(Numeric) ? Node.new(array[1][0]) : make_tree(array[1][0])
+    node.right = array[1][1].is_a?(Numeric) ? Node.new(array[1][1]) : make_tree(array[1][1])
+    node
    end
 end
-root=Tree.new.make_tree(content)
+root = Tree.new.make_tree(content)
 def do_array(tree)
-
-     $a[$b].push(tree.value)
-
-     if tree.left != nil
-        $b += 1
-        do_array(tree.left)
-     end
-     if tree.right!=nil
-        $b += 1
-        do_array(tree.right)
-     end
-     $b -= 1
+  $a[$b].push(tree.value)
+  if tree.left != nil
+    $b += 1
+    do_array(tree.left)
+  end
+  if tree.right!=nil
+    $b += 1
+    do_array(tree.right)
+  end
+  $b -= 1
 end
 
 def do_hight(tree)
-      if tree.left != nil
-         if $bool
-            $b += 1
-         end
-         do_hight(tree.left)
-      end
-      if tree.right!=nil
-         if $bool
-         $b += 1
-         end
-      do_hight(tree.right)
-      end
-      $bool = false
+  if tree.left != nil
+    if $bool
+      $b += 1
+    end
+    do_hight(tree.left)
+  end
+  if tree.right!=nil
+    if $bool
+      $b += 1
+    end
+    do_hight(tree.right)
+  end
+  $bool = false
 end
 
 def output(array)
-    i = 0
-    l = 0
-    while i<$a[$hight-1].size
-    l += $a[$hight-1][i].to_s.length;
-    i +=1
+  i = 0
+  l = 0
+  while i<$a[$hight - 1].size
+    l += $a[$hight - 1][i].to_s.length;
+    i += 1
+  end
+  l = l + $a[$hight - 1].size
+  i = 0
+  while i<$hight
+    j = 0
+    string = ''
+    while j < $a[i].size
+      s = space((l - $a[i][j].to_s.size))
+      s2 = space(l)
+      string = string + s2 + $a[i][j].to_s + s2
+      j += 1
     end
-    l = l + $a[$hight-1].size
-    i = 0
-    while i<$hight
-      j = 0
-      string = ''
-      while j<$a[i].size
-          s = space((l - $a[i][j].to_s.size))
-          s2 = space(l)
-          string = string + s2 + $a[i][j].to_s + s2
-          j += 1
-      end
-      puts string
-      l /= 2
-      s = space(l)
-      i += 1
-    end
+    puts string
+    l /= 2
+    s = space(l)
+    i += 1
+  end
 end
 
 def space(length)
