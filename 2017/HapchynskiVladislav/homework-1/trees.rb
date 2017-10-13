86643f67b28337c6b530697615b49ae69b9c2d36
Vlad <renkishby@gmail.com>
2017-10-13 22:17:09 +0300

    fix syntax

--- 2017/HapchynskiVladislav/homework-1/trees.rb
+++ 2017/HapchynskiVladislav/homework-1/trees.rb
@@ -1,39 +1,37 @@
 require 'json'
 require 'pry'
-###########
 
-name = ENV["NAME"]
+name = ENV['NAME']
 forest = []
 Dir.foreach('trees') do |filename|
   forest << filename if filename =~ /^\w/
 end
 forest.sort!
-if forest.include? (name + ".tree")
-  f = 'trees/' + name + ".tree"
+if forest.include? (name + '.tree')
+  f = 'trees/' + name + '.tree'
   tree_str = File.read(f).delete!("\n")
   tree = JSON.parse(tree_str)
-  #puts tree.class
   print tree
 elsif
-  puts "Данное дерево не растет в данном лесу."
+  puts 'Данное дерево не растет в данном лесу.'
 elsif name.empty?
-  puts "Безымянных деревев у нас не растет."
-  answer = ""
+  puts 'Безымянных деревев у нас не растет.'
+  answer = ''
   forest.each do |filename|
     puts filename
     f = 'trees/' + filename
     tree_str = File.read(f).delete!("\n")
     tree = JSON.parse(tree_str)
-    ##print tree
+    # print tree
     loop do
       print tree
-      print "Желаете продолжить? [y/n] "
+      print 'Желаете продолжить? [y/n] '
       answer = gets.chomp
       break if %w(y n).include? answer
-      puts "Желаете продолжить? [y/n] "
-      end
-    if answer == "n"
-      puts "Спасибо что были в нашем лесу."
+      puts 'Желаете продолжить? [y/n] '
+    end
+    if answer == 'n'
+      puts 'Спасибо что были в нашем лесу.'
       break
     end
   end
