f762e218772a97c20d04ebd85fc6489079d047bd
MoRain666 <morainrallez@gmail.com>
2017-10-13 18:45:23 +0300

    Fix5

--- 2017/VladKharitonovitch/hm-1/derevo.rb
+++ 2017/VladKharitonovitch/hm-1/derevo.rb
@@ -5,13 +5,13 @@ entry = ENV["NAME"].to_s.downcase
 if entry.empty?
   puts "Безымянных деревьев у нас не растет."
 else
-  hash_of_trees=Hash.new ("")
-  Dir.foreach("trees") {|tree_recording| hash_of_trees[tree_recording]="name" }
-  key_check=hash_of_trees.has_key?("#{entry}.tree")
+  hash_of_trees = Hash.new ("")
+  Dir.foreach("trees") {|tree_recording| hash_of_trees[tree_recording] = "name" }
+  key_check = hash_of_trees.has_key?("#{entry}.tree")
   if key_check
-    file=File.new ("trees/#{entry}.tree")
-    content=file.read
-    array=JSON.parse(content)
+    file = File.new ("trees/#{entry}.tree")
+    content = file.read
+    array = JSON.parse(content)
     parsing(array)
     after_update_content=@after_parsing_arr
     after_update_content.each_with_index do |lvl ,index|
--- 2017/VladKharitonovitch/hm-1/parsing.rb
+++ 2017/VladKharitonovitch/hm-1/parsing.rb
@@ -1,7 +1,7 @@
-@after_parsing_arr=[]
+@after_parsing_arr = []
 def parsing (array)
-  array_of_nodes=[]
-  current_list=[]
+  array_of_nodes = []
+  current_list = []
   array.each do |x|
     if  x.is_a? Array
       array_of_nodes += x
@@ -12,7 +12,7 @@ def parsing (array)
   if current_list.empty? == false 
     @after_parsing_arr<<current_list
   end
-  if array_of_nodes.size != 0
+  if array_of_nodes.size !=0
     parsing(array_of_nodes)
   end
 end
