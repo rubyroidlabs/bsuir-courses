6136ec8179c4cf9b19ebc4c72584cb70c6cb36c9
vlukyaneRubyLabs <vlukyane@gmail.com>
2017-10-13 05:02:48 +0300

    fix styling issues 4

--- 2017/VyacheslavLukyanenko/1/vlukyane.rb
+++ 2017/VyacheslavLukyanenko/1/vlukyane.rb
@@ -6,60 +6,60 @@ def is_number? string
   true if Float(string) rescue false
 end
 
-def build_Tree_Divide_And_Conquer(tree_Array, level_Of_Depth, branch_Dir)
+def build_tree_divide_and_conquer(tree_array, level_of_depth, branch_dir)
 
-  l = tree_Array[0]
-  r = tree_Array[1]
-  @maximal_depth = level_Of_Depth > @maximal_depth? level_Of_Depth : @maximal_depth
-  if (l.is_a?(Integer))
-    @a[level_Of_Depth].push([l, branch_Dir])
+  l = tree_array[0]
+  r = tree_array[1]
+  @maximal_depth = level_of_depth > @maximal_depth? level_of_depth : @maximal_depth
+  if l.is_a?(Integer)
+    @a[level_of_depth].push([l, branch_dir])
     @vertex_sum += l
-    level_Of_Depth += 1
+    level_of_depth += 1
   end
 
   if r.is_a?(Integer)
     if l.is_a?(Integer)
-      level_Of_Depth -= 1
+      level_of_depth -= 1
     end
-    @a[level_Of_Depth].push([r, branch_Dir])
-  end
+    @a[level_of_depth].push([r, branch_dir])
     @vertex_sum += r
+  end
 
   if l.is_a?(Array)
-    build_Tree_Divide_And_Conquer(l, level_Of_Depth, 'l')
+    build_tree_divide_dnd_donquer(l, level_of_depth, 'l')
   end
 
   if r.is_a?(Array)
-    build_Tree_Divide_And_Conquer(r, level_Of_Depth, 'r')
+    build_tree_divide_and_conquer(r, level_of_depth, 'r')
   end
 end
 
-def build_Print_Tree
+def build_print_tree
   19.times do
     @a.push([])
   end
 end
 
-def print_Tree
+def print_tree
   n = @maximal_depth
-  (0..2 * (2 ** (n - 1))).each do |i|
+  (0..2 * (2**(n - 1))).each do |i|
     print ' '
   end
   print @a[1][0][0]
   puts
 
-  (1 .. n - 1).each do |i|
+  (1..n - 1).each do |i|
     str = String.new('')
     count = 0
-    # Вывод значений
-    (1..(2 ** (n - i) - 1)).each do
+    # Print values
+    (1..(2**(n - i) - 1)).each do
       str += ' '
     end
     str += @a[i + 1][count][0].to_s
     count += 1
     k = 1
-    (1 .. (2 ** i) - 1).each do
-      (1..2 ** (n - i + 1) - 1).each do
+    (1..(2**i) - 1).each do
+      (1..2**(n - i + 1) - 1).each do
         str += ' '
       end
       if k.zero?
@@ -89,7 +89,6 @@ def print_Tree
         print ' '
       elsif i == ' '
         print ' '
-        calc += 1
         flag = false
       end
     end
@@ -116,12 +115,12 @@ def level3
       puts entry.name
       content = entry.get_input_stream.read
       @a = []
-      build_Print_Tree
+      build_print_tree
       @vertex_sum = 0
       @maximal_depth = 0
       parsed_json_tree = JSON.parse(content)
-      build_Tree_Divide_And_Conquer(parsed_json_tree, 1, 'c')
-      print_Tree
+      build_tree_divide_and_conquer(parsed_json_tree, 1, 'c')
+      print_tree
       make_choise
       puts "\nХотите продолжить? [y/n]: "
       user_ans = gets.to_s
@@ -144,7 +143,7 @@ else
     if zip_file.find_entry("trees/#{name}.tree")
       content = zip_file.read("trees/#{name}.tree")
       @a = []
-      buildPrintTree
+      build_print_tree
       @vertex_sum = 0
       @maximal_depth = 0
       parsed_json_tree = JSON.parse(content)
