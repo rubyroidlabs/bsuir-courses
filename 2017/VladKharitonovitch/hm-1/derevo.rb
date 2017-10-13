d00de85218eb4c03ab4dbdf31e25a6e2d1df5e8a
MoRain666 <morainrallez@gmail.com>
2017-10-13 14:27:01 +0300

    Fix

--- 2017/VladKharitonovitch/hm-1/derevo.rb
+++ 2017/VladKharitonovitch/hm-1/derevo.rb
@@ -1,34 +1,28 @@
 #!/usr/bin/env ruby
 require 'json'
 require_relative ("parsing.rb")
-  vhod=ENV["NAME"].to_s.downcase
-  if vhod.empty?
+entry=ENV["NAME"].to_s.downcase
+  if entry.empty?
     puts "Безымянных деревьев у нас не растет."
   else
-#просмотр файлов в каталоге trees
-  a=Dir.entries('trees')
-#puts a
-#делаем новый хэш имён из файлов в каталоге trees
-  hash_of_trees=Hash.new ("")
-  Dir.foreach("trees") {|x| hash_of_trees[x]="name" }
-#puts hash_of_trees
-#проверяет хэш на наличие искомого значения,если есть,то возвращает true
-  b=hash_of_trees.has_key?("#{vhod}.tree")
-    if b
-      f=File.new ("trees/#{vhod}.tree")
-      content=f.read
+    a=Dir.entries('trees')
+    hash_of_trees=Hash.new ("")
+    Dir.foreach("trees") {|tree_recording| hash_of_trees[tree_recording]="name" }
+    key_check=hash_of_trees.has_key?("#{entry}.tree")
+    if key_check
+      file=File.new ("trees/#{entry}.tree")
+      content=file.read
       array=JSON.parse(content)
       parsing(array)
       after_update_content=@after_parsing_arr
-        @i=1
+        @counter=1
         after_update_content.each do |lvl|
-          print "lvl #{@i} дерева : "
-          @i+=1
+          print "lvl #{@counter} дерева : "
+          @counter+=1
           lvl.each{|node| print"#{node} "}
           puts  
-        end
-               
+        end             
     else
       puts "нету такого дерева"
     end
-end
+  end
--- 2017/VladKharitonovitch/hm-1/derevo_second_task.rb
+++ 2017/VladKharitonovitch/hm-1/derevo_second_task.rb
@@ -1,33 +1,28 @@
 #!/usr/bin/env ruby
 require 'json'
 require_relative ("parsing.rb")
-#просмотр файлов в каталоге trees
-  a=Dir.entries('trees')
-#puts a
-#делаем новый хэш имён из файлов в каталоге trees
-  array_of_trees=Array.new
-  Dir.foreach("trees") {|x| array_of_trees<<x }
-#puts array_of_trees
-#сортировка массива
-a=array_of_trees.sort!
-a.delete(".")
-a.delete("..")
+entry=Dir.entries('trees')
+array_of_trees=Array.new
+Dir.foreach("trees") {|x| array_of_trees<<x }
+entry=array_of_trees.sort!
+entry.delete(".")
+entry.delete("..")
 puts "Добро пожаловать в лес,деревья сортированы по алфовиту!"
-a.each do |file_name|
-  f=File.new ("trees/#{file_name}")
+entry.each do |file_name|
+  file=File.new ("trees/#{file_name}")
   puts "Это дерево #{file_name}"
-  content=f.read
+  content=file.read
   array=JSON.parse(content)
   parsing(array)
   after_update_content=@after_parsing_arr
-    @i=1
-    after_update_content.each do |lvl|
-      print "lvl #{@i} дерева : "
-      @i+=1
-      lvl.each{|node| print"#{node} "}
-      puts  
-    end
-    puts "Желаете продолжить? [y/n]"
-    otvet=gets.chomp
-    break if otvet == "n"
+  @counter=1
+  after_update_content.each do |lvl|
+    print "lvl #{@counter} дерева : "
+    @counter+=1
+    lvl.each{|node| print"#{node} "}
+    puts  
+  end
+puts "Желаете продолжить? [y/n]"
+answer=gets.chomp
+break if answer == "n"
 end
