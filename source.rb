cec22763086479e0ef52b8bad100bb4d992b449f
vivialprt <ivan.grigoryev502@gmail.com>
2017-10-13 23:54:08 +0300

    Update source.rb
    Added choice method

--- source.rb
+++ source.rb
@@ -1,11 +1,11 @@
 #[30,[[1,[[5,[[10,[14,38]],[38,[7,18]]]],[19,[[10,[19,16]],[29,[8,7]]]]]],[35,[[46,[[36,[19,22]],[44,[7,15]]]],[22,[[19,[17,42]],[46,[16,7]]]]]]]]
 require 'pry'
 require 'json'
+
 file = IO.read("./trees/" + ENV["NAME"] + ".tree")
-folder = IO.read("./trees")
-puts folder
+#folder =IO.read("./trees") 
 puts file
-puts "#{JSON.parse(file.gsub(/(\d+)/,'\1'))}"
+
 tree = JSON.parse(file.gsub(/(\d+)/,'\1'))
 
 def draw tree
@@ -19,6 +19,16 @@ def draw tree
 
 	end
 end
-#if tree.flatten.sum
+
+def choice tree
+	if tree.flatten.sum  > 5000
+		puts "Дерево слишком старое. Нужно срубить."
+	elsif Math.log2(tree.flatten.count + 1) > 5
+		puts "Дерево слишком высокое. Нужно укоротить."
+	else
+		puts"Оставить все как есть."
+	end
+end
+choice tree
 #draw tree
 #puts "#{tree.flatten}"
