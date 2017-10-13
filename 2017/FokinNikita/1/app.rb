abbdb602e9fde105508c3880ecced98fd7ca9ab8
Nikita Fokin <nikitafok.work@gmail.com>
2017-10-13 23:51:36 +0300

    init commit

---
+++ 2017/FokinNikita/1/app.rb
@@ -0,0 +1,32 @@
+require_relative 'libs/tree'
+require 'json'
+
+if ENV['NAME'].nil?
+  file_arr = []
+  Dir.foreach('trees') do |i|
+    file_arr << i
+  end
+
+  until file_arr[0].nil?
+    file = File.read('trees/' + file_arr.shift)
+    array = JSON.parse(file)
+    node = Node.new(array.shift)
+    node.insert(node, array.shift) until array.nil?
+    # node.print
+    node.check(node)
+    puts 'Желаете продолжить? [y/n]'
+    break if gets.chomp == 'n'
+  end
+else
+  if File.exist?('trees/' + ENV['NAME']+ '.tree')
+    file = File.read('trees/' + ENV['NAME'] + '.tree')
+    array = JSON.parse(file)
+    node = Node.new(array.shift)
+    node.insert(node, array.shift) until array.nil?
+    # node.print
+    node.check(node)
+  else
+    puts 'Дерева с таким именем нет'
+
+  end
+end
