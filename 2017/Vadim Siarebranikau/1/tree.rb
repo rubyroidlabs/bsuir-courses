bf4fbe941cc3888ef09ff47f1f094cd273875438
Vadser <vadser1999@gmail.com>
2017-10-13 18:04:55 +0300

    implement hw1

---
+++ 2017/Vadim Siarebranikau/1/tree.rb
@@ -0,0 +1,106 @@
+#!/usr/bin/env ruby 
+require"json"
+if ENV['NAME']==nil
+    puts "Безымянных деревьев нет"
+else
+filename ='trees/'+ENV['NAME']+'.tree'
+if (File.file?(filename))
+$hight = 1
+$b = 1
+$bool = true
+fh = open filename
+content = fh.read
+fh.close
+content=JSON.parse(content)
+class Node
+attr_accessor :value,:left,:right
+  def initialize (value,left=nil,right=nil)
+    @value=value
+    @left=left
+    @right=right
+  end
+end
+class Tree
+  def make_tree(array)
+      node=Node.new(array[0])
+      node.left=array[1][0].is_a?(Numeric) ? Node.new(array[1][0]) : make_tree(array[1][0])
+      node.right=array[1][1].is_a?(Numeric) ? Node.new(array[1][1]) : make_tree(array[1][1])
+      node
+   end
+end
+root=Tree.new.make_tree(content)
+def do_array(tree)
+
+     $a[$b].push(tree.value)
+
+     if tree.left != nil
+        $b += 1
+        do_array(tree.left)
+     end
+     if tree.right!=nil
+        $b += 1
+        do_array(tree.right)
+     end
+     $b -= 1
+end
+
+def do_hight(tree)
+      if tree.left != nil
+         if $bool
+            $b += 1
+         end
+         do_hight(tree.left)
+      end
+      if tree.right!=nil
+         if $bool
+         $b += 1
+         end
+      do_hight(tree.right)
+      end
+      $bool = false
+end
+
+def output(array)
+    i = 0
+    l = 0
+    while i<$a[$hight-1].size
+    l += $a[$hight-1][i].to_s.length;
+    i +=1
+    end
+    l = l + $a[$hight-1].size
+    i = 0
+    while i<$hight
+      j = 0
+      string = ''
+      while j<$a[i].size
+          s = space((l - $a[i][j].to_s.size))
+          s2 = space(l)
+          string = string + s2 + $a[i][j].to_s + s2
+          j += 1
+      end
+      puts string
+      l = l / 2
+      s = space(l)
+      i += 1
+   end
+end
+
+def space (length)
+     i = 1
+     s = ' '
+     while i<length
+        s+=' '
+        i+= 1
+     end
+     s
+end
+do_hight(root)
+$hight = $b
+$a = Array.new($hight){ |index| [] }
+$as = Array.new($hight){ |index| '' }
+$b = 0
+do_array(root)
+output ($a)
+ else puts "такого дерева нет"
+ end
+end
