494d40bd5886d9526829e4ec137d20d0992cb722
SergeiKholodtsov <sergeikhaladtsou@gmail.com>
2017-10-13 03:34:04 +0300

    Fix styling issues part 8

--- 2017/Kholodtsov Sergei/1/Tree.rb
+++ 2017/Kholodtsov Sergei/1/Tree.rb
@@ -1,5 +1,3 @@
-#!/usr/bin/env ruby
-
 class Tree
   attr_reader :root, :sum, :depth, :flag
 
@@ -60,26 +58,26 @@ class Tree
       list = []
       list = get_elem_level(i, root, list)
       counter -= counter
-      first_tab = counter
+      f_t = counter
       counter /= 2
-      second_tab = counter
-      second_tab.zero? ? first_line = ' ' * (second_tab + 1) : first_line = ' ' * first_tab
-      second_line = ' ' * second_tab
+      s_t = counter
+      s_t.zero? ? f_l = ' ' * (s_t + 1) : f_l = ' ' * f_t
+      s_l = ' ' * s_t
       list.each do |j|
         j = j.to_s
         if j.size == 1
           j += ' '
         end
-        if second_tab != 0 && second_tab + 1 != first_tab
-          l = first_tab - second_tab - 1
-          first_line += '_' * l + j + '_' * l + ' ' * (2 * second_tab + 4)
+        if s_t != 0 && s_t + 1 != f_t
+          l = f_t - s_t - 1
+          f_l += '_' * l + j + '_' * l + ' ' * (2 * s_t + 4)
         else
-          first_line += j + '  ' * (first_tab + 1)
+          f_l += j + '  ' * (f_t + 1)
         end
-        second_line += '/' + ' ' * first_tab + '\\' + ' ' * (first_tab + 2)
+        s_l += '/' + ' ' * f_t + '\\' + ' ' * (f_t + 2)
       end
-      puts first_line
-      puts second_line if i != depth - 1
+      puts f_l
+      puts s_l if i != depth - 1
     end
     if @sum > 5000
       puts 'Срубить.'
