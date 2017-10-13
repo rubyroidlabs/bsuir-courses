de6605edb99a1ff0bb4ae433a82198ae6cf2a22d
SergeiKholodtsov <sergeikhaladtsou@gmail.com>
2017-10-13 03:19:19 +0300

    Fix styling issues part 8

--- 2017/Kholodtsov Sergei/1/Main.rb
+++ 2017/Kholodtsov Sergei/1/Main.rb
@@ -2,39 +2,37 @@
 require_relative('Tree')
 require 'json'
 
-fileName = ENV["NAME"]	
+file_name = ENV["NAME"]
 ans = 'y'	
 data = ''
-fileList = []
+file_list = []
 Dir.foreach('trees') do |i|
-	fileList << i
+	file_list << i
 end
-fileList.sort!
-2.times { fileList.shift }
-unless fileName.nil? 
-	if fileName == ''
+file_list.sort!
+2.times { file_list.shift }
+unless file_name.nil?
+	if file_name == ''
 		puts "Безымянных деревьев у нас не растет."
 		exit 
 	end
-	if fileList.include?(fileName)
-		data = File.read("trees/" + fileName)
+	if file_list.include?(file_name)
+		data = File.read("trees/" + file_name)
 		elements = JSON.parse(data)
 		tree = Tree.new(elements.shift)
-		tree.createAndOut(elements)
-		#обработка дерева и вывод.
+		tree.create_and_out(elements)
 	else 
 		puts "Данное дерево не растет в данном лесу."
 	end
 else 
-	while fileList[0] != nil and ans == 'y' do
+	while file_list[0] != nil and ans == 'y' do
 		ans = 'm'	
-		fileName = fileList.shift
-		puts fileName
-		data = File.read("trees/" + fileName)
+		file_name = file_list.shift
+		puts file_name
+		data = File.read("trees/" + file_name)
 		elements = JSON.parse(data)
 		tree = Tree.new(elements.shift)
-		tree.createAndOut(elements)
-		#обработка дерева и вывод.
+		tree.create_and_out(elements)
 		print "Желаете продолжить? [y/n] "
 		ans = gets.chomp.downcase
 		while ans!='y' and ans!='n' do
--- 2017/Kholodtsov Sergei/1/Tree.rb
+++ 2017/Kholodtsov Sergei/1/Tree.rb
@@ -9,81 +9,77 @@ class Tree
     @depth = 1
   end
 
-  def createAndOut(item)
-    addNode(item, @root)
-    out_Tree(@root, @depth)
+  def create_and_out(item)
+    add_node(item, @root)
+    out_tree(@root, @depth)
   end
 
-  def addNode(item, node)
+  def add_node(item, node)
     left = item[0][0]
     right = item[0][1]
     if left.class == Array
       i = left.shift
       node.left = Node.new(i)
-      @flag != 1 ? @depth = @depth + 1 : @depth
-      @sum = @sum + i
-      addNode(left, node.left)
+      @flag != 1 ? @depth += 1 : @depth
+      @sum += i
+      add_node(left, node.left)
     else
       node.left = left
-      @sum = @sum + left
-      @flag != 1 ? @depth = @depth + 1 : @depth
+      @sum += left
+      @flag != 1 ? @depth += 1 : @depth
       @flag = 1
     end
     if right.class == Array
       i = right.shift
       node.right = Node.new(i)
-      @sum = @sum + i
-      addNode(right, node.right)
+      @sum += i
+      add_node(right, node.right)
     else
       node.right = right
-      @sum = @sum + right
+      @sum += right
     end
   end
 
-  def getElemLevel (level, root, elems)
+  def get_elem_level(level, root, elems)
     if root.class == Node
       if level.zero?
         elems << root.data
         elems
       end
-      elems = getElemLevel(level - 1, root.left, elems)
-      elems = getElemLevel(level - 1, root.right, elems)
+      elems = get_elem_level(level - 1, root.left, elems)
+      elems = get_elem_level(level - 1, root.right, elems)
     elsif level.zero?
       elems << root
     end
     elems
   end
 
-  def out_Tree(root, depth)
-    counter = (2 **(depth + 1) - 2) / 2
+  def out_tree(root, depth)
+    counter = (2**(depth + 1) - 2) / 2
     depth.times do |i|
       list = []
-      list = getElemLevel(i, root, list)
+      list = get_elem_level(i, root, list)
       counter -= counter
-      first_Tab = counter
+      first_tab = counter
       counter /= 2
-      second_Tab = counter
-      if second_Tab != 0
-        first_Line = ' ' * (second_Tab + 1)
-      else
-        first_Line = ' ' * first_Tab
-      end
-      second_Line = ' ' * second_Tab
+      second_tab = counter
+      second_tab.zero? ? first_line = ' ' * (second_tab + 1) : first_line = ' ' * first_tab
+      second_line = ' ' * second_tab
       list.each do |j|
         j = j.to_s
         if j.size == 1
           j += ' '
         end
-        if second_Tab != 0 && second_Tab + 1 != first_Tab
-        	l = first_Tab - second_Tab - 1
-          first_Line += '_' * l + j + '_' * l + ' ' * (2 * second_Tab + 4)
+        if second_tab != 0 && second_tab + 1 != first_tab
+          l = first_tab - second_tab - 1
+          first_line += '_' * l + j + '_' * l + ' ' * (2 * second_tab + 4)
         else
-          first_Line += j + '  ' * (first_Tab + 1)
+          first_line += j + '  ' * (first_tab + 1)
         end
-        second_Line += '/' + ' ' * first_Tab + '\\' + ' ' * (first_Tab + 2)
+        second_line += '/' + ' ' * first_tab + '\\' + ' ' * (first_tab + 2)
       end
-      puts first_Line
-      puts second_Line if i != depth - 1
+      puts first_line
+      puts second_line if i != depth - 1
     end
     if @sum > 5000
       puts 'Срубить.'
