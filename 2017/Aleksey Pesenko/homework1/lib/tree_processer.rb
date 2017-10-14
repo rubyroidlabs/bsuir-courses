4ae163f1d58cadde089a5be5ee4f0219b2ae1c3d
QueenAleks <MrTwisterOriginal@gmail.com>
2017-10-14 23:42:45 +0300

    (6) implement homework 1

--- 2017/Aleksey Pesenko/homework1/lib/tree_processer.rb
+++ 2017/Aleksey Pesenko/homework1/lib/tree_processer.rb
@@ -21,26 +21,38 @@ class TreeProcesser
   end
 
   def self.tree_depth(brunch)
-    left = brunch[1][0].is_a?(Integer) ? 1 : tree_depth(brunch[1][0])
-    right = brunch[1][1].is_a?(Integer) ? 1 : tree_depth(brunch[1][1])
+    left = value_for_counting_depth(brunch[1][0])
+    right = value_for_counting_depth(brunch[1][1])
     (left >= right ? left : right) + 1
   end
 
   def self.tree_age(brunch)
-    left = brunch[1][0].is_a?(Integer) ? brunch[1][0] : tree_age(brunch[1][0])
-    right = brunch[1][1].is_a?(Integer) ? brunch[1][1] : tree_age(brunch[1][1])
+    left = value_for_counting_age(brunch[1][0])
+    right = value_for_counting_age(brunch[1][1])
     brunch[0] + left + right
+  end
+
+  private_class_method def self.value_for_counting_age(val)
+    val.is_a?(Integer) ? val : tree_age(val)
+  end
+
+  private_class_method def self.value_for_counting_depth(val)
+    val.is_a?(Integer) ? val : tree_depth(val)
   end
 
   private_class_method def self.print_level(brunch, level)
     level -= 1
     if level.zero?
-      p = brunch.class == Array ? brunch[0] : brunch
-      print '%-2d' % p
-      print '  ' * (2**@spaces - 1)
+      helper_method(brunch)
     else
       print_level(brunch[1][0], level)
       print_level(brunch[1][1], level)
     end
+  end
+
+  private_class_method def self.helper_method(brunch)
+    p = brunch.class == Array ? brunch[0] : brunch
+    print format('%-2d', p)
+    print '  ' * (2**@spaces - 1)
   end
 end
