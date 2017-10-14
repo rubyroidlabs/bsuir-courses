71c205b05979930c53d92842ea8e15580fea443f
Vitalik <vitalikslim6@gmail.com>
2017-10-14 17:10:34 +0300

    fixed hw1

--- 2016/VitalikKhakimov/1/1.rb
+++ 2016/VitalikKhakimov/1/1.rb
@@ -1,9 +1,8 @@
-require 'zip'
 require 'json'
 
 @treedepth = 0
 @elements = Hash.new([0, 0])
-@currentposition = Array.new(10) { 0 }
+@currentposition = Array.new(100) { 0 }
 
 def parse(arr, lvl = 0)
   @treedepth = lvl > @treedepth ? lvl : @treedepth
@@ -24,16 +23,9 @@ def parse(arr, lvl = 0)
   end
 end
 
-def printtree
-  str = []    
-      (0..2 * @treedepth).each { |i| str[i] = ' ' * (4 * 2**@treedepth) } 
-      (0...@treedepth).each { |y| (0...2**y).each { |x| str[2 * y + 1][2**(@treedepth + 1 - y) + x * 2**(@treedepth + 2 - y) + 1, 2] = '/\\' } }
-      @elements.each { |key, value| str[key[0] * 2][2**(@treedepth + 1 - key[0]) + key[1] * 2**(@treedepth + 2 - key[0]), 4] = '%3d ' % value }
-      str.each { |s| puts s }
-end
 name = ENV['NAME']
 
-  if name
+if name
   name_file = "trees/#{ENV['NAME']}.tree"
   content = File.read(name_file)
   name_file.chars.each do |f|
@@ -41,11 +33,25 @@ name = ENV['NAME']
       f = content
       tree = JSON.parse(f)
       parse(tree)
-      printtree
+      str = []
+      (0..2 * @treedepth).each do |i|
+        str[i] = ' ' * (4 * 2**@treedepth)
+      end
+      (0...@treedepth).each do |y|
+        (0...2**y).each do |x|
+          a = 2**(@treedepth + 1 - y) + x * 2**(@treedepth + 2 - y) + 1
+          str[2 * y + 1][a, 2] = '/\\'
+        end
+      end
+      @elements.each do |key, value|
+        a = 2**(@treedepth + 1 - key[0]) + key[1] * 2**(@treedepth + 2 - key[0])
+        str[key[0] * 2] [a, 4] = '%3d '.to_s % value.to_s
+      end
+      str.each { |s| puts s }
     else
       puts 'Данное дерево не растет в данном лесу.'
     end
   end
-  else
+else
   puts 'Безымянных деревьев у нас не растет.'
-  end
+end
