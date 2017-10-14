7b456b205b8f4df8bdc4008003f5fb37f9862aea
Artem <artembobko@gmail.com>
2017-10-14 17:40:01 +0300

    Fixed the code after processing by local rubocop

--- 2017/ArtemBobko/1/tree_functions.rb
+++ 2017/ArtemBobko/1/tree_functions.rb
@@ -1,51 +1,54 @@
+# class for work with tree
 class Tree
   def self.print_tree(array, prefix = '', is_left = false)
-    if array.is_a?  Fixnum
-      puts "#{prefix}#{(is_left ? "|-- " : "\\-- ")}#{array}"
-      print_tree(nil, prefix + (is_left ? "|   " : "    "), true)
-      print_tree(nil, prefix + (is_left ? "|   " : "    "), false)
+    if array.is_a? Integer
+      put_str(array, prefix, is_left)
     elsif array.is_a? Array
-      puts "#{prefix}#{(is_left ? "|-- " : "\\-- ")}#{array[0]}"
-      print_tree(array[1][0], prefix + (is_left ? "|   " : "    "), true)
-      print_tree(array[1][1], prefix + (is_left ? "|   " : "    "), false)
+      put_str(array[0], prefix, is_left)
+      print_tree(array[1][0], prefix + (is_left ? '|   ' : '    '), true)
+      print_tree(array[1][1], prefix + (is_left ? '|   ' : '    '), false)
     end
+  end
+
+  def self.put_str(number, prefix = '', is_left = false)
+    puts "#{prefix}#{(is_left ? '|-- ' : '\\-- ')}#{number}"
   end
 
   def self.sum(node)
     if node.nil? || node[0].nil?
-      return 0
-    elsif node.is_a?  Fixnum
-      return node
+      0
+    elsif node.is_a? Integer
+      node
     elsif node.is_a? Array
-      return node[0] + sum(node[1][0]) + sum(node[1][1])
+      node[0] + sum(node[1][0]) + sum(node[1][1])
     end
   end
 
   def self.height(node)
     if node.nil? || node[0].nil?
-      return 0
-    elsif node.is_a? Fixnum
-      return 1
+      0
+    elsif node.is_a? Integer
+      1
     elsif node.is_a? Array
-      return [height(node[1][0]), height(node[1][1])].max + 1
+      [height(node[1][0]), height(node[1][1])].max + 1
     end
   end
 
-  def self.cut(array, lvl=1)
-    if (array.is_a? Array) && (array[1][0].class != Fixnum) 
-      cut(array[1][0], lvl + 1)
+  def self.cut(node, lvl = 1)
+    node[1].map { |e| cut(e, lvl + 1) if e.is_a? Array } if node.is_a? Array
+    node[1] = check_integer(node[1]) if lvl + 1 > 5
+    node[0] = check_array(node[0]) if lvl > 5
+  end
+
+  def self.check_array(e)
+    if e.is_a? Array
+      nil
+    else
+      e
     end
-    if (array.is_a? Array) && (array[1][1].class != Fixnum)
-      cut(array[1][1], lvl + 1)
-    end
-    if (array[1][0].is_a? Fixnum) &&  (lvl + 1 > 5)
-        array[1][0] = nil
-    end
-    if (array[1][1].is_a? Fixnum) && (lvl + 1 > 5)
-        array[1][1] = nil
-    end
-    if(lvl > 5)
-      array[0] = nil
-    end
+  end
+
+  def self.check_integer(a)
+    a.map { |e| nil if e.is_a? Integer }
   end
 end
--- 2017/ArtemBobko/1/trees.rb
+++ 2017/ArtemBobko/1/trees.rb
@@ -2,48 +2,41 @@ require 'json'
 require 'zip'
 require_relative 'tree_functions'
 
-def check(tree)
-  
-end
-
 Zip.on_exists_proc = true
 
-if(ENV["NAME"] != nil)
-  file_name = "#{ENV["NAME"]}.tree"
+if !ENV['NAME'].nil?
+  file_name = "#{ENV['NAME']}.tree"
   Zip::File.open('trees.zip') do |zip_file|
     if zip_file.find_entry('trees/' + file_name)
       tree = JSON.parse(zip_file.read('trees/' + file_name))
-      Tree.print_tree(tree)                                                                                                                                                                                
+      Tree.print_tree(tree)
     else
       puts 'Данное дерево не растет в данном лесу.'
     end
   end
-else 
+else
   Zip::File.open('trees.zip') do |zip_file|
     zip_file.glob('trees/*.tree').sort.each do |file|
       tree = JSON.parse(zip_file.read(file.name))
-      Tree.print_tree(tree) #вывод дерева
+      Tree.print_tree(tree) # tree output
       puts file.name
-      sum = Tree.sum(tree) #считаем сумму узлов
-      height = Tree.height(tree) #считаем высоту дерева
+      sum = Tree.sum(tree) # find nodes sum
+      height = Tree.height(tree) # find nodes height
       puts "Количество листьев: #{sum}"
       puts "Глубина дерева: #{height}"
-      if(sum > 5000)
+      if sum > 5000
         zip_file.remove(file.name)
-        puts "Срубить" 
-      elsif(height > 5)
-        Tree.cut(tree) #обрезаем дерево
-        tree.each {|s| (s.is_a? Array) ? s.compact : break }
+        puts 'Срубить'
+      elsif height > 5
+        Tree.cut(tree) # cut tree
         new_tree = JSON.generate(tree)
-        zip_file.get_output_stream(file.name){ |f| f.puts new_tree}
-        puts "Обрезать"
+        zip_file.get_output_stream(file.name) { |f| f.puts new_tree }
+        puts 'Обрезать'
       end
       print "\nЖелаете продолжить?[y/n] "
-      if 'n' == gets.chomp
-        break
-      end
+      break if gets.chomp == 'n'
     end
     zip_file.commit
   end
-  puts "Спасибо, что были в нешем лесу!"
+  puts 'Спасибо, что были в нешем лесу!'
 end
