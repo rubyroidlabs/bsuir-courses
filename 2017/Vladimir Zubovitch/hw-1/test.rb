cf981806ceae93f8c84f51f20e1e611994992740
Morawiin <moraviin@gmail.com>
2017-10-16 00:28:37 +0300

    error

--- 2017/Vladimir Zubovitch/hw-1/test.rb
+++ 2017/Vladimir Zubovitch/hw-1/test.rb
@@ -1,6 +1,40 @@
 # !/usr/bin/env ruby
 require 'json'
 input = ENV['NAME'].to_s.downcase
+@list_arr = []
+  if input.empty?
+    puts 'problem'
+  else
+    garden = []
+    Dir.foreach('trees') { |x| garden << x }
+    if garden.include?("#{input}.tree")
+      puts 'Есть такое дерево'
+      f = File.new ("trees/#{input}.tree")
+      content = f.read
+      b = JSON.parse(content)
+      base(b)
+      filtred_array = @list_arr
+      draw(filtred_array)
+    end
+  end
+
+def base(tree)
+  arr_node = []
+  list_now = []
+  tree.each do |node|
+    if node.class.to_s != 'Array'
+      list_now << node
+    else
+      arr_node += node
+    end
+  end
+  if list_now != [] 
+    @list_arr<< list_now
+  end
+  unless arr_node.size.zero?
+    base(arr_node)
+  end
+end
 
 def draw(tree)
   tree.each do |lvl|
@@ -22,38 +56,3 @@ def draw(tree)
     puts
   end
 end
-
-def base(tree)
-  arr_node = []
-  list_now = []
-  tree.each do |node|
-    if node.class.to_s != 'Array'
-      list_now << node
-    else
-      arr_node += node
-    end
-  end
-  if list_now != [] 
-    @list_arr<< list_now
-  end
-  unless arr_node.size.zero?
-    base(arr_node)
-  end
-end
-
-@list_arr = []
-  if input.empty?
-    puts 'problem'
-  else
-    garden = []
-    Dir.foreach('trees') { |x| garden << x }
-    if garden.include?("#{input}.tree")
-      puts 'Есть такое дерево'
-      f = File.new ("trees/#{input}.tree")
-      content = f.read
-      b = JSON.parse(content)
-      base(b)
-      filtred_array = @list_arr
-      draw(filtred_array)
-    end
-  end
