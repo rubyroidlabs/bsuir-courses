abe6ae8ad540a70344a1eb35ee931f30f60cf407
Anastasiya Bordak <anastasiyabordak@gmail.com>
2017-10-15 22:49:11 +0300

    hw-1f

--- 2017/Anastasiya Bordak/1/homework-1.rb
+++ 2017/Anastasiya Bordak/1/homework-1.rb
@@ -10,32 +10,32 @@ class TREE
 end
 
 def creat_tree(tree, root)
-  if tree.size == 2 && root!=nil		
+  if tree.size == 2 && !root.nil?	
     hash2 = JSON.parse(tree[1][1].to_s)
-    if(hash2[2]  == 1 || hash2[2] == 0)
+    if hash2[2] == 1 || hash2[2].zero?
       root.left = TREE.new(hash2)
     else root.left = TREE.new(hash2[0])
       creat_tree(hash2, root.left)
     end
     hash1 = JSON.parse(tree[1][0].to_s)
-    if(hash1[2] == 1 || hash1[2] == 0)
+    if hash1[2] == 1 || hash1[2].zero?
       root.right = TREE.new(hash1)
     else root.right = TREE.new(hash1[0])
       creat_tree(hash1, root.right)
     end
   end
-  return root
 end
 
 def show(root, level)
-  if !root.nil?
+  until root.nil?
     show(root.right, level + 1)
-    0..level.each {
+    0..level.each
+    {
       print ' '
     }
     end
   if !root.nil?
-    print "@root.value.to_s"
+    print '@root.value.to_s'
   else print "\n"
   end
   unless root.nil?
@@ -54,7 +54,7 @@ def hight(node)
   return if node.nil?
   left = hight(node.left)
   right = hight(node.right)
-  if left>right
+  if left > right
     return left + 1
   else return right + 1
   end
@@ -75,7 +75,8 @@ unless ENV['NAME'].nil?
     files.unshift(ENV['NAME'])
   end
 end
-files.map {|name|
+files.map 
+{ |name|
   file = File.open(name)
   tree = JSON.parse(file)
   root = TREE.new(tree[0])
