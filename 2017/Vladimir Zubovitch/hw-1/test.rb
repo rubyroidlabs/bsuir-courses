c9bac5ab2d8df6a6b22895d7b534d2ff4bb0535a
Morawiin <moraviin@gmail.com>
2017-10-15 21:09:49 +0300

    fix

--- 2017/Vladimir Zubovitch/hw-1/test.rb
+++ 2017/Vladimir Zubovitch/hw-1/test.rb
@@ -33,27 +33,25 @@ def base (tree)
       arr_node += node 
      end
     end
- if list_now!=[] 
-         $list_arr<<list_now 
- end
-         if arr_node.size != 0 
+ if list_now !=[] 
+  $list_arr<<list_now 
+  end
+         if arr_node.size.empty? 
          base(arr_node)
-       end
+         end
 end
-
-
 if input.empty?
-         puts "problem"
+  puts "problem"
 else   
-      garden=[]
-      Dir.foreach("trees") {|x| garden<<x }
-if garden.include?("#{input}.tree")
-puts "Есть такое дерево"
-f=File.new ("trees/#{input}.tree")
-content=f.read
-b=JSON.parse(content)
-base(b)
-filtred_array=$list_arr
-draw(filtred_array)
+  garden = []
+  Dir.foreach("trees") { |x| garden << x }
+  if garden.include?("#{input}.tree")
+  puts "Есть такое дерево"
+  f = File.new ("trees/#{input}.tree")
+  content = f.read
+  b = JSON.parse(content)
+  base(b)
+  filtred_array=$list_arr
+  draw(filtred_array)
 end
 end
