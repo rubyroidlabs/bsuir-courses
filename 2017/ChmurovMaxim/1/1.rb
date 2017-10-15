5bd1a9cc98278eebee3d0bbe4988a96514157e68
Chmurov Maxim <di-night@bk.ru>
2017-10-15 14:49:40 +0300

    Chmurov hw1 2.3

--- 2017/ChmurovMaxim/1/1.rb
+++ 2017/ChmurovMaxim/1/1.rb
@@ -14,68 +14,71 @@ def func(arr, levels, level)
     func(arr.last, levels, level + 1)
   end
 end
+def print_slash(levels, interval, key, index)
+  if levels[key].count > 1
+    levels[key].each do
+      slash_interval_right = interval / 2
+      slash_interval_right -= 1
+      slash_interval_left = interval / 2
+      if (index % 2).zero?
+        slash = '\\'
+      else
+        slash = '/'
+      end
+      if slash_interval_right < 1
+        slash_interval_right = 1
+      end
+      if slash_interval_left < 1
+        slash_interval_left = 1
+      end
+      index += 1
+      print "#{' ' * slash_interval_right}#{slash}#{' ' * slash_interval_left}"
+    end
+  end
+end
+def print_digit(levels, interval, key, max_count)
+  levels[key].each do |val|
+    interval_right = interval / 2
+    interval_right -= 1
+    interval_left = interval / 2
+    if val.to_s.length == 2
+      interval_left -= 1
+    end
+    max_count += val
+    print "#{' ' * interval_right}#{val}#{' ' * interval_left}"
+  end
+  return max_count
+end
+def print_tree(tree)
+  if tree.name != 'trees/'
+    a = tree.get_input_stream.read
+    a = JSON.parse(a)
+    levels = {}
+    max_count = 0
+    func(a, levels, 0)
+    max = levels[levels.keys.last].count * 8
+    levels.keys.each do |key|
+      count = levels[key].count * 2
+      interval = max / count
+      print_slash(levels, interval, key, 1)
+      puts
+      max_count = print_digit(levels, interval, key, max_count)
+      puts
+    end
+    if levels.keys.count > 5
+      puts 'clip'
+    elsif max_count > 5000
+      puts 'chop down'
+    else
+      puts 'leave'
+    end
+  end
+end
 Zip::File.open("trees.zip") do |zipfile|
   zipfile.each do |tree|
     puts 'continue? y/n'
     if gets.chomp == 'y'
-      if tree.name != 'trees/'
-        a = tree.get_input_stream.read
-        a = JSON.parse(a)
-        levels = {}
-        max_count = 0
-        func(a, levels, 0)
-        max = levels[levels.keys.last].count * 8
-        levels.keys.each do |key|
-          count = levels[key].count * 2
-          interval = max / count
-          index = 1
-          if levels[key].count > 1
-            levels[key].each do
-              slash_interval_right = interval / 2
-              slash_interval_right -= 1
-              slash_interval_left = interval / 2
-              if (index % 2).zero?
-                slash = '\\'
-              else 
-                slash = '/'
-              end
-              if slash_interval_right < 1
-                slash_interval_right = 1
-              end
-              if slash_interval_left < 1
-                slash_interval_left = 1
-              end
-              index += 1
-              print "#{' ' * slash_interval_right}#{slash}#{' ' * slash_interval_left}"
-            end
-          end
-          puts
-          levels[key].each do |val|
-            interval_right = interval / 2
-            interval_right -= 1
-            interval_left = interval / 2    
-            if val.to_s.length == 2
-              interval_left -= 1
-            end
-            if interval_right < 1
-              interval_right = 1
-            end
-            if interval_left < 1
-              interval_left = 1
-            end
-            max += val
-            print "#{' ' * interval_right}#{val}#{' ' * interval_left}"
-          end
-          puts
-        end
-        if levels.keys.count > 5
-          puts 'clip'
-        elsif max_count > 5000
-          puts 'chop down'
-        else
-          puts 'leave'
-        end
-      end
+      print_tree(tree)
     else
       exit(0)
     end
