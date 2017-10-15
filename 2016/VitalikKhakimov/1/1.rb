1cafbd127d69d3bf29f36b981e5e2ebb149c0db8
Vitalik <vitalikslim6@gmail.com>
2017-10-15 22:26:58 +0300

    fixed 2

--- 2016/VitalikKhakimov/1/1.rb
+++ 2016/VitalikKhakimov/1/1.rb
@@ -2,7 +2,7 @@ require 'json'
 
 @treedepth = 0
 @elements = Hash.new([0, 0])
-@currentposition = Array.new(100) { 0 }
+@currentposition = Array.new(10) { 0 }
 
 def parse(arr, lvl = 0)
   @treedepth = lvl > @treedepth ? lvl : @treedepth
@@ -25,33 +25,52 @@ end
 
 name = ENV['NAME']
 
-if name
-  name_file = "trees/#{ENV['NAME']}.tree"
-  content = File.read(name_file)
-  name_file.chars.each do |f|
-    if content
-      f = content
-      tree = JSON.parse(f)
-      parse(tree)
-      str = []
-      (0..2 * @treedepth).each do |i|
-        str[i] = ' ' * (4 * 2**@treedepth)
-      end
-      (0...@treedepth).each do |y|
-        (0...2**y).each do |x|
-          a = 2**(@treedepth + 1 - y) + x * 2**(@treedepth + 2 - y) + 1
-          str[2 * y + 1][a, 2] = '/\\'
-        end
-      end
-      @elements.each do |key, value|
-        a = 2**(@treedepth + 1 - key[0]) + key[1] * 2**(@treedepth + 2 - key[0])
-        str[key[0] * 2] [a, 4] = '%3d '.to_s % value.to_s
-      end
-      str.each { |s| puts s }
-    else
-      puts 'Данное дерево не растет в данном лесу.'
+def printtree
+  str = []
+  (0..2 * @treedepth).each do |i|
+    str[i] = ' ' * (4 * 2**@treedepth)
+  end
+  (0...@treedepth).each do |y|
+    (0...2**y).each do |x|
+      a = 2**(@treedepth + 1 - y) + x * 2**(@treedepth + 2 - y) + 1
+      str[2 * y + 1][a, 2] = '/\\'
     end
   end
+  @elements.each do |key, value|
+    a = 2**(@treedepth + 1 - key[0]) + key[1] * 2**(@treedepth + 2 - key[0])
+    str[key[0] * 2] [a, 4] = '%3d '.to_s % value.to_s
+  end
+  str.each { |s| puts s }
+end
+
+if name
+  name_file = "trees/#{ENV['NAME']}.tree"
+  f = File.exist?(name_file)
+  if f
+    file = File.read(name_file)
+    tree = JSON.parse(file)
+    parse(tree)
+    printtree
+  else
+    puts 'Данное дерево не растет в данном лесу.'
+  end
 else
-  puts 'Безымянных деревьев у нас не растет.'
+  forest = []
+  Dir.foreach('trees') do |filename|
+    forest.push filename
+  end
+  forest.each do |n|
+    puts n
+    ff = File.read("trees/#{n}")
+    tree = JSON.parse(ff)
+    @treedepth = 0
+    @elements = Hash.new([0, 0])
+    @currentposition = Array.new(10) { 0 }
+    parse(tree)
+    printtree
+    puts 'Желаете продолжить? [y/n]'
+    answer = gets.chomp
+    break if answer == 'n'
+  end
+  puts 'Спасибо, что были в нашем лесу'
 end
