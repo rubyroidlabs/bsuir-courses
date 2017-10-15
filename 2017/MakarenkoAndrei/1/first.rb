eba7e28e468a4c8ff41af46122cf8a0effafcf59
dixent <dixent@inbox.ru>
2017-10-15 17:45:50 +0300

    fix style7

--- 2017/MakarenkoAndrei/1/first.rb
+++ 2017/MakarenkoAndrei/1/first.rb
@@ -1,6 +1,5 @@
 #! /usr/bin/env ruby	
 class Tree
-  
   attr_accessor :value,:left,:right
   @@b = 1
   @@bool = true
@@ -18,56 +17,65 @@ class Tree
     tree 
   end
     
+end
+class Root  
+  attr_accessor :tree,:hight,:mass,:bool,:index
+  def initialize
+    @index = 0
+    @bool = true
+    @hight = 1
+  end
+  
   def do_array(tree) 
-    @@a[@@b].push(tree.value)
+    @mass[@index].push(tree.value)
     if tree.left != nil
-        @@b += 1        
-        do_array(tree.left) 
+      @index += 1        
+      do_array(tree.left) 
     end
     if tree.right!=nil  
-        @@b += 1
-        do_array(tree.right)
+      @index += 1
+      do_array(tree.right)
     end
-    @@b -= 1
+      @index -= 1
   end
 
   def do_hight(tree) 
     if tree.left != nil  
-      if @@bool  
-        @@b += 1
+      if @bool  
+        @hight += 1
       end       
       do_hight(tree.left) 
     end
-    if tree.right!=nil    
-      if @@bool  
-        @@b += 1
+    if tree.right != nil    
+      if @bool  
+        @hight += 1
       end
       do_hight(tree.right)
     end
-    @@bool = false
-    @@b
+    @bool = false
+    @hight
   end
 
   def output
     i = 0
     l = 0
-    while i<@@a[@@hight-1].size
-      l += @@a[@@hight-1][i].to_s.length
+    while i < @mass[@hight-1].size
+      l += @mass[@hight-1][i].to_s.length
       i +=1
     end
-    l = l + @@a[@@hight-1].size 
+    l = l + @mass[@hight-1].size 
     i = 0
-    while i<@@hight
+    while i < @hight
       j = 0    
       string = ''    
-      while j<@@a[i].size  
-        s = space((l - @@a[i][j].to_s.size))  
+      while j < @mass[i].size  
+        s = space((l - @mass[i][j].to_s.size))  
         s2 = space(l)  
-        string = string + s2 + @@a[i][j].to_s + s2
+        string = string + s2 + @mass[i][j].to_s + s2
         j += 1
       end
       puts string    
-      l = l / 2
+      l /= 2
       i += 1
     end
   end
@@ -79,25 +87,23 @@ class Tree
       s += ' '
       i += 1
     end
-    s  
+    s
   end
   
   def main
     require 'json'
-    file ='trees/' + ENV['NAME'] + '.tree'
+    file = 'trees/' + ENV['NAME'] + '.tree'
     if ENV['NAME'] = nil
-      puts 'Пустых деревьев не растет'      
+      puts 'Пустых деревьев не растет'
     elsif File.file? file  
-      forest = Tree.make_tree(JSON.parse(File.read(file))) 
-    do_hight(forest)
-    @@hight = @@b
-    @@a = Array.new(@@hight) { [] }
-    @@b = 0
-    do_array(forest)
-    output
+      @tree = Tree.make_tree(JSON.parse(File.read(file))) 
+      do_hight(@tree)
+      @mass = Array.new(@hight) { [] }
+      do_array(@tree)
+      output
     else
-      puts 'Такого дерева не растет'  
-    end       
+      puts 'Такого дерева не растет'
+    end
   end
-end
-Tree.new.main
+end    
+Root.new.main
