333749cef6f0bea03910d489055cd7e7c4ff5fe8
Morawiin <moraviin@gmail.com>
2017-10-15 22:17:30 +0300

    not final.3

--- 2017/Vladimir Zubovitch/hw-1/test.rb
+++ 2017/Vladimir Zubovitch/hw-1/test.rb
@@ -7,7 +7,7 @@ def draw (tree)
         tree.each do |lvl|
                 sp=128/(lvl.size+1)
                sp+=1
-                if tree[0]!=lvl
+                if tree[0] != lvl
                   lvl.size.times do |i|
                     if (i % 2).zero?
                       print "%#{sp}s" % ["/"]
