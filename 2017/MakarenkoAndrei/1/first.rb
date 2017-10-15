7e04f1ee5cb1c0baf6bee9373fda7d3bd95519f0
dixent <dixent@inbox.ru>
2017-10-15 18:31:27 +0300

    fix

--- 2017/MakarenkoAndrei/1/first.rb
+++ 2017/MakarenkoAndrei/1/first.rb
@@ -9,20 +9,21 @@ class Tree
 
   def self.make_tree(array)
     tree = Tree.new(array[0])    
-    tree.left = if array[1][0].is_a?(Numeric)
-    Tree.new(array[1][0])
+    tree.left = 
+    if array[1][0].is_a?(Numeric)
+      Tree.new(array[1][0])
     else
-    make_tree(array[1][0])
+      make_tree(array[1][0])
     end
-    tree.right = if array[1][1].is_a?(Numeric)
-    Tree.new(array[1][1])
+    tree.right = 
+    if array[1][1].is_a?(Numeric)
+      Tree.new(array[1][1])
     else
-    make_tree(array[1][1])
+      make_tree(array[1][1])
     end
     tree
   end
-
-    end
+end
 class Root
   attr_accessor :tree, :hight, :mass, :bool, :index
   def initialize
@@ -33,11 +34,11 @@ class Root
 
   def do_array(tree)
     @mass[@index].push(tree.value)
-    if !tree.left.nil?
+    if unless tree.left.nil?
       @index += 1
       do_array(tree.left)
     end
-    if !tree.right.nil?
+    if unless tree.right.nil?
       @index += 1
       do_array(tree.right)
     end
@@ -45,13 +46,13 @@ class Root
   end
 
   def do_hight(tree)
-    if !tree.left.nil?
+    if unless tree.left.nil?
       if @bool
         @hight += 1
       end
       do_hight(tree.left)
     end
-    if !tree.right.nil?
+    if unless tree.right.nil?
       if @bool
         @hight += 1
       end
@@ -68,12 +69,12 @@ class Root
       l += @mass[@hight - 1][i].to_s.length
       i += 1
     end
-    l = l + @mass[@hight - 1].size
+    l += @mass[@hight - 1].size
     i = 0
     while i < @hight
       j = 0
       string = ''
-      while j < @mass[i].size  
+      while j < @mass[i].size
         s2 = space(l)
         string = string + s2 + @mass[i][j].to_s + s2
         j += 1
