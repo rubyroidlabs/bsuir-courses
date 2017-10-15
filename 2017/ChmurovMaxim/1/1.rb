60eac82d1d864e108586e038a071c636b7a6ba7c
Chmurov Maxim <di-night@bk.ru>
2017-10-15 12:02:33 +0300

    Chmurov hw-2.1

--- 2017/ChmurovMaxim/1/1.rb
+++ 2017/ChmurovMaxim/1/1.rb
@@ -18,66 +18,66 @@ Zip::File.open("trees.zip") do |zipfile|
   zipfile.each do |tree|
     puts "continue? y/n"
     if gets.chomp == 'y'
-		  if tree.name == "trees/"
-		    a = 5
-		  else
-		    a = tree.get_input_stream.read
-		    a = JSON.parse(a)
+      if tree.name == "trees/"
+        a = 5
+      else
+        a = tree.get_input_stream.read
+        a = JSON.parse(a)
         levels={}
         max_count = 0
-		    func(a, levels, 0)
-		    max = levels[levels.keys.last].count * 8
-				levels.keys.each do |key|
-					count = levels[key].count * 2
-					interval = max / count
-					index = 1
-					if levels[key].count > 1
-						levels[key].each do
-							slash_interval_right = interval / 2
-							slash_interval_right -= 1
-							slash_interval_left = interval / 2
-							if (index % 2).zero?
-								slash = '\\'
-							else 
-								slash = '/'
-							end
-		          if slash_interval_right < 1
-		            slash_interval_right = 1
-		          end
-		          if slash_interval_left < 1
-		            slash_interval_left = 1
-							end
-		          index += 1
-							print "#{' ' * slash_interval_right}#{slash}#{' ' * slash_interval_left}"
-						end
-					end
-					puts
-					levels[key].each do |val|
-						interval_right = interval / 2
-						interval_right -= 1
-						interval_left = interval / 2    
-						if val.to_s.length == 2
-							interval_left -= 1
-						end
-		        if interval_right < 1
-		          interval_right = 1
-		        end
-		        if interval_left < 1
-		          interval_left = 1
-						end
+        func(a, levels, 0)
+        max = levels[levels.keys.last].count * 8
+        levels.keys.each do |key|
+          count = levels[key].count * 2
+          interval = max / count
+          index = 1
+          if levels[key].count > 1
+            levels[key].each do
+              slash_interval_right = interval / 2
+              slash_interval_right -= 1
+              slash_interval_left = interval / 2
+              if (index % 2).zero?
+                slash = '\\'
+              else 
+                slash = '/'
+              end
+              if slash_interval_right < 1
+                slash_interval_right = 1
+              end
+              if slash_interval_left < 1
+                slash_interval_left = 1
+              end
+              index += 1
+              print "#{' ' * slash_interval_right}#{slash}#{' ' * slash_interval_left}"
+            end
+          end
+          puts
+          levels[key].each do |val|
+            interval_right = interval / 2
+            interval_right -= 1
+            interval_left = interval / 2    
+            if val.to_s.length == 2
+              interval_left -= 1
+            end
+            if interval_right < 1
+              interval_right = 1
+            end
+            if interval_left < 1
+              interval_left = 1
+            end
             max += val
-						print "#{' ' * interval_right}#{val}#{' ' * interval_left}"
-					end
+            print "#{' ' * interval_right}#{val}#{' ' * interval_left}"
+          end
           puts
         end
-        if levels.keys.count>5
+        if levels.keys.count > 5
           puts 'clip'
         elsif max_count > 5000
           puts 'chop down'
         else
           puts 'leave'
         end
-			end
+      end
     else
       exit(0)
     end
