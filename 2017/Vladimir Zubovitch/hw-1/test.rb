cc210fa6806467efdf9083059dda655a75f3bad7
Morawiin <moraviin@gmail.com>
2017-10-15 22:24:06 +0300

    not final.5

--- 2017/Vladimir Zubovitch/hw-1/test.rb
+++ 2017/Vladimir Zubovitch/hw-1/test.rb
@@ -4,24 +4,24 @@ require 'json'
 input=ENV["NAME"].to_s.downcase
 $list_arr=[]
 def draw (tree)
-        tree.each do |lvl|
-                sp=128/(lvl.size+1)
-               sp+=1
-                if tree[0] != lvl
-                  lvl.size.times do |i|
-                    if (i % 2).zero?
-                      print "%#{sp}s" % ["/"]
-                    else
-                      print "%#{sp}s" % ["\\"]
-                    end
-                  end
-                end
-                puts
-                lvl.size.times do |i|
-                  print "%#{sp}s" % [lvl[i]]
-                end
-                puts
+  tree.each do |lvl|
+    sp=128/(lvl.size+1)
+    sp+=1
+      if tree[0] != lvl
+        lvl.size.times do |i|
+          if (i % 2).zero?
+            print "%#{sp}s" % ["/"]
+          else
+            print "%#{sp}s" % ["\\"]
+          end
         end
+      end
+    puts
+    lvl.size.times do |i|
+      print "%#{sp}s" % [lvl[i]]
+    end
+    puts
+  end
 end
 
 def base(tree)
