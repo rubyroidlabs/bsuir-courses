cd558dad290211c16afe6c465eab5f5eb84a2a13
Morawiin <moraviin@gmail.com>
2017-10-16 00:25:50 +0300

    error

--- 2017/Vladimir Zubovitch/hw-1/test.rb
+++ 2017/Vladimir Zubovitch/hw-1/test.rb
@@ -1,1 +1,59 @@
-puts a=1
+# !/usr/bin/env ruby
+require 'json'
+input = ENV['NAME'].to_s.downcase
+
+def draw(tree)
+  tree.each do |lvl|
+    sp = 128 / (lvl.size + 1)
+    sp += 1
+    if tree[0] != lvl
+      lvl.size.times do |i|
+        if (i % 2).zero?
+          format("%#{sp}s", '/')
+        else
+          format("%#{sp}s", '\\')
+        end
+      end
+    end
+    puts
+    lvl.size.times do |i|
+      format("%#{sp}s", lvl[i])
+    end
+    puts
+  end
+end
+
+def base(tree)
+  arr_node = []
+  list_now = []
+  tree.each do |node|
+    if node.class.to_s != 'Array'
+      list_now << node
+    else
+      arr_node += node
+    end
+  end
+  if list_now != [] 
+    @list_arr<< list_now
+  end
+  unless arr_node.size.zero?
+    base(arr_node)
+  end
+end
+
+@list_arr = []
+  if input.empty?
+    puts 'problem'
+  else
+    garden = []
+    Dir.foreach('trees') { |x| garden << x }
+    if garden.include?("#{input}.tree")
+      puts 'Есть такое дерево'
+      f = File.new ("trees/#{input}.tree")
+      content = f.read
+      b = JSON.parse(content)
+      base(b)
+      filtred_array = @list_arr
+      draw(filtred_array)
+    end
+  end
