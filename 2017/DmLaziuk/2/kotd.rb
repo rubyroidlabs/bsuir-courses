2dfa71345ec27d558fc82b8cd4bbeab449fc7e7c
dmlaziuk <dm.laziuk@gmail.com>
2017-10-18 02:11:59 +0300

    second parse
    (cherry picked from commit 2638182)

--- 2017/DmLaziuk/2/kotd.rb
+++ 2017/DmLaziuk/2/kotd.rb
@@ -4,9 +4,16 @@ uri = 'https://genius.com/artists/King-of-the-dot'
 
 agent = Mechanize.new
 
-page = agent.get(uri).links_with(:href => %r{lyrics} )[0].click
+page = agent.get(uri).links_with(:href => %r{\w+-lyrics} )[0].click
 #  page = link.click
   puts page.uri
   puts page.title
-  puts page.css('.lyrics').text
+  text = page.css('.lyrics').text.strip
+  round = text.scan(%r{\[Round \d: \w+\]})
+  lyrics = text.split(%r{\s*\[Round \d: \w+\]\s*})
+  lyrics.shift # first element = "" (empty string)
+  (0...round.count).each do |i|
+    puts round[i]
+    p lyrics[i]
+  end
 
