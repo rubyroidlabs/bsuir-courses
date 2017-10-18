1a05624eb7bb54188033817c791ee773862f828e
dmlaziuk <dm.laziuk@gmail.com>
2017-10-18 23:50:16 +0300

    Level 1 completed (без классов)
    (cherry picked from commit c9ef6d5)

--- 2017/DmLaziuk/2/kotd.rb
+++ 2017/DmLaziuk/2/kotd.rb
@@ -7,27 +7,19 @@ agent = Mechanize.new
 agent.get(uri) do |parse_page|
   parse_page.links_with(:href => %r{\w+-lyrics} ).each do |link|
     page = link.click
-    pp link
-#    pp page
     count = Hash.new
     text = page.css('.lyrics').text.strip
-    round = text.scan(%r{\[Round \d+: \w+\s*\w*\]})
-    p round
-    lyrics = text.split(%r{\s*\[Round \d: \w+\s*\w*\]\s*})
+    round = text.scan(%r{\[Round.*?\]})
+    lyrics = text.split(%r{\s*\[Round.*?\]\s*})
     lyrics.shift # first element = "" (empty string)
     (0...round.count).each do |i|
-      key = round[i].split(':')[1].scan(/\w+/)[0]
-      p key
-      value = lyrics[i].scan(/\S+/).count
-      p value
-      if count[key]
-        count[key] += value
+      performer = round[i].split(%r{:|\u2013})[1].scan(/\w+/)[0]
+      word_count = lyrics[i].scan(%r{\S+}).count
+      if count[performer]
+        count[performer] += word_count
       else
-        count[key] = value
+        count[performer] = word_count
       end
-      # puts round[i].split(':')[0].scan(/\d+/)
-      # puts round[i].split(':')[1].scan(/\w+/)
-      # puts lyrics[i].scan(/\S+/).count
     end
     puts count.keys[0].to_s + ' vs ' + count.keys[1].to_s + ' - ' + page.uri.to_s
     count.each { |key, value| puts key.to_s + ' - ' + value.to_s }
