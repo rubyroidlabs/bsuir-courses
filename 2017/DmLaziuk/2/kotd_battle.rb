7e52cdca37e8fea006da377b267083aa0e23f5f8
dmlaziuk <dm.laziuk@gmail.com>
2017-10-19 17:55:33 +0300

    Level 2
    (cherry picked from commit ef71067)

---
+++ 2017/DmLaziuk/2/kotd_battle.rb
@@ -0,0 +1,75 @@
+require 'mechanize'
+
+class KotdBattle
+  attr_reader :title, :lyrics, :uri, :winner, :score
+
+  def initialize(page = nil)
+    @title = ''
+    @lyrics = ''
+    @count = Hash.new
+    @winner = ''
+    @score = 0
+    if page
+      @uri = page.uri
+      @title = page.title
+      @title.gsub!('King of the Dot –', '')
+      @title.gsub!('Lyrics | Genius Lyrics', '')
+      @title.strip!
+      @lyrics = page.css('.lyrics').text.strip
+      # remove [?]
+      @lyrics.gsub!(%r{\[\?\]}, '')
+      # remove [...]
+      @lyrics.gsub!(%r{\[\.\.\.\]}, '')
+      # remove […]
+      @lyrics.gsub!(%r{\[…\]}, '')
+      # remove [*text*]
+      @lyrics.gsub!(%r{\[\*.*?\*\]}, '')
+      battle
+      guess_winner
+    end
+  end
+
+  def to_s
+    str = @title + ' - ' + @uri.to_s
+    if @count.size > 1
+      str += "\n"
+      @count.each { |key, value| str += key.to_s + ' - ' + value.to_s + "\n" }
+      str += @winner.to_s + ' WINS!' + "\n"
+    end
+    str
+  end
+
+  private
+
+  def battle
+    round = @lyrics.scan(%r{\[.*?\]})
+    text = @lyrics.split(%r{\[.*?\]})
+    text.shift
+    (0...round.count).each do |i|
+      performer = round[i]
+      performer.gsub!(%r{\[}, '')
+      performer.gsub!(%r{\]}, '')
+      performer.strip!
+      performer.gsub!(%r{Round\s\d\s?[:|\-|\u2013]*\s*}, '')
+      counter = text[i].scan(%r{[A-Za-z]}).count if text[i]
+      if @count[performer]
+        @count[performer] += counter if counter
+      else
+        @count[performer] = counter if counter
+      end
+    end
+  end
+
+  def guess_winner
+    if @count.size > 1
+      @winner = @count.keys[0]
+      @score = @count.values[0]
+      @count.each do |key, value|
+        if value > @score
+          @winner = key
+          @score = value
+        end
+      end
+    end
+  end
+end
