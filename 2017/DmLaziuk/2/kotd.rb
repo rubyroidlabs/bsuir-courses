5f805d06afa2c143bdcfe084129a4f70ba474815
dmlaziuk <dm.laziuk@gmail.com>
2017-10-19 18:30:08 +0300

    Level 3
    (cherry picked from commit daebd54)

--- 2017/DmLaziuk/2/kotd.rb
+++ 2017/DmLaziuk/2/kotd.rb
@@ -1,57 +1,57 @@
 require 'mechanize'
+require_relative 'kotd_battle'
 
-def parse(parse_page)
-  parse_page.links_with(:href => %r{\w+-lyrics} ).each do |link|
-    page = link.click
-    count = Hash.new
-    text = page.css('.lyrics').text.strip
-    text.gsub!(%r{\[\?\]}, '')
-    text.gsub!(%r{\[\.\.\.\]}, '')
-    text.gsub!(%r{\[…\]}, '')
-    text.gsub!(%r{\[\*.*?\*\]}, '')
-    round = text.scan(%r{\[.*?\]})
-    lyrics = text.split(%r{\[.*?\]})
-    lyrics.shift # first element = "" (empty string)
-    (0...round.count).each do |i|
-      performer = round[i]
-      performer.gsub!(%r{\[}, '')
-      performer.gsub!(%r{\]}, '')
-      performer.strip!
-      performer.gsub!(%r{Round\s\d\s?[:|\-|\u2013]*\s*}, '')
-      word_count = lyrics[i].scan(%r{\S+}).count if lyrics[i]
-      if count[performer]
-        count[performer] += word_count if word_count
-      else
-        count[performer] = word_count if word_count
+class Kotd
+  START_PAGE = 'https://genius.com/artists/songs?for_artist_page=117146&id=King-of-the-dot&page=1&pagination=true'
+
+  attr_reader :links, :name, :criteria
+
+  def initialize(name = nil, criteria = nil)
+    agent = Mechanize.new
+    page = agent.get(START_PAGE)
+    @links = []
+    @name = name
+    @criteria = criteria
+    puts 'Initializing...'
+    page.links_with(:href => %r{\w+-lyrics} ).each { |link| @links << link }
+    next_page = page.links_with(:class => 'next_page')[0]
+    while next_page
+      page = next_page.click
+      page.links_with(:href => %r{\w+-lyrics} ).each { |link| @links << link }
+      next_page = page.links_with(:class => 'next_page')[0]
+    end
+  end
+
+  def run
+    if @name
+      run_name
+    else
+      @links.each do |link|
+        battle = KotdBattle.new(link.click, @criteria)
+        puts battle
+        puts
       end
     end
-    if count.size > 1
-      title = page.title
-      title.gsub!('King of the Dot –', '')
-      title.gsub!('Lyrics | Genius Lyrics', '')
-      puts title.strip + ' - ' + page.uri.to_s
-      winner = count.keys[0]
-      winner_value = count.values[0]
-      count.each do |key, value|
-        puts key.to_s + ' - ' + value.to_s
-        if value > winner_value
-          winner = key
-          winner_value = value
-        end
-      end
-      puts winner.to_s + ' WINS!'
+  end
+
+  private
+
+  def run_name
+    wins = 0
+    battles = @links.select { |link| link.text.scan(@name).size > 0 }
+    battles.each do |link|
+      battle = KotdBattle.new(link.click, @criteria)
+      wins += 1 if battle.winner == @name
+      puts battle
       puts
     end
+    loses = battles.size - wins
+    puts @name + ' wins ' + wins.to_s + ' times, loses ' + loses.to_s + ' times'
   end
 end
 
-uri = 'https://genius.com/artists/songs?for_artist_page=117146&id=King-of-the-dot&page=1&pagination=true'
-agent = Mechanize.new
-page = agent.get(uri)
-parse(page)
-next_page = page.links_with(:class => 'next_page')[0]
-while next_page
-  page = next_page.click
-  parse(page)
-  next_page = page.links_with(:class => 'next_page')[0]
-end
+name = ENV['NAME']
+criteria = ENV['CRITERIA']
+
+kb = Kotd.new(name, criteria)
+kb.run
