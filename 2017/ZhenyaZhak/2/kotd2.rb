5d2cddcf9e4be3924fbd52d5bf3ff64ce5d3c7a2
ZhenyaZhak <zhenya-zhak@mail.ru>
2017-10-21 23:13:49 +0300

    hw-2

--- 2017/ZhenyaZhak/2/kotd2.rb
+++ 2017/ZhenyaZhak/2/kotd2.rb
@@ -1,24 +1,22 @@
 class Kotd2
-
   def self.count_function(link)
-    ENV['NAME'] ? name_b = ENV['NAME'].downcase : name_b = nil
-    ENV['CRITERIA'] ? criteria_b = ENV['CRITERIA'].downcase : criteria_b = /\w/
-    page3 = link.click
+    name_b = ENV['NAME'] ? ENV['NAME'].downcase : ' '
+    criteria_b = ENV['CRITERIA'] ? ENV['CRITERIA'].downcase : /\w/
+    page = link.click
     txt_link = link.text.split('(')[0].strip!
-    if txt_link.include? "&"
+    if txt_link.include? '&'
       return
     end
     txt_link.sub('vs.', 'vs')
     name_batler = Array.new
     name_batler[0] = txt_link.split('vs')[0].strip
     name_batler[1] = txt_link.split('vs')[1].strip
-    if !name_b.nil?
-      print '+'
+    if name_b != ' '
       if name_b != name_batler[0].downcase && name_b != name_batler[1].downcase
         return
       end
     end
-    txt = page3.css('.lyrics p').text
+    txt = page.css('.lyrics p').text
     count = 0
     count_batler = Array.new
     count_batler[0] = 0
@@ -29,51 +27,49 @@ class Kotd2
       txt[i] = txt[i].downcase
       count = txt[i].scan(criteria_b).size
       if txt[i][0..30].include? name_batler[0].downcase
-        count_batler[0] += count - 20
+        count_batler[0] += count
         flag = 1
         next
       end
       if txt[i][0..30].include? name_batler[1].downcase
-        count_batler[1] += count - 20
+        count_batler[1] += count
         flag = 2
         next
       end
       if txt[i].scan(/\w/).size > 150
         if flag == 1
-          count_batler[0] += count - 10
+          count_batler[0] += count
         end
         if flag == 2
-          count_batler[1] += count - 10
+          count_batler[1] += count
         end
       end
     end
-    if count_batler[0] == 0 || count_batler[1] == 0
+    if count_batler[0].zero? || count_batler[1].zero?
       return
     end
     if count_batler[0] == count_batler[1]
       str = 'Dead heat!!!'
     else
-      count_batler[0] > count_batler[1] ? x = 0 : x = 1
+      x = count_batler[0] > count_batler[1] ? 0 : 1
       str = "#{name_batler[x]} REAL NIGGA!!!"
     end
-    puts "\n#{name_batler[0]} vs #{name_batler[1]} - #{link.uri}\n#{name_batler[0]} - #{count_batler[0]}\n#{name_batler[1]} - #{count_batler[1]}\n#{str}\n"
+    puts "\n#{name_batler[0]} vs #{name_batler[1]} - #{link.uri}"
+    puts "#{name_batler[0]} - #{count_batler[0]}\n#{name_batler[1]} - #{count_batler[1]}\n#{str}\n"
   end
 
-  def self.link_run(page2)
+  def self.link_run(page)
     loop do
-      review_links = page2.links_with(text: /vs/)
+      review_links = page.links_with(text: /vs/)
       threads = []
-      page2.links.each do |link|
-        if !link.text.include? 'vs'
-          next
-        end
+      review_links.map do |link|
         threads << Thread.new do
           Kotd2.count_function(link)
         end
       end
       threads.each(&:join)
-      if !page2.link_with(text: /Next »/).nil?
-        page2 = page2.link_with(text: /Next »/).click
+      if !page.link_with(text: /Next »/).nil?
+        page = page.link_with(text: /Next »/).click
       else
         puts 'That is all'
         break
@@ -84,8 +80,7 @@ class Kotd2
   def self.start
     agent = Mechanize.new
     page = agent.get('https://genius.com/artists/King-of-the-dot')
-    page2 = agent.page.link_with(text: /Show all songs by King of the Dot/).click
-    page2
+    page = agent.page.link_with(text: /Show all songs by King of the Dot/).click
+    page
   end
-
 end
