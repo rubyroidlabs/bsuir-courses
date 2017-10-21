90d15dd9722515d2befa13c677093a2e779e5a90
SashaShostyr <sashashostyr@gmail.com>
2017-10-21 03:45:32 +0300

    Fix issues

--- 2017/SashaShostyr/2/parser.rb
+++ 2017/SashaShostyr/2/parser.rb
@@ -6,24 +6,12 @@ class Parser
   def get_links
     agent = Mechanize.new
     page = agent.get('https://genius.com/artists/King-of-the-dot')
-    first_page = page.link_with(:text => "\n              Show all songs by King of the Dot\n            ").click
-    links = first_page.links_with(:href => /https:\/\/genius\.com\/King-of-the-dot/)
-    all_pages = first_page.links_with(:href => /\/artists\/songs\?for_artist_page=117146&id=King-of-the-dot&page=\d+&pagination=true/)
-    all_pages.pop
-    all_pages.pop
-    all_pages.pop
-    all_pages.pop
-    all_pages.pop
-    all_pages.pop
-    all_pages.pop
-    all_pages.pop
-    all_pages.pop
-    all_pages.pop
-    all_pages.pop
-    all_pages.pop
+    first_page = page.link_with(text: "\n              Show all songs by King of the Dot\n            ").click
+    links = first_page.links_with(href: %r{https:\/\/genius\.com\/King-of-the-dot})
+    all_pages = first_page.links_with(href: %r{\/artists\/songs\?for_artist_page=117146&id=King-of-the-dot&page=\d+&pagination=true})
     all_pages.pop
     all_pages.each do |next_page|
-      next_page.click.links_with(:href => /https:\/\/genius\.com\/King-of-the-dot/).each { |p| links << p }
+      next_page.click.links_with(href: %r{https:\/\/genius\.com\/King-of-the-dot}).each { |p| links << p }
     end
     links
   end
