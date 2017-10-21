d9846b12c0feeeecaf67ae437c1ddd5a0efa45a4
MoRain666 <morainrallez@gmail.com>
2017-10-21 17:56:39 +0300

    implement hm 2

---
+++ 2017/Vlad_kharitonovitch/hm-2/prohod_po_ssilkam.rb
@@ -0,0 +1,50 @@
+require_relative 'job_with_rap.rb'
+agent = Mechanize.new
+@count_pages = 1
+page = agent.get("https://genius.com/artists/songs?for_artist_page=117146&id=King-of-the-dot&page=#{@count_pages}&pagination=true")
+array_of_links_of_songs = []
+page.css('div#container.mecha--deprecated div#main ul li a.song_name.work_in_progress.song_link span.title_with_artists span.song_title').each do |el|
+  array_of_links_of_songs << el.text
+end
+@count_links = 0
+20.times do  
+  @name_of_battle = array_of_links_of_songs[@count_links]
+  @count_links += 1 
+  @name_of_battle_for_output = @name_of_battle
+  @name_of_battle.downcase!.gsub!(' ','-')
+  if  @name_of_battle.include? '/'
+    @name_of_battle.gsub!('/','-')
+  elsif @name_of_battle.include? '.'
+    @name_of_battle.gsub!('.','')
+  elsif @name_of_battle.include? '&'
+    @name_of_battle.gsub!('&','and')
+  elsif (@name_of_battle.include? '(') & (@name_of_battle.include? ')')
+    @name_of_battle.gsub!('(','')
+    @name_of_battle.gsub!(')','')
+  end
+  page_of_battle =  agent.get("https://genius.com/King-of-the-dot-#{@name_of_battle}-lyrics")
+  rap_on_paragraph = page_of_battle.css('p').text
+  @rap_on_string = rap_on_paragraph.to_json
+  JSON.parse(@rap_on_string, :quirks_mode => true)
+  @rap_on_string.gsub!('\n','')
+  @rap_on_string.gsub!(']','')
+  @rap_on_string.gsub!('.','')
+  @rap_on_string.gsub!(' ','')
+  @rap_on_string.gsub!(',','')
+  @rap_on_string.gsub!(':','')
+  @rap_on_string.gsub!('-','')
+  @rap_on_string.gsub!(';','')
+  @rap_on_string.gsub!(')','')
+  @rap_on_string.gsub!('(','')
+  @rap_on_string.gsub!('`','')
+  @rap_on_string.gsub!('?','')
+  @rap_on_string.gsub!('!','')
+  @rap_on_string.gsub!('’','')
+  @rap_on_string.gsub!('\\','')
+  @rap_on_string.gsub!('/','')
+  @rap_on_string.gsub!('\'','')
+  @rap_on_string.gsub!('"','')
+  @rap_on_string = @rap_on_string.split('[')
+  start_rap = RapProcessing.new(@rap_on_string , @name_of_battle_for_output)
+  start_rap.winner
+end
