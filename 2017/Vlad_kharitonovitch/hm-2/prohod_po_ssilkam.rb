fe6184e8b98904b9464bd26dc1fd9f9bd8d2c897
MoRain666 <morainrallez@gmail.com>
2017-10-21 20:12:54 +0300

    dobavil prohod po vsem str

--- 2017/Vlad_kharitonovitch/hm-2/prohod_po_ssilkam.rb
+++ 2017/Vlad_kharitonovitch/hm-2/prohod_po_ssilkam.rb
@@ -1,28 +1,60 @@
 require_relative 'job_with_rap.rb'
 agent = Mechanize.new
-@count_pages = 1
+@count_pages = 0
+for i in 1..12 do
+  @count_pages += 1
 page = agent.get("https://genius.com/artists/songs?for_artist_page=117146&id=King-of-the-dot&page=#{@count_pages}&pagination=true")
 array_of_links_of_songs = []
 page.css('div#container.mecha--deprecated div#main ul li a.song_name.work_in_progress.song_link span.title_with_artists span.song_title').each do |el|
   array_of_links_of_songs << el.text
 end
 @count_links = 0
-20.times do  
+(array_of_links_of_songs.length).times do  
   @name_of_battle = array_of_links_of_songs[@count_links]
   @count_links += 1 
   @name_of_battle_for_output = @name_of_battle
   @name_of_battle.downcase!.gsub!(' ','-')
-  if  @name_of_battle.include? '/'
-    @name_of_battle.gsub!('/','-')
+  if (@name_of_battle.include? '.') & (@name_of_battle.include? '/')
+    @name_of_battle.gsub!('.','')
+    @name_of_battle.gsub!('/','') 
+  elsif (@name_of_battle.include? '.') & (@name_of_battle.include? '[') & (@name_of_battle.include? ']') & (@name_of_battle.include? '(') & (@name_of_battle.include? ')')
+    @name_of_battle.gsub!('[','')
+    @name_of_battle.gsub!(']','')
+    @name_of_battle.gsub!(')','')
+    @name_of_battle.gsub!('(','')
+    @name_of_battle.gsub!('.','')
+  elsif (@name_of_battle.include? '[') & (@name_of_battle.include? ']') & (@name_of_battle.include? '-+')
+    @name_of_battle.gsub!('[','')
+    @name_of_battle.gsub!(']','')
+    @name_of_battle.gsub!('-+','')
+  elsif (@name_of_battle.include? '.') & (@name_of_battle.include? ':') & (@name_of_battle.include? '#')
+    @name_of_battle.gsub!('.','')
+    @name_of_battle.gsub!(':','')
+    @name_of_battle.gsub!('#','')   
+  elsif (@name_of_battle.include? '[') & (@name_of_battle.include? ']')
+    @name_of_battle.gsub!('[','')
+    @name_of_battle.gsub!(']','')
+  elsif (@name_of_battle.include? '(') & (@name_of_battle.include? ')') & (@name_of_battle.include? '.')
+    @name_of_battle.gsub!('(','')
+    @name_of_battle.gsub!(')','')
+    @name_of_battle.gsub!('.','')
   elsif @name_of_battle.include? '.'
     @name_of_battle.gsub!('.','')
+  elsif @name_of_battle.include? '/'
+    @name_of_battle.gsub!('/','-')
   elsif @name_of_battle.include? '&'
     @name_of_battle.gsub!('&','and')
   elsif (@name_of_battle.include? '(') & (@name_of_battle.include? ')')
     @name_of_battle.gsub!('(','')
     @name_of_battle.gsub!(')','')
+  elsif @name_of_battle.include? '\''
+    @name_of_battle.gsub!('\'','')
   end
+  if @name_of_battle == 'kid-twist-vs-madness'
+    page_of_battle = agent.get("https://genius.com/Grind-time-now-kid-twist-vs-madness-lyrics")
+  else
   page_of_battle =  agent.get("https://genius.com/King-of-the-dot-#{@name_of_battle}-lyrics")
+end
   rap_on_paragraph = page_of_battle.css('p').text
   @rap_on_string = rap_on_paragraph.to_json
   JSON.parse(@rap_on_string, :quirks_mode => true)
@@ -47,4 +79,5 @@ end
   @rap_on_string = @rap_on_string.split('[')
   start_rap = RapProcessing.new(@rap_on_string , @name_of_battle_for_output)
   start_rap.winner
+end
 end
