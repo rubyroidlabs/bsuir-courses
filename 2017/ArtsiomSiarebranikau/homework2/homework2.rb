bdbe8d4102572702609a61bcd52a635b38a00782
Art Ser <artser97@yandex.ru>
2017-10-22 23:32:10 +0300

    homework-2

--- 2017/ArtsiomSiarebranikau/homework2/homework2.rb
+++
@@ -1,5 +0,0 @@
-load 'battle.rb'
-
-ENV['NAME'] = '' if ENV['NAME'].nil?
-ENV['CRITERIA'] = '' if ENV['CRITERIA'].nil?+
-Battle.new.parse(ENV['NAME'], ENV['CRITERIA'])
--- 2017/ArtsiomSiarebranikau/homework2/wik.rb
+++ 2017/ArtsiomSiarebranikau/homework2/wik.rb
@@ -1,32 +1,33 @@
+require 'rubygems'
 require 'mechanize'
+require 'pry'
 
-class Battle
+
+class Task
   def initialize
-    @agn = Mechanize.new
+    @agent = Mechanize.new
   end
 
-  def parse(raper = '', criteria = '')
-    @agn.get('https://genius.com/artists/King-of-the-dot')
-    page = @agn.page.link_with(text: /Show all songs by King of the Dot/).click
-
+  def parse(artist = '')
+    @agent.get('https://genius.com/artists/King-of-the-dot')
+    page = @agent.page.link_with(text: /Show all songs by King of the Dot/).click
     loop do
       links = []
       page.links.each do |link|
         links.push(link) if link.text.split(' ').include?('vs')
       end
       links.each do |link|
-        txt = @agn.get(link.href).search('.song_body-lyrics').text.split("\n")
+        txt = @agent.get(link.href).search('.song_body-lyrics').text.split("\n")
         name = txt[2].split(' Lyrics')
         name = name.first.split('        ')
         names = name.last
         name = name.last.split(' vs ')
-        if name.include?(raper)
-          out(scan(txt, name, criteria), names, link)
-        elsif raper == ''
-          out(scan(txt, name, criteria), names, link)
+        if name.include?(artist)
+          print(scan(txt, name), names, link)
+        elsif artist == ''
+          print(scan(txt, name), names, link)
         end
       end
-
       page.links.each do |link|
         links = link if link.text.split(' ').include?('Next')
       end
@@ -35,30 +36,25 @@ class Battle
     end
   end
 
-  private def out(battle, name, link)
+  def print(battle, name, link)
     puts "#{name} - #{link.href}"
-    battle.each do |key, value|
-      puts "#{key} - #{value}" if key != ''
-    end
     battle = battle.sort_by(&:last)
     puts battle.last.first + ' WINS!'
     puts
   end
 
-  def scan(text, name, criteria)
+  def scan(text, name)
     key = ''
     results = Hash.new(0)
-    criteria == '' ? /\w/ : /#{criteria}/
-
     text.each do |line|
       if line.include?('[Round') && line.include?(name.first + ']')
         key = name.first
       elsif line.include?('[Round') && line.include?(name.last + ']')
         key = name.last
-      else
-        results[key] += line.scan(criteria).size
       end
     end
-    results
+    
   end
 end
+ENV['NAME'] = '' if ENV['NAME'].nil?
+Task.new.parse(ENV['NAME'])
