9f62faf315640aff49f4a1e986679cbcb0c37d59
whiterabbit8 <natashaivanyuk@gmail.com>
2017-10-22 22:14:07 +0300

    homework 2

--- 2017/NatalliaIvaniuk/2/kotd.rb
+++ 2017/NatalliaIvaniuk/2/kotd.rb
@@ -6,7 +6,8 @@ name = ENV['NAME']
 crit = ENV['CRITERIA']
 agent = Mechanize.new
 page = agent.get('https://genius.com/artists/songs?for_artist_page=117146&id=King-of-the-dot')
-song_links = page.links_with(href: %r{https://genius.com/King-of-the-dot-.+-lyrics})
+href_reg_exp = %r{https://genius.com/King-of-the-dot-.+-lyrics}
+song_links = page.links_with(href: /#{href_reg_exp}/)
 songs = []
 song_links.each do |link|
   song = link.click
@@ -19,12 +20,14 @@ end
 wins = 0
 loses = 0
 n = 0
+artist1text = []
+artist2text = []
+a1l = 0
+a2l = 0
 
-loop do
+20.times do
   artist1 = songs[n][:title].split(/[vV]s.?/)[0].strip
   artist2 = songs[n][:title].split(/[vV]s.?/)[1].strip
-  artist1text = []
-  artist2text = []
   battle_text = songs[n][:text].split(/\[Round [123].+\]/)
   battle_text.each_with_index do |val, index|
     if index.even?
@@ -33,8 +36,6 @@ loop do
       artist1text << val
     end
   end
-  a1l = 0
-  a2l = 0
   if crit.empty?
     artist1text.each { |x| a1l += x.scan(/\w/).size }
     artist2text.each { |x| a2l += x.scan(/\w/).size }
@@ -42,7 +43,6 @@ loop do
     artist1text.each { |x| a1l += x.scan(/#{crit}/).size }
     artist2text.each { |x| a2l += x.scan(/#{crit}/).size }
   end
-
   if songs[n][:title].include? name
     puts songs[n][:title] + ' - ' + songs[n][:href]
     puts artist1 + ' - ' + a1l.to_s
@@ -62,10 +62,8 @@ loop do
     else
       puts 'DRAW!'
     end
-
     puts '*' * 100
   end
   n += 1
-  break if n == 20
 end
 puts name + ' wins ' + wins.to_s + ' times, loses ' + loses.to_s + ' times.'
