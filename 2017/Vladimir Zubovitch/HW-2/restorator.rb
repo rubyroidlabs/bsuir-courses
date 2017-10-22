8b9eda390154fc7e2ee2fcabf9ec5ed9696bc945
Morawiin <moraviin@gmail.com>
2017-10-23 00:32:38 +0300

    fix.2

--- 2017/Vladimir Zubovitch/HW-2/restorator.rb
+++ 2017/Vladimir Zubovitch/HW-2/restorator.rb
@@ -1,35 +1,36 @@
 require_relative 'test.rb'
 class Restorator
-  def initialize 
+  def initialize
     @a = Obhod.new
     @text = @a.reload
-  end 
-  def next 
+  end
+  
+  def next
     otvet=[]
     @text.each do |battle|
-    battle = battle.gsub(/Round (1|2|3): /){''}
-    battle = battle.gsub(/^$/ , '')
-    battle = battle.split('[')
-    mc_round=[]
-    battle.each do |lvl|
-      unless lvl.empty?
-        mc_round += lvl.split(']')
-      end
-    end
-    raper_1 = mc_round[0]
-    raper_2 = mc_round[2]
-    rs_1=0
-    rs_2=0
-    unless mc_round[2].nil?
-      mc_round.size.times do |i|
-        if raper_1 == mc_round[i]
-          rs_1 += mc_round[i+1].size
-        elsif raper_2 == mc_round[i]
-          rs_2 += mc_round[i+1].size      
+      battle = battle.gsub(/Round (1|2|3): /){''}
+      battle = battle.gsub(/^$/, '')
+      battle = battle.split('[')
+      mc_round = []
+      battle.each do |lvl|
+        unless lvl.empty?
+          mc_round += lvl.split(']')
         end
       end
-    otvet << [raper_1 , raper_2 , rs_1 , rs_2] 
-    end
+      raper_1 = mc_round[0]
+      raper_2 = mc_round[2]
+      rs_1 = 0
+      rs_2 = 0
+      unless mc_round[2].nil?
+        mc_round.size.times do |i|
+          if raper_1 == mc_round[i]
+            rs_1 += mc_round[i + 1].size
+          elsif raper_2 == mc_round[i]
+            rs_2 += mc_round[i + 1].size
+          end
+        end
+      otvet << [raper_1 , raper_2 , rs_1 , rs_2]
+      end
     end
     @text = @a.reload
     return otvet
