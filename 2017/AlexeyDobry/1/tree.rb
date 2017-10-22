cb26aa4c8222e10974f0a27dfb4df30c74603350
ADwinion <ADwinion@gmail.com>
2017-10-22 23:06:50 +0300

    HW2

--- 2017/AlexeyDobry/1/tree.rb
+++ 2017/AlexeyDobry/1/tree.rb
@@ -27,7 +27,17 @@ class Tree
     if ENV['NAME'].nil?
       links.each do |file_name|
         read(file_name)
+        tree_name = 'trees/' + @name + '.tree'
+        arr = tree_name.scan(/\d\d*/)
+        str = tree_name.delete '0-9'
         tree_node(@json, @root)
+        binding.pry
+        tree = []
+        create(arr, str)
+        level = 0
+        level = get_tree_level(arr)
+        max_vertex = arr.size
+        tree_show(tree, level, max_vertex)
         puts
         puts @name
         puts
@@ -80,6 +90,70 @@ class Tree
       node.right = @right
     end
   end
+
+  def create(arr, str)
+    vertex = 1
+    i = 0
+    j = 0
+    tree = []
+    tree[vertex] = arr[j]
+    while i < str.size - 1
+      if str[i] + str[i + 1] == ',['
+        vertex *= 2
+        unless tree[vertex * 2].nil?
+          vertex += 1
+        end
+        j += 1
+        tree[vertex] = arr[j]
+      elsif str[i] == ','
+        vertex += 1
+        j += 1
+        tree[vertex] = arr[j]
+      elsif str[i] == ']'
+        if !tree[vertex * 2].nil? && tree[(vertex * 2) + 1].nil?
+          vertex *= 2
+        end
+        vertex /= 2
+      end
+      i += 1
+    end
+    tree
+  end
+
+  def get_tree_level(arr)
+    size = arr.size
+    level = 0
+    while size != 0
+      level += 1
+      size /= 2
+    end
+    level
+  end
+
+  def tree_show(tree, level, max_vertex)
+    i = 1
+    vertex = 1
+    max_vertex -= 1
+    while max_vertex != 0
+      print ' ' * (max_vertex - 1)
+      met = 0
+      while met != i
+        print tree[vertex]
+        if tree[vertex].to_i < 10
+          print ' ' * (2**(level + 1) - 1)
+        else
+          print ' ' * (2**(level + 1) - 2)
+        end
+        vertex += 1
+        met += 1
+      end
+      i *= 2
+      max_vertex /= 2
+      level -= 1
+      print "\n\n"
+    end
+  end
+
 
 end
 
