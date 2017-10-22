4c9223eb0d843a0322728a51200f4c5bd2047c8f
SergeiKholodtsov <31796851+SergeiKhaladtsou@users.noreply.github.com>
2017-10-22 16:58:41 +0300

    Homework - 2 (First commit)

---
+++ 2017/KholodtsovSergei/2/main.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+require_relative 'fight.rb'
+
+name = ENV['NAME']
+criteria = ENV['CRITERIA'] + ' '
+
+if name.nil?
+  name = 'lyrics'
+  Fight.new.start_figths(name, criteria)
+else
+  Fight.new.start_figth(name, criteria)
+end
+
