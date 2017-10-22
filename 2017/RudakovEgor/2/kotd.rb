92a4e452f1f40e1a25f8a369f1d34c1fbac4d8d7
Lifofx <egorrudakov3@gmail.com>
2017-10-22 21:28:41 +0300

    implement homework-2

---
+++ 2017/RudakovEgor/2/kotd.rb
@@ -0,0 +1,17 @@
+require_relative 'parser'
+require_relative 'battle'
+require 'pry'
+
+@stats = []
+parser = Parser.new
+data = parser.get_battle_data
+data.each do |t|
+  battle = Battle.new
+  battle.set_battle(t)
+  @stats << battle.stats unless ENV['NAME'].nil?
+end
+wins = @stats.count(1)
+losses = @stats.count(0)
+unless ENV['NAME'].nil?
+  puts "#{ENV['NAME']} wins #{wins} times, loses #{losses} times."
+end
