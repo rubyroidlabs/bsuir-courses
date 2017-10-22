0d77417a427af2435a7c812648ee219c3e3ded0c
Egor Shpalov <gudel1998@rambler.ru>
2017-10-22 18:43:48 +0000

    test fixing by rubocop

--- 2017/Egor Shpalov/2/multithread.rb
+++ 2017/Egor Shpalov/2/multithread.rb
@@ -1,46 +1,55 @@
 class Multithread
-
   def self.get_pagination_pages(pagination, battles)
     threads = []
     pagination.count.times do |n|
       threads << Thread.new(pagination[n]) do |link|
         battles << Parse.get_battle_page(link)
       end
-    end 
-    threads.each { |t| t.join }
+    end
+    threads.each(&:join)
+  end
+
+  def self.threads_iter(group, threads, total_links, total_results)
+    10.times do |n|
+      unless group[n].nil?
+        threads << Thread.new(group[n]) do |link|
+          put_info(total_results, total_links, link)
+        end
+      end
+    end
+  end
+
+  def self.multithread_iter(battles, total_links, total_results)
+    battles.each_slice(10) do |group|
+      threads = []
+      threads_iter(group, threads, total_links, total_results)
+      threads.each(&:join)
+    end
+  end
+
+  def self.simple_iter(battles, total_links, total_results)
+    battles.each do |link|
+      if link.text.include? ENV['NAME'].to_s
+        put_info(total_results, total_links, link)
+      end
+    end
   end
 
   def self.multiparse(battles)
     total_results = []
     total_links = []
-    if ENV["NAME"].nil?
-      battles.each_slice(10) do |group|
-        threads = []
-        10.times do |n|
-          if group[n] != nil
-            threads << Thread.new(group[n]) do |link|
-              put_info_to_structs(total_results, total_links, link)
-            end
-          end
-        end
-        threads.each { |t| t.join }
-      end
+    if ENV['NAME'].nil?
+      multithread_iter(battles, total_links, total_results)
     else
-      battles.each do |link|
-        if link.text.include? "#{ENV['NAME']}"
-          put_info_to_structs(total_results, total_links, link)
-        end
-      end
+      simple_iter(battles, total_links, total_results)
     end
     [total_results, total_links]
   end
 
-  private
-
-    def self.put_info_to_structs(total_results, total_links, link)
-      result = Parse.get_result(link)
-      total_results << result
-      total_links << link.href
-      puts link.href
-    end
+  def self.put_info(total_results, total_links, link)
+    result = Parse.get_result(link)
+    total_results << result
+    total_links << link.href
+    puts link.href
+  end
 end
--- 2017/Egor Shpalov/2/output.rb
+++ 2017/Egor Shpalov/2/output.rb
@@ -1,45 +1,52 @@
 class Output
-  
   def self.outprint(total_results, total_links)
-    scores = {wins: 0, loses: 0}
+    scores = { wins: 0, loses: 0 }
     total_results.each_with_index do |hash, i|
       scores = print_res(hash, total_links[i], scores)
     end
-    puts print_scores(scores) if !ENV["NAME"].nil?
+    puts print_scores(scores) unless ENV['NAME'].nil?
   end
 
-  private
+  def self.print_scores(scores)
+    "#{ENV['NAME']} wins #{scores[:wins]}, loses #{scores[:loses]}."
+  end
 
-    def self.print_scores(scores)
-      "#{ENV['NAME']} wins #{scores[:wins]} times, loses #{scores[:loses]} times."
+  def self.conditions_of_print(first_arr, last_arr)
+    if first_arr[1] > last_arr[1]
+      log = "#{first_arr[0]} WINS!"
+    elsif first_arr[1] < last_arr[1]
+      log = "#{last_arr[0]} WINS!"
+    else
+      log = 'DRAW or No information...'
     end
+    log
+  end
 
-    def self.print_text(first_arr, last_arr, total_link, scores)
-      puts "#{first_arr[0]} vs #{last_arr[0]} - #{total_link}"
-      puts "#{first_arr[0]} - #{first_arr[1]}"
-      puts "#{last_arr[0]} - #{last_arr[1]}"
-      if first_arr[1] > last_arr[1]
-        log = "#{first_arr[0]} WINS!"
-      elsif first_arr[1] < last_arr[1]
-        log = "#{last_arr[0]} WINS!"
-      else
-        log = "DRAW or No information..."
-      end
-      if log.include? "#{ENV['NAME']}"
-        scores[:wins] += 1
-      elsif log != "No information..."
-        scores[:loses] += 1
-      end
-      puts log
-      puts
-      scores
+  def self.increment_conditions(scores, log)
+    if log.include? ENV['NAME'].to_s
+      scores[:wins] += 1
+    elsif log != 'No information...'
+      scores[:loses] += 1
     end
+    scores
+  end
 
-    def self.print_res(hash, total_link, scores)
-      first_arr = hash.first
-      last_arr = hash.delete(first_arr.first)
-      last_arr = hash.first
-      scores = print_text(first_arr, last_arr, total_link, scores)
-      scores
-    end
+  def self.print_text(first_arr, last_arr, total_link, scores)
+    puts "#{first_arr[0]} vs #{last_arr[0]} - #{total_link}"
+    puts "#{first_arr[0]} - #{first_arr[1]}"
+    puts "#{last_arr[0]} - #{last_arr[1]}"
+    log = conditions_of_print(first_arr, last_arr)
+    scores = increment_conditions(scores, log)
+    puts log
+    puts
+    scores
+  end
+
+  def self.print_res(hash, total_link, scores)
+    first_arr = hash.first
+    last_arr = hash.delete(first_arr.first)
+    last_arr = hash.first
+    scores = print_text(first_arr, last_arr, total_link, scores)
+    scores
+  end
 end
