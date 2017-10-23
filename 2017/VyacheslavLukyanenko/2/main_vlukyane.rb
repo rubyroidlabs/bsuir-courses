352098487e5c25fafd8f5ff2c5e74ceae2222e66
vlukyaneRubyLabs <vlukyane@gmail.com>
2017-10-24 00:55:02 +0300

    homework-2 p.1

--- 2017/VyacheslavLukyanenko/2/main_vlukyane.rb
+++ 2017/VyacheslavLukyanenko/2/main_vlukyane.rb
@@ -1,7 +1,7 @@
 require_relative 'page_skrapping'
 
-name = ENV['NAME']
+name1 = ENV['NAME']
 criteria = ENV['CRITERIA']
 
 new_request = Rap_playground.new
-new_request.load_battles_links(criteria, name)
+new_request.load_battles_links(criteria, name1)
