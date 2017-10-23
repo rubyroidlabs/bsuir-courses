05a6054c719af95616e4129bf00ec4b0b3b2674c
IlyaStip <sii98@list.ru>
2017-10-23 14:28:15 +0300

    Update home_work_2.rb

--- 2017/IlyaStipakov/2/home_work_2.rb
+++ 2017/IlyaStipakov/2/home_work_2.rb
@@ -11,14 +11,16 @@ link = page.link_with(text: /\Show all songs by King of the Dot/)
 pages = link.click
 
 pages.links.each do |battle|
+  info_array = Array[]
   next unless battle.text.strip.index('vs')
   page = battle.click
-  info_array = parser.link_sort(page)
+  parser.link_sort(page) { |x| info_array.push(x) }
   head_post = info_array[0]
   text = info_array[1]
   mc_first = info_array[2]
   mc_second = info_array[3]
-  info_array = parser.line_section(text, mc_first, mc_second)
+  info_array.clear
+  parser.line_section(text, mc_first, mc_second){ |x| info_array.push(x) }
   left_str = info_array[0]
   right_str = info_array[1]
   parser.print_result(head_post, mc_first, mc_second, left_str, right_str)
