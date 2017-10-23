2fc85b2d455b2eac68254823159332b3f0de4a95
IlyaStip <sii98@list.ru>
2017-10-23 13:06:31 +0300

    Update home_work_2.rb

--- 2017/IlyaStipakov/2/home_work_2.rb
+++ 2017/IlyaStipakov/2/home_work_2.rb
@@ -14,12 +14,12 @@ pages.links.each do |battle|
   next unless battle.text.strip.index('vs')
     page = battle.click
     info_array = parser.link_sort(page)
-    head = info_array[0]
+    head_post = info_array[0]
     text = info_array[1]
-    mc_left = info_array[2]
-    mc_right = info_array[3]
-    info_array = parser.line_section(text, mc_left, mc_right)
+    mc_first = info_array[2]
+    mc_second = info_array[3]
+    info_array = parser.line_section(text, mc_first, mc_second)
     left_str = info_array[0]
     right_str = info_array[1]
-    parser.print_result(head, mc_left, mc_right, left_str, right_str)
+    parser.print_result(head_post, mc_first, mc_second, left_str,right_str)
 end
