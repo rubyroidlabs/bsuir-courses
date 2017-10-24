e84f2269771dcc0ffd53f54117fa731294551e7d
v.makarevich@hackspace.by <vitaliy.makarevich.work@gmail.com>
2017-10-24 17:56:45 +0300

    taleh issues

--- 2017/VitoMakarevich/1/tree_printer.rb
+++ 2017/VitoMakarevich/1/tree_printer.rb
@@ -3,28 +3,15 @@ class TreePrinter
   def initialize(parsed_tree)
     @parsed_tree = parsed_tree
     @rendered_tree = []
+    @depth = @parsed_tree.count
   end
 
   def render
-    depth = @parsed_tree.count
     @parsed_tree.each_with_index do |layer, index|
-      padding_left = ' ' * (2**(depth - index + 1) - 2)
-      numbers_level = '' << padding_left
-      pointers_level = '' << padding_left
-      pointers = ['/', '\\']
-      layer.each_with_index do |element, inner_index|
-        if inner_index.even?
-          pointers_level << ' ' << pointers[inner_index % 2]
-        else
-          pointers_level << pointers[inner_index % 2] << ' '
-        end
-        padding_between = ' ' * (2**(depth - index + 2) - 2)
-        inner_index != (layer.count - 1) &&
-          pointers_level << padding_between
-        numbers_level << numeric_to_str(element)
-        inner_index != (layer.count - 1) &&
-          numbers_level << padding_between
-      end
+      padding_left = ' ' * (2**(@depth - index + 1) - 2)
+      numbers_level = padding_left.clone
+      pointers_level = padding_left.clone
+      put_info(pointers_level, numbers_level, layer, index)
       @rendered_tree.push(pointers_level) unless index.zero?
       @rendered_tree.push(numbers_level)
     end
@@ -32,6 +19,20 @@ class TreePrinter
   end
 
   private
+
+  def put_info(pointers_level, numbers_level, layer, index)
+    pointers = ['/', '\\']
+    layer.each_with_index do |element, inner_index|
+      pointers_level << if inner_index.even?
+                          " #{pointers[inner_index % 2]}"
+                        else
+                          "#{pointers[inner_index % 2]} "
+                        end
+      padding_between = ' ' * (2**(@depth - index + 2) - 2)
+      pointers_level << padding_between
+      numbers_level.concat(numeric_to_str(element), padding_between)
+    end
+  end
 
   def numeric_to_str(numeric)
     '' << (numeric / 10 > 0 ? numeric / 10 : ' ').to_s \
