bbba6a4d3b6d68a49f86e17db7cf1d5d4f9c2b09
ZhenyaZhak <zhenya-zhak@mail.ru>
2017-10-25 18:16:16 +0300

    hw 2.3

--- 2017/ZhenyaZhak/2/kotd2.rb
+++ 2017/ZhenyaZhak/2/kotd2.rb
@@ -1,58 +1,74 @@
 class Kotd2
-  def self.count_function(link)
-    name_b = ENV['NAME'] ? ENV['NAME'].downcase : ' '
-    criteria_b = ENV['CRITERIA'] ? ENV['CRITERIA'].downcase : /\w/
-    page = link.click
-    txt_link = link.text.split('(')[0].strip!
-    txt_link.sub('vs.', 'vs')
-    name_batler = Array.new
-    name_batler[0] = txt_link.split('vs')[0].strip
-    name_batler[1] = txt_link.split('vs')[1].strip
-    if name_b != ' '
-      if name_b != name_batler[0].downcase && name_b != name_batler[1].downcase
-        return nil
-      end
+  # F_B - First Batler, S_B - Second Batler
+  INCORRECT_VALUE = 2
+  F_B = 0
+  S_B = 1
+  NAME_B = ENV['NAME'] ? ENV['NAME'].downcase : ' '
+  CRITERIA_B = ENV['CRITERIA'] ? ENV['CRITERIA'].downcase : /\w/
+
+  def self.puts_batler(count_batler, name_batler, link_uri)
+    count = INCORRECT_VALUE
+    if count_batler[F_B].zero? || count_batler[S_B].zero?
+      return nil
     end
-    txt = page.css('.lyrics p').text
-    count = 0
+    if count_batler[F_B] == count_batler[S_B]
+      str = 'Dead heat!!!'
+    else
+      x = count_batler[F_B] > count_batler[S_B] ? F_B : S_B
+      str = "#{name_batler[x]} REAL NIGGA!!!"
+      count = NAME_B == name_batler[x].downcase ? F_B : S_B
+    end
+    puts "\n#{name_batler[F_B]} vs #{name_batler[S_B]} - #{link_uri}"
+    puts "#{name_batler[F_B]} - #{count_batler[F_B]}"
+    puts "#{name_batler[S_B]} - #{count_batler[S_B]}\n#{str}\n"
+    count
+  end
+
+  def self.length_search(txt, name_batler)
     count_batler = Array.new(2, 0)
     txt = txt.split("\n\n")
-    flag = 2
+    flag = INCORRECT_VALUE
     txt.length.times do |i|
       txt[i] = txt[i].downcase
-      count = txt[i].scan(criteria_b).size
-      if txt[i][0..30].include? name_batler[0].downcase
-        count_batler[0] += count
-        flag = 0
+      count = txt[i].scan(CRITERIA_B).size
+      # 0..30 range where the name is located
+      if txt[i][0..30].include? name_batler[F_B].downcase
+        count_batler[F_B] += count
+        flag = F_B
         next
       end
-      if txt[i][0..30].include? name_batler[1].downcase
-        count_batler[1] += count
-        flag = 1
+      if txt[i][0..30].include? name_batler[S_B].downcase
+        count_batler[S_B] += count
+        flag = S_B
         next
       end
+      # 150 is the threshold number of characters in the block
       if txt[i].scan(/\w/).size > 150
-        if flag == 2
+        if flag == INCORRECT_VALUE
           next
         else
           count_batler[flag] += count
         end
       end
     end
-    count = 2
-    if count_batler[0].zero? || count_batler[1].zero?
-      return nil
+    count_batler
+  end
+
+  def self.count_function(link)
+    page = link.click
+    txt_link = link.text.split('(')[0].strip!
+    txt_link.sub('vs.', 'vs')
+    name_batler = Array.new
+    name_batler[F_B] = txt_link.split('vs')[F_B].strip
+    name_batler[S_B] = txt_link.split('vs')[S_B].strip
+    if NAME_B != ' '
+      if NAME_B != name_batler[F_B].downcase && NAME_B != name_batler[S_B].downcase
+        return nil
+      end
     end
-    if count_batler[0] == count_batler[1]
-      str = 'Dead heat!!!'
-    else
-      x = count_batler[0] > count_batler[1] ? 0 : 1
-      str = "#{name_batler[x]} REAL NIGGA!!!"
-      count = name_b == name_batler[x].downcase ? 0 : 1
-    end
-    puts "\n#{name_batler[0]} vs #{name_batler[1]} - #{link.uri}"
-    puts "#{name_batler[0]} - #{count_batler[0]}"
-    puts "#{name_batler[1]} - #{count_batler[1]}\n#{str}\n"
+    txt = page.css('.lyrics p').text
+    count_batler = Kotd2.length_search(txt, name_batler)
+    count = Kotd2.puts_batler(count_batler, name_batler, link.uri)
     count
   end
 
@@ -64,7 +80,7 @@ class Kotd2
       review_links.map do |link|
         threads << Thread.new do
           count = Kotd2.count_function(link)
-          if !count.nil? && count != 2
+          if !count.nil? && count != INCORRECT_VALUE
             w_l[count] += 1
           end
         end
@@ -78,7 +94,7 @@ class Kotd2
       end
     end
     if ENV['NAME']
-      puts "\n#{ENV['NAME']} wins #{w_l[0]} times, loses #{w_l[1]} times"
+      puts "\n#{ENV['NAME']} wins #{w_l[F_B]} times, loses #{w_l[S_B]} times"
     end
   end
 
