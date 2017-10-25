1ddd126f49eabe6c24363caefd502392bbe05f81
ZhenyaZhak <zhenya-zhak@mail.ru>
2017-10-25 09:46:10 +0300

    hw 2

--- 2017/ZhenyaZhak/2/kotd.rb
+++ 2017/ZhenyaZhak/2/kotd.rb
@@ -1,5 +1,4 @@
 require 'mechanize'
 require_relative 'kotd2'
 
-page = Kotd2.start
-Kotd2.link_run(page)
+Kotd2.link_run(Kotd2.start)
--- 2017/ZhenyaZhak/2/kotd2.rb
+++ 2017/ZhenyaZhak/2/kotd2.rb
@@ -4,23 +4,18 @@ class Kotd2
     criteria_b = ENV['CRITERIA'] ? ENV['CRITERIA'].downcase : /\w/
     page = link.click
     txt_link = link.text.split('(')[0].strip!
-    if txt_link.include? '&'
-      return
-    end
     txt_link.sub('vs.', 'vs')
     name_batler = Array.new
     name_batler[0] = txt_link.split('vs')[0].strip
     name_batler[1] = txt_link.split('vs')[1].strip
     if name_b != ' '
       if name_b != name_batler[0].downcase && name_b != name_batler[1].downcase
-        return
+        return nil
       end
     end
     txt = page.css('.lyrics p').text
     count = 0
-    count_batler = Array.new
-    count_batler[0] = 0
-    count_batler[1] = 0
+    count_batler = Array.new(2, 0)
     txt = txt.split("\n\n")
     flag = 0
     txt.length.times do |i|
@@ -45,27 +40,34 @@ class Kotd2
         end
       end
     end
+    count = 2
     if count_batler[0].zero? || count_batler[1].zero?
-      return
+      return nil
     end
     if count_batler[0] == count_batler[1]
       str = 'Dead heat!!!'
     else
       x = count_batler[0] > count_batler[1] ? 0 : 1
       str = "#{name_batler[x]} REAL NIGGA!!!"
+      count = name_b == name_batler[x].downcase ? 0 : 1
     end
     puts "\n#{name_batler[0]} vs #{name_batler[1]} - #{link.uri}"
     puts "#{name_batler[0]} - #{count_batler[0]}"
-    puts "#{name_batler[1]} - #{count_batler[1]}\n#{str}\n"    
+    puts "#{name_batler[1]} - #{count_batler[1]}\n#{str}\n"
+    count
   end
 
   def self.link_run(page)
+    w_l = Array.new(2, 0)
     loop do
       review_links = page.links_with(text: /vs/)
       threads = []
       review_links.map do |link|
         threads << Thread.new do
-          Kotd2.count_function(link)
+          count = Kotd2.count_function(link)
+          if !count.nil? && count != 2
+            w_l[count] += 1
+          end
         end
       end
       threads.each(&:join)
@@ -76,12 +78,14 @@ class Kotd2
         break
       end
     end
+    if ENV['NAME']
+      puts "\n#{ENV['NAME']} wins #{w_l[0]} times, loses #{w_l[1]} times"
+    end
   end
 
   def self.start
     agent = Mechanize.new
     page = agent.get('https://genius.com/artists/King-of-the-dot')
     page = page.link_with(text: /Show all songs by King of the Dot/).click
-    page
   end
 end
