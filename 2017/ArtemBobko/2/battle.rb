ea043993b44c7b7f9416b7f8b544d9fca1f2e06b
Artem <artembobko@gmail.com>
2017-10-26 23:50:36 +0300

    Remarks fixed, add multithreading

--- 2017/ArtemBobko/2/battle.rb
+++ 2017/ArtemBobko/2/battle.rb
@@ -3,12 +3,12 @@ class Battle
 
   def initialize(song_page, song)
     @song = song
-    rappers = song['title'].split(/[Vv]s/)
+    rappers = song['title'].split(/vs\.?/i)
     @rappers = rappers.map { |i| i.gsub(/\s+/, '') }
     @text = song_page.search('.lyrics p').text
   end
 
-  def count(text, i)
+  def count_s(text, i)
     sum = 0
     until text[i].nil?
       sum += text[i].length
@@ -20,20 +20,34 @@ class Battle
   def count_symbols
     text = @text.split(/\[Round [123].+\]\n/)
     text.shift
-    text.map { |i| i.gsub(/\s+/, '') }
-    @number1 = count(text, 0)
-    @number2 = count(text, 1)
+    text = text.map { |i| i.gsub(/\s+/, '') }
+    @number1 = count_s(text, 0)
+    @number2 = count_s(text, 1)
+  end
+
+  def count_c(text, i)
+    sum = 0
+    until text[i].nil?
+      t
+      i += 2
+    end
+    sum
+  end
+
+  def count_cryteria(cryteria)
+    text = @text.split(/\[Round [123].+\]\n/)
+    text.shift
+    @number1 = count_c(text, 0)
+    @number2 = count_c(text, 1)
   end
 
   def output
-    if @text.nil? || @rappers.first.nil? || @rappers.last.nil?
+    if @text.nil? || @rappers.any?(&:nil?)
       puts 'Error'
       return
     end
-    print "#{@rappers.first} - "
-    puts @number1
-    print "#{@rappers.last} - "
-    puts @number2
+    print "#{@rappers.first} - #{@number1}\n"
+    print "#{@rappers.last} - #{@number2}\n"
     if @number1 > @number2
       puts "#{@rappers.first} WINS!"
     elsif @number1 < @number2
--- 2017/ArtemBobko/2/battles.rb
+++ 2017/ArtemBobko/2/battles.rb
@@ -1,36 +1,44 @@
 require 'rubygems'
 require 'mechanize'
 require 'json'
+require 'uri'
 require_relative 'battle.rb'
+
 
 class Battles
   def initialize
     @agent = Mechanize.new
-    @request = 'https://genius.com/api/artists/117146/songs?page='
     @songs = []
   end
 
   def get_all_battles
     next_page = 1
-    puts "Подождите пока баттлы загрузятся."
-    until next_page.nil?
-      puts '...'
-      songs = @agent.get(@request + next_page.to_s).content
-      songs = JSON.parse(songs)
-      next_page = songs['response']['next_page']
+    threads = []
+    request = URI 'https://genius.com/api/artists/117146/songs?page=1'
+    puts 'Подождите пока батлы загрузятся.'
+    until request.query.eql? 'page='
+      songs = JSON.parse(@agent.get(request).content)
+      request.query = "page=#{songs['response']['next_page']}"
       songs = songs['response']['songs'].uniq
-      songs.map do |song|
-        battle = Battle.new(@agent.get(song['url']), song)
-        @songs << battle
+      threads << Thread.new do
+        songs.map do |song|
+          battle = Battle.new(@agent.get(song['url']), song)
+          @songs << battle
+        end
       end
     end
+    threads.each(&:join)
+  end
+
+  def output_battle(battle)
+    battle.count_symbols
+    puts "#{battle.song['title']} - #{battle.song['url']}"
+    battle.output
   end
 
   def put_all_battles
     @songs.map do |battle|
-      battle.count_symbols
-      puts battle.song['title'] + ' - ' + battle.song['url']
-      battle.output
+      process_battle(battle)
     end
   end
 
@@ -38,10 +46,8 @@ class Battles
     wins = 0
     loses = 0
     @songs.map do |battle|
-      if (battle.rappers.first == name) || (battle.rappers.last == name)
-        battle.count_symbols
-        puts battle.song['title'] + ' - ' + battle.song['url']
-        battle.output
+      if battle.rappers.any? {|r| r.eql? name}
+        output_battle(battle)
         if battle.win?(name)
           wins += 1
         else
