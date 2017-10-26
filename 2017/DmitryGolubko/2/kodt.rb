522e11bf4b1097c2425f7db28a07d39ae325a498
DmitryGolubko <golubko97@gmail.com>
2017-10-26 21:44:21 +0300

    Add files via upload
    hw - 2

---
+++ 2017/DmitryGolubko/2/kodt.rb
@@ -0,0 +1,20 @@
+require 'mechanize'
+require 'nokogiri'
+require 'open-uri'
+require 'net/http'
+require 'json'
+require_relative 'battle'
+require_relative 'parser'
+
+battler = ENV['NAME']
+word_to_count = ENV['CRITERIA']
+
+battles = Parser.get_battles('https://genius.com/api/artists/117146/songs')
+unless battler.nil?
+  battles = Parser.get_battles_by_name(battler)
+end
+Parser.get_content(battles, word_to_count)
+
+unless battler.nil?
+  Parser.count_wins(battles, battler)
+end
