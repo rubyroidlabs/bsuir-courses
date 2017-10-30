16c24db0163edd64570d3f39a7712b5e0789cd66
denis.nikolski <dzianisnikolski@gmail.com>
2017-10-30 20:36:11 +0300

    try

--- 2017/DenisNikolsky/1/trees.rb
+++ 2017/DenisNikolsky/1/trees.rb
@@ -3,10 +3,10 @@ require 'json'
 
 class Tree
   attr_accessor :left, :right, :data
+  MAX_SUM_NODE = 5000
+  MAX_DEPTH = 5
 
   def initialize(data = nil)
-    @left = nil
-    @right = nil
     @data = data
   end
 
@@ -25,7 +25,7 @@ class Tree
   end
 
   def insert_left(node)
-    if node.class == [].class
+    if node.is_a?(Array)
       @left = Tree.new(node[0])
       @left.insert(node[1])
     else
@@ -34,7 +34,7 @@ class Tree
   end
 
   def insert_right(node)
-    if node.class == [].class
+    if node.is_a?(Array)
       @right = Tree.new(node[0])
       @right.insert(node[1])
     else
@@ -56,11 +56,11 @@ class Tree
   def print_tree
     depth = max_depth
     spaces = 40 * depth
-    puts format("%#{spaces}i", @data)
-    puts format("%#{spaces}s", '/  \\')
-    list = []
-    list << @left
-    list << @right
+    print_format(spaces, @data.to_s)
+    puts
+    print_format(spaces, '/  \\')
+    puts
+    list = [@left, @right]    
     print_node(list, depth)
   end
 
@@ -73,13 +73,13 @@ class Tree
       node_left = list.shift
       node_right = list.shift
       spaces = 40 * depth / elements
-      print format("%#{spaces}i", node_left.data)
-      print format("%#{spaces * 2}i", node_right.data)
+      print_format(spaces, node_left.data.to_s)
+      print_format(spaces * 2, node_right.data.to_s)
       printed_elem += 2
       add_in_list(list, node_left)
       add_in_list(list, node_right)
       if printed_elem == elements
-        puts ''
+        puts
         return if level == depth
         print_branch(elements, spaces)
         level += 1
@@ -92,10 +92,14 @@ class Tree
 
   def print_branch(elements, spaces)
     elements.times do
-      print format("%#{spaces - 1}s ", '/  \\')
+      print_format(spaces - 1, '/  \\')
       print ' ' * (spaces + 1)
     end
-    puts ''
+    puts
+  end
+
+  def print_format(spaces, data)
+    print format("%#{spaces}s", data)
   end
 
   def add_in_list(list, node)
@@ -104,25 +108,10 @@ class Tree
     list
   end
 
-  def make_decision
-    return 'срубить' if sum_nodes > 5000
-    return 'обрезать' if max_depth > 5
+  def make_decision(arr)
+    return 'срубить' if arr.flatten.sum > MAX_SUM_NODE
+    return 'обрезать' if max_depth > MAX_DEPTH
     'оставить'
-  end
-
-  def sum_nodes
-    list = []
-    sum = @data
-    list << @left
-    list << @right
-    loop do
-      break if list.empty?
-      node = list.shift
-      sum += node.data
-      list << node.left unless node.left.nil?
-      list << node.right unless node.right.nil?
-    end
-    sum
   end
 end
 
@@ -136,7 +125,7 @@ if tree_name.nil?
     tree = Tree.new
     tree.insert(arr)
     tree.print_tree
-    puts tree.make_decision
+    puts tree.make_decision(arr)
     puts 'Желаете продолжить? [y/n]'
     answer = gets.chomp.downcase
     if answer == 'n'
@@ -144,7 +133,7 @@ if tree_name.nil?
       break
     end
   end
-elsif all_trees.include?('trees/' + tree_name + '.tree')
+elsif all_trees.include?('trees/'.concat(tree_name).concat('.tree'))
   string = File.open("trees/#{tree_name}.tree", &:read)
   arr = JSON.parse(string)
   tree = Tree.new
@@ -153,4 +142,4 @@ elsif all_trees.include?('trees/' + tree_name + '.tree')
 else
   puts 'Данное дерево не растет в данном лесу.'
 end
-puts "\n"
+
