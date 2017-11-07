0063e9a958cf22ba3c7b1f98b8a9444069e35e1c
ZhenyaZhak <zhenya-zhak@mail.ru>
2017-11-07 22:30:01 +0300

    hw3.2

--- 2017/ZhenyaZhak/3/zevs_bot.rb
+++ 2017/ZhenyaZhak/3/zevs_bot.rb
@@ -14,20 +14,19 @@ Telegram::Bot::Client.run(token) do |bot|
   st = 0
   bot.listen do |message|
     if flag == 1 && message.text == 'да'
-      bot.api.send_message(chat_id: message.chat.id, text: "#{str[st][ST]}")
-      bot.api.send_message(chat_id: message.chat.id, text: "#{str[st][IN]}")
+      bot.api.send_message(chat_id: message.chat.id, text: "#{str[st][ST]}
+#{str[st][IN]}")
       flag = 0
       next
     end
     if message.text == 'нет'
-      bot.api.send_message(chat_id: message.chat.id, text: 'Значит, 
-это нормальный человек.')
+      bot.api.send_message(chat_id: message.chat.id, text: 'Увы(')
       next
     end
     st = WorkWithMessage.find_by_name(message.text, str)
     if st >= 0 && st < str.size
-      bot.api.send_message(chat_id: message.chat.id, text: "#{str[st][ST]}")
-      bot.api.send_message(chat_id: message.chat.id, text: "#{str[st][IN]}")
+      bot.api.send_message(chat_id: message.chat.id, text: "#{str[st][ST]}
+#{str[st][IN]}")
     else
       bot.api.send_message(chat_id: message.chat.id, text: "Возможно
  вы имели в виду #{str[st - str.size][NA]}?(да/нет)")
