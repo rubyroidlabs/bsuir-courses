53bca58df77bd0b439668e149a5b5b4a6ebf155e
nikitasakov <nikitasakov@mail.ru>
2017-11-08 21:02:36 +0300

    tabs to spaces

--- 2017/NikitaSakov/3/imdb.rb
+++ 2017/NikitaSakov/3/imdb.rb
@@ -1,20 +1,20 @@
 require 'mechanize'
 
 class Parse
-	IMDB_LINK = 'http://www.imdb.com/list/ls072706884/'
+  IMDB_LINK = 'http://www.imdb.com/list/ls072706884/'
   FILE = 'status_list'
 
-	def self.imdb
-		agent = Mechanize.new
+  def self.imdb
+    agent = Mechanize.new
     page = agent.get(IMDB_LINK)
     file = File.open(FILE, 'w')
     page.css('.list.detail .list_item').size.times do |i|
-    	person = page.css('.list.detail .info b')[i].text
-    	status = page.css('.list.detail .description')[i].text.split(' ')[1]
+      person = page.css('.list.detail .info b')[i].text
+      status = page.css('.list.detail .description')[i].text.split(' ')[1]
       file.puts("#{person}-#{status}")
     end
     file.close
-	end
+  end
 end
 
 Parse.imdb
