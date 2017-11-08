4508a0235f97ed4cbd0972dbc3f8f11f3918a2e3
nikitasakov <nikitasakov@mail.ru>
2017-11-08 20:50:03 +0300

    first commit

---
+++ 2017/NikitaSakov/3/imdb.rb
@@ -0,0 +1,20 @@
+require 'mechanize'
+
+class Parse
+	IMDB_LINK = 'http://www.imdb.com/list/ls072706884/'
+  FILE = 'status_list'
+
+	def self.imdb
+		agent = Mechanize.new
+    page = agent.get(IMDB_LINK)
+    file = File.open(FILE, 'w')
+    page.css('.list.detail .list_item').size.times do |i|
+    	person = page.css('.list.detail .info b')[i].text
+    	status = page.css('.list.detail .description')[i].text.split(' ')[1]
+      file.puts("#{person}-#{status}")
+    end
+    file.close
+	end
+end
+
+Parse.imdb
