18b59d5e968de74fef1c0b15d3d6c854ddcf69e2
IlyaStip <sii98@list.ru>
2017-11-12 22:54:30 +0300

    Update bot.rb

--- 2017/IlyaStipakov/3/bot.rb
+++ 2017/IlyaStipakov/3/bot.rb
@@ -9,17 +9,17 @@ token = '489305805:AAELYgWpTBkzfJcA0WUzYnCWCrPeg9tLKyk'
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
-      when '/start'
-        bot.api.sendMessage(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
-      when '/stop'
-        bot.api.sendMessage(chat_id: message.chat.id, text: 'While my hero')
+    when '/start'
+      bot.api.sendMessage(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+    when '/stop'
+      bot.api.sendMessage(chat_id: message.chat.id, text: 'While my hero')
+    else
+      parser = Parser.new Translit.convert("#{message.text}", :english)
+      if parser.search_in_gay_actors || parser.search_in_bisexual_actors || parser.search_in_lesbian_actors
+        bot.api.sendMessage(chat_id: message.chat.id, text: parser.print)
       else
-        parser = Parser.new Translit.convert("#{message.text}", :english)
-        if parser.search_in_gay_actors || parser.search_in_bisexual_actors || parser.search_in_lesbian_actors
-          bot.api.sendMessage(chat_id: message.chat.id, text: parser.print)
-        else
-          bot.api.sendMessage(chat_id: message.chat.id, text: 'This person is not in this list')
-        end
+        bot.api.sendMessage(chat_id: message.chat.id, text: 'This person is not in this list')
+      end
     end
   end
 end
