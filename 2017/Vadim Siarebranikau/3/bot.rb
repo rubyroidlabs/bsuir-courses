d48f3fdd0a793b78c4b91f4b0b73fbf7c030e93f
Vadser <vadser1999@gmail.com>
2017-11-13 00:00:59 +0300

    hw3

--- 2017/Vadim Siarebranikau/3/bot.rb
+++ 2017/Vadim Siarebranikau/3/bot.rb
@@ -5,7 +5,7 @@ token = '466601948:AAHaRPXl2i_AweSfy-Qtxj6j3Xn8_EOv3-w'
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
-    options = {bot: bot, message: message}
+    options = { bot: bot, message: message }
 
     MessageResponder.new(options).respond
   end
