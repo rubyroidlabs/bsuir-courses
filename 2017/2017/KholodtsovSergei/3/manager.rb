c16b28c92fcbf6ba58419d9e5cc7c65c04afd1b8
SergeiKholodtsov <31796851+SergeiKhaladtsou@users.noreply.github.com>
2017-11-12 23:01:48 +0300

    Corrected manager file

--- 2017/2017/KholodtsovSergei/3/manager.rb
+++ 2017/2017/KholodtsovSergei/3/manager.rb
@@ -2,17 +2,18 @@ require_relative 'my_botinok'
 require_relative 'parse_newnownext'
 require_relative 'parse_imdb'
 
-LINK_1 = 'http://www.newnownext.com/gay-celebrities-coming-out-2016/10/2016/'
-LINK_2 = 'http://www.newnownext.com/gay-celebrities-coming-out-2017/10/2017/'
-LINK_3 = 'https://www.buzzfeed.com/louispeitzman/celebrities'\
-       '-you-might-not-know-are-bisexual?utm_term=.xvm3R4vGqa#.lwr5R3owlg'
-DESCRIPTION_1 = '.description-container'
-DESCRIPTION_2 = '.subbuzz__description'
-PEOPLE_1 = '.heading'
-PEOPLE_2 = '.js-subbuzz__title-text'
+LINK_1 = 'http://www.newnownext.com/gay-celebrities-'\
+       'coming-out-2016/10/2016/'.freeze
+LINK_2 = 'http://www.newnownext.com/gay-celebrities'\
+       '-coming-out-2017/10/2017/'.freeze
+LINK_3 = 'https://www.buzzfeed.com/louispeitzman/celebrities-you-might-not-k'\
+       'now-are-bisexual?utm_term=.xvm3R4vGqa#.lwr5R3owlg'.freeze
+DESCRIPTION_1 = '.description-container'.freeze
+DESCRIPTION_2 = '.subbuzz__description'.freeze
+PEOPLE_1 = '.heading'.freeze
+PEOPLE_2 = '.js-subbuzz__title-text'.freeze
 
 class Manager
-
   attr_accessor :list_celebrity
 
   def initialize
@@ -24,11 +25,11 @@ class Manager
     parse_newnownext(LINK_1, DESCRIPTION_1, PEOPLE_1)
     parse_newnownext(LINK_2, DESCRIPTION_1, PEOPLE_1)
     parse_newnownext(LINK_3, DESCRIPTION_2, PEOPLE_2)
-    My_botinok.new(@list_celebrity).run_bot
+    Mybotinok.new(@list_celebrity).run_bot
   end
 
   def parse_imdb
-    parser_imdb = Parse_imdb.new
+    parser_imdb = Parseimdb.new
     all_description = parser_imdb.doc.css('.description')
     all_description.shift
     orientation = []
@@ -37,11 +38,11 @@ class Manager
     @list_celebrity = parser_imdb.found_celebrity(people, orientation)
   end
 
-  def parse_newnownext(link, const_1, const_2)
-    parser_nnn = Parse_newnownext.new(link)
-    all_description = parser_nnn.doc.css(const_1)
+  def parse_newnownext(link, firstconst, secondconst)
+    parser_nnn = Parsenewnownext.new(link)
+    all_description = parser_nnn.doc.css(firstconst)
     all_description = to_array(all_description)
-    people = parser_nnn.doc.css(const_2)
+    people = parser_nnn.doc.css(secondconst)
     people.pop
     people = to_array(people)
     @list_celebrity = parser_nnn.found_celebrity(people,
