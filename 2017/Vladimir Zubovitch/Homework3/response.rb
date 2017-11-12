491e1e1e7a54e5dd0357093745ea432446d5e2c9
Morawiin <moraviin@gmail.com>
2017-11-12 16:59:58 +0300

    fix.prelast2

--- 2017/Vladimir Zubovitch/Homework3/response.rb
+++ 2017/Vladimir Zubovitch/Homework3/response.rb
@@ -1,1 +1,15 @@
-
+require 'rubygems'
+require 'mechanize'
+class Names
+  def load_names
+    agent = Mechanize.new
+    source = 'http://www.imdb.com/list/ls072706884/' 
+    agent.get(source)
+    faamous = agent.page.css('b').to_s.gsub(/<[^<^>]+>/) { '_' }
+    faamous = faamous.gsub(/^_+/) { '' }
+    faamous = faamous.gsub(/_+$/) { '' }
+    faamous = faamous.gsub(/_+/) { '|' }
+    faamous = faamous.split('|')
+    faamous
+  end
+end
