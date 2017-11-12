1e01441292049da02ce29371c82f5d16f1cac16b
SergeiKholodtsov <31796851+SergeiKhaladtsou@users.noreply.github.com>
2017-11-12 23:17:00 +0300

    Corrected celebrity file

--- 2017/2017/KholodtsovSergei/3/celebrity.rb
+++ 2017/2017/KholodtsovSergei/3/celebrity.rb
@@ -2,7 +2,7 @@ class Celebrity
   attr_accessor :celebrity
 
   def initialize(name, orientation, info)
-    @celebrity = { 'name' => name, 'orientation' => orientation, 
-                  'info' => info }
+    @celebrity = { 'name' => name, 'orientation' => orientation,
+                   'info' => info }
   end
 end
