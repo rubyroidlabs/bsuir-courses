810b10b9a4bc9509de9e33a9a1b3e2db7c0271d4
IlyaStip <sii98@list.ru>
2017-11-12 23:02:32 +0300

    Update bot.rb

--- 2017/IlyaStipakov/3/bot.rb
+++ 2017/IlyaStipakov/3/bot.rb
@@ -10,15 +10,19 @@ Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
     when '/start'
-      bot.api.sendMessage(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+      bot.api.sendMessage(chat_id: message.chat.id, 
+                          text: "Hello, #{message.from.first_name}")
     when '/stop'
       bot.api.sendMessage(chat_id: message.chat.id, text: 'While my hero')
     else
       parser = Parser.new Translit.convert("#{message.text}", :english)
-      if parser.search_in_gay_actors || parser.search_in_bisexual_actors || parser.search_in_lesbian_actors
+      if  parser.search_in_gay_actors ||
+          parser.search_in_bisexual_actors ||
+          parser.search_in_lesbian_actors
         bot.api.sendMessage(chat_id: message.chat.id, text: parser.print)
       else
-        bot.api.sendMessage(chat_id: message.chat.id, text: 'This person is not in this list')
+        bot.api.sendMessage(chat_id: message.chat.id, 
+                            text: 'This person is not in this list')
       end
     end
   end
