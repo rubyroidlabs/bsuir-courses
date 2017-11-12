d6a386c283d1687455567285c8b607514a44b3d1
DmitryGolubko <golubko97@gmail.com>
2017-11-12 16:54:57 +0300

    implemented hw-3

---
+++ 2017/DmitryGolubko/3/Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem 'telegram-bot-ruby'
+gem 'pry'
+gem 'redis'
+gem 'mechanize'
---
+++ 2017/DmitryGolubko/3/bot.rb
@@ -0,0 +1,16 @@
+require 'telegram/bot'
+require './lib/message_responder'
+require 'pry'
+require_relative 'parser'
+
+token = ENV['TELEGRAM_TOKEN']
+
+Parser.get_info("http://www.imdb.com/list/ls072706884/")
+
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    options = {bot: bot, message: message}
+
+    MessageResponder.new(options).respond
+  end
+end
