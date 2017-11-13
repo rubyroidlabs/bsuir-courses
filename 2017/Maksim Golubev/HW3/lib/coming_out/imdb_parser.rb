de023ffc855fb054093e27e2dec46f0c66e3d3fd
Mx <po-delu@tut.by>
2017-11-13 12:27:56 +0300

    fix style

--- 2017/Maksim Golubev/HW3/lib/coming_out/imdb_parser.rb
+++ 2017/Maksim Golubev/HW3/lib/coming_out/imdb_parser.rb
@@ -11,41 +11,41 @@ class WebParser
   def initialize
     @redis = Redis.new
     @agent = Mechanize.new
-    @uri_imdb = URI 'http://www.imdb.com/list/ls072706884/'  
+    @uri_imdb = URI 'http://www.imdb.com/list/ls072706884/'
     @uri_ranker = URI 'https://www.ranker.com/list/famous-gay-men-list-of-gay-men-throughout-history/famous-gay-and-lesbian'
     @uri_best_gay_checker = URI 'https://gay-cheker.herokuapp.com/gays.json'
 
-    @name = [] 
-    @status = [] 
-    @details = []   
-    @database = {} 
+    @name = []
+    @status = []
+    @details = []
+    @database = {}
   end
 
   def parser_imdb
     page = @agent.get @uri_imdb
     page.css('.list_item').each do |x|
       @name << x.css('.info b a').text
-      @status <<  x.css('.description > text()').text.gsub(IMDB_CUT_WORD, '')
+      @status << x.css('.description > text()').text.gsub(IMDB_CUT_WORD, '')
       @details <<  x.css('.item_description').text
     end
-    @details.map! {|el| el == "" ? IMDB_NO_DETAILS : el}
-    (@name.zip(@status.zip(@details))).to_h
+    @details.map! { |el| el == '' ? IMDB_NO_DETAILS : el }
+    @name.zip(@status.zip(@details)).to_h
   end
 
   def parser_ranker
-    page = @agent.get  @uri_ranker
-    page.css('.listItem__title').each {|x| @name << x.css('> text()').text}
+    page = @agent.get @uri_ranker
+    page.css('.listItem__title').each { |x| @name << x.css('> text()').text }
     page.css('.listItem__blather').each do |x|
-      @details << (x.text + ' ' +  x.css("a").first.attr('href').gsub('//m.', 'https://www.'))
-      @status << "Gay"     
-    end 
-    (@name.zip(@status.zip(@details))).to_h
+      @details << (x.text + ' ' + x.css('a').first.attr('href').gsub('//m.', 'https://www.'))
+      @status << 'Gay'
+    end
+    @name.zip(@status.zip(@details)).to_h
   end
 
   def parser_best_gay_checker
-    page = @agent.get  @uri_best_gay_checker
+    page = @agent.get @uri_best_gay_checker
     gays = JSON.parse page.body
-    gays.each_with_object({}) { |(k, v), h| h[k] = [v]}
+    gays.each_with_object({}) { |(k, v), h| h[k] = [v] }
   end
 
   def union_gays
@@ -53,13 +53,9 @@ class WebParser
   end
 
   def save_to_redis(hash)
-
-
     hash.each do |k, v|
       @redis.set(k, v)
       @redis.set(k.split.last, k)
     end
   end
 end
-
-
--- 2017/Maksim Golubev/HW3/test/run.rb
+++ 2017/Maksim Golubev/HW3/test/run.rb
@@ -2,5 +2,3 @@ require_relative '../lib/telegram'
 
 WebParser.new.save_to_redis(WebParser.new.union_gays)
 Bot_engine.new.bot_start
-
-
