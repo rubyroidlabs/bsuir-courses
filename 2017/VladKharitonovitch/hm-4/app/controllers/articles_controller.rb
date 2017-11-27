c9315595a887ef13160f12d24464fd3ae25f09e4
MoRain666 <morainrallez@gmail.com>
2017-11-27 19:26:33 +0300

    hm4

---
+++ 2017/VladKharitonovitch/hm-4/app/controllers/articles_controller.rb
@@ -0,0 +1,48 @@
+# класс,отвечающий за контроллеры приложения
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.paginate(:page => params[:page], per_page:6)
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+private
+
+  def article_params
+    params.require(:article).permit(:title, :text, :contact)
+  end
+end
