7c2261ab10706f7620613e5e98345c70386c451e
Vadser <vadser1999@gmail.com>
2017-11-28 22:52:29 +0300

    hw 4

--- 2017/Vadim Siarebranikau/4/b2b/app/controllers/articles_controller.rb
+++ 2017/Vadim Siarebranikau/4/b2b/app/controllers/articles_controller.rb
@@ -63,12 +63,11 @@ class ArticlesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+   
     def set_article
       @article = Article.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
       params.require(:article).permit(:title, :text, :contact)
     end
