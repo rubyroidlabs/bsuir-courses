96b4d81469f8ae5a5e66d0496e58f497a47e5d7a
denis.nikolski <dzianisnikolski@gmail.com>
2017-11-29 00:06:45 +0300

    hw-4

---
+++ 2017/DenisNikolsky/4/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+
+  resources :articles
+
+  root 'welcome#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
