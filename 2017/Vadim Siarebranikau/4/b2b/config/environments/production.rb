3c584ee4d883b236f909c3a02c9580ea0680dde2
Vadser <vadser1999@gmail.com>
2017-11-28 23:16:55 +0300

    hw 4

--- 2017/Vadim Siarebranikau/4/b2b/config/environments/production.rb
+++ 2017/Vadim Siarebranikau/4/b2b/config/environments/production.rb
@@ -1,49 +1,22 @@
 Rails.application.configure do
-
   config.cache_classes = true
-
   config.eager_load = true
-
-  # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
-
-  # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
-  # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
-  # `config/secrets.yml.key`.
   config.read_encrypted_secrets = true
-
-  # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
-  # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
-
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
   config.log_level = :debug
-
-  # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
-
   config.action_mailer.perform_caching = false
-
   config.i18n.fallbacks = true
-
-  # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
-
   config.log_formatter = ::Logger::Formatter.new
-
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
+  if ENV['RAILS_LOG_TO_STDOUT'].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
-
-  # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
--- 2017/Vadim Siarebranikau/4/b2b/test/controllers/articles_controller_test.rb
+++ 2017/Vadim Siarebranikau/4/b2b/test/controllers/articles_controller_test.rb
@@ -17,11 +17,12 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
 
   test 'should create article' do
     assert_difference('Article.count') do
-      post articles_url, params: { article: { 
-                                             contact: @article.contact,
-                                             text: @article.text,
-                                             title: @article.title
-                                             } }
+      post articles_url, params: { article: {
+                                     contact: @article.contact,
+                                     text: @article.text,
+                                     title: @article.title
+                                            }
+                                 }
     end
 
     assert_redirected_to article_url(Article.last)
@@ -38,11 +39,12 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should update article' do
-    patch article_url(@article), params: { article: { 
-                                                     contact: @article.contact,
-                                                     text: @article.text,
-                                                     title: @article.title
-                                                    } }
+    patch article_url(@article), params: { article: {
+                                             contact: @article.contact,
+                                             text: @article.text,
+                                             title: @article.title
+                                                    }
+                                         }
     assert_redirected_to article_url(@article)
   end
 
