77e855d44e45f69ff58ac8d59ec719882954fb98
dixent <dixent@inbox.ru>
2017-11-28 23:13:21 +0300

    homework4

---
+++ 2017/MakarenkoAndrei/4/b2b/app/controllers/ads_controller.rb
@@ -0,0 +1,74 @@
+class AdsController < ApplicationController
+  before_action :set_ad, only: [:show, :edit, :update, :destroy]
+
+  # GET /ads
+  # GET /ads.json
+  def index
+    @ads = Ad.all
+  end
+
+  # GET /ads/1
+  # GET /ads/1.json
+  def show
+  end
+
+  # GET /ads/new
+  def new
+    @ad = Ad.new
+  end
+
+  # GET /ads/1/edit
+  def edit
+  end
+
+  # POST /ads
+  # POST /ads.json
+  def create
+    @ad = Ad.new(ad_params)
+
+    respond_to do |format|
+      if @ad.save
+        format.html { redirect_to @ad, notice: 'Ad was successfully created.' }
+        format.json { render :show, status: :created, location: @ad }
+      else
+        format.html { render :new }
+        format.json { render json: @ad.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /ads/1
+  # PATCH/PUT /ads/1.json
+  def update
+    respond_to do |format|
+      if @ad.update(ad_params)
+        format.html { redirect_to @ad, notice: 'Ad was successfully updated.' }
+        format.json { render :show, status: :ok, location: @ad }
+      else
+        format.html { render :edit }
+        format.json { render json: @ad.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /ads/1
+  # DELETE /ads/1.json
+  def destroy
+    @ad.destroy
+    respond_to do |format|
+      format.html { redirect_to ads_url, notice: 'Ad was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_ad
+      @ad = Ad.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def ad_params
+      params.require(:ad).permit(:title, :content, :contact_details)
+    end
+end
---
+++ 2017/MakarenkoAndrei/4/b2b/app/views/ads/_ad.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! ad, :id, :title, :content, :contact_details, :created_at, :updated_at
+json.url ad_url(ad, format: :json)
