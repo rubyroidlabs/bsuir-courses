3bcbfcb7d077e66ddbc2098a0f290881dcb1e1d2
vlad <vladst90@gmail.com>
2017-11-29 01:56:33 +0300

    rubocop 2

--- 2017/VladislavStukov/hw-4/app/controllers/articles_controller.rb
+++ 2017/VladislavStukov/hw-4/app/controllers/articles_controller.rb
@@ -42,7 +42,8 @@ class ArticlesController < ApplicationController
   end
 
   private
-    def article_params
-      params.require(:article).permit(:title, :text)
-    end
+
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
--- 2017/VladislavStukov/hw-4/config/environments/development.rb
+++ 2017/VladislavStukov/hw-4/config/environments/development.rb
@@ -1,9 +1,4 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -12,7 +7,6 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
-  # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
 
@@ -38,7 +32,6 @@ Rails.application.configure do
   config.active_record.migration_error = :page_load
 
   # Debug mode disables concatenation and preprocessing of assets.
-  # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
 
@@ -48,7 +41,6 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
