c2df14283a79a4fa299a955b69fb6686238e37c4
Vadser <vadser1999@gmail.com>
2017-11-28 22:42:38 +0300

    hw 4

--- 2017/Vadim Siarebranikau/4/b2b/test/controllers/articles_controller_test.rb
+++ 2017/Vadim Siarebranikau/4/b2b/test/controllers/articles_controller_test.rb
@@ -5,40 +5,42 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
     @article = articles(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get articles_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_article_url
     assert_response :success
   end
 
-  test "should create article" do
+  test 'should create article' do
     assert_difference('Article.count') do
-      post articles_url, params: { article: { contact: @article.contact, text: @article.text, title: @article.title } }
+      post articles_url, params: { article: { contact: @article.contact, 
+      text: @article.text, title: @article.title } }
     end
 
     assert_redirected_to article_url(Article.last)
   end
 
-  test "should show article" do
+  test 'should show article' do
     get article_url(@article)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_article_url(@article)
     assert_response :success
   end
 
-  test "should update article" do
-    patch article_url(@article), params: { article: { contact: @article.contact, text: @article.text, title: @article.title } }
+  test 'should update article' do
+    patch article_url(@article), params: { article: { contact: @article.contact, 
+    text: @article.text, title: @article.title } }
     assert_redirected_to article_url(@article)
   end
 
-  test "should destroy article" do
+  test 'should destroy article' do
     assert_difference('Article.count', -1) do
       delete article_url(@article)
     end
--- 2017/Vadim Siarebranikau/4/b2b/test/test_helper.rb
+++ 2017/Vadim Siarebranikau/4/b2b/test/test_helper.rb
@@ -1,9 +1,10 @@
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+class ActiveSupport
+  class TestCase
 
-  # Add more helper methods to be used by all tests here...
+    fixtures :all
+
+  end
 end
