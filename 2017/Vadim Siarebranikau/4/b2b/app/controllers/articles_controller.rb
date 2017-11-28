7d1187437fefb735642afa65b5ea7bc9644dc8be
Vadser <vadser1999@gmail.com>
2017-11-28 23:30:36 +0300

    hw 4

--- 2017/Vadim Siarebranikau/4/b2b/app/controllers/articles_controller.rb
+++ 2017/Vadim Siarebranikau/4/b2b/app/controllers/articles_controller.rb
@@ -10,8 +10,7 @@ class ArticlesController < ApplicationController
 
   # GET /articles/1
   # GET /articles/1.json
-  def show
-  end
+  def show; end
 
   # GET /articles/new
   def new
@@ -19,21 +18,26 @@ class ArticlesController < ApplicationController
   end
 
   # GET /articles/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /articles
   # POST /articles.json
   def create
-    @article = Article.new(article_params)
+    @article = Article.new(articleparams)
 
     respond_to do |format|
       if @article.save
-        format.html { redirect_to @article, notice: 'Article was successfully created.' }
+        format.html {
+          redirect_to @article,
+          notice: 'Article was successfully created.'
+        }
         format.json { render :show, status: :created, location: @article }
       else
         format.html { render :new }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
+        format.json {
+          render json: @article.errors,
+          status: :unprocessable_entity
+        }
       end
     end
   end
@@ -42,12 +46,18 @@ class ArticlesController < ApplicationController
   # PATCH/PUT /articles/1.json
   def update
     respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to @article, notice: 'Article was successfully updated.' }
+      if @article.update(articleparams)
+        format.html {
+          redirect_to @article,
+          notice: 'Article was successfully updated.'
+        }
         format.json { render :show, status: :ok, location: @article }
       else
         format.html { render :edit }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
+        format.json {
+          render json: @article.errors,
+          status: :unprocessable_entity
+        }
       end
     end
   end
@@ -57,18 +67,21 @@ class ArticlesController < ApplicationController
   def destroy
     @article.destroy
     respond_to do |format|
-      format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
+      format.html {
+        redirect_to articles_url,
+        notice: 'Article was successfully destroyed.'
+      }
       format.json { head :no_content }
     end
   end
 
   private
-   
+
     def set_article
       @article = Article.find(params[:id])
     end
 
-    def article_params
+    def articleparams
       params.require(:article).permit(:title, :text, :contact)
     end
 end
