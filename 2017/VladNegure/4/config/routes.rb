aa6167d16be0bb6e6bad5405785e911380b1bde5
VladNegure <blacklabelgm@icloud.com>
2017-11-29 16:24:53 +0300

    Fix double quotes Fix long lines

--- 2017/VladNegure/4/config/routes.rb
+++ 2017/VladNegure/4/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :posts
   resources :comments
   root to: 'posts#index', as: 'index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see
+  # http://guides.rubyonrails.org/routing.html
 end
