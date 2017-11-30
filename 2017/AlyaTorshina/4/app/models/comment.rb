ef67351701bf8c5c0cffabd5584fba0ffb15aefb
Аля <ala@MacBook-Air-Ala.local>
2017-11-30 17:38:37 +0300

    deleting commit

--- 2017/AlyaTorshina/4/app/models/comment.rb
+++
@@ -1,3 +0,0 @@
-class Comment < ApplicationRecord
-  belongs_to :post
-end
--- 2017/AlyaTorshina/4/db/migrate/20171128180432_add_kind_item_to_posts.rb
+++
@@ -1,6 +0,0 @@
-class AddKindItemToPosts < ActiveRecord::Migration[5.1]
-  def change
-    add_column :posts, :kind, :string
-    add_column :posts, :item, :string
-  end
-end
