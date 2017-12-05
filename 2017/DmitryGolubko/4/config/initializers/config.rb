e47d4b56dc340cf5ab5422766b48ee098c16e03f
DmitryGolubko <golubko97@gmail.com>
2017-12-05 18:37:25 +0300

    fix styling issues -v5

--- 2017/DmitryGolubko/4/config/initializers/config.rb
+++ 2017/DmitryGolubko/4/config/initializers/config.rb
@@ -2,15 +2,18 @@ Config.setup do |config|
   # Name of the constant exposing loaded settings
   config.const_name = 'Settings'
 
-  # Ability to remove elements of the array set in earlier loaded settings file. For example value: '--'.
+  # Ability to remove elements of the array set in earlier loaded
+  # settings file. For example value: '--'.
   #
   # config.knockout_prefix = nil
 
-  # Overwrite arrays found in previously loaded settings file. When set to `false`, arrays will be merged.
+  # Overwrite arrays found in previously loaded settings file.
+  # When set to `false`, arrays will be merged.
   #
   # config.overwrite_arrays = true
 
-  # Load environment variables from the `ENV` object and override any settings defined in files.
+  # Load environment variables from the `ENV` object and
+  #override any settings defined in files.
   #
   # config.use_env = false
 
@@ -18,8 +21,10 @@ Config.setup do |config|
   #
   # config.env_prefix = 'Settings'
 
-  # What string to use as level separator for settings loaded from ENV variables. Default value of '.' works well
-  # with Heroku, but you might want to change it for example for '__' to easy override settings from command line, where
+  # What string to use as level separator for settings
+  # loaded from ENV variables. Default value of '.' works well
+  # with Heroku, but you might want to change it for example
+  # for '__' to easy override settings from command line, where
   # using dots in variable names might not be allowed (eg. Bash).
   #
   # config.env_separator = '.'
@@ -34,7 +39,8 @@ Config.setup do |config|
   #
   # config.env_parse_values = true
 
-  # Validate presence and type of specific config values. Check https://github.com/dry-rb/dry-validation for details.
+  # Validate presence and type of specific config values.
+  # Check https://github.com/dry-rb/dry-validation for details.
   #
   # config.schema do
   #   required(:name).filled
