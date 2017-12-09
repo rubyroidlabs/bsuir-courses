a23c3ff790bdd97dfc4410f0725031cff3ee9e0b
KulybaVadim <kulyba.vadim@gmail.com>
2017-12-09 16:29:26 +0300

    was correct, but now has mistake

--- 2017/VadimKulyba/4/blog/db/schema.rb
+++ 2017/VadimKulyba/4/blog/db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your
 # version control system.
-
-ActiveRecord::Schema.define(version: 20171206090108) do
+ActiveRecord::Schema.define(version: 20_171_206_090_108) do
   create_table 'articles', force: :cascade do |t|
     t.string 'title'
     t.text 'text'
