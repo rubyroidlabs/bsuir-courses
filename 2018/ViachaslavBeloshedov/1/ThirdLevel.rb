3fe6f6549da935047c9fdd28a03eee4d2225c75a
slavabelb <slavabelb@gmail.com>
2018-02-16 23:36:23 +0300

    fix styling issues

--- 2018/ViachaslavBeloshedov/1/ThirdLevel.rb
+++ 2018/ViachaslavBeloshedov/1/ThirdLevel.rb
@@ -2,39 +2,39 @@ def factorial(n)
   Math.gamma(n + 1).to_i
 end
 
-def pascalNumber(n, m)
+def pascal_number(n, m)
   factorial(n) / (factorial(m) * factorial(n - m))
 end
 
-def printTriangle(base_number, depth, length_of_terminal)
+def print_triangle(base_number, depth, length_of_terminal)
   begin
   length_of_base_number = base_number.digits.count
-  for i in 0 .. depth - 1 
-    print format("%2i:",i)
+  for i in 0..depth - 1
+    print format('%2i:',i)
     print ' ' * (length_of_terminal - (length_of_base_number + 2) * i )
     print base_number, ' ' * (length_of_base_number + length_of_base_number - 1)
     for j in 1 .. i
-      print format("%5i", pascalNumber(i,j) * base_number)
+      print format('%5i', pascal_number(i,j) * base_number)
       print ' ' * length_of_base_number
     end
   puts
   end
-rescue ArgumentError => e
-  raise "Больше нет места для вывода."
+rescue ArgumentError
+  raise 'Больше нет места для вывода.'
 end
 end
 
 if ENV['BASE_NUMBER'].nil?
-  puts "BASE_NUMBER пуста"
+  puts 'BASE_NUMBER пуста'
   return
 end
 
 base_number = ENV['BASE_NUMBER'].to_i
 puts "Базовый номер: #{base_number}"
 
-print "Введите глубину дерева: "
+print 'Введите глубину дерева: '
 depth = gets.chomp.to_i
 
 length = %x[tput cols].to_i / 2
 
-printTriangle(base_number,depth,length)
+print_triangle(base_number, depth, length)
