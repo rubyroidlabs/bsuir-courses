294fd4eabcd5ef9832e5bc28fba8913649b7969c
shkrub322 <vlad.shkrabkov@gmail.com>
2018-02-18 02:05:13 +0300

    implement hw-1

---
+++ 2018/VladislavShkrabkov/hw-1/pascTriangle.rb
@@ -0,0 +1,32 @@
+puts 'Enter deep'
+deep = gets.chomp.to_i
+puts 'Enter basic value'
+basic_val = gets.chomp.to_i
+pasc_tri = [[basic_val]]
+
+for i in  1..(deep - 1)
+  ar = []
+  for j in 0..i
+  	if (i == j || j == 0)
+  	  ar.push(basic_val)
+  	else 
+  	  ar.push(pasc_tri[i - 1][j] + pasc_tri[i - 1][j - 1])
+  	end
+  end
+  pasc_tri.push(ar)
+end
+
+require 'io/console'
+size = IO.console.winsize
+
+for i in 0..(deep - 1)
+  str = ''
+  for j in 0..i
+  	str += (pasc_tri[i][j].to_s + (' ' * ((size[1] - 1) / (deep * 2))))
+  	length = str.length
+  end
+  print ' ' * (size[1] / 2 - ((length - ((size[1] - 1) / (deep * 2))) /2 ))
+  puts str
+end
+
+
