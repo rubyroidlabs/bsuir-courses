7be5a3e2f831ca5d6cc037f3ed3a0151e8e0fc49
user <ivkarpechenko@gmail.com>
2018-02-18 23:02:50 +0300

    implement hw 1 fixed2

--- 2018/IvanKarpechanka/1/hw.rb
+++ 2018/IvanKarpechanka/1/hw.rb
@@ -14,8 +14,7 @@ puts 'Введите глубину дерева:'
 three_len = gets.chomp.to_i
 puts 'Введите базовый номер:'
 first_el = gets.chomp.to_i
-
-# to output a number with zeros in the beginning if its 
+# to output a number with zeros in the beginning if its
 # length is less than the length of the maximum element
 number_max_len = pascals_row(three_len, first_el).max.to_s.length
 console_len = 160
@@ -26,9 +25,9 @@ help_str = "\/#{gaps}\\#{gaps}" # Specifying a Tree Display Template
   pascals_row(i, first_el).each do |elem|
   	str += elem.to_s.rjust(number_max_len, '0') + (gaps + '  ')
   end
-  help1 = "#{' ' * three_len.to_s.length}"
+  help1 = ' ' * three_len.to_s.length
   puts  help1 + (help_str * i).chomp(gaps).center(console_len)
 
-  help2 = "#{i.to_s.rjust(three_len.to_s.length, '0')}"
+  help2 = i.to_s.rjust(three_len.to_s.length, '0')
   puts  help2 + str.chomp(gaps + '  ').center(console_len, ',')
 end
