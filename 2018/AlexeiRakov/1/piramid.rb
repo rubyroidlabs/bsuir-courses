6f049c7415f8234669e870ec549936ea8a4264ba
Rakovvv <fargon946@gmail.com>
2018-02-20 00:27:40 +0300

    implement hw 1

---
+++ 2018/AlexeiRakov/1/piramid.rb
@@ -0,0 +1,28 @@
+def triangle(n, z, c)
+(0..n).map do |r|
+  n = [0]
+  num = [c]
+  left = [z]
+  right = z
+  c = 1
+  k = 1
+(0..r-1).step(1) do |index|
+  right = right*(r-k+1)/k
+  left.push right 
+  c += 1
+  k += 1
+  end
+  m = "#{num}"
+  n = "#{left}"
+  puts "#{n.center(130).green}  #{m.rjust(13)}"                
+  end
+end
+print "enter the vertex of the triangle: " 
+z = gets.to_i
+print "enter the depth  of the triangle: "
+n = gets.to_i
+  puts "vertex  triangle = #{z} "
+  puts "depth   triangle = #{n} "
+  print "\n\n"
+  require 'colorize'
+triangle(n, z, c = 0)
