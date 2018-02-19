c17ef16a350ef2d46a3bb7edb7718dc07d06ba29
552687773 <dimka552687773@gmail.com>
2018-02-19 23:38:17 +0300

    hw 1

---
+++ 2018/DmitryShkuratov/1/homework-1.rb
@@ -0,0 +1,27 @@
+class PascalsTriangle
+  def initialize
+    print "Enter the depth of the triangle:\n n="
+    @rows = gets.chomp.to_i
+  end
+
+  def to_pascal
+    temp = []
+    @rows.times do |row|
+    line = [1]
+    (0..row-1).each {|x| line << (line[x] * (row-x) / (x+1)) }
+    temp << line
+  end
+  temp
+end
+
+  def print_triangle
+    max = to_pascal.flatten.max.to_s.length
+    strings = to_pascal.map {|arr| arr.map {|int| int.to_s.center(max + 3)} }
+    strings.each do |line|
+    puts line.join.center(strings[-1].join.length)
+    end
+   end
+end
+
+row = PascalsTriangle.new
+row.print_triangle
