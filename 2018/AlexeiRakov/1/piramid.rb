9ba3bd9fd05374d4ebe1ac4be72a4c60581a5cab
Rakovvv <fargon946@gmail.com>
2018-02-20 16:32:32 +0300

    fix4

--- 2018/AlexeiRakov/1/piramid.rb
+++ 2018/AlexeiRakov/1/piramid.rb
@@ -1,12 +1,12 @@
 def triangle(n, z, c)
-(0..n).map do |r|
+	(0..n).map do |r|
 n = [0]
 num = [c]
 left = [z]
 right = z
 c = 1
 k = 1
-(0..r - 1).step(1)do|index|
+(0..r - 1).step(1) do
 right = right * (r - k + 1) / k
 left.push right
 c += 1
@@ -15,7 +15,7 @@ end
 num.to_s
 left.to_s
 puts "#{n.center(130).green} #{m.rjust(13)}"
-end
+	end
 end
 print 'enter the vertex of the triangle:'
 z = gets.to_i
@@ -24,4 +24,4 @@ n = gets.to_i
 puts "vertex triangle = #{z}"
 puts "depth triangle = #{n}"
 require 'colorize'
-triangle(n, z, c = 0)
+triangle(n, z, c)
