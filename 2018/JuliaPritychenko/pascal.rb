461e07230acd76c7693964809ba94ae5387cbda9
Julia Pritychenko <juliasnoww@mail.ru>
2018-02-20 09:56:22 +0300

    fix_homework - 1

--- 2018/JuliaPritychenko/pascal.rb
+++ 2018/JuliaPritychenko/pascal.rb
@@ -1,13 +1,13 @@
-print "Enter depth: "
+print 'Enter depth: '
 depth = gets.to_i
-print "Enter basic number: "
+print 'Enter basic number: '
 base_number = gets.to_i
 array = []
-for i in 1..depth
+(1..depth).each do |i|
   array[i] = 0
 end
 array[0] = base_number
-for j in 1..depth
+(1..depth).each do |j|
   (1..j).reverse_each do |i|
     print "#{array[i - 1]} "
     array[i] = array[i - 1] + array[i]
