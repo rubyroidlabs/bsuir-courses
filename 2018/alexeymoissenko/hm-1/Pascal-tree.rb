c9e2ce1247564d7b16f94e8c82fde7788ce3df2d
alexeymoiseenko <36601463+alexeymoiseenko@users.noreply.github.com>
2018-02-21 01:35:52 +0300

    Second update Pascl-tree.rb

--- 2018/alexeymoissenko/hm-1/Pascal-tree.rb
+++ 2018/alexeymoissenko/hm-1/Pascal-tree.rb
@@ -1,15 +1,15 @@
-class Triangle 
-  
-  def initialize (base_number,deep) 
-    @base_number=base_number
-    @deep=deep
+class Triangle
+
+  def initialize (base_number, deep) 
+    @base_number = base_number
+    @deep = deep
   end
-  
+
   def fact(a)
     (1..a).reduce(:*)
   end
 
-  def binomial(n,k)
+  def binomial(n, k)
     return @base_number if n-k <= 0 || k <= 0
     fact(n) / (fact(k) * fact(n - k)) * @base_number
   end
@@ -17,17 +17,16 @@ class Triangle
   def curr_str (n)
     (0..n).map { |e| binomial(n, e) }
   end
- 
 
   def show
     max_row = curr_str(@deep)
     max_el_size = max_row.max.to_s.size
-    width = [`tput cols`.to_i,100].max
+    width = [`tput cols`.to_i, 100].max
     @deep.times do |i|
-        string = curr_str(i).map do |el|
+      string = curr_str(i).map do |el|
         el.to_s.center(max_el_size)
       end.join(" ").center(width)
-      puts "#{i + 1}: #{string}"
+      puts "#{i+1}: #{string}"
     end
   end
 end
@@ -43,3 +42,4 @@ deep = gets.chomp.to_i
 triangle = Triangle.new(base_number, deep)
 
 triangle.show
+                                                
