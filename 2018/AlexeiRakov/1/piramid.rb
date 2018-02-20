3e3f87affb7b2909a8ef35469463b5382fcf3150
Rakovvv <fargon946@gmail.com>
2018-02-20 17:48:31 +0300

    fix23

--- 2018/AlexeiRakov/1/piramid.rb
+++ 2018/AlexeiRakov/1/piramid.rb
@@ -1,19 +1,19 @@
 def triangle(n, z, c)
   (0..n).map do |r|
-n = [0]
-num = [c]
-left = [z]
-right = z
-c = 1
-k = 1
-(0..r - 1).step(1) do
-right = right * (r - k + 1) / k
-left.push right
-c += 1
-k += 1
-end
-num.to_s
-left.to_s
+  n = [0]
+  num = [c]
+  left = [z]
+  right = z
+  c = 1
+  k = 1
+    (0..r - 1).step(1) do
+    right = right * (r - k + 1) / k
+    left.push right
+    c += 1
+    k += 1
+    end
+  num.to_s
+  left.to_s
 puts "#{n.center(130).green} #{m.rjust(13)}"
   end
 end
