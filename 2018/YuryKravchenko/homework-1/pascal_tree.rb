0d73452b49869d5dd8422378f59bc5526869f57e
YuruKravchenko <yurezz08@mail.ru>
2018-02-19 22:50:00 -0800

    implement hw 1

--- 2018/YuryKravchenko/homework-1/pascal_tree.rb
+++ 2018/YuryKravchenko/homework-1/pascal_tree.rb
@@ -1,17 +1,19 @@
 # My Homework
-
 def pascal(n)
-
+print 'Введите базовый номер: '
+f = gets.chomp.to_i
+h = f
   (0..n).each{|r|
-    tree=[1]
-    term=1
-    k=1
-    (0..r-1).step(1){|index|
-      term=term*(r-k+1)/k
-      tree.push term
-      k+=1}
-    p tree}
+    tree = [h]
+    base = f
+      k = 1
+        (0..r - 1).step(1){|index|
+          base = base * ( r - k + 1 ) / k
+            tree.push base
+      k += 1}
+  p tree}
 end
-print "Введите глубину дерева: "
-n = gets.to_i
+print 'Введите глубину дерева: '
+n = gets.chomp.to_i
 pascal(n)
+
