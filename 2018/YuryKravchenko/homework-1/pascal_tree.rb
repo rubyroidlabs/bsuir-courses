4abf96566f461b4cfce2637e8c378aa746cf54de
YuryKravchenko <36518159+atom-unique@users.noreply.github.com>
2018-02-20 11:09:05 +0300

    Update pascal_tree.rb

--- 2018/YuryKravchenko/homework-1/pascal_tree.rb
+++ 2018/YuryKravchenko/homework-1/pascal_tree.rb
@@ -1,18 +1,18 @@
 # My Homework
-  def pascal(n)
+def pascal(n)
   print 'Введите базовый номер: '
   f = gets.chomp.to_i
   h = f
-  (0..n).each { |r|
-    tree = [h]
-    base = f
+    (0..n).each { |r|
+      tree = [h]
+      base = f
       k = 1
-          (0..r - 1).step(1) { |index|
-          base = base * ( r - k + 1 ) / k
+        (0..r - 1).step(1) { |index|
+        base = base * ( r - k + 1 ) / k
         tree.push base
       k += 1}
   p tree}
-  end
-  print 'Введите глубину дерева: '
-  n = gets.chomp.to_i
-  pascal(n)
+end
+print 'Введите глубину дерева: '
+n = gets.chomp.to_i
+pascal(n)
