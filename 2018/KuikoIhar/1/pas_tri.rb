ea46a75916e8b0bc88454f0687bd016bf761e94b
KuikoIhar <coooll81@tut.by>
2018-02-21 20:54:46 +0300

    fix styling issues

--- 2018/KuikoIhar/1/pas_tri.rb
+++ 2018/KuikoIhar/1/pas_tri.rb
@@ -1,33 +1,31 @@
 #!/usr/bin/env ruby
 
-def pas_tri (h, n)
+def pas_tri(h, n)
   l = [n]
   [l] + (1..h).map do
-    l = [n] + l[1..-1].map.with_index{|x, i| x+l[i]} + [n]
+    l = [n] + l[1..-1].map.with_index { |x, i| x + l[i] } + [n]
   end
 end
 
-def pad (s, n)
+def pad(s, n)
   l = n - s.size
-  ' '*(l/2) + s + ' '*(l-l/2)
+  ' ' * (l / 2) + s + ' ' * (l - l / 2)
 end
 
-def lines (rows)
+def lines(rows)
   n = "#{rows[-1].max}".size
   rows.map do |row|
   	row.map{|x| pad("#{x}", n)}.join(' ')
   end
 end
 
-def center (l)
+def center(l)
   n = l[-1].size
-  l.map{|s| pad(s, n)}
+  l.map { |s| pad(s, n) }
 end
 
-
-puts "Введите глубину дерева: "
+puts 'Введите глубину дерева: '
 h = gets.chomp.to_i
-puts "Введите базовый номер: "
+puts 'Введите базовый номер: '
 n = gets.chomp.to_i
 puts center lines pas_tri(h, n)
-
