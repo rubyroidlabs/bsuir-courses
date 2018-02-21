4a9497a767563255b5ee1d3731a5dae4d11ca7d5
Руслан Салянко <qwas1990@gmail.com>
2018-02-21 10:28:16 +0300

    implement hw 1

---
+++ 2018/SaliankoRuslan/1/pascal_tree.rb
@@ -0,0 +1,25 @@
+def pascal(n, number)
+  line = [number]
+  for k in 0...n
+      line.push(line[k].to_i * (n-k) / (k+1))
+     end 
+    return line
+  
+end
+
+def format_pascal(n,number)
+  col = `/usr/bin/tput cols`.chomp.to_i 
+  pascal(n,number).to_s.center(col)
+end
+
+print "Введите глубину дерева: "
+n = gets.chomp.to_i
+print "Введите базовый номер: "
+number = gets.chomp.to_i
+
+
+for i in 0...n
+  puts "#{i}: #{format_pascal(i,number)}"
+end 
+
+
