a95104bb63109db00e4bd00c4d0378a1a0aa9675
Руслан Салянко <qwas1990@gmail.com>
2018-02-21 10:53:15 +0300

    fix styling 2

--- 2018/SaliankoRuslan/1/pascal_tree.rb
+++ 2018/SaliankoRuslan/1/pascal_tree.rb
@@ -1,14 +1,14 @@
 def pascal(n, number)
   line = [number]
   for k in 0...n
-      line.push (line[k] .to_i * (n - k) / (k + 1))
+    line.push (line[k] .to_i * (n - k) / (k + 1))
   end 
-  return line 
+  line 
 end
 
 def format_pascal(n, number)
   col = `/usr/bin/tput cols`.chomp.to_i 
-  pascal(n,number).to_s.center(col)
+  pascal(n, number).to_s.center(col)
 end
 
 print 'Введите глубину дерева: '
