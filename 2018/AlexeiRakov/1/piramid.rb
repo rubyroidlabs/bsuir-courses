98feca965b0fb13642d8bff7d66cd46a730aeff3
Rakovvv <fargon946@gmail.com>
2018-02-21 04:14:45 +0300

    fix60

--- 2018/AlexeiRakov/1/piramid.rb
+++ 2018/AlexeiRakov/1/piramid.rb
@@ -1,6 +1,5 @@
-def triangle(n, z, c, q)
+def triangle(n, z, c)
   (0..n).map do |r|
-  	mas = []
     n = [0]
     num = [c]
     left = [z]
@@ -8,30 +7,21 @@ def triangle(n, z, c, q)
     c = 1
     k = 1
     (0..r - 1).step(1) do
-      o = right
       right = right * (r - k + 1) / k
-      u = right
-      if o + u == q
-        mas.push o
-        mas.push right
-      end
       left.push right
       c += 1
       k += 1
     end
-    puts "#{left.to_s.center(135).green} #{num.to_s.rjust(8)}"
-    puts "#{mas.to_s.ljust(5).red}"
+    num.to_s
+    left.to_s
+    puts "#{n.center(130).green} #{m.rjust(13)}"
   end
 end
 print 'enter the vertex of the triangle:'
 z = gets.to_i
 print 'enter the depth of the triangle:'
 n = gets.to_i
-print 'enter a child:'
-q = gets.to_i
 puts "vertex triangle = #{z}"
 puts "depth triangle = #{n}"
-puts 'parents will be selected from the left'
-c = 0
 require 'colorize'
-triangle(n, z, c, q)
+triangle(n, z, c)
