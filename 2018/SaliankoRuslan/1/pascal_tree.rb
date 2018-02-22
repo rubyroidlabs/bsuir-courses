f4f3f2af00a38a4816e0f68f56f76718725d6fd6
Руслан Салянко <36677403+RuslanSalianko@users.noreply.github.com>
2018-02-22 10:28:20 +0300

    10

--- 2018/SaliankoRuslan/1/pascal_tree.rb
+++ 2018/SaliankoRuslan/1/pascal_tree.rb
@@ -5,17 +5,14 @@ def pascal(n, number)
   end
   line
 end
-
 def format_pascal(n, number)
   col = `/usr/bin/tput cols`.chomp.to_i 
   pascal(n, number).to_s.center(col)
 end
-
 print 'Введите глубину дерева: '
 n = gets.chomp.to_i
 print 'Введите базовый номер: '
 number = gets.chomp.to_i
-
 for i in 0...n
   puts "#{i}: #{format_pascal(i, number)}"
 end 
