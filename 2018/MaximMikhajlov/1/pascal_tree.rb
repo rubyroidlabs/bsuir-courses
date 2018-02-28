680244ef7e1db9d90928e5932933d2476933d5c0
Max <krabuwek@outlook.com>
2018-02-28 18:31:15 +0300

    fix

--- 2018/MaximMikhajlov/1/pascal_tree.rb
+++ 2018/MaximMikhajlov/1/pascal_tree.rb
@@ -1,5 +1,5 @@
-
-w_display = `tput cols`.to_i
+display_width = `tput cols`.to_i
+CENTRAL_POSITION = 2
 
 puts 'Введите глубиу дерева'
 deep = gets.to_i
@@ -13,16 +13,19 @@ end
 
 fact = ->(x) { (1..x).inject(:*) || 1 }
 
-(deep + 1).times do |i|
-  arr = (0..i).map do |j|
-    base_number * fact.call(i) / (fact.call(j) * fact.call(i - j))
+(deep + 1).times do |level|
+  arr = (0..level).map do |element_number|
+    base_number * fact.call(level) / 
+    (fact.call(element_number) * fact.call(level - element_number))
   end
   str_elements = arr.join('  ')
-  str = ' ' * i.to_s.length + '   '
-  str += ' ' * ((w_display - str_elements.length) / 2)
-  str += arr[1..-2].map { |a| a.to_s.length }.map do |k|
-    k > 2 ? '\\' + '_' * (k - 2) + '/' : '\\/'
+  str = ' ' * level.to_s.length + '   '
+  str += ' ' * ((display_width - str_elements.length) / CENTRAL_POSITION)
+  length_elements = arr[1..-2].map { |element| element.to_s.length }
+  str += length_elements.map do |length_element|
+    length_element > 2 ? '\\' + '_' * (length_element - 2) + '/' : '\\/'
   end.join('  ')
   puts str
-  puts i.to_s + ' ' * ((w_display - str_elements.length) / 2) + str_elements
+  puts level.to_s + ' ' * ((display_width - str_elements.length) /
+  CENTRAL_POSITION) + str_elements
 end
