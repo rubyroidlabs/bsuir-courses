18886287db79f1444101af1f0c81638295be0fba
juliaSnoww <juliasnoww@mail.ru>
2018-03-14 20:10:54 +0300

    fixed homework-1

--- 2018/JuliaPrutychenko/1/pascal.rb
+++ 2018/JuliaPrutychenko/1/pascal.rb
@@ -4,18 +4,18 @@ print 'Enter basic number: '
 base_number = gets.to_i
 array = []
 (1..depth).each do |i|
-  array[i] = 0
+	array[i] = 0
 end
 z = depth
 array[0] = base_number
 (1..depth).each do |j|
-  (1..j).reverse_each do |i|
-    if(i==j)
-      x = " "*z
-      end
-    print "#{x}#{array[i - 1]} "
-    array[i] = array[i - 1] + array[i]
-      end
-  z-=1
-  puts
+	(1..j).reverse_each do |i|
+		if(i == j)
+			x = ' '*z
+		end
+		print "#{x}#{array[i - 1]} "
+		array[i] = array[i - 1] + array[i]
+	end
+	z -= 1
+	puts
 end
