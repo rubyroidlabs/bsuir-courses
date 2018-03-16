f97311330d8ecf320e1f1349f85059d060f2c618
SlepenkovDmitry <Slepenkov95@mail.ru>
2018-03-16 17:14:27 +0300

    last hw1

--- 2018/SlepenkovDmitry/HomeWork1/HomeWork1.rb
+++ 2018/SlepenkovDmitry/HomeWork1/HomeWork1.rb
@@ -1,23 +1,25 @@
 class PrintPascalTriangle
-def initialize
-print "Enter the depth of the triangle n = "
-@rows = gets.chomp.to_i 
+  def initialize
+  print "Enter the depth of the triangle n = "
+  @rows = gets.chomp.to_i 
 end
-def calculatedRow
-triangle = []
-@rows.times do |row|
-line = [1]
-(0..row-1).each {|x| line << (line[x] * (row-x) / (x+1)) }
-triangle << line
+
+  def calculatedRow
+  triangle = []
+  @rows.times do |row|
+    line = [1]
+    (0..row-1).each {|x| line << (line[x] * (row-x) / (x+1)) }
+    triangle << line
+  end
+  triangle
 end
-triangle
-end
-def printTriangle
-max = calculatedRow.flatten.max.to_s.length
-strings = calculatedRow.map {|arr| arr.map {|int| int.to_s.center(max + 3)} }
-strings.each do |line|
-puts line.join.center(strings[-1].join.length)
-end
+
+  def printTriangle
+  max = calculatedRow.flatten.max.to_s.length
+  strings = calculatedRow.map {|arr| arr.map {|int| int.to_s.center(max + 3)} }
+  strings.each do |line|
+    puts line.join.center(strings[-1].join.length)
+  end
 end
 
 row = PrintPascalTriangle.new
